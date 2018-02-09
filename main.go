55f2951cc0213cd01b475ae7bf571027b9688c96
Zachary Huff <zach.huff.386@gmail.com>
2018-02-08 22:15:18 -0500

    Update cmd in main

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/pritunl/pritunl-cloud/cmd"
 	"github.com/pritunl/pritunl-cloud/constants"
+	"github.com/pritunl/pritunl-cloud/logger"
+	"github.com/pritunl/pritunl-cloud/requires"
+	"github.com/pritunl/pritunl-cloud/task"
 	"time"
 )
 
@@ -12,8 +16,18 @@ Usage: pritunl-cloud COMMAND
 
 Commands:
   version     Show version
+  mongo       Set MongoDB URI
+  set         Set a setting
+  unset       Unset a setting
   start       Start node
+  clear-logs  Clear logs
 `
+
+func Init() {
+	logger.Init()
+	requires.Init()
+	task.Init()
+}
 
 func main() {
 	defer time.Sleep(500 * time.Millisecond)
@@ -22,9 +36,53 @@ func main() {
 
 	switch flag.Arg(0) {
 	case "start":
+		if flag.Arg(1) == "--debug" {
+			constants.Production = false
+		}
+
+		Init()
+		err := cmd.Node()
+		if err != nil {
+			panic(err)
+		}
 		return
 	case "version":
 		fmt.Printf("pritunl-cloud v%s\n", constants.Version)
+		return
+	case "mongo":
+		logger.Init()
+		err := cmd.Mongo()
+		if err != nil {
+			panic(err)
+		}
+		return
+	case "reset-id":
+		logger.Init()
+		err := cmd.ResetId()
+		if err != nil {
+			panic(err)
+		}
+		return
+	case "set":
+		Init()
+		err := cmd.SettingsSet()
+		if err != nil {
+			panic(err)
+		}
+		return
+	case "unset":
+		Init()
+		err := cmd.SettingsUnset()
+		if err != nil {
+			panic(err)
+		}
+		return
+	case "clear-logs":
+		Init()
+		err := cmd.ClearLogs()
+		if err != nil {
+			panic(err)
+		}
 		return
 	}
 
