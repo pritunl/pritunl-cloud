478151fabfef9abc453d5e99bf141e9d697e9d87
Zachary Huff <zach.huff.386@gmail.com>
2018-02-10 01:21:43 -0500

    Remove certificates from nodes

--- www/uapp/components/Nodes.tsx
+++ www/uapp/components/Nodes.tsx
@@ -1,18 +1,14 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
 import * as NodeTypes from '../types/NodeTypes';
-import * as CertificateTypes from '../types/CertificateTypes';
 import NodesStore from '../stores/NodesStore';
-import CertificatesStore from '../stores/CertificatesStore';
 import * as NodeActions from '../actions/NodeActions';
-import * as CertificateActions from '../actions/CertificateActions';
 import Node from './Node';
 import Page from './Page';
 import PageHeader from './PageHeader';
 
 interface State {
 	nodes: NodeTypes.NodesRo;
-	certificates: CertificateTypes.CertificatesRo;
 	disabled: boolean;
 }
 
@@ -30,28 +26,23 @@ export default class Nodes extends React.Component<{}, State> {
 		super(props, context);
 		this.state = {
 			nodes: NodesStore.nodes,
-			certificates: CertificatesStore.certificates,
 			disabled: false,
 		};
 	}
 
 	componentDidMount(): void {
 		NodesStore.addChangeListener(this.onChange);
-		CertificatesStore.addChangeListener(this.onChange);
 		NodeActions.sync();
-		CertificateActions.sync();
 	}
 
 	componentWillUnmount(): void {
 		NodesStore.removeChangeListener(this.onChange);
-		CertificatesStore.removeChangeListener(this.onChange);
 	}
 
 	onChange = (): void => {
 		this.setState({
 			...this.state,
 			nodes: NodesStore.nodes,
-			certificates: CertificatesStore.certificates,
 		});
 	}
 
@@ -62,7 +53,6 @@ export default class Nodes extends React.Component<{}, State> {
 			nodesDom.push(<Node
 				key={node.id}
 				node={node}
-				certificates={this.state.certificates}
 			/>);
 		});
 
