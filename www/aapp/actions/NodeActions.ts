3d1d05784929888471882053f90a8dab7e1f3487
Zachary Huff <zach.huff.386@gmail.com>
2018-02-14 02:48:04 -0500

    Add sync zone to node actions

--- www/aapp/actions/NodeActions.ts
+++ www/aapp/actions/NodeActions.ts
@@ -60,6 +60,53 @@ export function sync(): Promise<void> {
 	});
 }
 
+export function syncZone(zone: string): Promise<void> {
+	let curSyncId = MiscUtils.uuid();
+	syncId = curSyncId;
+
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.get('/node')
+			.query({
+				...NodesStore.filter,
+				zone: zone,
+			})
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (curSyncId !== syncId) {
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to load nodes names');
+					reject(err);
+					return;
+				}
+
+				Dispatcher.dispatch({
+					type: NodeTypes.SYNC_ZONE,
+					data: {
+						nodes: res.body,
+					},
+				});
+
+				resolve();
+			});
+	});
+}
+
 export function traverse(page: number): Promise<void> {
 	Dispatcher.dispatch({
 		type: NodeTypes.TRAVERSE,
