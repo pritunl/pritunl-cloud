faa8183ddd5c42bc5f4971847b402ac4c2ffa096
Zachary Huff <zach.huff.386@gmail.com>
2018-02-14 21:54:21 -0500

    Add vmdk utils

---
+++ vmdk/utils.go
@@ -0,0 +1,58 @@
+package vmdk
+
+import (
+	"bytes"
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"os"
+)
+
+func SetUuid(diskPath string, diskUuid string) (err error) {
+	diskFile, err := os.OpenFile(diskPath, os.O_RDWR, 0)
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "vmdk: Failed to open file"),
+		}
+		return
+	}
+	defer diskFile.Close()
+
+	buffer := make([]byte, 10000)
+	nRead, err := diskFile.Read(buffer)
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "vmdk: Failed to read file"),
+		}
+		return
+	}
+
+	i := bytes.Index(buffer, []byte("ddb.uuid.image="))
+
+	newBuffer := append(buffer[:i+16], []byte(diskUuid)...)
+	newBuffer = append(newBuffer, buffer[i+52:]...)
+
+	nWrite, err := diskFile.WriteAt(newBuffer, 0)
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrap(err, "vmdk: Failed to write file"),
+		}
+		return
+	}
+
+	if nRead != nWrite {
+		err = &errortypes.WriteError{
+			errors.New("vmdk: Write count mismatch"),
+		}
+		return
+	}
+
+	err = diskFile.Sync()
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrap(err, "vmdk: Failed to sync file"),
+		}
+		return
+	}
+
+	return
+}
