93cd1c5aeac0cc57b24bb093c406a4f81d7baa80
Zachary Huff <zach.huff.386@gmail.com>
2018-02-15 04:57:35 -0500

    Add vm utils

---
+++ vm/utils.go
@@ -0,0 +1,19 @@
+package vm
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2/bson"
+	"path"
+)
+
+func GetVmPath(id bson.ObjectId) string {
+	return path.Join(Root, id.Hex())
+}
+
+func GetDiskPath(id bson.ObjectId, num int) string {
+	return path.Join(GetVmPath(id), fmt.Sprintf("disk%d.vmdk", num))
+}
+
+func GetMacAddr(id bson.ObjectId) string {
+	return "0800" + id.Hex()[16:]
+}
