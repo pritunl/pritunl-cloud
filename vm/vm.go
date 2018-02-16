3e119a7e37cc0eff7e58b47ff27c4e14f1d59831
Zachary Huff <zach.huff.386@gmail.com>
2018-02-16 01:55:19 -0500

    Add commit to vm

--- vm/vm.go
+++ vm/vm.go
@@ -1,6 +1,7 @@
 package vm
 
 import (
+	"github.com/pritunl/pritunl-cloud/database"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -23,4 +24,33 @@ type NetworkAdapter struct {
 	BridgedInterface string
 	IpAddress        string
 	IpAddress6       string
+}
+
+func (v *VirtualMachine) Commit(db *database.Database) (err error) {
+	coll := db.Instances()
+
+	addr := ""
+	addr6 := ""
+	if len(v.NetworkAdapters) > 0 {
+		addr = v.NetworkAdapters[0].IpAddress
+		addr6 = v.NetworkAdapters[0].IpAddress6
+	}
+
+	err = coll.UpdateId(v.Id, &bson.M{
+		"$set": &bson.M{
+			"state":      v.State,
+			"public_ip":  addr,
+			"public_ip6": addr6,
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		if _, ok := err.(*database.NotFoundError); ok {
+			err = nil
+		} else {
+			return
+		}
+	}
+
+	return
 }
