5678f62627d1285b3e035865fa660971ce0161a3
Zachary Huff <zach.huff.386@gmail.com>
2018-02-16 19:51:15 -0500

    Add vm constants

--- vm/constants.go
+++ vm/constants.go
@@ -1,7 +1,18 @@
 package vm
 
 const (
-	Root = "/vms"
-	BridgedInterface = "enp0s31f6"
-	ImagePath = "/vms/centos7.vmdk"
+	Root               = "/vms"
+	BridgedInterface   = "enp0s31f6"
+	ImagePath          = "/vms/centos7.vmdk"
+	Stuck              = "stuck"
+	PowerOff           = "poweroff"
+	Aborted            = "aborted"
+	Starting           = "starting"
+	Running            = "running"
+	Paused             = "paused"
+	Saving             = "saving"
+	Saved              = "saved"
+	Restoring          = "restoring"
+	OnlineSnapshotting = "onlinesnapshotting"
+	//PoweredOff         = "poweredoff"
 )
