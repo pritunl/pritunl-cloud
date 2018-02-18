b20666dbbf136b57d68ee7bb7791837b0ddfde6d
Zachary Huff <zach.huff.386@gmail.com>
2018-02-18 03:00:37 -0500

    Add multilock

---
+++ utils/multilock.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"sync"
+)
+
+type MultiLock struct {
+	counts map[string]int
+	locks  map[string]*sync.Mutex
+	lock   sync.Mutex
+}
+
+func (m *MultiLock) Lock(id string) {
+	m.lock.Lock()
+	val := m.counts[id]
+	lock, ok := m.locks[id]
+	if !ok {
+		lock = &sync.Mutex{}
+		m.locks[id] = lock
+	}
+	m.counts[id] = val + 1
+	m.lock.Unlock()
+
+	lock.Lock()
+}
+
+func (m *MultiLock) Unlock(id string) {
+	m.lock.Lock()
+	val := m.counts[id]
+	lock, ok := m.locks[id]
+	if ok {
+		if val <= 1 {
+			delete(m.counts, id)
+			delete(m.locks, id)
+		} else {
+			m.counts[id] = val - 1
+		}
+	}
+	m.lock.Unlock()
+
+	if lock != nil {
+		lock.Unlock()
+	}
+}
+
+func NewMultiLock() *MultiLock {
+	return &MultiLock{
+		counts: map[string]int{},
+		locks:  map[string]*sync.Mutex{},
+		lock:   sync.Mutex{},
+	}
+}
