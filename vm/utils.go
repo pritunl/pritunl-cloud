db7f47f403bd484e03607b815cf0b5675838ab4f
Zachary Huff <zach.huff.386@gmail.com>
2018-02-18 17:20:01 -0500

    Add get iface to vm utils

--- vm/utils.go
+++ vm/utils.go
@@ -3,9 +3,11 @@ package vm
 import (
 	"bytes"
 	"crypto/md5"
+	"encoding/base32"
 	"fmt"
 	"gopkg.in/mgo.v2/bson"
 	"path"
+	"strings"
 )
 
 func GetVmPath(id bson.ObjectId) string {
@@ -18,17 +20,24 @@ func GetDiskPath(id bson.ObjectId, num int) string {
 
 func GetMacAddr(id bson.ObjectId) string {
 	hash := md5.New()
-	hash.Write([]byte(id.Hex()))
+	hash.Write([]byte(id))
 	macHash := fmt.Sprintf("%x", hash.Sum(nil))
-	macHash = macHash[:12]
+	macHash = macHash[:10]
 	macBuf := bytes.Buffer{}
 
 	for i, run := range macHash {
 		macBuf.WriteRune(run)
-		if i%2 == 1 && i != 11 {
+		if i%2 == 1 && i != 9 {
 			macBuf.WriteRune(':')
 		}
 	}
 
-	return macBuf.String()
+	return "00:" + macBuf.String()
+}
+
+func GetIface(id bson.ObjectId, n int) string {
+	hash := md5.New()
+	hash.Write([]byte(id))
+	hashSum := base32.StdEncoding.EncodeToString(hash.Sum(nil))[:12]
+	return fmt.Sprintf("p%s%d", strings.ToLower(hashSum), n)
 }
