d2c98d8ef00ffcdc4b38f74190514d72cec137a5
Zachary Huff <zach.huff.386@gmail.com>
2018-02-23 06:47:08 -0500

    Update static paths

--- constants/constants.go
+++ constants/constants.go
@@ -1,6 +1,8 @@
 package constants
 
 import (
+	"go/build"
+	"path"
 	"time"
 )
 
@@ -21,9 +23,17 @@ var (
 	StaticRoot = []string{
 		"www/dist",
 		"/usr/share/pritunl-cloud/www",
+		path.Join(
+			build.Default.GOPATH,
+			"src/github.com/pritunl/pritunl-cloud/www/dist",
+		),
 	}
 	StaticTestingRoot = []string{
 		"www",
 		"/usr/share/pritunl-cloud/www",
+		path.Join(
+			build.Default.GOPATH,
+			"src/github.com/pritunl/pritunl-cloud/www",
+		),
 	}
 )
