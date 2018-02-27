4ff08d3220162d569a64a032b30e5f1ad9c95a12
Zachary Huff <zach.huff.386@gmail.com>
2018-02-27 03:49:17 -0500

    Add authority components

---
+++ www/app/components/Authorities.tsx
@@ -0,0 +1,299 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as AuthorityTypes from '../types/AuthorityTypes';
+import * as OrganizationTypes from '../types/OrganizationTypes';
+import AuthoritiesStore from '../stores/AuthoritiesStore';
+import OrganizationsStore from '../stores/OrganizationsStore';
+import * as AuthorityActions from '../actions/AuthorityActions';
+import * as OrganizationActions from '../actions/OrganizationActions';
+import Authority from './Authority';
+import AuthoritiesPage from './AuthoritiesPage';
+import Page from './Page';
+import PageHeader from './PageHeader';
+import NonState from './NonState';
+import ConfirmButton from './ConfirmButton';
+
+interface Selected {
+	[key: string]: boolean;
+}
+
+interface Opened {
+	[key: string]: boolean;
+}
+
+interface State {
+	authorities: AuthorityTypes.AuthoritiesRo;
+	organizations: OrganizationTypes.OrganizationsRo;
+	selected: Selected;
+	opened: Opened;
+	newOpened: boolean;
+	lastSelected: string;
+	disabled: boolean;
+}
+
+const css = {
+	items: {
+		width: '100%',
+		marginTop: '-5px',
+		display: 'table',
+		borderSpacing: '0 5px',
+	} as React.CSSProperties,
+	itemsBox: {
+		width: '100%',
+		overflowY: 'auto',
+	} as React.CSSProperties,
+	placeholder: {
+		opacity: 0,
+		width: '100%',
+	} as React.CSSProperties,
+	header: {
+		marginTop: '-19px',
+	} as React.CSSProperties,
+	heading: {
+		margin: '19px 0 0 0',
+	} as React.CSSProperties,
+	button: {
+		margin: '10px 0 0 10px',
+	} as React.CSSProperties,
+};
+
+export default class Authorities extends React.Component<{}, State> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			authorities: AuthoritiesStore.authorities,
+			organizations: OrganizationsStore.organizations,
+			selected: {},
+			opened: {},
+			newOpened: false,
+			lastSelected: null,
+			disabled: false,
+		};
+	}
+
+	get selected(): boolean {
+		return !!Object.keys(this.state.selected).length;
+	}
+
+	get opened(): boolean {
+		return !!Object.keys(this.state.opened).length;
+	}
+
+	componentDidMount(): void {
+		AuthoritiesStore.addChangeListener(this.onChange);
+		OrganizationsStore.addChangeListener(this.onChange);
+		AuthorityActions.sync();
+		OrganizationActions.sync();
+	}
+
+	componentWillUnmount(): void {
+		AuthoritiesStore.removeChangeListener(this.onChange);
+		OrganizationsStore.removeChangeListener(this.onChange);
+	}
+
+	onChange = (): void => {
+		let authorities = AuthoritiesStore.authorities;
+		let selected: Selected = {};
+		let curSelected = this.state.selected;
+		let opened: Opened = {};
+		let curOpened = this.state.opened;
+
+		authorities.forEach((authority: AuthorityTypes.Authority): void => {
+			if (curSelected[authority.id]) {
+				selected[authority.id] = true;
+			}
+			if (curOpened[authority.id]) {
+				opened[authority.id] = true;
+			}
+		});
+
+		this.setState({
+			...this.state,
+			authorities: authorities,
+			organizations: OrganizationsStore.organizations,
+			selected: selected,
+			opened: opened,
+		});
+	}
+
+	onDelete = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		AuthorityActions.removeMulti(
+				Object.keys(this.state.selected)).then((): void => {
+			this.setState({
+				...this.state,
+				selected: {},
+				disabled: false,
+			});
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+		});
+	}
+
+	render(): JSX.Element {
+		let authoritiesDom: JSX.Element[] = [];
+
+		this.state.authorities.forEach((
+				authority: AuthorityTypes.AuthorityRo): void => {
+			authoritiesDom.push(<Authority
+				key={authority.id}
+				authority={authority}
+				organizations={this.state.organizations}
+				selected={!!this.state.selected[authority.id]}
+				open={!!this.state.opened[authority.id]}
+				onSelect={(shift: boolean): void => {
+					let selected = {
+						...this.state.selected,
+					};
+
+					if (shift) {
+						let authorities = this.state.authorities;
+						let start: number;
+						let end: number;
+
+						for (let i = 0; i < authorities.length; i++) {
+							let usr = authorities[i];
+
+							if (usr.id === authority.id) {
+								start = i;
+							} else if (usr.id === this.state.lastSelected) {
+								end = i;
+							}
+						}
+
+						if (start !== undefined && end !== undefined) {
+							if (start > end) {
+								end = [start, start = end][0];
+							}
+
+							for (let i = start; i <= end; i++) {
+								selected[authorities[i].id] = true;
+							}
+
+							this.setState({
+								...this.state,
+								lastSelected: authority.id,
+								selected: selected,
+							});
+
+							return;
+						}
+					}
+
+					if (selected[authority.id]) {
+						delete selected[authority.id];
+					} else {
+						selected[authority.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						lastSelected: authority.id,
+						selected: selected,
+					});
+				}}
+				onOpen={(): void => {
+					let opened = {
+						...this.state.opened,
+					};
+
+					if (opened[authority.id]) {
+						delete opened[authority.id];
+					} else {
+						opened[authority.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						opened: opened,
+					});
+				}}
+			/>);
+		});
+
+		return <Page>
+			<PageHeader>
+				<div className="layout horizontal wrap" style={css.header}>
+					<h2 style={css.heading}>Authorities</h2>
+					<div className="flex"/>
+					<div>
+						<button
+							className="pt-button pt-intent-warning pt-icon-chevron-up"
+							style={css.button}
+							disabled={!this.opened}
+							type="button"
+							onClick={(): void => {
+								this.setState({
+									...this.state,
+									opened: {},
+								});
+							}}
+						>
+							Collapse All
+						</button>
+						<ConfirmButton
+							label="Delete Selected"
+							className="pt-intent-danger pt-icon-delete"
+							progressClassName="pt-intent-danger"
+							style={css.button}
+							disabled={!this.selected || this.state.disabled}
+							onConfirm={this.onDelete}
+						/>
+						<button
+							className="pt-button pt-intent-success pt-icon-add"
+							style={css.button}
+							disabled={this.state.disabled}
+							type="button"
+							onClick={(): void => {
+								this.setState({
+									...this.state,
+									disabled: true,
+								});
+								AuthorityActions.create({
+									name: 'New Authority',
+								} as AuthorityTypes.Authority).then((): void => {
+									this.setState({
+										...this.state,
+										disabled: false,
+									});
+								}).catch((): void => {
+									this.setState({
+										...this.state,
+										disabled: false,
+									});
+								});
+							}}
+						>New</button>
+					</div>
+				</div>
+			</PageHeader>
+			<div style={css.itemsBox}>
+				<div style={css.items}>
+					{authoritiesDom}
+					<tr className="pt-card pt-row" style={css.placeholder}>
+						<td colSpan={5} style={css.placeholder}/>
+					</tr>
+				</div>
+			</div>
+			<NonState
+				hidden={!!authoritiesDom.length}
+				iconClass="pt-icon-office"
+				title="No authorities"
+				description="Add a new authority to get started."
+			/>
+			<AuthoritiesPage
+				onPage={(): void => {
+					this.setState({
+						lastSelected: null,
+					});
+				}}
+			/>
+		</Page>;
+	}
+}
---
+++ www/app/components/AuthorityDetailed.tsx
@@ -0,0 +1,592 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as AuthorityTypes from '../types/AuthorityTypes';
+import * as AuthorityActions from '../actions/AuthorityActions';
+import * as OrganizationTypes from "../types/OrganizationTypes";
+import PageInput from './PageInput';
+import PageSelect from './PageSelect';
+import PageInfo from './PageInfo';
+import PageInputButton from './PageInputButton';
+import PageTextArea from './PageTextArea';
+import PageSave from './PageSave';
+import ConfirmButton from './ConfirmButton';
+import Help from './Help';
+
+interface Props {
+	organizations: OrganizationTypes.OrganizationsRo;
+	authority: AuthorityTypes.AuthorityRo;
+	selected: boolean;
+	onSelect: (shift: boolean) => void;
+	onClose: () => void;
+}
+
+interface State {
+	disabled: boolean;
+	changed: boolean;
+	message: string;
+	addRole: string;
+	addNetworkRole: string;
+	authority: AuthorityTypes.Authority;
+}
+
+const css = {
+	card: {
+		position: 'relative',
+		padding: '48px 10px 0 10px',
+		width: '100%',
+	} as React.CSSProperties,
+	button: {
+		height: '30px',
+	} as React.CSSProperties,
+	buttons: {
+		cursor: 'pointer',
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		padding: '4px',
+		height: '39px',
+		backgroundColor: 'rgba(0, 0, 0, 0.13)',
+	} as React.CSSProperties,
+	item: {
+		margin: '9px 5px 0 5px',
+		height: '20px',
+	} as React.CSSProperties,
+	itemsLabel: {
+		display: 'block',
+	} as React.CSSProperties,
+	itemsAdd: {
+		margin: '8px 0 15px 0',
+	} as React.CSSProperties,
+	group: {
+		flex: 1,
+		minWidth: '250px',
+	} as React.CSSProperties,
+	save: {
+		paddingBottom: '10px',
+	} as React.CSSProperties,
+	label: {
+		width: '100%',
+		maxWidth: '280px',
+	} as React.CSSProperties,
+	status: {
+		margin: '6px 0 0 1px',
+	} as React.CSSProperties,
+	icon: {
+		marginRight: '3px',
+	} as React.CSSProperties,
+	inputGroup: {
+		width: '100%',
+	} as React.CSSProperties,
+	protocol: {
+		flex: '0 1 auto',
+	} as React.CSSProperties,
+	port: {
+		flex: '1',
+	} as React.CSSProperties,
+	select: {
+		margin: '7px 0px 0px 6px',
+	} as React.CSSProperties,
+	role: {
+		margin: '9px 5px 0 5px',
+		height: '20px',
+	} as React.CSSProperties,
+	rules: {
+		marginBottom: '15px',
+	} as React.CSSProperties,
+};
+
+export default class AuthorityDetailed extends React.Component<Props, State> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			disabled: false,
+			changed: false,
+			message: '',
+			addRole: null,
+			addNetworkRole: null,
+			authority: null,
+		};
+	}
+
+	set(name: string, val: any): void {
+		let authority: any;
+
+		if (this.state.changed) {
+			authority = {
+				...this.state.authority,
+			};
+		} else {
+			authority = {
+				...this.props.authority,
+			};
+		}
+
+		authority[name] = val;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			authority: authority,
+		});
+	}
+
+	onAddRole = (): void => {
+		let authority: AuthorityTypes.Authority;
+
+		if (!this.state.addRole) {
+			return;
+		}
+
+		if (this.state.changed) {
+			authority = {
+				...this.state.authority,
+			};
+		} else {
+			authority = {
+				...this.props.authority,
+			};
+		}
+
+		let roles = [
+			...(authority.roles || []),
+		];
+
+
+		if (roles.indexOf(this.state.addRole) === -1) {
+			roles.push(this.state.addRole);
+		}
+
+		roles.sort();
+		authority.roles = roles;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			message: '',
+			addRole: '',
+			authority: authority,
+		});
+	}
+
+	onRemoveRole = (role: string): void => {
+		let authority: AuthorityTypes.Authority;
+
+		if (this.state.changed) {
+			authority = {
+				...this.state.authority,
+			};
+		} else {
+			authority = {
+				...this.props.authority,
+			};
+		}
+
+		let roles = [
+			...(authority.roles || []),
+		];
+
+		let i = roles.indexOf(role);
+		if (i === -1) {
+			return;
+		}
+
+		roles.splice(i, 1);
+		authority.roles = roles;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			message: '',
+			addRole: '',
+			authority: authority,
+		});
+	}
+
+	onAddNetworkRole = (): void => {
+		let authority: AuthorityTypes.Authority;
+
+		if (!this.state.addNetworkRole) {
+			return;
+		}
+
+		if (this.state.changed) {
+			authority = {
+				...this.state.authority,
+			};
+		} else {
+			authority = {
+				...this.props.authority,
+			};
+		}
+
+		let networkRoles = [
+			...(authority.network_roles || []),
+		];
+
+
+		if (networkRoles.indexOf(this.state.addNetworkRole) === -1) {
+			networkRoles.push(this.state.addNetworkRole);
+		}
+
+		networkRoles.sort();
+		authority.network_roles = networkRoles;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			message: '',
+			addNetworkRole: '',
+			authority: authority,
+		});
+	}
+
+	onRemoveNetworkRole = (networkRole: string): void => {
+		let authority: AuthorityTypes.Authority;
+
+		if (this.state.changed) {
+			authority = {
+				...this.state.authority,
+			};
+		} else {
+			authority = {
+				...this.props.authority,
+			};
+		}
+
+		let networkRoles = [
+			...(authority.network_roles || []),
+		];
+
+		let i = networkRoles.indexOf(networkRole);
+		if (i === -1) {
+			return;
+		}
+
+		networkRoles.splice(i, 1);
+		authority.network_roles = networkRoles;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			message: '',
+			addNetworkRole: '',
+			authority: authority,
+		});
+	}
+
+	onSave = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+
+
+		let authority: any = {
+			...this.state.authority,
+		};
+
+		if (this.props.organizations.length && !authority.organization) {
+			authority.organization = this.props.organizations[0].id;
+		}
+
+		AuthorityActions.commit(this.state.authority).then((): void => {
+			this.setState({
+				...this.state,
+				message: 'Your changes have been saved',
+				changed: false,
+				disabled: false,
+			});
+
+			setTimeout((): void => {
+				if (!this.state.changed) {
+					this.setState({
+						...this.state,
+						authority: null,
+						changed: false,
+					});
+				}
+			}, 1000);
+
+			setTimeout((): void => {
+				if (!this.state.changed) {
+					this.setState({
+						...this.state,
+						message: '',
+					});
+				}
+			}, 3000);
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				message: '',
+				disabled: false,
+			});
+		});
+	}
+
+	onDelete = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		AuthorityActions.remove(this.props.authority.id).then((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+		});
+	}
+
+	render(): JSX.Element {
+		let authority: AuthorityTypes.Authority = this.state.authority ||
+			this.props.authority;
+
+		let hasOrganizations = !!this.props.organizations.length;
+		let organizationsSelect: JSX.Element[] = [];
+		if (this.props.organizations.length) {
+			for (let organization of this.props.organizations) {
+				organizationsSelect.push(
+					<option
+						key={organization.id}
+						value={organization.id}
+					>{organization.name}</option>,
+				);
+			}
+		}
+
+		if (!hasOrganizations) {
+			organizationsSelect.push(
+				<option key="null" value="">No Organizations</option>);
+		}
+
+		let roles: JSX.Element[] = [];
+		for (let role of (authority.roles || [])) {
+			roles.push(
+				<div
+					className="pt-tag pt-tag-removable pt-intent-primary"
+					style={css.role}
+					key={role}
+				>
+					{role}
+					<button
+						className="pt-tag-remove"
+						disabled={this.state.disabled}
+						onMouseUp={(): void => {
+							this.onRemoveRole(role);
+						}}
+					/>
+				</div>,
+			);
+		}
+
+		let networkRoles: JSX.Element[] = [];
+		for (let networkRole of (authority.network_roles || [])) {
+			networkRoles.push(
+				<div
+					className="pt-tag pt-tag-removable pt-intent-primary"
+					style={css.role}
+					key={networkRole}
+				>
+					{networkRole}
+					<button
+						className="pt-tag-remove"
+						disabled={this.state.disabled}
+						onMouseUp={(): void => {
+							this.onRemoveNetworkRole(networkRole);
+						}}
+					/>
+				</div>,
+			);
+		}
+
+		return <td
+			className="pt-cell"
+			colSpan={5}
+			style={css.card}
+		>
+			<div className="layout horizontal wrap">
+				<div style={css.group}>
+					<div
+						className="layout horizontal"
+						style={css.buttons}
+						onClick={(evt): void => {
+							let target = evt.target as HTMLElement;
+
+							if (target.className.indexOf('open-ignore') !== -1) {
+								return;
+							}
+
+							this.props.onClose();
+						}}
+					>
+            <div>
+              <label
+                className="pt-control pt-checkbox open-ignore"
+                style={css.select}
+              >
+                <input
+                  type="checkbox"
+                  className="open-ignore"
+                  checked={this.props.selected}
+                  onClick={(evt): void => {
+										this.props.onSelect(evt.shiftKey);
+									}}
+                />
+                <span className="pt-control-indicator open-ignore"/>
+              </label>
+            </div>
+						<div className="flex"/>
+						<ConfirmButton
+							className="pt-minimal pt-intent-danger pt-icon-trash open-ignore"
+							style={css.button}
+							progressClassName="pt-intent-danger"
+							confirmMsg="Confirm authority remove"
+							disabled={this.state.disabled}
+							onConfirm={this.onDelete}
+						/>
+					</div>
+					<PageInput
+						label="Name"
+						help="Name of authority"
+						type="text"
+						placeholder="Enter name"
+						value={authority.name}
+						onChange={(val): void => {
+							this.set('name', val);
+						}}
+					/>
+					<PageSelect
+						label="Type"
+						help="Authority type. SSH keys will be saved to ~/.ssh/authorized_keys. SSH certificates will be saved to the SSH server configuration."
+						value={authority.type}
+						onChange={(val): void => {
+							this.set('type', val);
+						}}
+					>
+						<option value="ssh_key">SSH Key</option>
+						<option value="ssh_certificate">SSH Certificate</option>
+					</PageSelect>
+					<PageTextArea
+						hidden={authority.type !== 'ssh_key'}
+						label="SSH Key"
+						help="SSH authorized public key in PEM format."
+						placeholder="Public key"
+						rows={6}
+						value={authority.key}
+						onChange={(val: string): void => {
+							this.set('key', val);
+						}}
+					/>
+					<PageTextArea
+						hidden={authority.type !== 'ssh_certificate'}
+						label="SSH Certificate"
+						help="SSH certificate authority in PEM format."
+						placeholder="Certificate authority"
+						rows={6}
+						value={authority.certificate}
+						onChange={(val: string): void => {
+							this.set('certificate', val);
+						}}
+					/>
+					<label
+						className="pt-label"
+						hidden={authority.type !== 'ssh_certificate'}
+					>
+						Roles
+						<Help
+							title="Roles"
+							content="Roles that will be matched with authority principles. Roles are case-sensitive."
+						/>
+						<div>
+							{roles}
+						</div>
+					</label>
+					<PageInputButton
+						disabled={this.state.disabled}
+						buttonClass="pt-intent-success pt-icon-add"
+						hidden={authority.type !== 'ssh_certificate'}
+						label="Add"
+						type="text"
+						placeholder="Add role"
+						value={this.state.addRole}
+						onChange={(val): void => {
+							this.setState({
+								...this.state,
+								addRole: val,
+							});
+						}}
+						onSubmit={this.onAddRole}
+					/>
+				</div>
+				<div style={css.group}>
+					<PageInfo
+						fields={[
+							{
+								label: 'ID',
+								value: this.props.authority.id || 'Unknown',
+							},
+						]}
+					/>
+					<PageSelect
+						disabled={this.state.disabled || !hasOrganizations}
+						label="Organization"
+						help="Organization for authority, both the organaization and role must match. Select node authority to match node network roles."
+						value={authority.organization}
+						onChange={(val): void => {
+							this.set('organization', val);
+						}}
+					>
+						{organizationsSelect}
+					</PageSelect>
+					<label className="pt-label">
+						Network Roles
+						<Help
+							title="Network Roles"
+							content="Network roles that will be matched with authorities. Network roles are case-sensitive."
+						/>
+						<div>
+							{networkRoles}
+						</div>
+					</label>
+					<PageInputButton
+						disabled={this.state.disabled}
+						buttonClass="pt-intent-success pt-icon-add"
+						label="Add"
+						type="text"
+						placeholder="Add role"
+						value={this.state.addNetworkRole}
+						onChange={(val): void => {
+							this.setState({
+								...this.state,
+								addNetworkRole: val,
+							});
+						}}
+						onSubmit={this.onAddNetworkRole}
+					/>
+				</div>
+			</div>
+			<PageSave
+				style={css.save}
+				hidden={!this.state.authority && !this.state.message}
+				message={this.state.message}
+				changed={this.state.changed}
+				disabled={this.state.disabled}
+				light={true}
+				onCancel={(): void => {
+					this.setState({
+						...this.state,
+						changed: false,
+						authority: null,
+					});
+				}}
+				onSave={this.onSave}
+			/>
+		</td>;
+	}
+}
