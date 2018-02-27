645e939d16fda5f08826d0b51356811900a9b718
Zachary Huff <zach.huff.386@gmail.com>
2018-02-26 23:30:59 -0500

    Add authority handlers

---
+++ ahandlers/authority.go
@@ -0,0 +1,227 @@
+package ahandlers
+
+import (
+	"fmt"
+	"github.com/dropbox/godropbox/container/set"
+	"github.com/gin-gonic/gin"
+	"github.com/pritunl/pritunl-cloud/authority"
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/demo"
+	"github.com/pritunl/pritunl-cloud/event"
+	"github.com/pritunl/pritunl-cloud/utils"
+	"gopkg.in/mgo.v2/bson"
+	"strconv"
+	"strings"
+)
+
+type authorityData struct {
+	Id             bson.ObjectId `json:"id"`
+	Name           string        `json:"name"`
+	Organization   bson.ObjectId `json:"organization"`
+	AuthorityRoles []string      `json:"network_roles"`
+}
+
+type authoritiesData struct {
+	Authorities []*authority.Authority `json:"authorities"`
+	Count       int                    `json:"count"`
+}
+
+func authorityPut(c *gin.Context) {
+	if demo.Blocked(c) {
+		return
+	}
+
+	db := c.MustGet("db").(*database.Database)
+	data := &authorityData{}
+
+	authorityId, ok := utils.ParseObjectId(c.Param("authority_id"))
+	if !ok {
+		utils.AbortWithStatus(c, 400)
+		return
+	}
+
+	err := c.Bind(data)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	fire, err := authority.Get(db, authorityId)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	fire.Name = data.Name
+	fire.Organization = data.Organization
+	fire.AuthorityRoles = data.AuthorityRoles
+
+	fields := set.NewSet(
+		"state",
+		"name",
+		"organization",
+		"authority_roles",
+	)
+
+	errData, err := fire.Validate(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		c.JSON(400, errData)
+		return
+	}
+
+	err = fire.CommitFields(db, fields)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	event.PublishDispatch(db, "authority.change")
+
+	c.JSON(200, fire)
+}
+
+func authorityPost(c *gin.Context) {
+	if demo.Blocked(c) {
+		return
+	}
+
+	db := c.MustGet("db").(*database.Database)
+	data := &authorityData{
+		Name: "New Authority",
+	}
+
+	err := c.Bind(data)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	fire := &authority.Authority{
+		Name:           data.Name,
+		Organization:   data.Organization,
+		AuthorityRoles: data.AuthorityRoles,
+	}
+
+	errData, err := fire.Validate(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		c.JSON(400, errData)
+		return
+	}
+
+	err = fire.Insert(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	event.PublishDispatch(db, "authority.change")
+
+	c.JSON(200, fire)
+}
+
+func authorityDelete(c *gin.Context) {
+	if demo.Blocked(c) {
+		return
+	}
+
+	db := c.MustGet("db").(*database.Database)
+
+	authorityId, ok := utils.ParseObjectId(c.Param("authority_id"))
+	if !ok {
+		utils.AbortWithStatus(c, 400)
+		return
+	}
+
+	err := authority.Remove(db, authorityId)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	event.PublishDispatch(db, "authority.change")
+
+	c.JSON(200, nil)
+}
+
+func authoritiesDelete(c *gin.Context) {
+	if demo.Blocked(c) {
+		return
+	}
+
+	db := c.MustGet("db").(*database.Database)
+	data := []bson.ObjectId{}
+
+	err := c.Bind(&data)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	err = authority.RemoveMulti(db, data)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	event.PublishDispatch(db, "authority.change")
+
+	c.JSON(200, nil)
+}
+
+func authorityGet(c *gin.Context) {
+	db := c.MustGet("db").(*database.Database)
+
+	authorityId, ok := utils.ParseObjectId(c.Param("authority_id"))
+	if !ok {
+		utils.AbortWithStatus(c, 400)
+		return
+	}
+
+	fire, err := authority.Get(db, authorityId)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	c.JSON(200, fire)
+}
+
+func authoritiesGet(c *gin.Context) {
+	db := c.MustGet("db").(*database.Database)
+
+	page, _ := strconv.Atoi(c.Query("page"))
+	pageCount, _ := strconv.Atoi(c.Query("page_count"))
+
+	query := bson.M{}
+
+	name := strings.TrimSpace(c.Query("name"))
+	if name != "" {
+		query["name"] = &bson.M{
+			"$regex":   fmt.Sprintf(".*%s.*", name),
+			"$options": "i",
+		}
+	}
+
+	authorities, count, err := authority.GetAllPaged(db, &query, page, pageCount)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	data := &authoritiesData{
+		Authorities: authorities,
+		Count:       count,
+	}
+
+	c.JSON(200, data)
+}
