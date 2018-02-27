bcbb11600eec91f8df19dc408a1b57f921c814cf
Zachary Huff <zach.huff.386@gmail.com>
2018-02-26 23:29:06 -0500

    Add authority

---
+++ authority/authority.go
@@ -0,0 +1,65 @@
+package authority
+
+import (
+	"github.com/dropbox/godropbox/container/set"
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Authority struct {
+	Id             bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	Name           string        `bson:"name" json:"name"`
+	Organization   bson.ObjectId `bson:"organization,omitempty" json:"organization"`
+	AuthorityRoles []string      `bson:"network_roles" json:"network_roles"`
+}
+
+func (f *Authority) Validate(db *database.Database) (
+	errData *errortypes.ErrorData, err error) {
+
+	return
+}
+
+func (f *Authority) Commit(db *database.Database) (err error) {
+	coll := db.Authorities()
+
+	err = coll.Commit(f.Id, f)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (f *Authority) CommitFields(db *database.Database, fields set.Set) (
+	err error) {
+
+	coll := db.Authorities()
+
+	err = coll.CommitFields(f.Id, f, fields)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (f *Authority) Insert(db *database.Database) (err error) {
+	coll := db.Authorities()
+
+	if f.Id != "" {
+		err = &errortypes.DatabaseError{
+			errors.New("firewall: Authority already exists"),
+		}
+		return
+	}
+
+	err = coll.Insert(f)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
