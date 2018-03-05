941f02e78714a14e2aad53ec38a5a8ed2c8687ac
Zachary Huff <zach.huff.386@gmail.com>
2018-03-04 22:34:52 -0500

    Add vpc

---
+++ vpc/utils.go
@@ -0,0 +1,112 @@
+package vpc
+
+import (
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/utils"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func Get(db *database.Database, vcId bson.ObjectId) (
+	vc *Vpc, err error) {
+
+	coll := db.Vpcs()
+	vc = &Vpc{}
+
+	err = coll.FindOneId(vcId, vc)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func GetAll(db *database.Database, query *bson.M) (
+	vcs []*Vpc, err error) {
+
+	coll := db.Vpcs()
+	vcs = []*Vpc{}
+
+	cursor := coll.Find(query).Iter()
+
+	nde := &Vpc{}
+	for cursor.Next(nde) {
+		vcs = append(vcs, nde)
+		nde = &Vpc{}
+	}
+
+	err = cursor.Close()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func GetAllPaged(db *database.Database, query *bson.M, page, pageCount int) (
+	vcs []*Vpc, count int, err error) {
+
+	coll := db.Vpcs()
+	vcs = []*Vpc{}
+
+	qury := coll.Find(query)
+
+	count, err = qury.Count()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	skip := utils.Min(page*pageCount, utils.Max(0, count-pageCount))
+
+	cursor := qury.Sort("name").Skip(skip).Limit(pageCount).Iter()
+
+	vc := &Vpc{}
+	for cursor.Next(vc) {
+		vcs = append(vcs, vc)
+		vc = &Vpc{}
+	}
+
+	err = cursor.Close()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func Remove(db *database.Database, vcId bson.ObjectId) (err error) {
+	coll := db.Vpcs()
+
+	err = coll.Remove(&bson.M{
+		"_id": vcId,
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		switch err.(type) {
+		case *database.NotFoundError:
+			err = nil
+		default:
+			return
+		}
+	}
+
+	return
+}
+
+func RemoveMulti(db *database.Database, vcIds []bson.ObjectId) (err error) {
+	coll := db.Vpcs()
+
+	_, err = coll.RemoveAll(&bson.M{
+		"_id": &bson.M{
+			"$in": vcIds,
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
---
+++ vpc/vpc.go
@@ -0,0 +1,66 @@
+package vpc
+
+import (
+	"github.com/dropbox/godropbox/container/set"
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Vpc struct {
+	Id           bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	Name         string        `bson:"name" json:"name"`
+	Network      string        `bson:"network" json:"network"`
+	Organization bson.ObjectId `bson:"organization" json:"organization"`
+	Datacenter   bson.ObjectId `bson:"datacenter" json:"datacenter"`
+}
+
+func (v *Vpc) Validate(db *database.Database) (
+	errData *errortypes.ErrorData, err error) {
+
+	return
+}
+
+func (v *Vpc) Commit(db *database.Database) (err error) {
+	coll := db.Vpcs()
+
+	err = coll.Commit(v.Id, v)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (v *Vpc) CommitFields(db *database.Database, fields set.Set) (
+	err error) {
+
+	coll := db.Vpcs()
+
+	err = coll.CommitFields(v.Id, v, fields)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (v *Vpc) Insert(db *database.Database) (err error) {
+	coll := db.Vpcs()
+
+	if v.Id != "" {
+		err = &errortypes.DatabaseError{
+			errors.New("firewall: Vpc already exists"),
+		}
+		return
+	}
+
+	err = coll.Insert(v)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
