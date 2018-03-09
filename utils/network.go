05700610e5057f338b216946cabc3a28a00edf8b
Zachary Huff <zach.huff.386@gmail.com>
2018-03-09 00:20:04 -0500

    Add ip address int utils

--- utils/network.go
+++ utils/network.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"encoding/binary"
 	"net"
 )
 
@@ -17,4 +18,17 @@ func CopyIpAddress(src net.IP) net.IP {
 	dst := make(net.IP, len(src))
 	copy(dst, src)
 	return dst
+}
+
+func IpAddress2Int(ip net.IP) int64 {
+	if len(ip) == 16 {
+		return int64(binary.BigEndian.Uint32(ip[12:16]))
+	}
+	return int64(binary.BigEndian.Uint32(ip))
+}
+
+func Int2IpAddress(n int64) net.IP {
+	ip := make(net.IP, 4)
+	binary.BigEndian.PutUint32(ip, uint32(n))
+	return ip
 }
