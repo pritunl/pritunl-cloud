92f08322184f6dbab54826d4eb146a4110b5213d
Zachary Huff <zach.huff.386@gmail.com>
2018-03-09 00:20:59 -0500

    Add get ip to vpc

--- vpc/vpc.go
+++ vpc/vpc.go
@@ -5,6 +5,8 @@ import (
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/utils"
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"math/rand"
 	"net"
@@ -74,6 +76,127 @@ func (v *Vpc) GetNetwork() (network *net.IPNet, err error) {
 
 func (v *Vpc) GenerateVpcId() {
 	v.VpcId = rand.Intn(16777100) + 110
+}
+
+func (v *Vpc) GetIp(db *database.Database, instId bson.ObjectId) (
+	ip net.IP, err error) {
+
+	coll := db.VpcsIp()
+	vpcIp := &VpcIp{}
+
+	err = coll.FindOne(&bson.M{
+		"vpc":      v.Id,
+		"instance": instId,
+	}, vpcIp)
+	if err != nil {
+		vpcIp = nil
+		if _, ok := err.(*database.NotFoundError); ok {
+			err = nil
+		} else {
+			return
+		}
+	}
+
+	if vpcIp == nil {
+		vpcIp = &VpcIp{}
+		change := mgo.Change{
+			Update: &bson.M{
+				"$set": &bson.M{
+					"instance": instId,
+				},
+			},
+			ReturnNew: true,
+		}
+
+		info, e := coll.Find(&bson.M{
+			"vpc":      v.Id,
+			"instance": nil,
+		}).Apply(change, vpcIp)
+		if e != nil {
+			err = database.ParseError(e)
+			vpcIp = nil
+			if _, ok := err.(*database.NotFoundError); ok {
+				err = nil
+			} else {
+				return
+			}
+		} else if info.Updated == 0 {
+			vpcIp = nil
+		}
+	}
+
+	if vpcIp == nil {
+		vpcIp = &VpcIp{}
+
+		err = coll.Find(&bson.M{
+			"vpc": v.Id,
+		}).Sort("-ip").One(vpcIp)
+		if err != nil {
+			vpcIp = nil
+			err = database.ParseError(err)
+			if _, ok := err.(*database.NotFoundError); ok {
+				err = nil
+			} else {
+				return
+			}
+		}
+
+		network, e := v.GetNetwork()
+		if e != nil {
+			err = e
+			return
+		}
+
+		var curIp net.IP
+		if vpcIp == nil {
+			curIp = utils.CopyIpAddress(network.IP)
+			utils.IncIpAddress(curIp)
+		} else {
+			curIp = utils.Int2IpAddress(vpcIp.Ip)
+		}
+
+		for {
+			utils.IncIpAddress(curIp)
+
+			vpcIp = &VpcIp{
+				Vpc:      v.Id,
+				Ip:       utils.IpAddress2Int(curIp),
+				Instance: instId,
+			}
+
+			if !network.Contains(curIp) {
+				vpcIp = nil
+				err = &errortypes.NotFoundError{
+					errors.New("vpc: Address pool full"),
+				}
+				return
+			}
+
+			err = coll.Insert(vpcIp)
+			if err != nil {
+				vpcIp = nil
+				err = database.ParseError(err)
+				if _, ok := err.(*database.DuplicateKeyError); ok {
+					err = nil
+					continue
+				}
+				return
+			}
+
+			break
+		}
+	}
+
+	if vpcIp == nil {
+		err = &errortypes.NotFoundError{
+			errors.New("vpc: Address not found"),
+		}
+		return
+	}
+
+	ip = utils.Int2IpAddress(vpcIp.Ip)
+
+	return
 }
 
 func (v *Vpc) Commit(db *database.Database) (err error) {
