1e183f8a5c70fecb18a2782f9652612a1f77d402
Zachary Huff <zach.huff.386@gmail.com>
2018-03-09 00:21:56 -0500

    Add vpc ips index

--- database/database.go
+++ database/database.go
@@ -448,6 +448,18 @@ func addIndexes() (err error) {
 		}
 	}
 
+	coll = db.VpcsIp()
+	err = coll.EnsureIndex(mgo.Index{
+		Key:        []string{"vpc", "ip"},
+		Unique:     true,
+		Background: true,
+	})
+	if err != nil {
+		err = &IndexError{
+			errors.Wrap(err, "database: Index error"),
+		}
+	}
+
 	coll = db.Sessions()
 	err = coll.EnsureIndex(mgo.Index{
 		Key:        []string{"user"},
