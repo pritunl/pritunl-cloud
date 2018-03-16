f83488c1c365d48f9b3990faacd1e258da064c64
Zachary Huff <zach.huff.386@gmail.com>
2018-03-16 03:24:19 -0400

    Add type to vpc ip

--- vpc/ip.go
+++ vpc/ip.go
@@ -6,5 +6,6 @@ type VpcIp struct {
 	Id       bson.ObjectId `bson:"_id,omitempty"`
 	Vpc      bson.ObjectId `bson:"vpc"`
 	Ip       int64         `bson:"ip"`
+	Type     string        `bson:"type"`
 	Instance bson.ObjectId `bson:"instance"`
 }
