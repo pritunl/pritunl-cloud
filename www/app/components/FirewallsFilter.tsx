70608d4a815a877d7bde767d124bf9d607873576
Zachary Huff <zach.huff.386@gmail.com>
2018-05-06 17:12:45 -0400

    Add firewalls filter component

---
+++ www/app/components/FirewallsFilter.tsx
@@ -0,0 +1,125 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as FirewallTypes from '../types/FirewallTypes';
+import SearchInput from './SearchInput';
+import * as OrganizationTypes from "../types/OrganizationTypes";
+
+interface Props {
+	filter: FirewallTypes.Filter;
+	onFilter: (filter: FirewallTypes.Filter) => void;
+	organizations: OrganizationTypes.OrganizationsRo;
+}
+
+const css = {
+	filters: {
+		margin: '-15px 0 5px 0',
+	} as React.CSSProperties,
+	input: {
+		width: '200px',
+		margin: '5px',
+	} as React.CSSProperties,
+	keybase: {
+		width: '175px',
+		margin: '5px',
+	} as React.CSSProperties,
+	role: {
+		width: '150px',
+		margin: '5px',
+	} as React.CSSProperties,
+	type: {
+		margin: '5px',
+	} as React.CSSProperties,
+	check: {
+		margin: '12px 5px 8px 5px',
+	} as React.CSSProperties,
+};
+
+export default class FirewallsFilter extends React.Component<Props, {}> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			menu: false,
+		};
+	}
+
+	render(): JSX.Element {
+		if (this.props.filter === null) {
+			return <div/>;
+		}
+
+		let organizationsSelect: JSX.Element[] = [
+			<option key="key" value="any">Any</option>,
+		];
+		if (this.props.organizations && this.props.organizations.length) {
+			for (let organization of this.props.organizations) {
+				organizationsSelect.push(
+					<option
+						key={organization.id}
+						value={organization.id}
+					>{organization.name}</option>,
+				);
+			}
+		}
+
+		return <div className="layout horizontal wrap" style={css.filters}>
+			<SearchInput
+				style={css.input}
+				placeholder="Name"
+				value={this.props.filter.name}
+				onChange={(val: string): void => {
+					let filter = {
+						...this.props.filter,
+					};
+
+					if (val) {
+						filter.name = val;
+					} else {
+						delete filter.name;
+					}
+
+					this.props.onFilter(filter);
+				}}
+			/>
+			<SearchInput
+				style={css.role}
+				placeholder="Network Role"
+				value={this.props.filter.network_role}
+				onChange={(val: string): void => {
+					let filter = {
+						...this.props.filter,
+					};
+
+					if (val) {
+						filter.network_role = val;
+					} else {
+						delete filter.network_role;
+					}
+
+					this.props.onFilter(filter);
+				}}
+			/>
+			<div className="pt-select" style={css.type}>
+				<select
+					value={this.props.filter.organization || 'any'}
+					onChange={(evt): void => {
+						let filter = {
+							...this.props.filter,
+						};
+
+						let val = evt.target.value;
+
+						if (val === 'any') {
+							delete filter.organization;
+						} else {
+							filter.organization = val;
+						}
+
+						this.props.onFilter(filter);
+					}}
+				>
+					{organizationsSelect}
+				</select>
+			</div>
+		</div>;
+	}
+}
