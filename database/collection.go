16614c1311a610450a802260d87b65860c02b3c3
Zachary Huff <zach.huff.386@gmail.com>
2018-06-02 22:26:17 -0400

    Update database collection

--- database/collection.go
+++ database/collection.go
@@ -52,9 +52,7 @@ func (c *Collection) Commit(id interface{}, data interface{}) (err error) {
 func (c *Collection) CommitFields(id interface{}, data interface{},
 	fields set.Set) (err error) {
 
-	err = c.UpdateId(id, bson.M{
-		"$set": SelectFields(data, fields),
-	})
+	err = c.UpdateId(id, SelectFieldsAll(data, fields))
 	if err != nil {
 		err = ParseError(err)
 		return
