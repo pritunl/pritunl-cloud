8609f62b9b3438b83aeeadfca5236378fd960859
Zachary Huff <zach.huff.386@gmail.com>
2018-06-06 01:46:10 -0400

    Change bridge name

--- settings/hypervisor.go
+++ settings/hypervisor.go
@@ -6,7 +6,7 @@ type hypervisor struct {
 	Id           string `bson:"_id"`
 	SystemdPath  string `bson:"systemd_path" default:"/etc/systemd/system"`
 	LibPath      string `bson:"systemd_path" default:"/var/lib/pritunl-cloud"`
-	BridgeName   string `bson:"bridge_name" default:"pritunlbr"`
+	BridgeName   string `bson:"bridge_name" default:"pritunlbr0"`
 	StartTimeout int    `bson:"start_timeout" default:"30"`
 	StopTimeout  int    `bson:"stop_timeout" default:"60"`
 }
