4a3fd8aeeb864a23a40d300a2f1e85ac7fe0315b
Zachary Huff <zach.huff.386@gmail.com>
2018-07-04 00:12:41 -0400

    Add delay to secondary utils

--- secondary/utils.go
+++ secondary/utils.go
@@ -5,6 +5,7 @@ import (
 	"github.com/pritunl/pritunl-cloud/settings"
 	"github.com/pritunl/pritunl-cloud/utils"
 	"gopkg.in/mgo.v2/bson"
+	"math/rand"
 	"time"
 )
 
@@ -40,6 +41,8 @@ func Get(db *database.Database, token string, typ string) (
 
 	timestamp := time.Now().Add(
 		-time.Duration(settings.Auth.SecondaryExpire) * time.Second)
+
+	time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
 
 	err = coll.FindOne(&bson.M{
 		"_id":  token,
