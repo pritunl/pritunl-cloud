9e41acc5d57f60e7c76c2ef760c1810c960a4020
Zachary Huff <zach.huff.386@gmail.com>
2018-11-17 04:43:41 -0500

    Add devices to uhandlers

---
+++ uhandlers/devices.go
@@ -0,0 +1,607 @@
+package uhandlers
+
+import (
+	"github.com/dropbox/godropbox/container/set"
+	"github.com/gin-gonic/gin"
+	"github.com/pritunl/pritunl-cloud/audit"
+	"github.com/pritunl/pritunl-cloud/authorizer"
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/demo"
+	"github.com/pritunl/pritunl-cloud/device"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/event"
+	"github.com/pritunl/pritunl-cloud/secondary"
+	"github.com/pritunl/pritunl-cloud/u2flib"
+	"github.com/pritunl/pritunl-cloud/utils"
+	"github.com/pritunl/pritunl-cloud/validator"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type deviceData struct {
+	Name string `json:"name"`
+}
+
+func devicePut(c *gin.Context) {
+	if demo.Blocked(c) {
+		return
+	}
+
+	db := c.MustGet("db").(*database.Database)
+	authr := c.MustGet("authorizer").(*authorizer.Authorizer)
+	data := &deviceData{}
+
+	devcId, ok := utils.ParseObjectId(c.Param("device_id"))
+	if !ok {
+		utils.AbortWithStatus(c, 400)
+		return
+	}
+
+	err := c.Bind(data)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	usr, err := authr.GetUser(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	devc, err := device.GetUser(db, devcId, usr.Id)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	devc.Name = data.Name
+
+	fields := set.NewSet(
+		"name",
+	)
+
+	errData, err := devc.Validate(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		c.JSON(400, errData)
+		return
+	}
+
+	err = devc.CommitFields(db, fields)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	event.PublishDispatch(db, "device.change")
+
+	c.JSON(200, devc)
+}
+
+func deviceDelete(c *gin.Context) {
+	if demo.Blocked(c) {
+		return
+	}
+
+	db := c.MustGet("db").(*database.Database)
+	authr := c.MustGet("authorizer").(*authorizer.Authorizer)
+
+	usr, err := authr.GetUser(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	devcId, ok := utils.ParseObjectId(c.Param("device_id"))
+	if !ok {
+		utils.AbortWithStatus(c, 400)
+		return
+	}
+
+	count, err := device.Count(db, usr.Id)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if count <= 1 {
+		usr.Disabled = true
+		err = usr.CommitFields(db, set.NewSet("disabled"))
+		if err != nil {
+			utils.AbortWithError(c, 500, err)
+			return
+		}
+	}
+
+	err = device.RemoveUser(db, devcId, usr.Id)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	count, err = device.Count(db, usr.Id)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if count == 0 {
+		if !usr.Disabled {
+			usr.Disabled = true
+			err = usr.CommitFields(db, set.NewSet("disabled"))
+			if err != nil {
+				utils.AbortWithError(c, 500, err)
+				return
+			}
+		}
+
+		err = audit.New(
+			db,
+			c.Request,
+			usr.Id,
+			audit.UserAccountDisable,
+			audit.Fields{
+				"reason": "All authentication devices removed",
+			},
+		)
+		if err != nil {
+			utils.AbortWithError(c, 500, err)
+			return
+		}
+
+		errData := &errortypes.ErrorData{
+			Error:   "device_empty",
+			Message: "Account disabled contact an administrator",
+		}
+		c.JSON(401, errData)
+		return
+	}
+
+	event.PublishDispatch(db, "device.change")
+
+	c.JSON(200, nil)
+}
+
+func devicesGet(c *gin.Context) {
+	db := c.MustGet("db").(*database.Database)
+	authr := c.MustGet("authorizer").(*authorizer.Authorizer)
+
+	usr, err := authr.GetUser(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	devices, err := device.GetAllSorted(db, usr.Id)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	c.JSON(200, devices)
+}
+
+type devicesU2fRegisterRespData struct {
+	Token   string      `json:"token"`
+	Request interface{} `json:"request"`
+}
+
+func deviceU2fRegisterGet(c *gin.Context) {
+	if demo.Blocked(c) {
+		return
+	}
+
+	db := c.MustGet("db").(*database.Database)
+	authr := c.MustGet("authorizer").(*authorizer.Authorizer)
+
+	usr, err := authr.GetUser(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	_, secProviderId, errAudit, errData, err := validator.ValidateUser(
+		db, usr, false, c.Request)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		if errAudit == nil {
+			errAudit = audit.Fields{
+				"error":   errData.Error,
+				"message": errData.Message,
+			}
+		}
+		errAudit["method"] = "add_device_register"
+
+		err = audit.New(
+			db,
+			c.Request,
+			usr.Id,
+			audit.UserAuthFailed,
+			errAudit,
+		)
+		if err != nil {
+			utils.AbortWithError(c, 500, err)
+			return
+		}
+
+		c.JSON(400, errData)
+		return
+	}
+
+	deviceCount, err := device.Count(db, usr.Id)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if deviceCount > 0 || secProviderId != "" {
+		secType := ""
+		var secProvider bson.ObjectId
+
+		if deviceCount == 0 {
+			secType = secondary.UserManage
+			secProvider = secProviderId
+		} else {
+			secType = secondary.UserManageDevice
+			secProvider = secondary.DeviceProvider
+		}
+
+		secd, err := secondary.New(db, usr.Id, secType, secProvider)
+		if err != nil {
+			utils.AbortWithError(c, 500, err)
+			return
+		}
+
+		data, err := secd.GetData()
+		if err != nil {
+			utils.AbortWithError(c, 500, err)
+			return
+		}
+
+		c.JSON(201, data)
+		return
+	}
+
+	secd, err := secondary.New(db, usr.Id, secondary.UserManageDeviceRegister,
+		secondary.DeviceProvider)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	jsonResp, errData, err := secd.DeviceRegisterRequest(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		c.JSON(400, errData)
+		return
+	}
+
+	resp := &devicesU2fRegisterRespData{
+		Token:   secd.Id,
+		Request: jsonResp,
+	}
+
+	c.JSON(200, resp)
+}
+
+type devicesU2fRegisterData struct {
+	Type     string                   `json:"type"`
+	Token    string                   `json:"token"`
+	Name     string                   `json:"name"`
+	Response *u2flib.RegisterResponse `json:"response"`
+}
+
+func deviceU2fRegisterPost(c *gin.Context) {
+	if demo.Blocked(c) {
+		return
+	}
+
+	db := c.MustGet("db").(*database.Database)
+	authr := c.MustGet("authorizer").(*authorizer.Authorizer)
+
+	data := &devicesU2fRegisterData{}
+
+	err := c.Bind(data)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	usr, err := authr.GetUser(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	secd, err := secondary.Get(db, data.Token,
+		secondary.UserManageDeviceRegister)
+	if err != nil {
+		if _, ok := err.(*database.NotFoundError); ok {
+			errData := &errortypes.ErrorData{
+				Error:   "secondary_expired",
+				Message: "Secondary authentication has expired",
+			}
+			c.JSON(400, errData)
+		} else {
+			utils.AbortWithError(c, 500, err)
+		}
+		return
+	}
+
+	devc, errData, err := secd.DeviceRegisterResponse(
+		db, data.Response, data.Name)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		c.JSON(400, errData)
+		return
+	}
+
+	err = audit.New(
+		db,
+		c.Request,
+		usr.Id,
+		audit.DeviceRegister,
+		audit.Fields{
+			"device_id": devc.Id,
+		},
+	)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	event.PublishDispatch(db, "device.change")
+
+	c.JSON(200, nil)
+}
+
+type deviceU2fSecondaryData struct {
+	Type     string `json:"type"`
+	Token    string `json:"token"`
+	Factor   string `json:"factor"`
+	Passcode string `json:"passcode"`
+}
+
+func deviceU2fSecondaryPut(c *gin.Context) {
+	db := c.MustGet("db").(*database.Database)
+	authr := c.MustGet("authorizer").(*authorizer.Authorizer)
+	data := &deviceU2fSecondaryData{}
+
+	err := c.Bind(data)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	secd, err := secondary.Get(db, data.Token, secondary.UserManage)
+	if err != nil {
+		if _, ok := err.(*database.NotFoundError); ok {
+			errData := &errortypes.ErrorData{
+				Error:   "secondary_expired",
+				Message: "Secondary authentication has expired",
+			}
+			c.JSON(400, errData)
+		} else {
+			utils.AbortWithError(c, 500, err)
+		}
+		return
+	}
+
+	errData, err := secd.Handle(db, c.Request, data.Factor, data.Passcode)
+	if err != nil {
+		if _, ok := err.(*secondary.IncompleteError); ok {
+			c.Status(206)
+		} else {
+			utils.AbortWithError(c, 500, err)
+		}
+		return
+	}
+
+	if errData != nil {
+		c.JSON(400, errData)
+		return
+	}
+
+	usr, err := authr.GetUser(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	secd, err = secondary.New(db, usr.Id, secondary.UserManageDeviceRegister,
+		secondary.DeviceProvider)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	jsonResp, errData, err := secd.DeviceRegisterRequest(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		c.JSON(400, errData)
+		return
+	}
+
+	resp := &devicesU2fRegisterRespData{
+		Token:   secd.Id,
+		Request: jsonResp,
+	}
+
+	c.JSON(200, resp)
+}
+
+func deviceU2fSignGet(c *gin.Context) {
+	db := c.MustGet("db").(*database.Database)
+	token := c.Query("token")
+
+	secd, err := secondary.Get(db, token, secondary.UserManageDevice)
+	if err != nil {
+		if _, ok := err.(*database.NotFoundError); ok {
+			errData := &errortypes.ErrorData{
+				Error:   "secondary_expired",
+				Message: "Secondary authentication has expired",
+			}
+			c.JSON(400, errData)
+		} else {
+			utils.AbortWithError(c, 500, err)
+		}
+		return
+	}
+
+	resp, errData, err := secd.DeviceSignRequest(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		c.JSON(400, errData)
+		return
+	}
+
+	c.JSON(200, resp)
+}
+
+type deviceU2fSignData struct {
+	Type     string               `json:"type"`
+	Token    string               `json:"token"`
+	Response *u2flib.SignResponse `json:"response"`
+}
+
+func deviceU2fSignPost(c *gin.Context) {
+	db := c.MustGet("db").(*database.Database)
+	authr := c.MustGet("authorizer").(*authorizer.Authorizer)
+	data := &deviceU2fSignData{}
+
+	err := c.Bind(data)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	secd, err := secondary.Get(db, data.Token, secondary.UserManageDevice)
+	if err != nil {
+		if _, ok := err.(*database.NotFoundError); ok {
+			errData := &errortypes.ErrorData{
+				Error:   "secondary_expired",
+				Message: "Secondary authentication has expired",
+			}
+			c.JSON(400, errData)
+		} else {
+			utils.AbortWithError(c, 500, err)
+		}
+		return
+	}
+
+	errData, err := secd.DeviceSignResponse(db, data.Response)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		c.JSON(400, errData)
+		return
+	}
+
+	usr, err := authr.GetUser(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	_, secProviderId, errAudit, errData, err := validator.ValidateUser(
+		db, usr, false, c.Request)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		if errAudit == nil {
+			errAudit = audit.Fields{
+				"error":   errData.Error,
+				"message": errData.Message,
+			}
+		}
+		errAudit["method"] = "add_device_register"
+
+		err = audit.New(
+			db,
+			c.Request,
+			usr.Id,
+			audit.UserAuthFailed,
+			errAudit,
+		)
+		if err != nil {
+			utils.AbortWithError(c, 500, err)
+			return
+		}
+
+		c.JSON(400, errData)
+		return
+	}
+
+	if secProviderId != "" {
+		secd, err := secondary.New(db, usr.Id,
+			secondary.UserManage, secProviderId)
+		if err != nil {
+			utils.AbortWithError(c, 500, err)
+			return
+		}
+
+		data, err := secd.GetData()
+		if err != nil {
+			utils.AbortWithError(c, 500, err)
+			return
+		}
+
+		c.JSON(201, data)
+		return
+	}
+
+	secd, err = secondary.New(db, usr.Id, secondary.UserManageDeviceRegister,
+		secondary.DeviceProvider)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	jsonResp, errData, err := secd.DeviceRegisterRequest(db)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	if errData != nil {
+		c.JSON(400, errData)
+		return
+	}
+
+	resp := &devicesU2fRegisterRespData{
+		Token:   secd.Id,
+		Request: jsonResp,
+	}
+
+	c.JSON(200, resp)
+}
