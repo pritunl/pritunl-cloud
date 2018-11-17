c4df86575813db7fc28d37fd205b2d97a0126bf2
Zachary Huff <zach.huff.386@gmail.com>
2018-11-17 04:40:25 -0500

    Add nodes sync

---
+++ sync/nodes.go
@@ -0,0 +1,67 @@
+package sync
+
+import (
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/node"
+	"github.com/pritunl/pritunl-cloud/settings"
+	"time"
+)
+
+func nodeSync() (err error) {
+	db := database.GetDatabase()
+	defer db.Close()
+
+	appId := ""
+	facets := []string{}
+
+	if node.Self.UserDomain != "" {
+		appId = fmt.Sprintf("https://%s/auth/u2f/app.json",
+			node.Self.UserDomain)
+	}
+
+	nodes, err := node.GetAll(db)
+	if err != nil {
+		return
+	}
+
+	for _, nde := range nodes {
+		if appId == "" {
+			appId = fmt.Sprintf("https://%s/auth/u2f/app.json",
+				nde.UserDomain)
+		}
+		if nde.UserDomain != "" {
+			facets = append(facets,
+				fmt.Sprintf("https://%s", nde.UserDomain))
+		}
+		if nde.AdminDomain != "" {
+			facets = append(facets,
+				fmt.Sprintf("https://%s", nde.AdminDomain))
+		}
+	}
+
+	settings.Local.AppId = appId
+	settings.Local.Facets = facets
+
+	return
+}
+
+func nodeRunner() {
+	time.Sleep(1 * time.Second)
+
+	for {
+		err := nodeSync()
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Error("sync: Failed to sync node status")
+		}
+
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func initNode() {
+	go nodeRunner()
+}
