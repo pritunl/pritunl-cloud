0218e9b60fd75c003f13786f6d8a18465cff6063
Zachary Huff <zach.huff.386@gmail.com>
2018-11-17 04:37:24 -0500

    Update secondary constants

--- secondary/constants.go
+++ secondary/constants.go
@@ -1,5 +1,9 @@
 package secondary
 
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
 const (
 	Duo      = "duo"
 	OneLogin = "one_login"
@@ -9,6 +13,19 @@ const (
 	Passcode = "passcode"
 	Sms      = "sms"
 
-	Admin = "admin"
-	User  = "user"
+	Admin                    = "admin"
+	AdminDevice              = "admin_device"
+	AdminDeviceRegister      = "admin_device_register"
+	User                     = "user"
+	UserDevice               = "user_device"
+	UserDeviceRegister       = "user_device_register"
+	UserManage               = "user_manage"
+	UserManageDevice         = "user_manage_device"
+	UserManageDeviceRegister = "user_manage_device_register"
+	Authority                = "authority"
+	AuthorityDevice          = "authority_device"
+)
+
+var (
+	DeviceProvider = bson.ObjectIdHex("100000000000000000000000")
 )
