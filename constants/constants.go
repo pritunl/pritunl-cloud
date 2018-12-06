dd4eac40d1953bd29f86fa1a688a275992c4bb7f
Zachary Huff <zach.huff.386@gmail.com>
2018-12-06 18:51:45 -0500

    Create new release

--- constants/constants.go
+++ constants/constants.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	Version         = "1.0.1091.33"
+	Version         = "1.0.1108.99"
 	DatabaseVersion = 1
 	ConfPath        = "/cloud/pritunl-cloud.json"
 	LogPath         = "/var/log/pritunl-cloud.log"
