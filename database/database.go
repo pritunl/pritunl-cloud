5fd571262c5ab1c0c09ae25aa30c617e79eed3a1
Zachary Huff <zach.huff.386@gmail.com>
2018-12-27 01:03:17 -0500

    Add disk index

--- database/database.go
+++ database/database.go
@@ -554,6 +554,15 @@ func addIndexes() (err error) {
 			errors.Wrap(err, "database: Index error"),
 		}
 	}
+	err = coll.EnsureIndex(mgo.Index{
+		Key:        []string{"backing_image"},
+		Background: true,
+	})
+	if err != nil {
+		err = &IndexError{
+			errors.Wrap(err, "database: Index error"),
+		}
+	}
 
 	coll = db.Domains()
 	err = coll.EnsureIndex(mgo.Index{
