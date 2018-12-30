636702b18a58e809142f5ba15c679e9beacd2512
Zachary Huff <zach.huff.386@gmail.com>
2018-12-30 02:25:13 -0500

    Add ipset deploy

---
+++ deploy/ipset.go
@@ -0,0 +1,50 @@
+package deploy
+
+import (
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/ipset"
+	"github.com/pritunl/pritunl-cloud/state"
+)
+
+type Ipset struct {
+	stat *state.State
+}
+
+func (t *Ipset) Deploy() (err error) {
+	db := database.GetDatabase()
+	defer db.Close()
+
+	instaces := t.stat.Instances()
+	namespaces := t.stat.Namespaces()
+	nodeFirewall := t.stat.NodeFirewall()
+	firewalls := t.stat.Firewalls()
+
+	err = ipset.UpdateState(instaces, namespaces, nodeFirewall, firewalls)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (t *Ipset) Clean() (err error) {
+	db := database.GetDatabase()
+	defer db.Close()
+
+	instaces := t.stat.Instances()
+	nodeFirewall := t.stat.NodeFirewall()
+	firewalls := t.stat.Firewalls()
+
+	err = ipset.UpdateNamesState(instaces, nodeFirewall, firewalls)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func NewIpset(stat *state.State) *Ipset {
+	return &Ipset{
+		stat: stat,
+	}
+}
