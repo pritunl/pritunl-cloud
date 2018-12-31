c53223a1aec6b053a029d135e912138cd2434f56
Zachary Huff <zach.huff.386@gmail.com>
2018-12-31 17:08:31 -0500

    Add lock open to multi timeout lock

--- utils/multitimeoutlock.go
+++ utils/multitimeoutlock.go
@@ -19,8 +19,6 @@ type MultiTimeoutLock struct {
 }
 
 func (m *MultiTimeoutLock) Lock(id string) (lockId bson.ObjectId) {
-	lockId = bson.NewObjectId()
-
 	m.lock.Lock()
 	val := m.counts[id]
 	lock, ok := m.locks[id]
@@ -38,6 +36,60 @@ func (m *MultiTimeoutLock) Lock(id string) (lockId bson.ObjectId) {
 		errors.New("utils: Multi lock timeout"),
 	}
 
+	lockId = bson.NewObjectId()
+	m.stateLock.Lock()
+	m.state[lockId] = true
+	m.stateLock.Unlock()
+
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+
+			m.stateLock.Lock()
+			state := m.state[lockId]
+			m.stateLock.Unlock()
+			if !state {
+				return
+			}
+
+			if time.Since(start) > m.timeout {
+				logrus.WithFields(logrus.Fields{
+					"error": err,
+				}).Error("utils: Multi lock timed out")
+				return
+			}
+		}
+	}()
+
+	return
+}
+
+func (m *MultiTimeoutLock) LockOpen(id string) (
+	acquired bool, lockId bson.ObjectId) {
+
+	m.lock.Lock()
+	val := m.counts[id]
+	lock, ok := m.locks[id]
+	if ok {
+		m.lock.Unlock()
+		return
+	}
+
+	lock = &sync.Mutex{}
+	m.locks[id] = lock
+	m.counts[id] = val + 1
+	m.lock.Unlock()
+
+	acquired = true
+
+	lock.Lock()
+
+	start := time.Now()
+	err := &errortypes.TimeoutError{
+		errors.New("utils: Multi lock timeout"),
+	}
+
+	lockId = bson.NewObjectId()
 	m.stateLock.Lock()
 	m.state[lockId] = true
 	m.stateLock.Unlock()
@@ -68,8 +120,6 @@ func (m *MultiTimeoutLock) Lock(id string) (lockId bson.ObjectId) {
 func (m *MultiTimeoutLock) LockTimeout(id string,
 	timeout time.Duration) (lockId bson.ObjectId) {
 
-	lockId = bson.NewObjectId()
-
 	m.lock.Lock()
 	val := m.counts[id]
 	lock, ok := m.locks[id]
@@ -87,6 +137,60 @@ func (m *MultiTimeoutLock) LockTimeout(id string,
 		errors.New("utils: Multi lock timeout"),
 	}
 
+	lockId = bson.NewObjectId()
+	m.stateLock.Lock()
+	m.state[lockId] = true
+	m.stateLock.Unlock()
+
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+
+			m.stateLock.Lock()
+			state := m.state[lockId]
+			m.stateLock.Unlock()
+			if !state {
+				return
+			}
+
+			if time.Since(start) > timeout {
+				logrus.WithFields(logrus.Fields{
+					"error": err,
+				}).Error("utils: Multi lock timed out")
+				return
+			}
+		}
+	}()
+
+	return
+}
+
+func (m *MultiTimeoutLock) LockOpenTimeout(id string,
+	timeout time.Duration) (acquired bool, lockId bson.ObjectId) {
+
+	m.lock.Lock()
+	val := m.counts[id]
+	lock, ok := m.locks[id]
+	if ok {
+		m.lock.Unlock()
+		return
+	}
+
+	lock = &sync.Mutex{}
+	m.locks[id] = lock
+	m.counts[id] = val + 1
+	m.lock.Unlock()
+
+	acquired = true
+
+	lock.Lock()
+
+	start := time.Now()
+	err := &errortypes.TimeoutError{
+		errors.New("utils: Multi lock timeout"),
+	}
+
+	lockId = bson.NewObjectId()
 	m.stateLock.Lock()
 	m.state[lockId] = true
 	m.stateLock.Unlock()
