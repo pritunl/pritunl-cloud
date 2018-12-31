1602aa9d6a2ba82f29b6c0539d07d8080314be72
Zachary Huff <zach.huff.386@gmail.com>
2018-12-31 16:50:04 -0500

    Add iptables setup

---
+++ setup/iptables.go
@@ -0,0 +1,57 @@
+package setup
+
+import (
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/disk"
+	"github.com/pritunl/pritunl-cloud/firewall"
+	"github.com/pritunl/pritunl-cloud/instance"
+	"github.com/pritunl/pritunl-cloud/ipset"
+	"github.com/pritunl/pritunl-cloud/iptables"
+	"github.com/pritunl/pritunl-cloud/node"
+	"github.com/pritunl/pritunl-cloud/utils"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func Iptables() (err error) {
+	db := database.GetDatabase()
+	defer db.Close()
+
+	namespaces, err := utils.GetNamespaces()
+	if err != nil {
+		return
+	}
+
+	disks, err := disk.GetNode(db, node.Self.Id)
+	if err != nil {
+		return
+	}
+
+	instances, err := instance.GetAllVirt(db, &bson.M{
+		"node": node.Self.Id,
+	}, disks)
+	if err != nil {
+		return
+	}
+
+	nodeFirewall, firewalls, err := firewall.GetAllIngress(db, instances)
+	if err != nil {
+		return
+	}
+
+	err = ipset.Init(namespaces, instances, nodeFirewall, firewalls)
+	if err != nil {
+		return
+	}
+
+	err = iptables.Init(namespaces, instances, nodeFirewall, firewalls)
+	if err != nil {
+		return
+	}
+
+	err = ipset.InitNames(namespaces, instances, nodeFirewall, firewalls)
+	if err != nil {
+		return
+	}
+
+	return
+}
