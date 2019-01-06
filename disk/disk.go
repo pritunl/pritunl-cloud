06e2e04d1cab7b689cf1c2ff5c81980f600e9c8f
Zachary Huff <zach.huff.386@gmail.com>
2019-01-06 00:44:51 -0500

    Add delete protection to disk

--- disk/disk.go
+++ disk/disk.go
@@ -14,18 +14,19 @@ import (
 )
 
 type Disk struct {
-	Id             bson.ObjectId `bson:"_id,omitempty" json:"id"`
-	Name           string        `bson:"name" json:"name"`
-	State          string        `bson:"state" json:"state"`
-	Node           bson.ObjectId `bson:"node" json:"node"`
-	Organization   bson.ObjectId `bson:"organization,omitempty" json:"organization"`
-	Instance       bson.ObjectId `bson:"instance,omitempty" json:"instance"`
-	SourceInstance bson.ObjectId `bson:"source_instance,omitempty" json:"source_instance"`
-	Image          bson.ObjectId `bson:"image,omitempty" json:"image"`
-	Backing        bool          `bson:"backing" json:"backing"`
-	BackingImage   string        `bson:"backing_image" json:"backing_image"`
-	Index          string        `bson:"index" json:"index"`
-	Size           int           `bson:"size" json:"size"`
+	Id               bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	Name             string        `bson:"name" json:"name"`
+	State            string        `bson:"state" json:"state"`
+	Node             bson.ObjectId `bson:"node" json:"node"`
+	Organization     bson.ObjectId `bson:"organization,omitempty" json:"organization"`
+	Instance         bson.ObjectId `bson:"instance,omitempty" json:"instance"`
+	SourceInstance   bson.ObjectId `bson:"source_instance,omitempty" json:"source_instance"`
+	DeleteProtection bool          `bson:"delete_protection" json:"delete_protection"`
+	Image            bson.ObjectId `bson:"image,omitempty" json:"image"`
+	Backing          bool          `bson:"backing" json:"backing"`
+	BackingImage     string        `bson:"backing_image" json:"backing_image"`
+	Index            string        `bson:"index" json:"index"`
+	Size             int           `bson:"size" json:"size"`
 }
 
 func (d *Disk) Validate(db *database.Database) (
