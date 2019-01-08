2214d3e8dd81322aa223bf13ce6c9d181472f31b
Zachary Huff <zach.huff.386@gmail.com>
2019-01-08 03:06:15 -0500

    Add delay to systemd lock

--- systemd/systemd.go
+++ systemd/systemd.go
@@ -4,10 +4,11 @@ import (
 	"github.com/pritunl/pritunl-cloud/utils"
 	"strings"
 	"sync"
+	"time"
 )
 
 var (
-	systemdLock = sync.RWMutex{}
+	systemdLock = sync.Mutex{}
 )
 
 func Reload() (err error) {
@@ -19,36 +20,42 @@ func Reload() (err error) {
 		return
 	}
 
+	time.Sleep(100 * time.Millisecond)
+
 	return
 }
 
 func Start(unit string) (err error) {
-	systemdLock.RLock()
-	defer systemdLock.RUnlock()
+	systemdLock.Lock()
+	defer systemdLock.Unlock()
 
 	err = utils.Exec("", "systemctl", "start", unit)
 	if err != nil {
 		return
 	}
 
+	time.Sleep(300 * time.Millisecond)
+
 	return
 }
 
 func Stop(unit string) (err error) {
-	systemdLock.RLock()
-	defer systemdLock.RUnlock()
+	systemdLock.Lock()
+	defer systemdLock.Unlock()
 
 	err = utils.Exec("", "systemctl", "stop", unit)
 	if err != nil {
 		return
 	}
 
+	time.Sleep(300 * time.Millisecond)
+
 	return
 }
 
 func GetState(unit string) (state string, err error) {
-	systemdLock.RLock()
-	defer systemdLock.RUnlock()
+	systemdLock.Lock()
+	defer systemdLock.Unlock()
 
 	output, _ := utils.ExecOutput("", "systemctl", "is-active", unit)
 	state = strings.TrimSpace(output)
