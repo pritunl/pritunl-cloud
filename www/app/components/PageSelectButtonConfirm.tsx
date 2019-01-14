9aa400d4ccf6a81cce3d7a8bdb965e713bb1ba50
Zachary Huff <zach.huff.386@gmail.com>
2019-01-14 02:15:26 -0500

    Add page select confirm button

---
+++ www/app/components/PageSelectButtonConfirm.tsx
@@ -0,0 +1,72 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import ConfirmButton from './ConfirmButton';
+
+interface Props {
+	hidden?: boolean;
+	label: string;
+	value: string;
+	confirmMsg?: string;
+	disabled?: boolean;
+	buttonClass?: string;
+	progressClassName?: string;
+	onChange: (val: string) => void;
+	onSubmit: () => void;
+}
+
+const css = {
+	group: {
+		marginBottom: '15px',
+		width: '100%',
+		maxWidth: '280px',
+	} as React.CSSProperties,
+	select: {
+		width: '100%',
+		borderTopLeftRadius: '3px',
+		borderBottomLeftRadius: '3px',
+	} as React.CSSProperties,
+	selectInner: {
+		width: '100%',
+	} as React.CSSProperties,
+	selectBox: {
+		flex: '1',
+	} as React.CSSProperties,
+};
+
+export default class PageSelectButton extends React.Component<Props, {}> {
+	render(): JSX.Element {
+		let buttonClass = 'pt-button';
+		if (this.props.buttonClass) {
+			buttonClass += ' ' + this.props.buttonClass;
+		}
+
+		return <div
+			className="pt-control-group"
+			style={css.group}
+			hidden={this.props.hidden}
+		>
+			<div style={css.selectBox}>
+				<div className="pt-select" style={css.select}>
+					<select
+						style={css.selectInner}
+						disabled={this.props.disabled}
+						value={this.props.value || ''}
+						onChange={(evt): void => {
+							this.props.onChange(evt.target.value);
+						}}
+					>
+						{this.props.children}
+					</select>
+				</div>
+			</div>
+			<ConfirmButton
+				label={this.props.label}
+				className={buttonClass}
+				progressClassName={this.props.progressClassName}
+				confirmMsg={this.props.confirmMsg}
+				disabled={this.props.disabled}
+				onConfirm={this.props.onSubmit}
+			/>
+		</div>;
+	}
+}
