4eda0af22c927075bb9e4eb33bdc3d6e8d73afec
Zachary Huff <zach.huff.386@gmail.com>
2019-01-17 02:16:40 -0500

    Add mobile os to constants

--- www/app/Constants.ts
+++ www/app/Constants.ts
@@ -6,6 +6,7 @@ let md = new MobileDetect(window.navigator.userAgent);
 export const user: boolean = !!(window as any).user;
 
 export const mobile = !!md.mobile();
+export const mobileOs = md.os();
 
 export const loadDelay = 700;
 
