feee02458eca8738f024dc168ea78fc9c6438b5a
Zachary Huff <zach.huff.386@gmail.com>
2019-01-19 19:38:06 -0500

    Add storage classes to storage constants

--- storage/constants.go
+++ storage/constants.go
@@ -3,4 +3,8 @@ package storage
 const (
 	Public  = "public"
 	Private = "private"
+
+	AwsStandard         = "aws_standard"
+	AwsInfrequentAccess = "aws_infrequent_access"
+	AwsGlacier          = "aws_glacier"
 )
