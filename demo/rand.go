ea2a890a51db5cd67db00824a3fb749ca72e7916
Zachary Huff <zach.huff.386@gmail.com>
2019-01-20 02:43:59 -0500

    Add private ip to demo

--- demo/rand.go
+++ demo/rand.go
@@ -7,9 +7,11 @@ import (
 )
 
 var (
-	lock     = sync.Mutex{}
-	ipStore  = map[bson.ObjectId]string{}
-	ip6Store = map[bson.ObjectId]string{}
+	lock            = sync.Mutex{}
+	ipStore         = map[bson.ObjectId]string{}
+	ip6Store        = map[bson.ObjectId]string{}
+	privateIpStore  = map[bson.ObjectId]string{}
+	privateIp6Store = map[bson.ObjectId]string{}
 )
 
 func RandIp(instId bson.ObjectId) (addr string) {
@@ -33,6 +35,32 @@ func RandIp6(instId bson.ObjectId) (addr string) {
 	if addr == "" {
 		addr = utils.RandIp6()
 		ip6Store[instId] = addr
+	}
+
+	return
+}
+
+func RandPrivateIp(instId bson.ObjectId) (addr string) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	addr = privateIpStore[instId]
+	if addr == "" {
+		addr = utils.RandPrivateIp()
+		privateIpStore[instId] = addr
+	}
+
+	return
+}
+
+func RandPrivateIp6(instId bson.ObjectId) (addr string) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	addr = privateIp6Store[instId]
+	if addr == "" {
+		addr = utils.RandPrivateIp6()
+		privateIp6Store[instId] = addr
 	}
 
 	return
