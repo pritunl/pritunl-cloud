3cf89df57109504ddf587db302d4a45c3dcf8bbd
Zachary Huff <zach.huff.386@gmail.com>
2019-02-04 17:38:54 -0500

    Update iptables rules

--- iptables/iptables.go
+++ iptables/iptables.go
@@ -875,6 +875,15 @@ func generate(namespace, iface string, ingress []*firewall.Rule) (
 		Holds6:    [][]string{},
 	}
 
+	if rules.Interface != "host" {
+		cmd := rules.newCommand()
+		cmd = append(cmd,
+			"-i", "lo",
+			"-j", "ACCEPT",
+		)
+		rules.Ingress = append(rules.Ingress, cmd)
+	}
+
 	cmd := rules.newCommand()
 	if rules.Interface != "host" {
 		cmd = append(cmd,
