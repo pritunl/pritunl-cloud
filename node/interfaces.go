1957ff3e0de741d2d9aecaaaf9288406098bc949
Zachary Huff <zach.huff.386@gmail.com>
2019-02-08 01:06:35 -0500

    Add get default interface to node

--- node/interfaces.go
+++ node/interfaces.go
@@ -10,6 +10,8 @@ import (
 var (
 	netIfaces         = []string{}
 	netLastIfacesSync time.Time
+	defaultIface      = ""
+	defaultIfaceSync  time.Time
 )
 
 func GetInterfaces() (ifaces []string, err error) {
@@ -36,6 +38,36 @@ func GetInterfaces() (ifaces []string, err error) {
 	ifaces = ifacesNew
 	netLastIfacesSync = time.Now()
 	netIfaces = ifacesNew
+
+	return
+}
+
+func getDefaultIface() (iface string, err error) {
+	if time.Since(defaultIfaceSync) < 900*time.Second {
+		iface = defaultIface
+		return
+	}
+
+	output, err := utils.ExecCombinedOutput("", "route", "-n")
+	if err != nil {
+		return
+	}
+
+	outputLines := strings.Split(output, "\n")
+	for _, line := range outputLines {
+		fields := strings.Fields(line)
+		if len(fields) < 2 {
+			continue
+		}
+
+		if fields[0] == "0.0.0.0" {
+			iface = strings.TrimSpace(fields[len(fields)-1])
+			_ = strings.TrimSpace(fields[1])
+		}
+	}
+
+	defaultIface = iface
+	defaultIfaceSync = time.Now()
 
 	return
 }
