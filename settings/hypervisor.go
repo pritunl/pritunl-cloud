49716ec57b640881fea5d936ff8e57a12c8d6546
Zachary Huff <zach.huff.386@gmail.com>
2019-02-10 02:16:56 -0500

    Add host network name to hypervisor settings

--- settings/hypervisor.go
+++ settings/hypervisor.go
@@ -3,16 +3,17 @@ package settings
 var Hypervisor *hypervisor
 
 type hypervisor struct {
-	Id            string `bson:"_id"`
-	SystemdPath   string `bson:"systemd_path" default:"/etc/systemd/system"`
-	LibPath       string `bson:"systemd_path" default:"/var/lib/pritunl-cloud"`
-	NormalMtu     int    `bson:"normal_mtu" default:"1500"`
-	JumboMtu      int    `bson:"jumbo_mtu" default:"9000"`
-	VxlanId       int    `bson:"vxlan_id" default:"9417"`
-	VxlanDestPort int    `bson:"vxlan_dest_port" default:"4789"`
-	StartTimeout  int    `bson:"start_timeout" default:"45"`
-	StopTimeout   int    `bson:"stop_timeout" default:"120"`
-	RefreshRate   int    `bson:"refresh_rate" default:"90"`
+	Id              string `bson:"_id"`
+	SystemdPath     string `bson:"systemd_path" default:"/etc/systemd/system"`
+	LibPath         string `bson:"systemd_path" default:"/var/lib/pritunl-cloud"`
+	NormalMtu       int    `bson:"normal_mtu" default:"1500"`
+	JumboMtu        int    `bson:"jumbo_mtu" default:"9000"`
+	VxlanId         int    `bson:"vxlan_id" default:"9417"`
+	VxlanDestPort   int    `bson:"vxlan_dest_port" default:"4789"`
+	HostNetworkName string `bson:"host_network_name" default:"pritunlhost0"`
+	StartTimeout    int    `bson:"start_timeout" default:"45"`
+	StopTimeout     int    `bson:"stop_timeout" default:"120"`
+	RefreshRate     int    `bson:"refresh_rate" default:"90"`
 }
 
 func newHypervisor() interface{} {
