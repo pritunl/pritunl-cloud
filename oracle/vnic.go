850a8d2e11be64146a64bdc120e90ad7482bf395
Zachary Huff <zach.huff.386@gmail.com>
2019-02-13 17:30:13 -0500

    Add oracle vnic

---
+++ oracle/vnic.go
@@ -0,0 +1,88 @@
+package oracle
+
+import (
+	"context"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+
+	"github.com/oracle/oci-go-sdk/core"
+)
+
+type Vnic struct {
+	Id                  string
+	SubnetId            string
+	IsPrimary           bool
+	MacAddress          string
+	PrivateIp           string
+	PublicIp            string
+	SkipSourceDestCheck bool
+}
+
+func (v *Vnic) SetSkipSourceDestCheck(pv *Provider, val bool) (err error) {
+	client, err := pv.GetNetworkClient()
+	if err != nil {
+		return
+	}
+
+	req := core.UpdateVnicRequest{
+		VnicId: &v.Id,
+		UpdateVnicDetails: core.UpdateVnicDetails{
+			SkipSourceDestCheck: &val,
+		},
+	}
+
+	_, err = client.UpdateVnic(context.Background(), req)
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "oracle: Failed to update vnic"),
+		}
+		return
+	}
+
+	return
+}
+
+func GetVnic(pv *Provider, vnicId string) (vnic *Vnic, err error) {
+	client, err := pv.GetNetworkClient()
+	if err != nil {
+		return
+	}
+
+	req := core.GetVnicRequest{
+		VnicId: &vnicId,
+	}
+
+	orcVnic, err := client.GetVnic(context.Background(), req)
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "oracle: Failed to get vnic"),
+		}
+		return
+	}
+
+	vnic = &Vnic{}
+	if orcVnic.Id != nil {
+		vnic.Id = *orcVnic.Id
+	}
+	if orcVnic.SubnetId != nil {
+		vnic.SubnetId = *orcVnic.SubnetId
+	}
+	if orcVnic.IsPrimary != nil {
+		vnic.IsPrimary = *orcVnic.IsPrimary
+	}
+	if orcVnic.MacAddress != nil {
+		vnic.MacAddress = *orcVnic.MacAddress
+	}
+	if orcVnic.PrivateIp != nil {
+		vnic.PrivateIp = *orcVnic.PrivateIp
+	}
+	if orcVnic.PublicIp != nil {
+		vnic.PublicIp = *orcVnic.PublicIp
+	}
+	if orcVnic.SkipSourceDestCheck != nil {
+		vnic.SkipSourceDestCheck = *orcVnic.SkipSourceDestCheck
+	}
+
+	return
+}
