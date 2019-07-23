e1f736c4228a8445856f214ad71ef23350e043f9
Zachary Huff <zach.huff.386@gmail.com>
2019-07-22 21:07:03 -0400

    Add systemd utils

---
+++ systemd/utils.go
@@ -0,0 +1,30 @@
+package systemd
+
+import (
+	"fmt"
+	"time"
+)
+
+func FormatUptime(timestamp time.Time) (uptime string) {
+	since := time.Since(timestamp)
+	minutes := int64(since.Minutes())
+	days := minutes / 1440
+	hours := (minutes % 1440) / 60
+	minutes = (minutes % 1440) % 60
+
+	if days > 0 {
+		uptime = fmt.Sprintf("%d days", days)
+	}
+	if hours > 0 || uptime != "" {
+		if uptime != "" {
+			uptime += " "
+		}
+		uptime += fmt.Sprintf("%d hours", hours)
+	}
+	if uptime != "" {
+		uptime += " "
+	}
+	uptime += fmt.Sprintf("%d mins", minutes)
+
+	return
+}
