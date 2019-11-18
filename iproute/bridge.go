f50118761e14394bd6b26b63b7abe4666f08356a
Zachary Huff <zach.huff.386@gmail.com>
2019-11-17 21:16:48 -0500

    Add bridge to iproute

---
+++ iproute/bridge.go
@@ -0,0 +1,61 @@
+package iproute
+
+import (
+	"github.com/pritunl/pritunl-cloud/utils"
+)
+
+func BridgeAdd(namespace, name string) (err error) {
+	if namespace != "" {
+		_, err = utils.ExecCombinedOutputLogged(
+			[]string{
+				"File exists",
+			},
+			"ip", "netns", "exec", namespace,
+			"ip", "link",
+			"add", name,
+			"type", "bridge",
+		)
+	} else {
+		_, err = utils.ExecCombinedOutputLogged(
+			[]string{
+				"File exists",
+			},
+			"ip", "link",
+			"add", name,
+			"type", "bridge",
+		)
+	}
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func BridgeDelete(namespace, name string) (err error) {
+	if namespace != "" {
+		_, err = utils.ExecCombinedOutputLogged(
+			[]string{
+				"File exists",
+			},
+			"ip", "netns", "exec", namespace,
+			"ip", "link",
+			"delete", name,
+			"type", "bridge",
+		)
+	} else {
+		_, err = utils.ExecCombinedOutputLogged(
+			[]string{
+				"File exists",
+			},
+			"ip", "link",
+			"delete", name,
+			"type", "bridge",
+		)
+	}
+	if err != nil {
+		return
+	}
+
+	return
+}
