43add23dd103eea67a3e260e27808e90d2f21104
Zachary Huff <zach.huff.386@gmail.com>
2019-11-17 22:09:52 -0500

    Update address in bridges

--- bridges/bridges.go
+++ bridges/bridges.go
@@ -1,14 +1,11 @@
 package bridges
 
 import (
-	"net"
-	"strings"
 	"time"
 
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-cloud/errortypes"
 	"github.com/pritunl/pritunl-cloud/iproute"
-	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 var (
@@ -61,61 +58,17 @@ func GetIpAddrs(iface string) (addr string, addr6 string, err error) {
 		return
 	}
 
-	ipData, err := utils.ExecCombinedOutputLogged(
-		[]string{
-			"No such file or directory",
-			"does not exist",
-		},
-		"ip", "-f", "inet", "-o", "addr",
-		"show", "dev", iface,
-	)
+	address, address6, err := iproute.AddressGetIface("", iface)
 	if err != nil {
 		return
 	}
 
-	if strings.Contains(ipData, "not exist") {
-		err = &errortypes.NotFoundError{
-			errors.New("bridges: Failed to find external node interface"),
-		}
-		return
+	if address != nil {
+		addr = address.Local
 	}
 
-	fields := strings.Fields(ipData)
-	if len(fields) > 3 {
-		ipAddr := net.ParseIP(strings.Split(fields[3], "/")[0])
-		if ipAddr != nil && len(ipAddr) > 0 {
-			addr = ipAddr.String()
-		}
-	}
-
-	ipData, err = utils.ExecCombinedOutputLogged(
-		[]string{
-			"No such file or directory",
-			"does not exist",
-		},
-		"ip", "-f", "inet6", "-o", "addr",
-		"show", "dev", iface,
-	)
-	if err != nil {
-		return
-	}
-
-	if !strings.Contains(ipData, "not exist") {
-		for _, line := range strings.Split(ipData, "\n") {
-			if !strings.Contains(line, "global") {
-				continue
-			}
-
-			fields = strings.Fields(ipData)
-			if len(fields) > 3 {
-				ipAddr := net.ParseIP(strings.Split(fields[3], "/")[0])
-				if ipAddr != nil && len(ipAddr) > 0 {
-					addr6 = ipAddr.String()
-				}
-			}
-
-			break
-		}
+	if address6 != nil {
+		addr6 = address6.Local
 	}
 
 	curAddr[iface] = addr
