dd4a6a0ca4d199d55c50d7e35cb9432dc1df4f54
Zachary Huff <zach.huff.386@gmail.com>
2019-11-17 20:56:11 -0500

    Add iproute

---
+++ iproute/iface.go
@@ -0,0 +1,95 @@
+package iproute
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/utils"
+)
+
+type Iface struct {
+	Name  string `json:"ifname"`
+	State string `json:"operstate"`
+}
+
+func IfaceGetBridges(namespace string) (ifaces []*Iface, err error) {
+	ifaces = []*Iface{}
+
+	var output string
+	if namespace != "" {
+		output, err = utils.ExecOutputLogged(
+			nil,
+			"ip", "netns", "exec",
+			"ip", "--json", "--brief",
+			"link", "show",
+			"type", "bridge",
+		)
+	} else {
+		output, err = utils.ExecOutputLogged(
+			nil,
+			"ip", "--json", "--brief",
+			"link", "show",
+			"type", "bridge",
+		)
+	}
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal([]byte(output), &ifaces)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "bridges: Failed to prase bridges"),
+		}
+		return
+	}
+
+	return
+}
+
+func IfaceGetBridgeIfaces(namespace, bridge string) (
+	ifaces []*Iface, err error) {
+
+	ifaces = []*Iface{}
+
+	var output string
+	if namespace != "" {
+		output, err = utils.ExecCombinedOutputLogged(
+			[]string{
+				"does not exist",
+			},
+			"ip", "netns", "exec",
+			"ip", "--json", "--brief",
+			"link", "show",
+			"master", bridge,
+		)
+	} else {
+		output, err = utils.ExecCombinedOutputLogged(
+			[]string{
+				"does not exist",
+			},
+			"ip", "--json", "--brief",
+			"link", "show",
+			"master", bridge,
+		)
+	}
+	if err != nil {
+		return
+	}
+
+	if strings.Contains(output, "does not exist") {
+		return
+	}
+
+	err = json.Unmarshal([]byte(output), &ifaces)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "bridges: Failed to prase bridges"),
+		}
+		return
+	}
+
+	return
+}
