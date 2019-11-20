8d7a3d0db4c4d532d2eba6ce9c43aedbd7fc6d9c
Zachary Huff <zach.huff.386@gmail.com>
2019-11-20 04:42:43 -0500

    Add get network range to vpc subnet

--- vpc/subnet.go
+++ vpc/subnet.go
@@ -1,11 +1,46 @@
 package vpc
 
 import (
+	"net"
+
+	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 type Subnet struct {
 	Id      primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name    string             `bson:"name" json:"name"`
 	Network string             `bson:"network" json:"network"`
+}
+
+func (s *Subnet) GetNetwork() (network *net.IPNet, err error) {
+	_, network, err = net.ParseCIDR(s.Network)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "vpc: Failed to parse subnet"),
+		}
+		return
+	}
+	return
+}
+
+func (s *Subnet) GetIndexRange() (start, stop int64, err error) {
+	network, err := s.GetNetwork()
+	if err != nil {
+		return
+	}
+
+	start, err = utils.GetFirstIpIndex(network)
+	if err != nil {
+		return
+	}
+
+	stop, err = utils.GetLastIpIndex(network)
+	if err != nil {
+		return
+	}
+
+	return
 }
