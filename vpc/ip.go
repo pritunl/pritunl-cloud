ded3ceaf07acb1bf8e07f79136c6115bb7380e0d
Zachary Huff <zach.huff.386@gmail.com>
2019-11-20 04:41:50 -0500

    Add subnet to vpc

--- vpc/ip.go
+++ vpc/ip.go
@@ -1,13 +1,16 @@
 package vpc
 
 import (
+	"net"
+
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 type VpcIp struct {
 	Id       primitive.ObjectID `bson:"_id,omitempty"`
 	Vpc      primitive.ObjectID `bson:"vpc"`
+	Subnet   primitive.ObjectID `bson:"subnet"`
 	Ip       int64              `bson:"ip"`
-	Type     string             `bson:"type"`
 	Instance primitive.ObjectID `bson:"instance"`
 }
