fab30ef22c826df5a825df89b2b1e775509b218a
Zachary Huff <zach.huff.386@gmail.com>
2020-01-28 23:13:58 -0500

    Add balancer types

---
+++ www/app/types/BalancerTypes.ts
@@ -0,0 +1,52 @@
+/// <reference path="../References.d.ts"/>
+export const SYNC = 'balancer.sync';
+export const TRAVERSE = 'balancer.traverse';
+export const FILTER = 'balancer.filter';
+export const CHANGE = 'balancer.change';
+
+export interface Domain {
+	domain: string;
+	host?: string;
+}
+
+export interface Backend {
+	protocol: string;
+	hostname?: string;
+	port?: string;
+}
+
+export interface Balancer {
+	id?: string;
+	name?: string;
+	organization?: string;
+	zone?: string;
+	certificates?: string[];
+	web_sockets?: boolean;
+	domains?: Domain[];
+	backends?: Backend[];
+}
+
+export interface Filter {
+	id?: string;
+	name?: string;
+	organization?: string;
+	zone?: boolean;
+}
+
+export type Balancers = Balancer[];
+
+export type BalancerRo = Readonly<Balancer>;
+export type BalancersRo = ReadonlyArray<BalancerRo>;
+
+export interface BalancerDispatch {
+	type: string;
+	data?: {
+		id?: string;
+		balancer?: Balancer;
+		balancers?: Balancers;
+		page?: number;
+		pageCount?: number;
+		filter?: Filter;
+		count?: number;
+	};
+}
