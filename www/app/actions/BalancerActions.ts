d136652206602a6b598f426c6dbddb22ea0aaa53
Zachary Huff <zach.huff.386@gmail.com>
2020-01-28 23:14:29 -0500

    Add balancer actions

---
+++ www/app/actions/BalancerActions.ts
@@ -0,0 +1,187 @@
+/// <reference path="../References.d.ts"/>
+import * as SuperAgent from 'superagent';
+import Dispatcher from '../dispatcher/Dispatcher';
+import EventDispatcher from '../dispatcher/EventDispatcher';
+import * as Alert from '../Alert';
+import * as Csrf from '../Csrf';
+import Loader from '../Loader';
+import * as BalancerTypes from '../types/BalancerTypes';
+import BalancersStore from '../stores/BalancersStore';
+import OrganizationsStore from '../stores/OrganizationsStore';
+import * as MiscUtils from '../utils/MiscUtils';
+
+let syncId: string;
+
+export function sync(noLoading?: boolean): Promise<void> {
+	let curSyncId = MiscUtils.uuid();
+	syncId = curSyncId;
+
+	let loader: Loader;
+	if (!noLoading) {
+		loader = new Loader().loading();
+	}
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.get('/balancer')
+			.query({
+				...BalancersStore.filter,
+				page: BalancersStore.page,
+				page_count: BalancersStore.pageCount,
+			})
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.set('Organization', OrganizationsStore.current)
+			.end((err: any, res: SuperAgent.Response): void => {
+				if (loader) {
+					loader.done();
+				}
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (curSyncId !== syncId) {
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to load balancers');
+					reject(err);
+					return;
+				}
+
+				Dispatcher.dispatch({
+					type: BalancerTypes.SYNC,
+					data: {
+						balancers: res.body.balancers,
+						count: res.body.count,
+					},
+				});
+
+				resolve();
+			});
+	});
+}
+
+export function traverse(page: number): Promise<void> {
+	Dispatcher.dispatch({
+		type: BalancerTypes.TRAVERSE,
+		data: {
+			page: page,
+		},
+	});
+
+	return sync();
+}
+
+export function filter(filt: BalancerTypes.Filter): Promise<void> {
+	Dispatcher.dispatch({
+		type: BalancerTypes.FILTER,
+		data: {
+			filter: filt,
+		},
+	});
+
+	return sync();
+}
+
+export function commit(balancer: BalancerTypes.Balancer): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.put('/balancer/' + balancer.id)
+			.send(balancer)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.set('Organization', OrganizationsStore.current)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to save balancer');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function create(balancer: BalancerTypes.Balancer): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.post('/balancer')
+			.send(balancer)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.set('Organization', OrganizationsStore.current)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to create balancer');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function remove(balancerId: string): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.delete('/balancer/' + balancerId)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.set('Organization', OrganizationsStore.current)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to delete balancer');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+EventDispatcher.register((action: BalancerTypes.BalancerDispatch) => {
+	switch (action.type) {
+		case BalancerTypes.CHANGE:
+			sync();
+			break;
+	}
+});
