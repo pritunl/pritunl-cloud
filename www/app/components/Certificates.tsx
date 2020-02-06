846fa91b69ea9195a288a6e888da28c5ef1cb55b
Zachary Huff <zach.huff.386@gmail.com>
2020-02-06 04:59:58 -0500

    Add organizations to certificates component

--- www/app/components/Certificates.tsx
+++ www/app/components/Certificates.tsx
@@ -1,8 +1,11 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
 import * as CertificateTypes from '../types/CertificateTypes';
+import * as OrganizationTypes from '../types/OrganizationTypes';
 import CertificatesStore from '../stores/CertificatesStore';
+import OrganizationsStore from '../stores/OrganizationsStore';
 import * as CertificateActions from '../actions/CertificateActions';
+import * as OrganizationActions from '../actions/OrganizationActions';
 import NonState from './NonState';
 import Certificate from './Certificate';
 import Page from './Page';
@@ -10,6 +13,7 @@ import PageHeader from './PageHeader';
 
 interface State {
 	certificates: CertificateTypes.CertificatesRo;
+	organizations: OrganizationTypes.OrganizationsRo;
 	disabled: boolean;
 }
 
@@ -36,23 +40,28 @@ export default class Certificates extends React.Component<{}, State> {
 		super(props, context);
 		this.state = {
 			certificates: CertificatesStore.certificates,
+			organizations: OrganizationsStore.organizations,
 			disabled: false,
 		};
 	}
 
 	componentDidMount(): void {
 		CertificatesStore.addChangeListener(this.onChange);
+		OrganizationsStore.addChangeListener(this.onChange);
 		CertificateActions.sync();
+		OrganizationActions.sync();
 	}
 
 	componentWillUnmount(): void {
 		CertificatesStore.removeChangeListener(this.onChange);
+		OrganizationsStore.removeChangeListener(this.onChange);
 	}
 
 	onChange = (): void => {
 		this.setState({
 			...this.state,
 			certificates: CertificatesStore.certificates,
+			organizations: OrganizationsStore.organizations,
 		});
 	}
 
@@ -64,6 +73,7 @@ export default class Certificates extends React.Component<{}, State> {
 			certsDom.push(<Certificate
 				key={cert.id}
 				certificate={cert}
+				organizations={this.state.organizations}
 			/>);
 		});
 
