0e556c0cc58c0156a800d4de6c4b49acd9796a45
Zachary Huff <zach.huff.386@gmail.com>
2020-02-18 05:15:32 -0500

    Add resp handler to proxy types

--- proxy/types.go
+++ proxy/types.go
@@ -4,5 +4,7 @@ import (
 	"net/http"
 )
 
+type RespHandler func(hand *Handler, resp *http.Response) (err error)
+
 type ErrorHandler func(hand *Handler, rw http.ResponseWriter,
 	r *http.Request, err error)
