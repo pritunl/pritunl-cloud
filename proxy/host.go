87611b78e4a4a4ffd0b6e63e0e21ca66512cef40
Zachary Huff <zach.huff.386@gmail.com>
2020-02-18 05:08:28 -0500

    Add proxy host

---
+++ proxy/host.go
@@ -0,0 +1,477 @@
+package proxy
+
+import (
+	"crypto/md5"
+	"crypto/tls"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/pritunl/pritunl-cloud/authority"
+	"github.com/pritunl/pritunl-cloud/balancer"
+)
+
+type Domain struct {
+	Hash              []byte
+	Requests          int
+	RequestsPrev      [5]int
+	Retries           int
+	RetriesPrev       [5]int
+	Lock              sync.Mutex
+	ProxyProto        string
+	ProxyPort         int
+	SkipVerify        bool
+	Balancer          *balancer.Balancer
+	Domain            *balancer.Domain
+	ClientAuthority   *authority.Authority
+	ClientCertificate *tls.Certificate
+
+	OnlineWebFirst      []*Handler
+	UnknownHighWebFirst []*Handler
+	UnknownMidWebFirst  []*Handler
+	UnknownLowWebFirst  []*Handler
+	OfflineWebFirst     []*Handler
+
+	OnlineWebSecond      []*Handler
+	UnknownHighWebSecond []*Handler
+	UnknownMidWebSecond  []*Handler
+	UnknownLowWebSecond  []*Handler
+	OfflineWebSecond     []*Handler
+
+	OnlineWebThird      []*Handler
+	UnknownHighWebThird []*Handler
+	UnknownMidWebThird  []*Handler
+	UnknownLowWebThird  []*Handler
+	OfflineWebThird     []*Handler
+
+	OnlineWebSocket  []*Handler
+	UnknownWebSocket []*Handler
+	OfflineWebSocket []*Handler
+}
+
+func (d *Domain) CalculateHash() {
+	h := md5.New()
+
+	h.Write([]byte(d.ProxyProto))
+	h.Write([]byte(strconv.Itoa(d.ProxyPort)))
+	h.Write([]byte(strconv.FormatBool(d.SkipVerify)))
+
+	h.Write([]byte(d.Balancer.Id.Hex()))
+	h.Write([]byte(d.Balancer.Name))
+	h.Write([]byte(d.Domain.Domain))
+	h.Write([]byte(d.Domain.Host))
+
+	if !d.Balancer.ClientAuthority.IsZero() {
+		h.Write([]byte(d.Balancer.ClientAuthority.Hex()))
+	}
+	for _, backend := range d.Balancer.Backends {
+		h.Write([]byte(backend.Protocol))
+		h.Write([]byte(backend.Hostname))
+		h.Write([]byte(strconv.Itoa(backend.Port)))
+	}
+
+	d.Hash = h.Sum(nil)
+}
+
+func (d *Domain) Init() {
+	d.Lock.Lock()
+	defer d.Lock.Unlock()
+
+	if !d.Balancer.ClientAuthority.IsZero() {
+		//clientAuthr, err := authority.Get(db, d.Balancer.ClientAuthority)
+		//if err != nil {
+		//	if _, ok := err.(*database.NotFoundError); ok {
+		//		err = nil
+		//
+		//		logrus.WithFields(logrus.Fields{
+		//			"balancer_id":         d.Balancer.Id.Hex(),
+		//			"client_authority_id": d.Balancer.ClientAuthority.Hex(),
+		//		}).Warn("proxy: Service client authority not found")
+		//	} else {
+		//		return
+		//	}
+		//}
+		//
+		// var cert *tls.Certificate
+		//if clientAuthr != nil {
+		//	cert, err = clientAuthr.CreateClientCertificate(db)
+		//	if err != nil {
+		//		return
+		//	}
+		//}
+	}
+
+	unknownHighWebFirst := []*Handler{}
+	unknownHighWebSecond := []*Handler{}
+	unknownHighWebThird := []*Handler{}
+
+	for i, backend := range d.Balancer.Backends {
+		hand := NewHandler(i, UnknownHigh, d.ProxyProto, d.ProxyPort, d,
+			backend, d.ErrorHandlerFirst)
+		unknownHighWebFirst = append(unknownHighWebFirst, hand)
+
+		hand = NewHandler(i, UnknownHigh, d.ProxyProto, d.ProxyPort, d,
+			backend, d.ErrorHandlerSecond)
+		unknownHighWebSecond = append(unknownHighWebSecond, hand)
+
+		hand = NewHandler(i, UnknownHigh, d.ProxyProto, d.ProxyPort, d,
+			backend, d.ErrorHandlerThird)
+		unknownHighWebThird = append(unknownHighWebThird, hand)
+	}
+
+	d.OnlineWebFirst = []*Handler{}
+	d.UnknownHighWebFirst = unknownHighWebFirst
+	d.UnknownMidWebFirst = []*Handler{}
+	d.UnknownLowWebFirst = []*Handler{}
+	d.OfflineWebFirst = []*Handler{}
+
+	d.OnlineWebSecond = []*Handler{}
+	d.UnknownHighWebSecond = unknownHighWebSecond
+	d.UnknownMidWebSecond = []*Handler{}
+	d.UnknownLowWebSecond = []*Handler{}
+	d.OfflineWebSecond = []*Handler{}
+
+	d.OnlineWebThird = []*Handler{}
+	d.UnknownHighWebThird = unknownHighWebThird
+	d.UnknownMidWebThird = []*Handler{}
+	d.UnknownLowWebThird = []*Handler{}
+	d.OfflineWebThird = []*Handler{}
+}
+
+func (d *Domain) ServeHTTPFirst(rw http.ResponseWriter, r *http.Request) {
+	d.Requests += 1
+
+	onlineWebFirst := d.OnlineWebFirst
+	l := len(onlineWebFirst)
+	if l != 0 {
+		onlineWebFirst[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	unknownHighWebFirst := d.UnknownHighWebFirst
+	l = len(unknownHighWebFirst)
+	if l != 0 {
+		unknownHighWebFirst[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	unknownMidWebFirst := d.UnknownMidWebFirst
+	l = len(unknownMidWebFirst)
+	if l != 0 {
+		unknownMidWebFirst[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	unknownLowWebFirst := d.UnknownLowWebFirst
+	l = len(unknownLowWebFirst)
+	if l != 0 {
+		unknownLowWebFirst[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	offlineWebFirst := d.OfflineWebFirst
+	l = len(offlineWebFirst)
+	if l != 0 {
+		offlineWebFirst[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusBadGateway)
+}
+
+func (d *Domain) ServeHTTPSecond(rw http.ResponseWriter, r *http.Request) {
+	d.Retries += 1
+
+	//println("second")
+
+	onlineWebSecond := d.OnlineWebSecond
+	l := len(onlineWebSecond)
+	if l != 0 {
+		onlineWebSecond[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	unknownHighWebSecond := d.UnknownHighWebSecond
+	l = len(unknownHighWebSecond)
+	if l != 0 {
+		unknownHighWebSecond[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	unknownMidWebSecond := d.UnknownMidWebSecond
+	l = len(unknownMidWebSecond)
+	if l != 0 {
+		unknownMidWebSecond[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	unknownLowWebSecond := d.UnknownLowWebSecond
+	l = len(unknownLowWebSecond)
+	if l != 0 {
+		unknownLowWebSecond[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	offlineWebSecond := d.OfflineWebSecond
+	l = len(offlineWebSecond)
+	if l != 0 {
+		offlineWebSecond[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusBadGateway)
+}
+
+func (d *Domain) ServeHTTPThird(rw http.ResponseWriter, r *http.Request) {
+	d.Retries += 1
+
+	//println("third")
+
+	onlineWebThird := d.OnlineWebThird
+	l := len(onlineWebThird)
+	if l != 0 {
+		onlineWebThird[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	unknownHighWebThird := d.UnknownHighWebThird
+	l = len(unknownHighWebThird)
+	if l != 0 {
+		unknownHighWebThird[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	unknownMidWebThird := d.UnknownMidWebThird
+	l = len(unknownMidWebThird)
+	if l != 0 {
+		unknownMidWebThird[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	unknownLowWebThird := d.UnknownLowWebThird
+	l = len(unknownLowWebThird)
+	if l != 0 {
+		unknownLowWebThird[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	offlineWebThird := d.OfflineWebThird
+	l = len(offlineWebThird)
+	if l != 0 {
+		offlineWebThird[rand.Intn(l)].ServeHTTP(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusBadGateway)
+}
+
+func (d *Domain) downgradeHandler(hand *Handler) {
+	d.Lock.Lock()
+
+	index := hand.Index
+	state := hand.State
+
+	switch state {
+	case Online:
+		hand = d.OnlineWebFirst[index]
+		d.OnlineWebFirst[index] = d.OnlineWebFirst[len(d.OnlineWebFirst)-1]
+		d.OnlineWebFirst[len(d.OnlineWebFirst)-1] = nil
+		d.OnlineWebFirst = d.OnlineWebFirst[:len(d.OnlineWebFirst)-1]
+		for i, h := range d.OnlineWebFirst {
+			h.Index = i
+		}
+		hand.Index = len(d.UnknownMidWebFirst)
+		hand.State = UnknownMid
+		hand.LastState = time.Now()
+		d.UnknownMidWebFirst = append(d.UnknownMidWebFirst, hand)
+
+		hand = d.OnlineWebSecond[index]
+		d.OnlineWebSecond[index] = d.OnlineWebSecond[len(d.OnlineWebSecond)-1]
+		d.OnlineWebSecond[len(d.OnlineWebSecond)-1] = nil
+		d.OnlineWebSecond = d.OnlineWebSecond[:len(d.OnlineWebSecond)-1]
+		for i, h := range d.OnlineWebSecond {
+			h.Index = i
+		}
+		hand.Index = len(d.UnknownMidWebSecond)
+		hand.State = UnknownMid
+		hand.LastState = time.Now()
+		d.UnknownMidWebSecond = append(d.UnknownMidWebSecond, hand)
+
+		hand = d.OnlineWebThird[index]
+		d.OnlineWebThird[index] = d.OnlineWebThird[len(d.OnlineWebThird)-1]
+		d.OnlineWebThird[len(d.OnlineWebThird)-1] = nil
+		d.OnlineWebThird = d.OnlineWebThird[:len(d.OnlineWebThird)-1]
+		for i, h := range d.OnlineWebThird {
+			h.Index = i
+		}
+		hand.Index = len(d.UnknownMidWebThird)
+		hand.State = UnknownMid
+		hand.LastState = time.Now()
+		d.UnknownMidWebThird = append(d.UnknownMidWebThird, hand)
+
+		break
+	case UnknownHigh:
+		hand = d.UnknownHighWebFirst[index]
+		d.UnknownHighWebFirst[index] =
+			d.UnknownHighWebFirst[len(d.UnknownHighWebFirst)-1]
+		d.UnknownHighWebFirst[len(d.UnknownHighWebFirst)-1] = nil
+		d.UnknownHighWebFirst =
+			d.UnknownHighWebFirst[:len(d.UnknownHighWebFirst)-1]
+		for i, h := range d.UnknownHighWebFirst {
+			h.Index = i
+		}
+		hand.Index = len(d.UnknownMidWebFirst)
+		hand.State = UnknownMid
+		hand.LastState = time.Now()
+		d.UnknownMidWebFirst = append(d.UnknownMidWebFirst, hand)
+
+		hand = d.UnknownHighWebSecond[index]
+		d.UnknownHighWebSecond[index] =
+			d.UnknownHighWebSecond[len(d.UnknownHighWebSecond)-1]
+		d.UnknownHighWebSecond[len(d.UnknownHighWebSecond)-1] = nil
+		d.UnknownHighWebSecond =
+			d.UnknownHighWebSecond[:len(d.UnknownHighWebSecond)-1]
+		for i, h := range d.UnknownHighWebSecond {
+			h.Index = i
+		}
+		hand.Index = len(d.UnknownMidWebSecond)
+		hand.State = UnknownMid
+		hand.LastState = time.Now()
+		d.UnknownMidWebSecond = append(d.UnknownMidWebSecond, hand)
+
+		hand = d.UnknownHighWebThird[index]
+		d.UnknownHighWebThird[index] =
+			d.UnknownHighWebThird[len(d.UnknownHighWebThird)-1]
+		d.UnknownHighWebThird[len(d.UnknownHighWebThird)-1] = nil
+		d.UnknownHighWebThird =
+			d.UnknownHighWebThird[:len(d.UnknownHighWebThird)-1]
+		for i, h := range d.UnknownHighWebThird {
+			h.Index = i
+		}
+		hand.Index = len(d.UnknownMidWebThird)
+		hand.State = UnknownMid
+		hand.LastState = time.Now()
+		d.UnknownMidWebThird = append(d.UnknownMidWebThird, hand)
+
+		break
+	case UnknownMid:
+		if time.Since(hand.LastState) > 1*time.Second {
+			hand = d.UnknownMidWebFirst[index]
+			d.UnknownMidWebFirst[index] =
+				d.UnknownMidWebFirst[len(d.UnknownMidWebFirst)-1]
+			d.UnknownMidWebFirst[len(d.UnknownMidWebFirst)-1] = nil
+			d.UnknownMidWebFirst =
+				d.UnknownMidWebFirst[:len(d.UnknownMidWebFirst)-1]
+			for i, h := range d.UnknownMidWebFirst {
+				h.Index = i
+			}
+			hand.Index = len(d.UnknownLowWebFirst)
+			hand.State = UnknownLow
+			hand.LastState = time.Now()
+			d.UnknownLowWebFirst = append(d.UnknownLowWebFirst, hand)
+
+			hand = d.UnknownMidWebSecond[index]
+			d.UnknownMidWebSecond[index] =
+				d.UnknownMidWebSecond[len(d.UnknownMidWebSecond)-1]
+			d.UnknownMidWebSecond[len(d.UnknownMidWebSecond)-1] = nil
+			d.UnknownMidWebSecond =
+				d.UnknownMidWebSecond[:len(d.UnknownMidWebSecond)-1]
+			for i, h := range d.UnknownMidWebSecond {
+				h.Index = i
+			}
+			hand.Index = len(d.UnknownLowWebSecond)
+			hand.State = UnknownLow
+			hand.LastState = time.Now()
+			d.UnknownLowWebSecond = append(d.UnknownLowWebSecond, hand)
+
+			hand = d.UnknownMidWebThird[index]
+			d.UnknownMidWebThird[index] =
+				d.UnknownMidWebThird[len(d.UnknownMidWebThird)-1]
+			d.UnknownMidWebThird[len(d.UnknownMidWebThird)-1] = nil
+			d.UnknownMidWebThird =
+				d.UnknownMidWebThird[:len(d.UnknownMidWebThird)-1]
+			for i, h := range d.UnknownMidWebThird {
+				h.Index = i
+			}
+			hand.Index = len(d.UnknownLowWebThird)
+			hand.State = UnknownLow
+			hand.LastState = time.Now()
+			d.UnknownLowWebThird = append(d.UnknownLowWebThird, hand)
+		}
+
+		break
+	case UnknownLow:
+		if time.Since(hand.LastState) > 2*time.Second {
+			hand = d.UnknownLowWebFirst[index]
+			d.UnknownLowWebFirst[index] =
+				d.UnknownLowWebFirst[len(d.UnknownLowWebFirst)-1]
+			d.UnknownLowWebFirst[len(d.UnknownLowWebFirst)-1] = nil
+			d.UnknownLowWebFirst =
+				d.UnknownLowWebFirst[:len(d.UnknownLowWebFirst)-1]
+			for i, h := range d.UnknownLowWebFirst {
+				h.Index = i
+			}
+			hand.Index = len(d.OfflineWebFirst)
+			hand.State = Offline
+			hand.LastState = time.Now()
+			d.OfflineWebFirst = append(d.OfflineWebFirst, hand)
+
+			hand = d.UnknownLowWebSecond[index]
+			d.UnknownLowWebSecond[index] =
+				d.UnknownLowWebSecond[len(d.UnknownLowWebSecond)-1]
+			d.UnknownLowWebSecond[len(d.UnknownLowWebSecond)-1] = nil
+			d.UnknownLowWebSecond =
+				d.UnknownLowWebSecond[:len(d.UnknownLowWebSecond)-1]
+			for i, h := range d.UnknownLowWebSecond {
+				h.Index = i
+			}
+			hand.Index = len(d.OfflineWebSecond)
+			hand.State = Offline
+			hand.LastState = time.Now()
+			d.OfflineWebSecond = append(d.OfflineWebSecond, hand)
+
+			hand = d.UnknownLowWebThird[index]
+			d.UnknownLowWebThird[index] =
+				d.UnknownLowWebThird[len(d.UnknownLowWebThird)-1]
+			d.UnknownLowWebThird[len(d.UnknownLowWebThird)-1] = nil
+			d.UnknownLowWebThird =
+				d.UnknownLowWebThird[:len(d.UnknownLowWebThird)-1]
+			for i, h := range d.UnknownLowWebThird {
+				h.Index = i
+			}
+			hand.Index = len(d.OfflineWebThird)
+			hand.State = Offline
+			hand.LastState = time.Now()
+			d.OfflineWebThird = append(d.OfflineWebThird, hand)
+		}
+
+		break
+	case Offline:
+		break
+	}
+
+	d.Lock.Unlock()
+}
+
+func (d *Domain) ErrorHandlerFirst(hand *Handler, rw http.ResponseWriter,
+	r *http.Request, err error) {
+
+	d.downgradeHandler(hand)
+	d.ServeHTTPSecond(rw, r)
+}
+
+func (d *Domain) ErrorHandlerSecond(hand *Handler, rw http.ResponseWriter,
+	r *http.Request, err error) {
+
+	d.downgradeHandler(hand)
+	d.ServeHTTPThird(rw, r)
+}
+
+func (d *Domain) ErrorHandlerThird(hand *Handler, rw http.ResponseWriter,
+	r *http.Request, err error) {
+
+	d.downgradeHandler(hand)
+	rw.WriteHeader(http.StatusBadGateway)
+}
