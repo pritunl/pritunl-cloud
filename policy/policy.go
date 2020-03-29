e0c58f0721699fdf20dddcf92e4ea6a7f357f772
Zachary Huff <zach.huff.386@gmail.com>
2020-03-29 10:01:40 -0400

    Add comment to policy

--- policy/policy.go
+++ policy/policy.go
@@ -27,6 +27,7 @@ type Rule struct {
 type Policy struct {
 	Id                   primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name                 string             `bson:"name" json:"name"`
+	Comment              string             `bson:"comment" json:"comment"`
 	Disabled             bool               `bson:"disabled" json:"disabled"`
 	Roles                []string           `bson:"roles" json:"roles"`
 	Rules                map[string]*Rule   `bson:"rules" json:"rules"`
