77ffc10396c6274ef94146a8da05b3bfb46d9d36
Zachary Huff <zach.huff.386@gmail.com>
2020-04-27 00:29:01 -0400

    Add websocket to proxy

---
+++ proxy/ws.go
@@ -0,0 +1,70 @@
+package proxy
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/gorilla/websocket"
+)
+
+type webSocketConn struct {
+	r     *http.Request
+	back  *websocket.Conn
+	front *websocket.Conn
+}
+
+func (w *webSocketConn) Run(domain *Domain) {
+	domain.WebSocketConnsLock.Lock()
+	domain.WebSocketConns.Add(w)
+	domain.WebSocketConnsLock.Unlock()
+
+	defer func() {
+		domain.WebSocketConnsLock.Lock()
+		domain.WebSocketConns.Remove(w)
+		domain.WebSocketConnsLock.Unlock()
+	}()
+
+	wait := make(chan bool, 4)
+	go func() {
+		defer func() {
+			rec := recover()
+			if rec != nil {
+				logrus.WithFields(logrus.Fields{
+					"panic": rec,
+				}).Error("proxy: WebSocket back panic")
+				wait <- true
+			}
+		}()
+		io.Copy(w.back.UnderlyingConn(), w.front.UnderlyingConn())
+		wait <- true
+	}()
+	go func() {
+		defer func() {
+			rec := recover()
+			if rec != nil {
+				logrus.WithFields(logrus.Fields{
+					"panic": rec,
+				}).Error("proxy: WebSocket front panic")
+				wait <- true
+			}
+		}()
+		io.Copy(w.front.UnderlyingConn(), w.back.UnderlyingConn())
+		wait <- true
+	}()
+	<-wait
+
+	w.Close()
+}
+
+func (w *webSocketConn) Close() {
+	defer func() {
+		recover()
+	}()
+	if w.back != nil {
+		w.back.Close()
+	}
+	if w.front != nil {
+		w.front.Close()
+	}
+}
