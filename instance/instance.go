1a2987388f656e8ff9088465462be313e99a95d0
Zachary Huff <zach.huff.386@gmail.com>
2020-10-04 17:45:04 -0400

    Add network namespace to instance

--- instance/instance.go
+++ instance/instance.go
@@ -43,6 +43,7 @@ type Instance struct {
 	PrivateIps          []string           `bson:"private_ips" json:"private_ips"`
 	PrivateIps6         []string           `bson:"private_ips6" json:"private_ips6"`
 	HostIps             []string           `bson:"host_ips" json:"host_ips"`
+	NetworkNamespace    string             `bson:"network_namespace" json:"network_namespace"`
 	NoPublicAddress     bool               `bson:"no_public_address" json:"no_public_address"`
 	NoHostAddress       bool               `bson:"no_host_address" json:"no_host_address"`
 	Node                primitive.ObjectID `bson:"node" json:"node"`
