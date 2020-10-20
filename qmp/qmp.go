885071bb199a49e0a7c183d67fa27881984acadb
Zachary Huff <zach.huff.386@gmail.com>
2020-10-20 09:51:22 -0400

    Query device name in qmp disk backup

--- qmp/qmp.go
+++ qmp/qmp.go
@@ -3,8 +3,8 @@ package qmp
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net"
+	"path"
 	"strings"
 	"time"
 
@@ -26,6 +26,24 @@ type driveBackupArgs struct {
 	Device string `json:"device"`
 	Sync   string `json:"sync"`
 	Target string `json:"target"`
+}
+
+type blockDeviceImage struct {
+	Filename string `json:"filename"`
+}
+
+type blockDeviceInserted struct {
+	Image blockDeviceImage `json:"image"`
+}
+
+type blockDevice struct {
+	Device   string              `json:"device"`
+	Inserted blockDeviceInserted `json:"inserted"`
+}
+
+type blockDeviceReturn struct {
+	Return []*blockDevice `json:"return"`
+	Error  *cmdError      `json:"error"`
 }
 
 type jobStatus struct {
@@ -174,13 +192,70 @@ func runCommand(vmId primitive.ObjectID, cmd *cmdBase,
 	return
 }
 
+func driveGetDevice(vmId primitive.ObjectID, dsk *disk.Disk) (
+	name string, err error) {
+
+	cmd := &cmdBase{
+		Execute: "query-block",
+	}
+
+	returnData := &blockDeviceReturn{}
+	err = runCommand(vmId, cmd, returnData)
+	if err != nil {
+		return
+	}
+
+	if returnData.Error != nil {
+		err = &errortypes.ApiError{
+			errors.Newf("qmp: Return error %s", returnData.Error.Desc),
+		}
+		return
+	}
+
+	if returnData.Return == nil {
+		err = &errortypes.ParseError{
+			errors.Newf("qmp: Return nil"),
+		}
+		return
+	}
+
+	for _, blockDev := range returnData.Return {
+		idStr := strings.Split(path.Base(
+			blockDev.Inserted.Image.Filename), ".")[0]
+
+		diskId, err := primitive.ObjectIDFromHex(idStr)
+		if err != nil {
+			continue
+		}
+
+		if diskId == dsk.Id {
+			name = blockDev.Device
+			break
+		}
+	}
+
+	return
+}
+
 func driveBackup(vmId primitive.ObjectID, dsk *disk.Disk,
-	virtIndex int, destPth string) (err error) {
+	destPth string) (deviceName string, err error) {
+
+	deviceName, err = driveGetDevice(vmId, dsk)
+	if err != nil {
+		return
+	}
+
+	if deviceName == "" {
+		err = &DiskNotFound{
+			errors.Newf("qmp: Disk not found %s", dsk.Id.Hex()),
+		}
+		return
+	}
 
 	cmd := &cmdBase{
 		Execute: "drive-backup",
 		Arguments: &driveBackupArgs{
-			Device: fmt.Sprintf("virtio%d", virtIndex),
+			Device: deviceName,
 			Sync:   "full",
 			Target: destPth,
 		},
@@ -204,8 +279,8 @@ func driveBackup(vmId primitive.ObjectID, dsk *disk.Disk,
 	return
 }
 
-func driveBackupCheck(vmId primitive.ObjectID, dsk *disk.Disk,
-	virtIndex int, destPth string) (complete bool, err error) {
+func driveBackupCheck(vmId primitive.ObjectID, deviceName string) (
+	complete bool, err error) {
 
 	cmd := &cmdBase{
 		Execute: "query-jobs",
@@ -233,7 +308,7 @@ func driveBackupCheck(vmId primitive.ObjectID, dsk *disk.Disk,
 
 	for _, status := range returnData.Return {
 		if status.Type == "backup" &&
-			status.Id == fmt.Sprintf("virtio%d", virtIndex) &&
+			status.Id == deviceName &&
 			status.Status != "concluded" {
 
 			return
@@ -246,20 +321,20 @@ func driveBackupCheck(vmId primitive.ObjectID, dsk *disk.Disk,
 }
 
 func BackupDisk(vmId primitive.ObjectID, dsk *disk.Disk,
-	virtIndex int, destPth string) (err error) {
+	destPth string) (err error) {
 
 	logrus.WithFields(logrus.Fields{
 		"instance_id": vmId.Hex(),
 		"disk_id":     dsk.Id.Hex(),
 	}).Info("qmp: Backing up disk")
 
-	err = driveBackup(vmId, dsk, virtIndex, destPth)
+	deviceName, err := driveBackup(vmId, dsk, destPth)
 	if err != nil {
 		return
 	}
 
 	for {
-		complete, e := driveBackupCheck(vmId, dsk, virtIndex, destPth)
+		complete, e := driveBackupCheck(vmId, deviceName)
 		if e != nil {
 			err = e
 			return
