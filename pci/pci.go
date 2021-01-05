c522e2d9ae1c2161f7b3d69c1aefee67b6d5effc
Zachary Huff <zach.huff.386@gmail.com>
2021-01-05 13:49:31 -0500

    Add get all vfio to pci

--- pci/pci.go
+++ pci/pci.go
@@ -1,11 +1,8 @@
 package pci
 
 import (
-	"strings"
 	"sync"
 	"time"
-
-	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 var (
@@ -19,65 +16,4 @@ type Device struct {
 	Class  string `bson:"class" json:"class"`
 	Name   string `bson:"name" json:"name"`
 	Driver string `bson:"driver" json:"driver"`
-}
-
-func GetVfio() (devices []*Device, err error) {
-	if time.Since(syncLast) < 30*time.Second {
-		devices = syncCache
-		return
-	}
-	syncLock.Lock()
-	defer syncLock.Unlock()
-
-	devices = []*Device{}
-
-	output, err := utils.ExecOutput("", "lspci", "-v")
-	if err != nil {
-		return
-	}
-
-	dev := &Device{}
-
-	outputLines := strings.Split(output, "\n")
-	for _, line := range outputLines {
-		if strings.TrimSpace(line) == "" {
-			if dev.Slot != "" && dev.Name != "" &&
-				dev.Driver == "vfio-pci" && CheckSlot(dev.Slot) {
-
-				devices = append(devices, dev)
-			}
-
-			dev = &Device{}
-
-			continue
-		}
-
-		if dev.Slot == "" {
-			lines := strings.SplitN(line, " ", 2)
-			if len(lines) != 2 {
-				continue
-			}
-
-			names := strings.SplitN(lines[1], ":", 2)
-			if len(names) != 2 {
-				continue
-			}
-
-			dev.Slot = strings.TrimSpace(lines[0])
-			dev.Class = strings.TrimSpace(names[0])
-			dev.Name = strings.TrimSpace(names[1])
-		} else if strings.Contains(line, "Kernel driver in use:") {
-			lines := strings.SplitN(line, ":", 2)
-			if len(lines) != 2 {
-				continue
-			}
-
-			dev.Driver = strings.TrimSpace(lines[1])
-		}
-	}
-
-	syncCache = devices
-	syncLast = time.Now()
-
-	return
 }
--- pci/utils.go
+++ pci/utils.go
@@ -2,6 +2,10 @@ package pci
 
 import (
 	"regexp"
+	"strings"
+	"time"
+
+	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 var (
@@ -11,4 +15,65 @@ var (
 
 func CheckSlot(slot string) bool {
 	return reg.MatchString(slot)
+}
+
+func GetAllVfio() (devices []*Device, err error) {
+	if time.Since(syncLast) < 30*time.Second {
+		devices = syncCache
+		return
+	}
+	syncLock.Lock()
+	defer syncLock.Unlock()
+
+	devices = []*Device{}
+
+	output, err := utils.ExecOutput("", "lspci", "-v")
+	if err != nil {
+		return
+	}
+
+	dev := &Device{}
+
+	outputLines := strings.Split(output, "\n")
+	for _, line := range outputLines {
+		if strings.TrimSpace(line) == "" {
+			if dev.Slot != "" && dev.Name != "" &&
+				dev.Driver == "vfio-pci" && CheckSlot(dev.Slot) {
+
+				devices = append(devices, dev)
+			}
+
+			dev = &Device{}
+
+			continue
+		}
+
+		if dev.Slot == "" {
+			lines := strings.SplitN(line, " ", 2)
+			if len(lines) != 2 {
+				continue
+			}
+
+			names := strings.SplitN(lines[1], ":", 2)
+			if len(names) != 2 {
+				continue
+			}
+
+			dev.Slot = strings.TrimSpace(lines[0])
+			dev.Class = strings.TrimSpace(names[0])
+			dev.Name = strings.TrimSpace(names[1])
+		} else if strings.Contains(line, "Kernel driver in use:") {
+			lines := strings.SplitN(line, ":", 2)
+			if len(lines) != 2 {
+				continue
+			}
+
+			dev.Driver = strings.TrimSpace(lines[1])
+		}
+	}
+
+	syncCache = devices
+	syncLast = time.Now()
+
+	return
 }
