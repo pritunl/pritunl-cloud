910bc712cafc7c98d17fbc0c05b739048f2d4f82
Zachary Huff <zach.huff.386@gmail.com>
2021-01-22 04:54:33 -0500

    Add update vm disk to qemu manage

--- qemu/manage.go
+++ qemu/manage.go
@@ -210,6 +210,31 @@ func GetVmInfo(vmId primitive.ObjectID, getDisks, force bool) (
 	return
 }
 
+func UpdateVmDisk(virt *vm.VirtualMachine) (err error) {
+	for i := 0; i < 20; i++ {
+		if virt.State == vm.Running {
+			disks, e := qms.GetDisks(virt.Id)
+			if e != nil {
+				if i < 19 {
+					time.Sleep(100 * time.Millisecond)
+					_ = UpdateVmState(virt)
+					continue
+				}
+				err = e
+
+				return
+			}
+			virt.Disks = disks
+
+			store.SetDisks(virt.Id, disks)
+		}
+
+		break
+	}
+
+	return
+}
+
 func UpdateVmState(virt *vm.VirtualMachine) (err error) {
 	unitName := paths.GetUnitName(virt.Id)
 	state, timestamp, err := systemd.GetState(unitName)
