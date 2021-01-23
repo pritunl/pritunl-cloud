239f74c46bd47c4b30da6bfd95460784abf54bd7
Zachary Huff <zach.huff.386@gmail.com>
2021-01-23 07:44:59 -0500

    Refactor qms

---
+++ qms/power.go
@@ -0,0 +1,53 @@
+package qms
+
+import (
+	"net"
+	"time"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+)
+
+func Shutdown(vmId primitive.ObjectID) (err error) {
+	sockPath, err := GetSockPath(vmId)
+	if err != nil {
+		return
+	}
+
+	lockId := socketsLock.Lock(vmId.Hex())
+	defer socketsLock.Unlock(vmId.Hex(), lockId)
+
+	conn, err := net.DialTimeout(
+		"unix",
+		sockPath,
+		3*time.Second,
+	)
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "qemu: Failed to open socket"),
+		}
+		return
+	}
+	defer conn.Close()
+
+	err = conn.SetDeadline(time.Now().Add(5 * time.Second))
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "qemu: Failed set deadline"),
+		}
+		return
+	}
+
+	_, err = conn.Write([]byte("system_powerdown\n"))
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "qemu: Failed to write socket"),
+		}
+		return
+	}
+
+	time.Sleep(500 * time.Millisecond)
+
+	return
+}
---
+++ qms/vnc.go
@@ -0,0 +1,55 @@
+package qms
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+)
+
+func VncPassword(vmId primitive.ObjectID, passwd string) (err error) {
+	sockPath, err := GetSockPath(vmId)
+	if err != nil {
+		return
+	}
+
+	lockId := socketsLock.Lock(vmId.Hex())
+	defer socketsLock.Unlock(vmId.Hex(), lockId)
+
+	conn, err := net.DialTimeout(
+		"unix",
+		sockPath,
+		3*time.Second,
+	)
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "qemu: Failed to open socket"),
+		}
+		return
+	}
+	defer conn.Close()
+
+	err = conn.SetDeadline(time.Now().Add(5 * time.Second))
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "qemu: Failed set deadline"),
+		}
+		return
+	}
+
+	_, err = conn.Write(
+		[]byte(fmt.Sprintf("change vnc password\n%s\n", passwd)))
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "qemu: Failed to write socket"),
+		}
+		return
+	}
+
+	time.Sleep(800 * time.Millisecond)
+
+	return
+}
