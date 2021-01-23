d88d2bde480fc0c0c6ef93c420e0f28173cbae85
Zachary Huff <zach.huff.386@gmail.com>
2021-01-23 07:25:54 -0500

    Add vnc to qmp

---
+++ qmp/vnc.go
@@ -0,0 +1,39 @@
+package qmp
+
+import (
+	"time"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+)
+
+type vncPasswordArgs struct {
+	Password string `json:"password"`
+}
+
+func VncPassword(vmId primitive.ObjectID, passwd string) (err error) {
+	cmd := &cmdBase{
+		Execute: "change-vnc-password",
+		Arguments: &vncPasswordArgs{
+			Password: passwd,
+		},
+	}
+
+	returnData := &cmdReturn{}
+	err = runCommand(vmId, cmd, returnData)
+	if err != nil {
+		return
+	}
+
+	if returnData.Error != nil {
+		err = &errortypes.ApiError{
+			errors.Newf("qmp: Return error %s", returnData.Error.Desc),
+		}
+		return
+	}
+
+	time.Sleep(1 * time.Second)
+
+	return
+}
