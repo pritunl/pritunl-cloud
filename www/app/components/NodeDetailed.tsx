c533375f89ed7d7675e273c122798594619578fe
Zachary Huff <zach.huff.386@gmail.com>
2021-01-26 08:40:18 -0500

    Update help in node detailed component

--- www/app/components/NodeDetailed.tsx
+++ www/app/components/NodeDetailed.tsx
@@ -1764,7 +1764,7 @@ export default class NodeDetailed extends React.Component<Props, State> {
 						hidden={types.indexOf('hypervisor') === -1}
 						disabled={this.state.disabled}
 						label="Hypervisor VGA Type"
-						help="Type of VGA card to emulate. Virtio provides the best performance but requires the Pritunl KVM repository. VMware provides better performance then standard."
+						help="Type of VGA card to emulate. Virtio provides the best performance. VMware provides better performance then standard. Virtio is required for UEFI guests."
 						value={node.vga}
 						onChange={(val): void => {
 							this.set('vga', val);
