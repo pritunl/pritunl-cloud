3bb253e737e139db8eb58c7df511ddc0d256fdbb
Zachary Huff <zach.huff.386@gmail.com>
2021-02-01 10:54:33 -0500

    Add parse to iscsi device

--- iscsi/iscsi.go
+++ iscsi/iscsi.go
@@ -3,6 +3,10 @@ package iscsi
 import (
 	"fmt"
 	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/pritunl/pritunl-cloud/errortypes"
 )
 
 type Device struct {
@@ -27,4 +31,116 @@ func (d *Device) Json() {
 	}
 
 	d.Uri = uri.String()
+}
+
+func (d *Device) Parse() (errData *errortypes.ErrorData, err error) {
+	if d.Uri == "" {
+		errData = &errortypes.ErrorData{
+			Error:   "invalid_iscsi_uri",
+			Message: "Invalid iSCSI URI",
+		}
+		return
+	}
+
+	uri, err := url.Parse(d.Uri)
+	if err != nil {
+		err = nil
+		errData = &errortypes.ErrorData{
+			Error:   "invalid_iscsi_uri",
+			Message: "Invalid iSCSI URI",
+		}
+		return
+	}
+
+	if uri.Scheme != "iscsi" {
+		errData = &errortypes.ErrorData{
+			Error:   "invalid_iscsi_uri",
+			Message: "Invalid iSCSI URI",
+		}
+		return
+	}
+
+	port := 0
+	portStr := uri.Port()
+	if portStr != "" {
+		port, err = strconv.Atoi(portStr)
+		if err != nil {
+			err = nil
+			errData = &errortypes.ErrorData{
+				Error:   "invalid_iscsi_port",
+				Message: "Invalid iSCSI port",
+			}
+			return
+		}
+	}
+
+	host := uri.Hostname()
+	if host == "" {
+		errData = &errortypes.ErrorData{
+			Error:   "invalid_iscsi_uri",
+			Message: "Invalid iSCSI URI",
+		}
+		return
+	}
+
+	username := ""
+	password := ""
+	if uri.User != nil {
+		username = uri.User.Username()
+		password, _ = uri.User.Password()
+
+		if username != "" || password != "" {
+			if username == "" {
+				errData = &errortypes.ErrorData{
+					Error:   "invalid_iscsi_username",
+					Message: "Missing iSCSI username",
+				}
+				return
+			}
+			if password == "" {
+				errData = &errortypes.ErrorData{
+					Error:   "invalid_iscsi_password",
+					Message: "Missing iSCSI password",
+				}
+				return
+			}
+		}
+	}
+
+	path := strings.Split(uri.Path, "/")
+	if len(path) != 3 {
+		errData = &errortypes.ErrorData{
+			Error:   "missing_iscsi_iqn_lun",
+			Message: "Missing iSCSI IQN and LUN",
+		}
+		return
+	}
+
+	iqn := path[1]
+	if iqn == "" {
+		errData = &errortypes.ErrorData{
+			Error:   "invalid_iscsi_iqn",
+			Message: "Invalid iSCSI IQN",
+		}
+		return
+	}
+
+	lun := path[2]
+	if lun == "" {
+		errData = &errortypes.ErrorData{
+			Error:   "invalid_iscsi_lun",
+			Message: "Invalid iSCSI LUN",
+		}
+		return
+	}
+
+	d.Host = host
+	d.Port = port
+	d.Iqn = iqn
+	d.Lun = lun
+	d.Username = username
+	d.Password = password
+	d.Uri = ""
+
+	return
 }
