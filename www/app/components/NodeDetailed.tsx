c8abd1c7b901b39ebab367294e9829815dcda9ef
Zachary Huff <zach.huff.386@gmail.com>
2021-02-01 17:56:47 -0500

    Fix label style in node detailed component

--- www/app/components/NodeDetailed.tsx
+++ www/app/components/NodeDetailed.tsx
@@ -70,7 +70,7 @@ const css = {
 	} as React.CSSProperties,
 	item: {
 		margin: '9px 5px 0 5px',
-		height: '20px',
+		wordBreak: 'break-all',
 	} as React.CSSProperties,
 	itemsLabel: {
 		display: 'block',
