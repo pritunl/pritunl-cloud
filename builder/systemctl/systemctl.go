65d0bc9bdc9b84b1bc23d2bf5b10d5615a13255d
Zachary Huff <zach.huff.386@gmail.com>
2021-02-20 16:44:35 -0500

    Add systemctl to builder

---
+++ builder/systemctl/systemctl.go
@@ -0,0 +1,48 @@
+package systemctl
+
+import (
+	"github.com/pritunl/pritunl-cloud/builder/prompt"
+	"github.com/pritunl/pritunl-cloud/utils"
+	"github.com/sirupsen/logrus"
+)
+
+func Firewall() (err error) {
+	resp, err := prompt.ConfirmDefault(
+		"Disable firewalld [Y/n]",
+		true,
+	)
+	if err != nil {
+		return
+	}
+
+	if !resp {
+		return
+	}
+
+	_, err = utils.ExecOutputLogged(nil,
+		"/usr/bin/systemctl", "disable", "firewalld")
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecOutputLogged(nil,
+		"/usr/bin/systemctl", "stop", "firewalld")
+	if err != nil {
+		return
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"service": "firewalld",
+	}).Info("systemctl: Firewalld disabled")
+
+	return
+}
+
+func Systemctl() (err error) {
+	err = Firewall()
+	if err != nil {
+		return
+	}
+
+	return
+}
