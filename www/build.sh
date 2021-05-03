7ee1449b9106f63e8dbdddc74b5cbf384c79a9a8
Zachary Huff <zach.huff.386@gmail.com>
2021-05-03 14:47:41 -0400

    Update www build

--- www/build.sh
+++ www/build.sh
@@ -1,8 +1,28 @@
-cd packages/novnc-core
-jspm link github:novnc-core@dev
-cd ../../
-
 tsc
+
+# development
+rm -rf dist-dev/static
+mkdir -p dist-dev/static
+cp styles/global.css dist-dev/static/
+cp node_modules/normalize.css/normalize.css dist-dev/static/
+cp node_modules/@blueprintjs/core/lib/css/blueprint.css dist-dev/static/
+cp node_modules/@blueprintjs/datetime/lib/css/blueprint-datetime.css dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.eot dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.ttf dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.woff dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.eot dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.ttf dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.woff dist-dev/static/
+sed -i 's|../../resources/icons/||g' dist-dev/static/blueprint-icons.css
+
+webpack --config webpack.dev.config
+
+cp index.html dist-dev/index.html
+cp uindex.html dist-dev/uindex.html
+cp login.html dist-dev/login.html
+
+# production
 rm -rf dist/static
 mkdir -p dist/static
 cp styles/global.css dist/static/
@@ -16,11 +36,10 @@ cp node_modules/@blueprintjs/icons/resources/icons/icons-16.woff dist/static/
 cp node_modules/@blueprintjs/icons/resources/icons/icons-20.eot dist/static/
 cp node_modules/@blueprintjs/icons/resources/icons/icons-20.ttf dist/static/
 cp node_modules/@blueprintjs/icons/resources/icons/icons-20.woff dist/static/
-cp jspm_packages/system.js dist/static/
 sed -i 's|../../resources/icons/||g' dist/static/blueprint-icons.css
-jspm bundle app/App.js
-mv build.js dist/static/app.js
-mv build.js.map dist/static/app.js.map
+
+webpack
+
 cp index_dist.html dist/index.html
 cp uindex_dist.html dist/uindex.html
 cp login.html dist/login.html
@@ -28,7 +47,7 @@ cp login.html dist/login.html
 APP_HASH=`md5sum dist/static/app.js | cut -c1-6`
 
 mv dist/static/app.js dist/static/app.${APP_HASH}.js
-mv dist/static/app.js.map dist/static/app.${APP_HASH}.js.map
+#mv dist/static/app.js.map dist/static/app.${APP_HASH}.js.map
 
 sed -i -e "s|static/app.js|static/app.${APP_HASH}.js|g" dist/index.html
 sed -i -e "s|static/app.js|static/app.${APP_HASH}.js|g" dist/uindex.html
