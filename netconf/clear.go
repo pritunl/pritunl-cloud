579a52a40a57eeadcf68aa0a5e188c5af78bd179
Zachary Huff <zach.huff.386@gmail.com>
2021-10-01 03:57:49 -0400

    Add clear all to netconf clear

--- netconf/clear.go
+++ netconf/clear.go
@@ -1,8 +1,17 @@
 package netconf
 
 import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/interfaces"
+	"github.com/pritunl/pritunl-cloud/store"
 	"github.com/pritunl/pritunl-cloud/utils"
+	"github.com/pritunl/pritunl-cloud/vm"
 )
 
 func (n *NetConf) Clear(db *database.Database) (err error) {
@@ -15,7 +24,6 @@ func (n *NetConf) Clear(db *database.Database) (err error) {
 		return
 	}
 
-	// TODO Clear br0 namespace interface
 	clearIface(n.SystemExternalIface)
 	clearIface(n.SystemExternalIface6)
 	clearIface(n.SystemInternalIface)
@@ -24,6 +32,44 @@ func (n *NetConf) Clear(db *database.Database) (err error) {
 	clearIface(n.SpaceExternalIface6)
 	clearIface(n.SpaceInternalIface)
 	clearIface(n.SpaceHostIface)
+
+	interfaces.RemoveVirtIface(n.SystemExternalIface)
+	interfaces.RemoveVirtIface(n.SystemExternalIface6)
+	interfaces.RemoveVirtIface(n.SystemInternalIface)
+
+	return
+}
+
+func (n *NetConf) ClearAll(db *database.Database) (err error) {
+	if len(n.Virt.NetworkAdapters) == 0 {
+		err = &errortypes.NotFoundError{
+			errors.New("qemu: Missing network interfaces"),
+		}
+		return
+	}
+
+	ifaceExternal := vm.GetIfaceExternal(n.Virt.Id, 0)
+	pidPath := fmt.Sprintf("/var/run/dhclient-%s.pid", ifaceExternal)
+
+	pid := ""
+	pidData, _ := ioutil.ReadFile(pidPath)
+	if pidData != nil {
+		pid = strings.TrimSpace(string(pidData))
+	}
+
+	if pid != "" {
+		_, _ = utils.ExecCombinedOutput("", "kill", pid)
+	}
+
+	_ = utils.RemoveAll(pidPath)
+
+	err = n.Clear(db)
+	if err != nil {
+		return
+	}
+
+	store.RemAddress(n.Virt.Id)
+	store.RemRoutes(n.Virt.Id)
 
 	return
 }
