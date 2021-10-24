fd5cd4cbcdddf14c2159ab2a601086c3d395cca6
Zachary Huff <zach.huff.386@gmail.com>
2021-10-24 16:29:29 -0400

    Add orace subnet to instance

--- instance/instance.go
+++ instance/instance.go
@@ -38,6 +38,8 @@ type Instance struct {
 	Zone                primitive.ObjectID `bson:"zone" json:"zone"`
 	Vpc                 primitive.ObjectID `bson:"vpc" json:"vpc"`
 	Subnet              primitive.ObjectID `bson:"subnet" json:"subnet"`
+	OracleSubnet        string             `bson:"oracle_subnet" json:"oracle_subnet"`
+	OracleVnic          string             `bson:"oracle_vnic" json:"oracle_vnic"`
 	Image               primitive.ObjectID `bson:"image" json:"image"`
 	ImageBacking        bool               `bson:"image_backing" json:"image_backing"`
 	Status              string             `bson:"-" json:"status"`
