a8b90f33ce13f1c0fe6930a8c338c2eee339646a
Zachary Huff <zach.huff.386@gmail.com>
2021-10-29 21:35:27 -0400

    Add copy iptables to namespace

--- namespace/namespace.go
+++ namespace/namespace.go
@@ -11,9 +11,138 @@ var (
 	lock = sync.Mutex{}
 )
 
+func CopyIptables(cur, new string) (err error) {
+	_, err = utils.ExecCombinedOutputLogged(
+		[]string{},
+		"ip", "netns", "exec", new,
+		"iptables", "-t", "nat", "-F",
+	)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecCombinedOutputLogged(
+		[]string{},
+		"ip", "netns", "exec", new,
+		"iptables", "-t", "mangle", "-F",
+	)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecCombinedOutputLogged(
+		[]string{},
+		"ip", "netns", "exec", new,
+		"iptables", "-F",
+	)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecCombinedOutputLogged(
+		[]string{},
+		"ip", "netns", "exec", new,
+		"ip6tables", "-t", "nat", "-F",
+	)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecCombinedOutputLogged(
+		[]string{},
+		"ip", "netns", "exec", new,
+		"ip6tables", "-t", "mangle", "-F",
+	)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecCombinedOutputLogged(
+		[]string{},
+		"ip", "netns", "exec", new,
+		"ip6tables", "-F",
+	)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecCombinedOutputLogged(
+		[]string{},
+		"ip", "netns", "exec", new,
+		"ipset", "destroy",
+	)
+	if err != nil {
+		return
+	}
+
+	ipsetData, err := utils.ExecOutputLogged(
+		[]string{},
+		"ip", "netns", "exec", cur,
+		"ipset", "save",
+	)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecInputOutputLogged(
+		[]string{},
+		ipsetData,
+		"ip", "netns", "exec", new,
+		"ipset", "restore",
+	)
+	if err != nil {
+		return
+	}
+
+	iptablesData, err := utils.ExecOutputLogged(
+		[]string{},
+		"ip", "netns", "exec", cur,
+		"iptables-save",
+	)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecInputOutputLogged(
+		[]string{},
+		iptablesData,
+		"ip", "netns", "exec", new,
+		"iptables-restore",
+	)
+	if err != nil {
+		return
+	}
+
+	ip6tablesData, err := utils.ExecOutputLogged(
+		[]string{},
+		"ip", "netns", "exec", cur,
+		"ip6tables-save",
+	)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecInputOutputLogged(
+		[]string{},
+		ip6tablesData,
+		"ip", "netns", "exec", new,
+		"ip6tables-restore",
+	)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func Rename(cur, new string) (err error) {
 	lock.Lock()
 	defer lock.Unlock()
+
+	err = CopyIptables(new, cur)
+	if err != nil {
+		return
+	}
 
 	_, err = utils.ExecCombinedOutputLogged(
 		[]string{
