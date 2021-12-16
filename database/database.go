90c62edca868f56adfbaf13cbd0807dd7b5fea0c
Zachary Huff <zach.huff.386@gmail.com>
2021-12-16 10:09:59 -0500

    Change read concern in database

--- database/database.go
+++ database/database.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/mongo"
 	"github.com/pritunl/mongo-go-driver/mongo/options"
+	"github.com/pritunl/mongo-go-driver/mongo/readconcern"
 	"github.com/pritunl/mongo-go-driver/mongo/writeconcern"
 	"github.com/pritunl/pritunl-cloud/config"
 	"github.com/pritunl/pritunl-cloud/constants"
@@ -262,9 +263,9 @@ func Connect() (err error) {
 	opts := options.Client().ApplyURI(config.Config.MongoUri)
 	opts.WriteConcern = writeconcern.New(
 		writeconcern.WMajority(),
-		writeconcern.J(true),
 		writeconcern.WTimeout(15*time.Second),
 	)
+	opts.ReadConcern = readconcern.Majority()
 
 	client, err := mongo.NewClient(opts)
 	if err != nil {
