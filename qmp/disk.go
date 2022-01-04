6a50c00a1fcfbe351dc3ab898559eadc9173f16d
Zachary Huff <zach.huff.386@gmail.com>
2022-01-04 12:13:41 -0500

    Add remove disk to qmp

--- qmp/disk.go
+++ qmp/disk.go
@@ -40,6 +40,16 @@ type deviceAddArgs struct {
 	Bus    string `json:"bus"`
 }
 
+type blockDevEventData struct {
+	Device string `json:"device"`
+	Path   string `json:"path"`
+}
+
+type blockDevEvent struct {
+	Event string            `json:"event"`
+	Data  blockDevEventData `json:"data"`
+}
+
 func AddDisk(vmId primitive.ObjectID, dsk *vm.Disk, virt *vm.VirtualMachine) (
 	err error) {
 
@@ -154,6 +164,142 @@ func AddDisk(vmId primitive.ObjectID, dsk *vm.Disk, virt *vm.VirtualMachine) (
 	}
 
 	time.Sleep(1 * time.Second)
+
+	return
+}
+
+func RemoveDisk(vmId primitive.ObjectID, dsk *vm.Disk) (err error) {
+	dskId := fmt.Sprintf("fd_%s", dsk.Id.Hex())
+	dskFileId := fmt.Sprintf("fdf_%s", dsk.Id.Hex())
+	dskDevId := fmt.Sprintf("fdd_%s", dsk.Id.Hex())
+
+	diskAio := settings.Hypervisor.DiskAio
+	if diskAio == "" {
+		supported, e := features.GetUringSupport()
+		if e != nil {
+			err = e
+			return
+		}
+
+		if supported {
+			diskAio = "io_uring"
+		} else {
+			diskAio = "threads"
+		}
+	}
+
+	conn := NewConnection(vmId)
+	defer conn.Close()
+
+	conn.SetDeadline(30 * time.Second)
+
+	err = conn.Connect()
+	if err != nil {
+		return
+	}
+
+	cmd := &Command{
+		Execute: "device_del",
+		Arguments: &CommandId{
+			Id: dskDevId,
+		},
+	}
+
+	returnData := &CommandReturn{}
+	err = conn.Send(cmd, returnData)
+	if err != nil {
+		return
+	}
+
+	skipEvent := false
+	if returnData.Error != nil && (strings.Contains(
+		strings.ToLower(returnData.Error.Desc),
+		"process of unplug") || strings.Contains(
+		strings.ToLower(returnData.Error.Desc),
+		"not found") || strings.Contains(
+		strings.ToLower(returnData.Error.Desc),
+		"failed to find")) {
+
+		skipEvent = true
+	} else if returnData.Error != nil {
+		err = &errortypes.ApiError{
+			errors.Newf("qmp: Return error %s", returnData.Error.Desc),
+		}
+		return
+	}
+
+	if !skipEvent {
+		event := &blockDevEvent{}
+		err = conn.Event(event, func() (resp interface{}, err error) {
+			if event.Event == "DEVICE_DELETED" &&
+				event.Data.Device == dskDevId {
+
+				return
+			}
+
+			event = &blockDevEvent{}
+			resp = event
+
+			return
+		})
+		if err != nil {
+			return
+		}
+	}
+
+	cmd = &Command{
+		Execute: "blockdev-del",
+		Arguments: &CommandNode{
+			NodeName: dskId,
+		},
+	}
+
+	returnData = &CommandReturn{}
+	err = conn.Send(cmd, returnData)
+	if err != nil {
+		return
+	}
+
+	if returnData.Error != nil && !strings.Contains(
+		strings.ToLower(returnData.Error.Desc),
+		"process of unplug") && !strings.Contains(
+		strings.ToLower(returnData.Error.Desc),
+		"not found") && !strings.Contains(
+		strings.ToLower(returnData.Error.Desc),
+		"failed to find") {
+
+		err = &errortypes.ApiError{
+			errors.Newf("qmp: Return error %s", returnData.Error.Desc),
+		}
+		return
+	}
+
+	cmd = &Command{
+		Execute: "blockdev-del",
+		Arguments: &CommandNode{
+			NodeName: dskFileId,
+		},
+	}
+
+	returnData = &CommandReturn{}
+	err = conn.Send(cmd, returnData)
+	if err != nil {
+		return
+	}
+
+	if returnData.Error != nil && !strings.Contains(
+		strings.ToLower(returnData.Error.Desc),
+		"process of unplug") && !strings.Contains(
+		strings.ToLower(returnData.Error.Desc),
+		"not found") && !strings.Contains(
+		strings.ToLower(returnData.Error.Desc),
+		"failed to find") {
+
+		err = &errortypes.ApiError{
+			errors.Newf("qmp: Return error %s", returnData.Error.Desc),
+		}
+		return
+	}
 
 	return
 }
