28b367ab93778a6adc937016e49b9b725edc9a2f
Zachary Huff <zach.huff.386@gmail.com>
2022-01-07 10:03:39 -0500

    Add permission

---
+++ permission/permission.go
@@ -0,0 +1,77 @@
+package permission
+
+import (
+	"os"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/paths"
+	"github.com/pritunl/pritunl-cloud/utils"
+	"github.com/pritunl/pritunl-cloud/vm"
+)
+
+func chown(virt *vm.VirtualMachine, path string) (err error) {
+	err = os.Chown(path, virt.UnixId, virt.UnixId)
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Newf(
+				"permission: Failed to set owner of '%s' to '%d'",
+				path, virt.UnixId,
+			),
+		}
+		return
+	}
+
+	return
+}
+
+func touchChown(virt *vm.VirtualMachine, path string) (err error) {
+	_, err = utils.ExecCombinedOutputLogged(nil,
+		"touch", path,
+	)
+	if err != nil {
+		return
+	}
+
+	err = chown(virt, path)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func InitVirt(virt *vm.VirtualMachine) (err error) {
+	err = UserAdd(virt)
+	if err != nil {
+		return
+	}
+
+	if virt.Uefi {
+		err = chown(virt, paths.GetOvmfVarsPath(virt.Id))
+		if err != nil {
+			return
+		}
+	}
+
+	err = chown(virt, paths.GetInitPath(virt.Id))
+	if err != nil {
+		return
+	}
+
+	for _, disk := range virt.Disks {
+		err = chown(virt, disk.Path)
+		if err != nil {
+			return
+		}
+	}
+
+	for _, device := range virt.DriveDevices {
+		err = chown(virt, paths.GetDrivePath(device.Id))
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
