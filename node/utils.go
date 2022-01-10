f996adafea014810b1107e8e5c38a3d2ab92fe05
Zachary Huff <zach.huff.386@gmail.com>
2022-01-10 15:46:11 -0500

    Add gui to node hypervisor query

--- node/utils.go
+++ node/utils.go
@@ -70,6 +70,7 @@ func GetAllHypervisors(db *database.Database, query *bson.M) (
 			Projection: &bson.D{
 				{"name", 1},
 				{"types", 1},
+				{"gui", 1},
 				{"oracle_subnets", 1},
 			},
 		},
