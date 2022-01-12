986885e55bc71102aa57a7643d5f3943b8257268
Zachary Huff <zach.huff.386@gmail.com>
2022-01-12 06:08:56 -0500

    Update qemu usb settings

--- qemu/qemu.go
+++ qemu/qemu.go
@@ -518,10 +518,8 @@ func (q *Qemu) Marshal() (output string, err error) {
 	}
 
 	if node.Self.UsbPassthrough {
-		if len(q.UsbDevices) > 0 {
-			cmd = append(cmd, "-device")
-			cmd = append(cmd, "qemu-xhci")
-		}
+		cmd = append(cmd, "-device")
+		cmd = append(cmd, "qemu-xhci")
 
 		for _, device := range q.UsbDevices {
 			vendor := usb.FilterId(device.Vendor)
