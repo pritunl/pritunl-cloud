ad6026641d7d826c3393709a415379acd9093bb8
Zachary Huff <zach.huff.386@gmail.com>
2022-01-21 06:55:57 -0500

    Update iptables in deploy

--- deploy/iptables.go
+++ deploy/iptables.go
@@ -1,7 +1,6 @@
 package deploy
 
 import (
-	"github.com/sirupsen/logrus"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/iptables"
 	"github.com/pritunl/pritunl-cloud/state"
@@ -21,25 +20,8 @@ func (t *Iptables) Deploy() (err error) {
 	nodeFirewall := t.stat.NodeFirewall()
 	firewalls := t.stat.Firewalls()
 
-	err = iptables.UpdateState(nodeSelf, instaces, namespaces,
+	iptables.UpdateStateRecover(nodeSelf, instaces, namespaces,
 		nodeFirewall, firewalls)
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"error": err,
-		}).Error("deploy: Failed to update iptables, resetting state")
-		for {
-			err = iptables.Recover()
-			if err != nil {
-				logrus.WithFields(logrus.Fields{
-					"error": err,
-				}).Error("deploy: Failed to recover iptables, retrying")
-				continue
-			}
-			break
-		}
-		err = nil
-		return
-	}
 
 	return
 }
