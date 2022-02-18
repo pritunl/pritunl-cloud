b320859077db3e5592c159e989ec9c7b6439f1ff
Zachary Huff <zach.huff.386@gmail.com>
2022-02-18 17:17:03 -0500

    Update source maps in webpack config

--- www/webpack.dev.config.js
+++ www/webpack.dev.config.js
@@ -4,7 +4,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'eval-source-map',
   entry: {
     app: {
       import: './app/App.js',
@@ -19,6 +19,15 @@ module.exports = {
     aggregateTimeout: 100,
     ignored: [
       path.resolve(__dirname, 'node_modules'),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
     ],
   },
   plugins: [
