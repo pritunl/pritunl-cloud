d3dc3f46655b922a1813d30169eda518f922605e
Zachary Huff <zach.huff.386@gmail.com>
2022-04-10 20:36:04 -0400

    Add external net systemd template to qemu

--- qemu/constants.go
+++ qemu/constants.go
@@ -10,4 +10,29 @@ After=network.target
 Type=simple
 User=root
 ExecStart=%s
+PrivateTmp=%s
+ProtectHome=%s
+ProtectSystem=full
+ProtectHostname=true
+ProtectKernelTunables=true
+PrivateIPC=true
+NetworkNamespacePath=/var/run/netns/%s
+`
+
+const systemdTemplateExternalNet = `# PritunlData=%s
+
+[Unit]
+Description=Pritunl Cloud Virtual Machine
+After=network.target
+
+[Service]%s
+Type=simple
+User=root
+ExecStart=%s
+PrivateTmp=%s
+ProtectHome=%s
+ProtectSystem=full
+ProtectHostname=true
+ProtectKernelTunables=true
+PrivateIPC=true
 `
