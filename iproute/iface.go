ce73a144b2a4d16fc13cfbc65aaaedb1cd5aa478
Zachary Huff <zach.huff.386@gmail.com>
2022-04-10 20:33:44 -0400

    Add iface get all to iproute

--- iproute/iface.go
+++ iproute/iface.go
@@ -2,6 +2,7 @@ package iproute
 
 import (
 	"encoding/json"
+
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-cloud/errortypes"
 	"github.com/pritunl/pritunl-cloud/utils"
@@ -10,6 +11,39 @@ import (
 type Iface struct {
 	Name  string `json:"ifname"`
 	State string `json:"operstate"`
+}
+
+func IfaceGetAll(namespace string) (ifaces []*Iface, err error) {
+	ifaces = []*Iface{}
+
+	var output string
+	if namespace != "" {
+		output, err = utils.ExecOutputLogged(
+			nil,
+			"ip", "netns", "exec", namespace,
+			"ip", "--json", "--brief",
+			"link", "show",
+		)
+	} else {
+		output, err = utils.ExecOutputLogged(
+			nil,
+			"ip", "--json", "--brief",
+			"link", "show",
+		)
+	}
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal([]byte(output), &ifaces)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "iproute: Failed to prase ifaces"),
+		}
+		return
+	}
+
+	return
 }
 
 func IfaceGetBridges(namespace string) (ifaces []*Iface, err error) {
