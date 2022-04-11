ab30ec2efbff56a167b758464cc0fbfa0ee172c1
Zachary Huff <zach.huff.386@gmail.com>
2022-04-10 20:33:04 -0400

    Add create and clear add to hnetwork utils

--- hnetwork/utils.go
+++ hnetwork/utils.go
@@ -8,6 +8,24 @@ import (
 	"github.com/pritunl/pritunl-cloud/utils"
 )
 
+func create() (err error) {
+	err = iproute.BridgeAdd("", settings.Hypervisor.HostNetworkName)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecCombinedOutputLogged(
+		nil,
+		"ip", "link", "set",
+		"dev", settings.Hypervisor.HostNetworkName, "up",
+	)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func getAddr() (addr string, err error) {
 	address, _, err := iproute.AddressGetIface(
 		"", settings.Hypervisor.HostNetworkName)
@@ -44,6 +62,28 @@ func setAddr(addr string) (err error) {
 	_, err = utils.ExecCombinedOutputLogged(
 		nil,
 		"ip", "addr", "add", addr,
+		"dev", settings.Hypervisor.HostNetworkName,
+	)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func clearAddr() (err error) {
+	_, err = utils.ExecCombinedOutputLogged(
+		nil,
+		"ip", "link", "set",
+		"dev", settings.Hypervisor.HostNetworkName, "up",
+	)
+	if err != nil {
+		return
+	}
+
+	_, err = utils.ExecCombinedOutputLogged(
+		nil,
+		"ip", "addr", "flush",
 		"dev", settings.Hypervisor.HostNetworkName,
 	)
 	if err != nil {
