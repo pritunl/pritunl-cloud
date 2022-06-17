0731ad3b42c3540eb56bd37ab2277d987415741d
Zachary Huff <zach.huff.386@gmail.com>
2022-06-17 16:02:01 -0400

    Update get state in systemd

--- systemd/systemd.go
+++ systemd/systemd.go
@@ -62,16 +62,28 @@ func GetState(unit string) (state string, timestamp time.Time, err error) {
 		"--no-page", unit)
 
 	timestampStr := ""
+	exitCode := ""
+	exitStatus := ""
 
 	for _, line := range strings.Split(output, "\n") {
 		n := len(line)
 		if state == "" && n > 13 && line[:12] == "ActiveState=" {
 			state = line[12:]
+		} else if exitCode == "" && n > 13 && line[:13] == "ExecMainCode=" {
+			exitCode = line[13:]
+		} else if exitStatus == "" && n > 15 &&
+			line[:15] == "ExecMainStatus=" {
+
+			exitStatus = line[15:]
 		} else if timestampStr == "" && n > 24 &&
 			line[:23] == "ExecMainStartTimestamp=" {
 
 			timestampStr = line[23:]
 		}
+	}
+
+	if state == "failed" && exitCode == "2" && exitStatus == "31" {
+		state = "inactive"
 	}
 
 	if timestampStr != "" && timestampStr != "0" && timestampStr != "n/a" {
