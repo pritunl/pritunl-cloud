5010d7e7fdd02f006b1bc0c2b8f9212cac095f73
Zachary Huff <zach.huff.386@gmail.com>
2022-06-27 19:49:57 -0400

    Add dhcps to cmd

---
+++ cmd/dhcps.go
@@ -0,0 +1,84 @@
+package cmd
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/dhcps"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/sirupsen/logrus"
+)
+
+func Dhcp4Server() (err error) {
+	config := os.Getenv("CONFIG")
+	server4 := &dhcps.Server4{}
+
+	err = json.Unmarshal([]byte(config), server4)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "cmd: Failed to parse DHCP4 configuration"),
+		}
+		return
+	}
+
+	for {
+		err = server4.Start()
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Error("cmd: DHCP4 server error")
+		}
+
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func Dhcp6Server() (err error) {
+	config := os.Getenv("CONFIG")
+	server6 := &dhcps.Server6{}
+
+	err = json.Unmarshal([]byte(config), server6)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "cmd: Failed to parse DHCP6 configuration"),
+		}
+		return
+	}
+
+	for {
+		err = server6.Start()
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Error("cmd: DHCP6 server error")
+		}
+
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func NdpServer() (err error) {
+	config := os.Getenv("CONFIG")
+	serverNdp := &dhcps.ServerNdp{}
+
+	err = json.Unmarshal([]byte(config), serverNdp)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "cmd: Failed to parse NDP configuration"),
+		}
+		return
+	}
+
+	for {
+		err = serverNdp.Start()
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Error("cmd: NDP server error")
+		}
+
+		time.Sleep(3 * time.Second)
+	}
+}
