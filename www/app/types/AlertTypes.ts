7c733eb1401920f237059e0a605a5b03a9eded81
Zachary Huff <zach.huff.386@gmail.com>
2022-12-10 17:19:08 -0500

    Add alert types

---
+++ www/app/types/AlertTypes.ts
@@ -0,0 +1,44 @@
+/// <reference path="../References.d.ts"/>
+export const SYNC = 'alert.sync';
+export const SYNC_NAMES = 'alert.sync_names';
+export const TRAVERSE = 'alert.traverse';
+export const FILTER = 'alert.filter';
+export const CHANGE = 'alert.change';
+
+export interface Alert {
+	id: string;
+	name?: string;
+	organization?: string;
+	roles?: string[];
+	resource?: string;
+	level?: number;
+	frequency?: number;
+	ignores?: string[];
+	value_int?: number;
+	value_str?: string;
+}
+
+export interface Filter {
+	id?: string;
+	name?: string;
+	role?: string;
+	organization?: string;
+}
+
+export type Alerts = Alert[];
+
+export type AlertRo = Readonly<Alert>;
+export type AlertsRo = ReadonlyArray<AlertRo>;
+
+export interface AlertDispatch {
+	type: string;
+	data?: {
+		id?: string;
+		alert?: Alert;
+		alerts?: Alerts;
+		page?: number;
+		pageCount?: number;
+		filter?: Filter;
+		count?: number;
+	};
+}
