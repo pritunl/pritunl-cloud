3ec6021f64e26e44ee0670ab79fd452b00505f24
Zachary Huff <zach.huff.386@gmail.com>
2022-12-11 18:57:58 -0500

    Fix count in user utils

--- user/utils.go
+++ user/utils.go
@@ -113,10 +113,18 @@ func GetAll(db *database.Database, query *bson.M, page, pageCount int64) (
 	coll := db.Users()
 	users = []*User{}
 
-	count, err = coll.CountDocuments(db, query)
-	if err != nil {
-		err = database.ParseError(err)
-		return
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
 	}
 
 	opts := options.FindOptions{
