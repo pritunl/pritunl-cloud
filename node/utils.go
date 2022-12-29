ea0f86f4a7d1af60970a7cb924220b2d6b1253e0
Zachary Huff <zach.huff.386@gmail.com>
2022-12-29 15:46:59 -0500

    Fix document count in node utils

--- node/utils.go
+++ node/utils.go
@@ -115,10 +115,18 @@ func GetAllPaged(db *database.Database, query *bson.M,
 	coll := db.Nodes()
 	nodes = []*Node{}
 
-	count, err = coll.CountDocuments(db, query)
-	if err != nil {
-		err = database.ParseError(err)
-		return
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
 	}
 
 	maxPage := count / pageCount
