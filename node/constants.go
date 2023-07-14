692ef4aab0af4f70e320719b191c45d516fc701f
Zachary Huff <zach.huff.386@gmail.com>
2023-07-14 15:42:40 -0400

    Update render types in node constants

--- node/constants.go
+++ node/constants.go
@@ -9,10 +9,11 @@ const (
 	Qemu = "qemu"
 	Kvm  = "kvm"
 
-	Std       = "std"
-	Vmware    = "vmware"
-	Virtio    = "virtio"
-	VirtioEgl = "virtio_egl"
+	Std             = "std"
+	Vmware          = "vmware"
+	Virtio          = "virtio"
+	VirtioEgl       = "virtio_egl"
+	VirtioEglVulkan = "virtio_egl_vulkan"
 
 	Sdl = "sdl"
 	Gtk = "gtk"
