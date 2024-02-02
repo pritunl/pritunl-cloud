ce073b32f40750f9c575e71d70182aa870ed9eb2
Zachary Huff <zach.huff.386@gmail.com>
2024-02-02 13:39:05 -0500

    Add broadcast to firewall constants

--- firewall/constants.go
+++ firewall/constants.go
@@ -1,8 +1,10 @@
 package firewall
 
 const (
-	All  = "all"
-	Icmp = "icmp"
-	Tcp  = "tcp"
-	Udp  = "udp"
+	All       = "all"
+	Icmp      = "icmp"
+	Tcp       = "tcp"
+	Udp       = "udp"
+	Multicast = "multicast"
+	Broadcast = "broadcast"
 )
