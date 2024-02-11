0b2184007301c5e50470ba9e5afe46d732377945
Zachary Huff <zach.huff.386@gmail.com>
2024-02-10 22:41:46 -0500

    Add pool detailed component to app

---
+++ www/app/components/PoolDetailed.tsx
@@ -0,0 +1,426 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as Constants from '../Constants';
+import * as PoolTypes from '../types/PoolTypes';
+import * as PoolActions from '../actions/PoolActions';
+import * as OrganizationTypes from "../types/OrganizationTypes";
+import * as DatacenterTypes from "../types/DatacenterTypes";
+import * as ZoneTypes from "../types/ZoneTypes";
+import PageInput from './PageInput';
+import PageSelect from './PageSelect';
+import PageInfo from './PageInfo';
+import PageInputButton from './PageInputButton';
+import PageSave from './PageSave';
+import ConfirmButton from './ConfirmButton';
+import Help from './Help';
+import PageTextArea from "./PageTextArea";
+import PageSwitch from "./PageSwitch";
+
+interface Props {
+	datacenters: DatacenterTypes.DatacentersRo;
+	zones: ZoneTypes.ZonesRo;
+	pool: PoolTypes.PoolRo;
+	selected: boolean;
+	onSelect: (shift: boolean) => void;
+	onClose: () => void;
+}
+
+interface State {
+	disabled: boolean;
+	changed: boolean;
+	message: string;
+	pool: PoolTypes.Pool;
+	datacenter: string;
+	zone: string;
+}
+
+const css = {
+	card: {
+		position: 'relative',
+		padding: '48px 10px 0 10px',
+		width: '100%',
+	} as React.CSSProperties,
+	button: {
+		height: '30px',
+	} as React.CSSProperties,
+	buttons: {
+		cursor: 'pointer',
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		padding: '4px',
+		height: '39px',
+		backgroundColor: 'rgba(0, 0, 0, 0.13)',
+	} as React.CSSProperties,
+	item: {
+		margin: '9px 5px 0 5px',
+		height: '20px',
+	} as React.CSSProperties,
+	itemsLabel: {
+		display: 'block',
+	} as React.CSSProperties,
+	itemsAdd: {
+		margin: '8px 0 15px 0',
+	} as React.CSSProperties,
+	group: {
+		flex: 1,
+		minWidth: '280px',
+		margin: '0 10px',
+	} as React.CSSProperties,
+	save: {
+		paddingBottom: '10px',
+	} as React.CSSProperties,
+	label: {
+		width: '100%',
+		maxWidth: '280px',
+	} as React.CSSProperties,
+	status: {
+		margin: '6px 0 0 1px',
+	} as React.CSSProperties,
+	icon: {
+		marginRight: '3px',
+	} as React.CSSProperties,
+	inputGroup: {
+		width: '100%',
+	} as React.CSSProperties,
+	protocol: {
+		flex: '0 1 auto',
+	} as React.CSSProperties,
+	port: {
+		flex: '1',
+	} as React.CSSProperties,
+	select: {
+		margin: '7px 0px 0px 6px',
+		paddingTop: '3px',
+	} as React.CSSProperties,
+	role: {
+		margin: '9px 5px 0 5px',
+		height: '20px',
+	} as React.CSSProperties,
+	rules: {
+		marginBottom: '15px',
+	} as React.CSSProperties,
+};
+
+export default class PoolDetailed extends React.Component<Props, State> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			disabled: false,
+			changed: false,
+			message: '',
+			pool: null,
+			datacenter: '',
+			zone: '',
+		};
+	}
+
+	set(name: string, val: any): void {
+		let pool: any;
+
+		if (this.state.changed) {
+			pool = {
+				...this.state.pool,
+			};
+		} else {
+			pool = {
+				...this.props.pool,
+			};
+		}
+
+		pool[name] = val;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			pool: pool,
+		});
+	}
+
+	onSave = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		PoolActions.commit(this.state.pool).then((): void => {
+			this.setState({
+				...this.state,
+				message: 'Your changes have been saved',
+				changed: false,
+				disabled: false,
+			});
+
+			setTimeout((): void => {
+				if (!this.state.changed) {
+					this.setState({
+						...this.state,
+						pool: null,
+						changed: false,
+					});
+				}
+			}, 1000);
+
+			setTimeout((): void => {
+				if (!this.state.changed) {
+					this.setState({
+						...this.state,
+						message: '',
+					});
+				}
+			}, 3000);
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				message: '',
+				disabled: false,
+			});
+		});
+	}
+
+	onDelete = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		PoolActions.remove(this.props.pool.id).then((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+		});
+	}
+
+	render(): JSX.Element {
+		let pool: PoolTypes.Pool = this.state.pool ||
+			this.props.pool;
+
+		let defaultDatacenter = '';
+		let hasDatacenters = false;
+		let datacentersSelect: JSX.Element[] = [];
+		if (this.props.datacenters.length) {
+			hasDatacenters = true;
+			defaultDatacenter = this.props.datacenters[0].id;
+			for (let datacenter of this.props.datacenters) {
+				datacentersSelect.push(
+					<option
+						key={datacenter.id}
+						value={datacenter.id}
+					>{datacenter.name}</option>,
+				);
+			}
+		}
+
+		if (!hasDatacenters) {
+			datacentersSelect.push(
+				<option key="null" value="">No Datacenters</option>);
+		}
+
+		let datacenter = this.state.datacenter || defaultDatacenter;
+		let hasZones = false;
+		let zonesSelect: JSX.Element[] = [];
+		if (this.props.zones.length) {
+			zonesSelect.push(<option key="null" value="">Select Zone</option>);
+
+			for (let zone of this.props.zones) {
+				if (!this.props.pool.zone && zone.datacenter !== datacenter) {
+					continue;
+				}
+				hasZones = true;
+
+				zonesSelect.push(
+					<option
+						key={zone.id}
+						value={zone.id}
+					>{zone.name}</option>,
+				);
+			}
+		}
+
+		if (!hasZones) {
+			zonesSelect = [<option key="null" value="">No Zones</option>];
+		}
+
+		return <td
+			className="bp3-cell"
+			colSpan={2}
+			style={css.card}
+		>
+			<div className="layout horizontal wrap">
+				<div style={css.group}>
+					<div
+						className="layout horizontal tab-close"
+						style={css.buttons}
+						onClick={(evt): void => {
+							let target = evt.target as HTMLElement;
+
+							if (target.className.indexOf('tab-close') !== -1) {
+								this.props.onClose();
+							}
+						}}
+					>
+						<div>
+							<label
+								className="bp3-control bp3-checkbox"
+								style={css.select}
+							>
+								<input
+									type="checkbox"
+									checked={this.props.selected}
+									onChange={(evt): void => {
+									}}
+									onClick={(evt): void => {
+										this.props.onSelect(evt.shiftKey);
+									}}
+								/>
+								<span className="bp3-control-indicator"/>
+							</label>
+						</div>
+						<div className="flex tab-close"/>
+						<ConfirmButton
+							className="bp3-minimal bp3-intent-danger bp3-icon-trash"
+							style={css.button}
+							safe={true}
+							progressClassName="bp3-intent-danger"
+							dialogClassName="bp3-intent-danger bp3-icon-delete"
+							dialogLabel="Delete Pool"
+							confirmMsg="Permanently delete this pool"
+							confirmInput={true}
+							items={[pool.name]}
+							disabled={this.state.disabled}
+							onConfirm={this.onDelete}
+						/>
+					</div>
+					<PageInput
+						label="Name"
+						help="Name of pool"
+						type="text"
+						placeholder="Enter name"
+						value={pool.name}
+						onChange={(val): void => {
+							this.set('name', val);
+						}}
+					/>
+					<PageTextArea
+						label="Comment"
+						help="Pool comment."
+						placeholder="Pool comment"
+						rows={3}
+						value={pool.comment}
+						onChange={(val: string): void => {
+							this.set('comment', val);
+						}}
+					/>
+					<PageSelect
+						disabled={this.state.disabled || !hasDatacenters}
+						hidden={!!this.props.pool.zone}
+						label="Datacenter"
+						help="Pool datacenter."
+						value={this.state.datacenter}
+						onChange={(val): void => {
+							if (this.state.changed) {
+								pool = {
+									...this.state.pool,
+								};
+							} else {
+								pool = {
+									...this.props.pool,
+								};
+							}
+
+							this.setState({
+								...this.state,
+								changed: true,
+								pool: pool,
+								datacenter: val,
+								zone: '',
+							});
+						}}
+					>
+						{datacentersSelect}
+					</PageSelect>
+					<PageSelect
+						disabled={!!this.props.pool.zone || this.state.disabled ||
+							!hasZones}
+						label="Zone"
+						help="Pool zone, cannot be changed once set. Clear pool ID in configuration file to reset pool."
+						value={this.props.pool.zone ? this.props.pool.zone :
+							this.state.zone}
+						onChange={(val): void => {
+							let pool: PoolTypes.Pool;
+							if (this.state.changed) {
+								pool = {
+									...this.state.pool,
+								};
+							} else {
+								pool = {
+									...this.props.pool,
+								};
+							}
+
+							this.setState({
+								...this.state,
+								changed: true,
+								pool: pool,
+								zone: val,
+							});
+						}}
+					>
+						{zonesSelect}
+					</PageSelect>
+				</div>
+				<div style={css.group}>
+					<PageInfo
+						fields={[
+							{
+								label: 'ID',
+								value: this.props.pool.id || 'None',
+							},
+						]}
+					/>
+					<PageInput
+						disabled={true}
+						label="Volume Group Name"
+						help="LVM volume group name. Name will be used to match nodes that have the LVM volume group available."
+						type="text"
+						placeholder="Enter name"
+						value={pool.vg_name}
+						onChange={(val): void => {
+							this.set('vg_name', val);
+						}}
+					/>
+					<PageSwitch
+						disabled={this.state.disabled}
+						label="Delete protection"
+						help="Block pool from being deleted."
+						checked={pool.delete_protection}
+						onToggle={(): void => {
+							this.set('delete_protection', !pool.delete_protection);
+						}}
+					/>
+				</div>
+			</div>
+			<PageSave
+				style={css.save}
+				hidden={!this.state.pool && !this.state.message}
+				message={this.state.message}
+				changed={this.state.changed}
+				disabled={this.state.disabled}
+				light={true}
+				onCancel={(): void => {
+					this.setState({
+						...this.state,
+						changed: false,
+						pool: null,
+					});
+				}}
+				onSave={this.onSave}
+			/>
+		</td>;
+	}
+}
