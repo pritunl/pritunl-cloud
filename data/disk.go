d5c565e4350cacb6fd91bb0325e8d765f76820bc
Zachary Huff <zach.huff.386@gmail.com>
2024-02-19 18:51:33 -0500

    Add lvm to data disk

--- data/disk.go
+++ data/disk.go
@@ -3,20 +3,23 @@ package data
 import (
 	"fmt"
 
+	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/disk"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/lvm"
 	"github.com/pritunl/pritunl-cloud/paths"
+	"github.com/pritunl/pritunl-cloud/pool"
 	"github.com/pritunl/pritunl-cloud/utils"
 )
 
-func CreateDisk(db *database.Database, dsk *disk.Disk) (
+func createDiskQcow(db *database.Database, dsk *disk.Disk) (
 	newSize int, backingImage string, err error) {
 
 	diskPath := paths.GetDiskPath(dsk.Id)
 
 	if !dsk.Image.IsZero() {
-		newSize, backingImage, err = WriteImage(
-			db, dsk.Image, dsk.Id, dsk.Size, dsk.Backing)
+		newSize, backingImage, err = writeImageQcow(db, dsk)
 		if err != nil {
 			return
 		}
@@ -31,6 +34,55 @@ func CreateDisk(db *database.Database, dsk *disk.Disk) (
 		if err != nil {
 			return
 		}
+	}
+
+	return
+}
+
+func createDiskLvm(db *database.Database, dsk *disk.Disk) (
+	newSize int, err error) {
+
+	pl, err := pool.Get(db, dsk.Pool)
+	if err != nil {
+		return
+	}
+
+	if !dsk.Image.IsZero() {
+		newSize, err = writeImageLvm(db, dsk, pl)
+		if err != nil {
+			return
+		}
+	} else {
+		err = lvm.CreateLv(pl.VgName, dsk.Id.Hex(), dsk.Size)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func CreateDisk(db *database.Database, dsk *disk.Disk) (
+	newSize int, backingImage string, err error) {
+
+	switch dsk.Type {
+	case disk.Lvm:
+		newSize, err = createDiskLvm(db, dsk)
+		if err != nil {
+			return
+		}
+		break
+	case "", disk.Qcow2:
+		newSize, backingImage, err = createDiskQcow(db, dsk)
+		if err != nil {
+			return
+		}
+		break
+	default:
+		err = &errortypes.ParseError{
+			errors.Newf("data: Unknown disk type %s", dsk.Type),
+		}
+		return
 	}
 
 	return
