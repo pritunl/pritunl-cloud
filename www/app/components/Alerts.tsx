5de642bf2824c59ad9f06512b9afa73662aeafc3
Zachary Huff <zach.huff.386@gmail.com>
2024-05-06 09:57:59 -0400

    Sync organizations in alerts component

--- www/app/components/Alerts.tsx
+++ www/app/components/Alerts.tsx
@@ -96,6 +96,7 @@ export default class Alerts extends React.Component<{}, State> {
 		OrganizationsStore.addChangeListener(this.onChange);
 		AlertActions.sync();
 		AuthorityActions.sync();
+		OrganizationActions.sync();
 	}
 
 	componentWillUnmount(): void {
