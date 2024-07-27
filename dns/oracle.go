cc274fe76b83e81f448c5b66b50efb1147181763
Zachary Huff <zach.huff.386@gmail.com>
2024-07-27 17:33:33 -0400

    Add ip records to oracle dns

--- dns/oracle.go
+++ dns/oracle.go
@@ -1,8 +1,6 @@
 package dns
 
 import (
-	"strings"
-
 	"github.com/dropbox/godropbox/errors"
 	"github.com/oracle/oci-go-sdk/v65/dns"
 	"github.com/pritunl/pritunl-cloud/database"
@@ -79,7 +77,7 @@ func (o *Oracle) DnsZoneFind(db *database.Database, domain string) (
 	}
 
 	for _, zone := range zones.Items {
-		if strings.Trim(*zone.Name, ".") == strings.Trim(domain, ".") {
+		if matchDomains(*zone.Name, domain) {
 			zoneId = *zone.Id
 			break
 		}
@@ -93,6 +91,41 @@ func (o *Oracle) DnsZoneFind(db *database.Database, domain string) (
 	}
 
 	o.cacheZoneId[domain] = zoneId
+
+	return
+}
+
+func (o *Oracle) DnsTxtGet(db *database.Database,
+	domain string) (vals []string, err error) {
+
+	zoneName := extractDomain(domain)
+	domain = cleanDomain(domain)
+
+	req := dns.GetZoneRecordsRequest{
+		ZoneNameOrId: utils.PointerString(zoneName),
+		Domain:       utils.PointerString(domain),
+	}
+
+	client, err := o.provider.GetDnsClient()
+	if err != nil {
+		return
+	}
+
+	resp, err := client.GetZoneRecords(db, req)
+	if err != nil {
+		err = &errortypes.ApiError{
+			errors.Wrap(err, "acme: Oracle zone record get error"),
+		}
+		return
+	}
+
+	for _, record := range resp.Items {
+		if record.Rtype != nil && *record.Rtype == "TXT" &&
+			record.Rdata != nil {
+
+			vals = append(vals, *record.Rdata)
+		}
+	}
 
 	return
 }
@@ -171,7 +204,7 @@ func (o *Oracle) DnsTxtDelete(db *database.Database,
 	return
 }
 
-func (o *Oracle) DnsTxtGet(db *database.Database,
+func (o *Oracle) DnsAGet(db *database.Database,
 	domain string) (vals []string, err error) {
 
 	zoneName := extractDomain(domain)
@@ -196,11 +229,194 @@ func (o *Oracle) DnsTxtGet(db *database.Database,
 	}
 
 	for _, record := range resp.Items {
-		if record.Rtype != nil && *record.Rtype == "TXT" &&
+		if record.Rtype != nil && *record.Rtype == "A" &&
 			record.Rdata != nil {
 
 			vals = append(vals, *record.Rdata)
 		}
+	}
+
+	return
+}
+
+func (o *Oracle) DnsAUpsert(db *database.Database,
+	domain, val string) (err error) {
+
+	zoneName := extractDomain(domain)
+	domain = cleanDomain(domain)
+
+	req := dns.PatchZoneRecordsRequest{
+		ZoneNameOrId: utils.PointerString(zoneName),
+		PatchZoneRecordsDetails: dns.PatchZoneRecordsDetails{
+			Items: []dns.RecordOperation{
+				{
+					Domain: &domain,
+					Rtype:  utils.PointerString("A"),
+					Ttl: utils.PointerInt(
+						settings.Acme.DnsOracleCloudTtl),
+					Rdata:     utils.PointerString(val),
+					Operation: dns.RecordOperationOperationAdd,
+				},
+			},
+		},
+	}
+
+	client, err := o.provider.GetDnsClient()
+	if err != nil {
+		return
+	}
+
+	_, err = client.PatchZoneRecords(db, req)
+	if err != nil {
+		err = &errortypes.ApiError{
+			errors.Wrap(err, "acme: Oracle zone patch error"),
+		}
+		return
+	}
+
+	return
+}
+
+func (o *Oracle) DnsADelete(db *database.Database,
+	domain, val string) (err error) {
+
+	zoneName := extractDomain(domain)
+	domain = cleanDomain(domain)
+
+	req := dns.PatchZoneRecordsRequest{
+		ZoneNameOrId: utils.PointerString(zoneName),
+		PatchZoneRecordsDetails: dns.PatchZoneRecordsDetails{
+			Items: []dns.RecordOperation{
+				{
+					Domain:    &domain,
+					Rtype:     utils.PointerString("A"),
+					Rdata:     utils.PointerString(val),
+					Operation: dns.RecordOperationOperationRemove,
+				},
+			},
+		},
+	}
+
+	client, err := o.provider.GetDnsClient()
+	if err != nil {
+		return
+	}
+
+	_, err = client.PatchZoneRecords(db, req)
+	if err != nil {
+		err = &errortypes.ApiError{
+			errors.Wrap(err, "acme: Oracle zone patch error"),
+		}
+		return
+	}
+
+	return
+}
+
+func (o *Oracle) DnsAAAAGet(db *database.Database,
+	domain string) (vals []string, err error) {
+
+	zoneName := extractDomain(domain)
+	domain = cleanDomain(domain)
+
+	req := dns.GetZoneRecordsRequest{
+		ZoneNameOrId: utils.PointerString(zoneName),
+		Domain:       utils.PointerString(domain),
+	}
+
+	client, err := o.provider.GetDnsClient()
+	if err != nil {
+		return
+	}
+
+	resp, err := client.GetZoneRecords(db, req)
+	if err != nil {
+		err = &errortypes.ApiError{
+			errors.Wrap(err, "acme: Oracle zone record get error"),
+		}
+		return
+	}
+
+	for _, record := range resp.Items {
+		if record.Rtype != nil && *record.Rtype == "AAAA" &&
+			record.Rdata != nil {
+
+			vals = append(vals, *record.Rdata)
+		}
+	}
+
+	return
+}
+
+func (o *Oracle) DnsAAAAUpsert(db *database.Database,
+	domain, val string) (err error) {
+
+	zoneName := extractDomain(domain)
+	domain = cleanDomain(domain)
+
+	req := dns.PatchZoneRecordsRequest{
+		ZoneNameOrId: utils.PointerString(zoneName),
+		PatchZoneRecordsDetails: dns.PatchZoneRecordsDetails{
+			Items: []dns.RecordOperation{
+				{
+					Domain: &domain,
+					Rtype:  utils.PointerString("AAAA"),
+					Ttl: utils.PointerInt(
+						settings.Acme.DnsOracleCloudTtl),
+					Rdata:     utils.PointerString(val),
+					Operation: dns.RecordOperationOperationAdd,
+				},
+			},
+		},
+	}
+
+	client, err := o.provider.GetDnsClient()
+	if err != nil {
+		return
+	}
+
+	_, err = client.PatchZoneRecords(db, req)
+	if err != nil {
+		err = &errortypes.ApiError{
+			errors.Wrap(err, "acme: Oracle zone patch error"),
+		}
+		return
+	}
+
+	return
+}
+
+func (o *Oracle) DnsAAAADelete(db *database.Database,
+	domain, val string) (err error) {
+
+	zoneName := extractDomain(domain)
+	domain = cleanDomain(domain)
+
+	req := dns.PatchZoneRecordsRequest{
+		ZoneNameOrId: utils.PointerString(zoneName),
+		PatchZoneRecordsDetails: dns.PatchZoneRecordsDetails{
+			Items: []dns.RecordOperation{
+				{
+					Domain:    &domain,
+					Rtype:     utils.PointerString("AAAA"),
+					Rdata:     utils.PointerString(val),
+					Operation: dns.RecordOperationOperationRemove,
+				},
+			},
+		},
+	}
+
+	client, err := o.provider.GetDnsClient()
+	if err != nil {
+		return
+	}
+
+	_, err = client.PatchZoneRecords(db, req)
+	if err != nil {
+		err = &errortypes.ApiError{
+			errors.Wrap(err, "acme: Oracle zone patch error"),
+		}
+		return
 	}
 
 	return
