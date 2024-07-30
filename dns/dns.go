c2081f2926ead54ab1ceda2cdd76a82388e34e75
Zachary Huff <zach.huff.386@gmail.com>
2024-07-30 12:16:59 -0400

    Add a and aaaa to dns interface

--- dns/dns.go
+++ dns/dns.go
@@ -10,4 +10,8 @@ type Service interface {
 	DnsTxtGet(db *database.Database, domain string) (vals []string, err error)
 	DnsTxtUpsert(db *database.Database, domain, val string) (err error)
 	DnsTxtDelete(db *database.Database, domain, val string) (err error)
+	DnsAUpsert(db *database.Database, domain, val string) (err error)
+	DnsADelete(db *database.Database, domain, val string) (err error)
+	DnsAAAAUpsert(db *database.Database, domain, val string) (err error)
+	DnsAAAADelete(db *database.Database, domain, val string) (err error)
 }
