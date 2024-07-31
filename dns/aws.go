1f3fc87277ac009d66c14e825c8583717057b0b2
Zachary Huff <zach.huff.386@gmail.com>
2024-07-31 03:30:55 -0400

    Add aws dns commit

--- dns/aws.go
+++ dns/aws.go
@@ -10,6 +10,7 @@ import (
 	"github.com/pritunl/pritunl-cloud/errortypes"
 	"github.com/pritunl/pritunl-cloud/secret"
 	"github.com/pritunl/pritunl-cloud/settings"
+	"github.com/sirupsen/logrus"
 )
 
 type Aws struct {
@@ -80,6 +81,127 @@ func (a *Aws) DnsZoneFind(domain string) (zoneId string, err error) {
 	}
 
 	a.cacheZoneId[domain] = zoneId
+
+	return
+}
+
+func (a *Aws) DnsCommit(db *database.Database,
+	domain, recordType string, ops []*Operation) (err error) {
+
+	domain = cleanDomain(domain)
+
+	zoneId, err := a.DnsZoneFind(domain)
+	if err != nil {
+		return
+	}
+
+	action := aws.String("DELETE")
+	resourceRecs := []*route53.ResourceRecord{}
+	values := []string{}
+	for _, op := range ops {
+		if op.Operation == UPSERT || op.Operation == RETAIN {
+			action = aws.String("UPSERT")
+		}
+
+		if recordType == "AAAA" {
+			val := normalizeIp(op.Value)
+			if val == "" {
+				err = &errortypes.ParseError{
+					errors.Newf("dns: Invalid ipv6 address %s", op.Value),
+				}
+				return
+			}
+			op.Value = val
+		}
+
+		resourceRecs = append(resourceRecs, &route53.ResourceRecord{
+			Value: aws.String(op.Value),
+		})
+		values = append(values, op.Value)
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"operation": *action,
+		"domain":    domain,
+		"values":    values,
+	}).Info("domain: AWS dns batch operation")
+
+	input := &route53.ChangeResourceRecordSetsInput{
+		ChangeBatch: &route53.ChangeBatch{
+			Changes: []*route53.Change{
+				{
+					Action: action,
+					ResourceRecordSet: &route53.ResourceRecordSet{
+						Name: aws.String(domain),
+						Type: aws.String(recordType),
+						TTL: aws.Int64(int64(
+							settings.Acme.DnsAwsTtl)),
+						ResourceRecords: resourceRecs,
+					},
+				},
+			},
+			Comment: aws.String("Pritunl update record"),
+		},
+		HostedZoneId: aws.String(zoneId),
+	}
+
+	_, err = a.sessRoute53.ChangeResourceRecordSets(input)
+	if err != nil {
+		err = &errortypes.ApiError{
+			errors.Wrap(err, "acme: AWS record change error"),
+		}
+		return
+	}
+
+	return
+}
+
+func (a *Aws) DnsFind(db *database.Database, domain, recordType string) (
+	vals []string, err error) {
+
+	vals = []string{}
+	domain = cleanDomain(domain)
+
+	zoneId, err := a.DnsZoneFind(domain)
+	if err != nil {
+		return
+	}
+
+	input := &route53.ListResourceRecordSetsInput{
+		HostedZoneId:    aws.String(zoneId),
+		StartRecordName: aws.String(domain),
+		StartRecordType: aws.String(recordType),
+	}
+
+	result, err := a.sessRoute53.ListResourceRecordSets(input)
+	if err != nil {
+		err = &errortypes.ApiError{
+			errors.Wrap(err, "acme: AWS record list error"),
+		}
+		return
+	}
+
+	for _, recordSet := range result.ResourceRecordSets {
+		if recordSet.Type != nil && *recordSet.Type == recordType &&
+			recordSet.Name != nil && matchDomains(*recordSet.Name, domain) {
+
+			for _, record := range recordSet.ResourceRecords {
+				if record.Value != nil {
+					val := *record.Value
+
+					if recordType == "AAAA" {
+						val = normalizeIp(val)
+					}
+
+					if val == "" {
+						continue
+					}
+
+					vals = append(vals, val)
+				}
+			}
+		}
+	}
 
 	return
 }
@@ -190,248 +312,6 @@ func (a *Aws) DnsTxtDelete(db *database.Database,
 				},
 			},
 			Comment: aws.String("Pritunl delete TXT record"),
-		},
-		HostedZoneId: aws.String(zoneId),
-	}
-
-	_, err = a.sessRoute53.ChangeResourceRecordSets(input)
-	if err != nil {
-		err = &errortypes.ApiError{
-			errors.Wrap(err, "acme: AWS route53 record set error"),
-		}
-		return
-	}
-
-	return
-}
-
-func (a *Aws) DnsAGet(db *database.Database, domain string) (
-	vals []string, err error) {
-
-	vals = []string{}
-
-	zoneId, err := a.DnsZoneFind(domain)
-	if err != nil {
-		return
-	}
-
-	input := &route53.ListResourceRecordSetsInput{
-		HostedZoneId:    aws.String(zoneId),
-		StartRecordName: aws.String(domain),
-		StartRecordType: aws.String("A"),
-	}
-
-	result, err := a.sessRoute53.ListResourceRecordSets(input)
-	if err != nil {
-		err = &errortypes.ApiError{
-			errors.Wrap(err, "acme: AWS route53 record set error"),
-		}
-		return
-	}
-
-	for _, recordSet := range result.ResourceRecordSets {
-		if recordSet.Type != nil && *recordSet.Type == "A" &&
-			recordSet.Name != nil && matchDomains(*recordSet.Name, domain) {
-
-			for _, record := range recordSet.ResourceRecords {
-				if record.Value != nil {
-					vals = append(vals, *record.Value)
-				}
-			}
-		}
-	}
-
-	return
-}
-
-func (a *Aws) DnsAUpsert(db *database.Database,
-	domain, val string) (err error) {
-
-	zoneId, err := a.DnsZoneFind(domain)
-	if err != nil {
-		return
-	}
-
-	input := &route53.ChangeResourceRecordSetsInput{
-		ChangeBatch: &route53.ChangeBatch{
-			Changes: []*route53.Change{
-				{
-					Action: aws.String("UPSERT"),
-					ResourceRecordSet: &route53.ResourceRecordSet{
-						Name: aws.String(domain),
-						Type: aws.String("A"),
-						TTL:  aws.Int64(int64(settings.Acme.DnsAwsTtl)),
-						ResourceRecords: []*route53.ResourceRecord{
-							{
-								Value: aws.String(val),
-							},
-						},
-					},
-				},
-			},
-			Comment: aws.String("Pritunl update A record"),
-		},
-		HostedZoneId: aws.String(zoneId),
-	}
-
-	_, err = a.sessRoute53.ChangeResourceRecordSets(input)
-	if err != nil {
-		err = &errortypes.ApiError{
-			errors.Wrap(err, "acme: AWS route53 record set error"),
-		}
-		return
-	}
-
-	return
-}
-
-func (a *Aws) DnsADelete(db *database.Database,
-	domain, val string) (err error) {
-
-	zoneId, err := a.DnsZoneFind(domain)
-	if err != nil {
-		return
-	}
-
-	input := &route53.ChangeResourceRecordSetsInput{
-		ChangeBatch: &route53.ChangeBatch{
-			Changes: []*route53.Change{
-				{
-					Action: aws.String("DELETE"),
-					ResourceRecordSet: &route53.ResourceRecordSet{
-						Name: aws.String(domain),
-						Type: aws.String("A"),
-						TTL:  aws.Int64(int64(settings.Acme.DnsAwsTtl)),
-						ResourceRecords: []*route53.ResourceRecord{
-							{
-								Value: aws.String(val),
-							},
-						},
-					},
-				},
-			},
-			Comment: aws.String("Pritunl delete A record"),
-		},
-		HostedZoneId: aws.String(zoneId),
-	}
-
-	_, err = a.sessRoute53.ChangeResourceRecordSets(input)
-	if err != nil {
-		err = &errortypes.ApiError{
-			errors.Wrap(err, "acme: AWS route53 record set error"),
-		}
-		return
-	}
-
-	return
-}
-
-func (a *Aws) DnsAAAAGet(db *database.Database, domain string) (
-	vals []string, err error) {
-
-	vals = []string{}
-
-	zoneId, err := a.DnsZoneFind(domain)
-	if err != nil {
-		return
-	}
-
-	input := &route53.ListResourceRecordSetsInput{
-		HostedZoneId:    aws.String(zoneId),
-		StartRecordName: aws.String(domain),
-		StartRecordType: aws.String("AAAA"),
-	}
-
-	result, err := a.sessRoute53.ListResourceRecordSets(input)
-	if err != nil {
-		err = &errortypes.ApiError{
-			errors.Wrap(err, "acme: AWS route53 record set error"),
-		}
-		return
-	}
-
-	for _, recordSet := range result.ResourceRecordSets {
-		if recordSet.Type != nil && *recordSet.Type == "AAAA" &&
-			recordSet.Name != nil && matchDomains(*recordSet.Name, domain) {
-
-			for _, record := range recordSet.ResourceRecords {
-				if record.Value != nil {
-					vals = append(vals, *record.Value)
-				}
-			}
-		}
-	}
-
-	return
-}
-
-func (a *Aws) DnsAAAAUpsert(db *database.Database,
-	domain, val string) (err error) {
-
-	zoneId, err := a.DnsZoneFind(domain)
-	if err != nil {
-		return
-	}
-
-	input := &route53.ChangeResourceRecordSetsInput{
-		ChangeBatch: &route53.ChangeBatch{
-			Changes: []*route53.Change{
-				{
-					Action: aws.String("UPSERT"),
-					ResourceRecordSet: &route53.ResourceRecordSet{
-						Name: aws.String(domain),
-						Type: aws.String("AAAA"),
-						TTL:  aws.Int64(int64(settings.Acme.DnsAwsTtl)),
-						ResourceRecords: []*route53.ResourceRecord{
-							{
-								Value: aws.String(val),
-							},
-						},
-					},
-				},
-			},
-			Comment: aws.String("Pritunl update AAAA record"),
-		},
-		HostedZoneId: aws.String(zoneId),
-	}
-
-	_, err = a.sessRoute53.ChangeResourceRecordSets(input)
-	if err != nil {
-		err = &errortypes.ApiError{
-			errors.Wrap(err, "acme: AWS route53 record set error"),
-		}
-		return
-	}
-
-	return
-}
-
-func (a *Aws) DnsAAAADelete(db *database.Database,
-	domain, val string) (err error) {
-
-	zoneId, err := a.DnsZoneFind(domain)
-	if err != nil {
-		return
-	}
-
-	input := &route53.ChangeResourceRecordSetsInput{
-		ChangeBatch: &route53.ChangeBatch{
-			Changes: []*route53.Change{
-				{
-					Action: aws.String("DELETE"),
-					ResourceRecordSet: &route53.ResourceRecordSet{
-						Name: aws.String(domain),
-						Type: aws.String("AAAA"),
-						TTL:  aws.Int64(int64(settings.Acme.DnsAwsTtl)),
-						ResourceRecords: []*route53.ResourceRecord{
-							{
-								Value: aws.String(val),
-							},
-						},
-					},
-				},
-			},
-			Comment: aws.String("Pritunl delete AAAA record"),
 		},
 		HostedZoneId: aws.String(zoneId),
 	}
