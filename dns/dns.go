443b6f2763d1ccaf3297c9ed7e24383107a6c66a
Zachary Huff <zach.huff.386@gmail.com>
2024-07-31 03:24:30 -0400

    Add dns commit and find to dns interface

--- dns/dns.go
+++ dns/dns.go
@@ -5,13 +5,19 @@ import (
 	"github.com/pritunl/pritunl-cloud/secret"
 )
 
+type Operation struct {
+	Operation string
+	Value     string
+}
+
 type Service interface {
 	Connect(db *database.Database, secr *secret.Secret) (err error)
+	DnsCommit(db *database.Database, domain, recordType string,
+		ops []*Operation) (err error)
+	DnsFind(db *database.Database, domain, recordType string) (
+		vals []string, err error)
+
 	DnsTxtGet(db *database.Database, domain string) (vals []string, err error)
 	DnsTxtUpsert(db *database.Database, domain, val string) (err error)
 	DnsTxtDelete(db *database.Database, domain, val string) (err error)
-	DnsAUpsert(db *database.Database, domain, val string) (err error)
-	DnsADelete(db *database.Database, domain, val string) (err error)
-	DnsAAAAUpsert(db *database.Database, domain, val string) (err error)
-	DnsAAAADelete(db *database.Database, domain, val string) (err error)
 }
