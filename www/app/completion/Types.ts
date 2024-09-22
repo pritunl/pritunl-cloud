46e83e9e3c7713ba42395f6312bb96b65d85e5c1
Zachary Huff <zach.huff.386@gmail.com>
2024-09-22 08:35:36 -0400

    Add resources to completion types

--- www/app/completion/Types.ts
+++ www/app/completion/Types.ts
@@ -1,4 +1,29 @@
 /// <reference path="../References.d.ts"/>
+import * as OrganizationTypes from "../types/OrganizationTypes";
+import * as DomainTypes from "../types/DomainTypes";
+import * as VpcTypes from "../types/VpcTypes";
+import * as DatacenterTypes from "../types/DatacenterTypes";
+import * as NodeTypes from "../types/NodeTypes";
+import * as PoolTypes from "../types/PoolTypes";
+import * as ZoneTypes from "../types/ZoneTypes";
+import * as ShapeTypes from "../types/ShapeTypes";
+import * as ImageTypes from "../types/ImageTypes";
+import * as InstanceTypes from "../types/InstanceTypes";
+import * as PlanTypes from "../types/PlanTypes";
+
+export interface Resources {
+	organizations: OrganizationTypes.OrganizationsRo;
+	domains: DomainTypes.DomainsRo;
+	vpcs: VpcTypes.VpcsRo;
+	datacenters: DatacenterTypes.DatacentersRo;
+	nodes: NodeTypes.NodesRo;
+	pools: PoolTypes.PoolsRo;
+	zones: ZoneTypes.ZonesRo;
+	shapes: ShapeTypes.ShapesRo;
+	images: ImageTypes.ImagesRo;
+	instances: InstanceTypes.InstancesRo;
+	plans: PlanTypes.PlansRo;
+}
 
 export interface Kind {
 	name: string
@@ -7,13 +32,14 @@ export interface Kind {
 }
 
 export interface Resource {
+	id: string
 	name: string
 	info: ResourceInfo[]
 }
 
 export interface ResourceInfo {
 	label: string
-	value: string
+	value: string | number
 }
 
 export interface Dispatch {
