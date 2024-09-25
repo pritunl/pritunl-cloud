55e6ec671cee0fd004af7b5583d83f9b5b5779a3
Zachary Huff <zach.huff.386@gmail.com>
2024-09-24 22:20:56 -0400

    Add plan types

---
+++ www/app/types/PlanTypes.ts
@@ -0,0 +1,39 @@
+/// <reference path="../References.d.ts"/>
+export const SYNC = 'plan.sync';
+export const SYNC_NAME = 'plan.sync_name';
+export const TRAVERSE = 'plan.traverse';
+export const FILTER = 'plan.filter';
+export const CHANGE = 'plan.change';
+
+export interface Plan {
+	id?: string;
+	name?: string;
+	comment?: string;
+	organization?: string;
+	type?: string;
+}
+
+export interface Filter {
+	id?: string;
+	name?: string;
+	network_role?: string;
+	organization?: string;
+}
+
+export type Plans = Plan[];
+
+export type PlanRo = Readonly<Plan>;
+export type PlansRo = ReadonlyArray<PlanRo>;
+
+export interface PlanDispatch {
+	type: string;
+	data?: {
+		id?: string;
+		plan?: Plan;
+		plans?: Plans;
+		page?: number;
+		pageCount?: number;
+		filter?: Filter;
+		count?: number;
+	};
+}
