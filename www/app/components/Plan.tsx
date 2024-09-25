0517f0e5ebf69fe165519494262118bd39277077
Zachary Huff <zach.huff.386@gmail.com>
2024-09-24 22:19:24 -0400

    Add plan component

---
+++ www/app/components/Plan.tsx
@@ -0,0 +1,160 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as PlanTypes from '../types/PlanTypes';
+import * as OrganizationTypes from "../types/OrganizationTypes";
+import OrganizationsStore from '../stores/OrganizationsStore';
+import PlanDetailed from './PlanDetailed';
+import * as SecretTypes from "../types/SecretTypes";
+
+interface Props {
+	organizations: OrganizationTypes.OrganizationsRo;
+	secrets: SecretTypes.SecretsRo;
+	plan: PlanTypes.PlanRo;
+	selected: boolean;
+	onSelect: (shift: boolean) => void;
+	open: boolean;
+	onOpen: () => void;
+}
+
+const css = {
+	card: {
+		display: 'table-row',
+		width: '100%',
+		padding: 0,
+		boxShadow: 'none',
+		cursor: 'pointer',
+	} as React.CSSProperties,
+	cardOpen: {
+		display: 'table-row',
+		width: '100%',
+		padding: 0,
+		boxShadow: 'none',
+		position: 'relative',
+	} as React.CSSProperties,
+	select: {
+		margin: '2px 0 0 0',
+		paddingTop: '3px',
+		minHeight: '18px',
+	} as React.CSSProperties,
+	name: {
+		verticalAlign: 'top',
+		display: 'table-cell',
+		padding: '8px',
+	} as React.CSSProperties,
+	nameSpan: {
+		margin: '1px 5px 0 0',
+	} as React.CSSProperties,
+	item: {
+		verticalAlign: 'top',
+		display: 'table-cell',
+		padding: '9px',
+		whiteSpace: 'nowrap',
+	} as React.CSSProperties,
+	icon: {
+		marginRight: '3px',
+	} as React.CSSProperties,
+	bars: {
+		verticalAlign: 'top',
+		display: 'table-cell',
+		padding: '8px',
+		width: '30px',
+	} as React.CSSProperties,
+	bar: {
+		height: '6px',
+		marginBottom: '1px',
+	} as React.CSSProperties,
+	barLast: {
+		height: '6px',
+	} as React.CSSProperties,
+	roles: {
+		verticalAlign: 'top',
+		display: 'table-cell',
+		padding: '0 8px 8px 8px',
+	} as React.CSSProperties,
+	tag: {
+		margin: '8px 5px 0 5px',
+		height: '20px',
+	} as React.CSSProperties,
+};
+
+export default class Plan extends React.Component<Props, {}> {
+	render(): JSX.Element {
+		let plan = this.props.plan;
+
+		if (this.props.open) {
+			return <div
+				className="bp5-card bp5-row"
+				style={css.cardOpen}
+			>
+				<PlanDetailed
+					organizations={this.props.organizations}
+					secrets={this.props.secrets}
+					plan={this.props.plan}
+					selected={this.props.selected}
+					onSelect={this.props.onSelect}
+					onClose={(): void => {
+						this.props.onOpen();
+					}}
+				/>
+			</div>;
+		}
+
+		let cardStyle = {
+			...css.card,
+		};
+
+		let orgName = '';
+		if (plan.organization) {
+			let org = OrganizationsStore.organization(plan.organization);
+			orgName = org ? org.name : plan.organization;
+		} else {
+			orgName = 'Unknown Organization';
+		}
+
+		return <div
+			className="bp5-card bp5-row"
+			style={cardStyle}
+			onClick={(evt): void => {
+				let target = evt.target as HTMLElement;
+
+				if (target.className.indexOf('open-ignore') !== -1) {
+					return;
+				}
+
+				this.props.onOpen();
+			}}
+		>
+			<div className="bp5-cell" style={css.name}>
+				<div className="layout horizontal">
+					<label
+						className="bp5-control bp5-checkbox open-ignore"
+						style={css.select}
+					>
+						<input
+							type="checkbox"
+							className="open-ignore"
+							checked={this.props.selected}
+							onChange={(evt): void => {
+							}}
+							onClick={(evt): void => {
+								this.props.onSelect(evt.shiftKey);
+							}}
+						/>
+						<span className="bp5-control-indicator open-ignore"/>
+					</label>
+					<div style={css.nameSpan}>
+						{plan.name}
+					</div>
+				</div>
+			</div>
+			<div className="bp5-cell" style={css.item}>
+				<span
+					style={css.icon}
+					className={'bp5-icon-standard bp5-text-muted ' + (
+						plan.organization ? 'bp5-icon-people' : 'bp5-icon-layers')}
+				/>
+				{orgName}
+			</div>
+		</div>;
+	}
+}
