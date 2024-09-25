79c8c1cf182a4c2b2ef29468b08bc6d08f171afe
Zachary Huff <zach.huff.386@gmail.com>
2024-09-24 22:19:54 -0400

    Add plan new component

---
+++ www/app/components/PlanNew.tsx
@@ -0,0 +1,245 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as OrganizationTypes from '../types/OrganizationTypes';
+import * as PlanActions from '../actions/PlanActions';
+import PageInput from './PageInput';
+import PageCreate from './PageCreate';
+import PageSelect from './PageSelect';
+import * as SecretTypes from "../types/SecretTypes";
+import * as PlanTypes from "../types/PlanTypes";
+import * as Constants from "../Constants";
+import OrganizationsStore from "../stores/OrganizationsStore";
+import PageTextArea from "./PageTextArea";
+
+interface Props {
+	organizations: OrganizationTypes.OrganizationsRo;
+	secrets: SecretTypes.SecretsRo;
+	onClose: () => void;
+}
+
+interface State {
+	closed: boolean;
+	disabled: boolean;
+	changed: boolean;
+	message: string;
+	plan: PlanTypes.Plan;
+}
+
+const css = {
+	row: {
+		display: 'table-row',
+		width: '100%',
+		padding: 0,
+		boxShadow: 'none',
+		position: 'relative',
+	} as React.CSSProperties,
+	card: {
+		position: 'relative',
+		padding: '10px 10px 0 10px',
+		width: '100%',
+	} as React.CSSProperties,
+	buttons: {
+		position: 'absolute',
+		top: '5px',
+		right: '5px',
+	} as React.CSSProperties,
+	item: {
+		margin: '9px 5px 0 5px',
+		height: '20px',
+	} as React.CSSProperties,
+	itemsLabel: {
+		display: 'block',
+	} as React.CSSProperties,
+	itemsAdd: {
+		margin: '8px 0 15px 0',
+	} as React.CSSProperties,
+	group: {
+		flex: 1,
+		minWidth: '280px',
+		margin: '0 10px',
+	} as React.CSSProperties,
+	save: {
+		paddingBottom: '10px',
+	} as React.CSSProperties,
+	label: {
+		width: '100%',
+		maxWidth: '280px',
+	} as React.CSSProperties,
+	inputGroup: {
+		width: '100%',
+	} as React.CSSProperties,
+	protocol: {
+		flex: '0 1 auto',
+	} as React.CSSProperties,
+	port: {
+		flex: '1',
+	} as React.CSSProperties,
+	role: {
+		margin: '9px 5px 0 5px',
+		height: '20px',
+	} as React.CSSProperties,
+};
+
+export default class PlanNew extends React.Component<Props, State> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			closed: false,
+			disabled: false,
+			changed: false,
+			message: '',
+			plan: {},
+		};
+	}
+
+	set(name: string, val: any): void {
+		let plan: any = {
+			...this.state.plan,
+		};
+
+		plan[name] = val;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			plan: plan,
+		});
+	}
+
+	onCreate = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+
+		let plan: any = {
+			...this.state.plan,
+		};
+
+		if (this.props.organizations.length && !plan.organization) {
+			plan.organization = this.props.organizations[0].id;
+		}
+
+		PlanActions.create(plan).then((): void => {
+			this.setState({
+				...this.state,
+				message: 'Plan created successfully',
+				changed: false,
+			});
+
+			setTimeout((): void => {
+				this.setState({
+					...this.state,
+					disabled: false,
+					changed: true,
+				});
+			}, 2000);
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				message: '',
+				disabled: false,
+			});
+		});
+	}
+
+	render(): JSX.Element {
+		let plan = this.state.plan;
+		if (this.props.organizations.length && !plan.organization) {
+			plan.organization = this.props.organizations[0].id;
+		}
+
+		let hasOrganizations = false
+		let organizationsSelect: JSX.Element[] = [];
+		if (this.props.organizations.length) {
+			for (let organization of this.props.organizations) {
+				hasOrganizations = true
+				organizationsSelect.push(
+					<option
+						key={organization.id}
+						value={organization.id}
+					>{organization.name}</option>,
+				);
+			}
+		}
+
+		if (!hasOrganizations) {
+			organizationsSelect.push(
+				<option key="null" value="">No Organizations</option>);
+		}
+
+		return <div
+			className="bp5-card bp5-row"
+			style={css.row}
+		>
+			<td
+				className="bp5-cell"
+				colSpan={5}
+				style={css.card}
+			>
+				<div className="layout horizontal wrap">
+					<div style={css.group}>
+						<div style={css.buttons}>
+						</div>
+						<PageInput
+							label="Name"
+							help="Plan name."
+							type="text"
+							placeholder="Enter name"
+							value={plan.name}
+							onChange={(val): void => {
+								this.set('name', val);
+							}}
+						/>
+						<PageTextArea
+							label="Comment"
+							help="Plan comment."
+							placeholder="Plan comment"
+							rows={3}
+							value={plan.comment}
+							onChange={(val: string): void => {
+								this.set('comment', val);
+							}}
+						/>
+					</div>
+					<div style={css.group}>
+						<PageSelect
+							disabled={this.state.disabled}
+							hidden={Constants.user}
+							label="Organization"
+							help="Organization for plan."
+							value={plan.organization}
+							onChange={(val): void => {
+								this.set('organization', val);
+							}}
+						>
+							{organizationsSelect}
+						</PageSelect>
+						<PageSelect
+							label="Type"
+							help="Plan type."
+							value={plan.type}
+							onChange={(val): void => {
+								this.set('type', val);
+							}}
+						>
+							<option value="rolling">Rolling</option>
+							<option value="recreate">Recreate</option>
+						</PageSelect>
+					</div>
+				</div>
+				<PageCreate
+					style={css.save}
+					hidden={!this.state.plan}
+					message={this.state.message}
+					changed={this.state.changed}
+					disabled={this.state.disabled}
+					closed={this.state.closed}
+					light={true}
+					onCancel={this.props.onClose}
+					onCreate={this.onCreate}
+				/>
+			</td>
+		</div>;
+	}
+}
