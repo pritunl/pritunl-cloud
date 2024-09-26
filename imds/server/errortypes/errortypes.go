09795a1e72f23fa5a32e041518650ade453ce961
Zachary Huff <zach.huff.386@gmail.com>
2024-09-26 12:02:45 -0400

    Add imds errortypes

---
+++ imds/server/errortypes/errortypes.go
@@ -0,0 +1,70 @@
+package errortypes
+
+import (
+	"github.com/dropbox/godropbox/errors"
+)
+
+type UnknownError struct {
+	errors.DropboxError
+}
+
+type NotFoundError struct {
+	errors.DropboxError
+}
+
+type ReadError struct {
+	errors.DropboxError
+}
+
+type WriteError struct {
+	errors.DropboxError
+}
+
+type ParseError struct {
+	errors.DropboxError
+}
+
+type AuthenticationError struct {
+	errors.DropboxError
+}
+
+type VerificationError struct {
+	errors.DropboxError
+}
+
+type ApiError struct {
+	errors.DropboxError
+}
+
+type DatabaseError struct {
+	errors.DropboxError
+}
+
+type RequestError struct {
+	errors.DropboxError
+}
+
+type ConnectionError struct {
+	errors.DropboxError
+}
+
+type TimeoutError struct {
+	errors.DropboxError
+}
+
+type ExecError struct {
+	errors.DropboxError
+}
+
+type NetworkError struct {
+	errors.DropboxError
+}
+
+type TypeError struct {
+	errors.DropboxError
+}
+
+type ErrorData struct {
+	Error   string `json:"error"`
+	Message string `json:"error_msg"`
+}
