4b1642ebccc260ef1656c8f0c642f7db24f3d100
Zachary Huff <zach.huff.386@gmail.com>
2024-09-26 12:03:43 -0400

    Add imds router

---
+++ imds/server/router/router.go
@@ -0,0 +1,70 @@
+package router
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/gin-gonic/gin"
+	"github.com/pritunl/pritunl-cloud/imds/server/constants"
+	"github.com/pritunl/pritunl-cloud/imds/server/errortypes"
+	"github.com/pritunl/pritunl-cloud/imds/server/handlers"
+	"github.com/pritunl/pritunl-cloud/imds/server/logger"
+)
+
+type Router struct {
+	server *http.Server
+}
+
+func (r *Router) Run() (err error) {
+	logger.WithFields(logger.Fields{
+		"host":    constants.Host,
+		"port":    constants.Port,
+		"version": constants.Version,
+	}).Info("main: Starting imds server")
+
+	err = r.server.ListenAndServe()
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrap(err, "main: Server listen error"),
+		}
+		return
+	}
+
+	return
+}
+
+func (r *Router) Shutdown() {
+	defer func() {
+		recover()
+	}()
+
+	webCtx, webCancel := context.WithTimeout(
+		context.Background(),
+		1*time.Second,
+	)
+	defer webCancel()
+
+	_ = r.server.Shutdown(webCtx)
+	_ = r.server.Close()
+}
+
+func (r *Router) Init() {
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.New()
+	handlers.Register(router)
+
+	r.server = &http.Server{
+		Addr:           fmt.Sprintf("%s:%d", constants.Host, constants.Port),
+		Handler:        router,
+		ReadTimeout:    30 * time.Second,
+		WriteTimeout:   30 * time.Second,
+		IdleTimeout:    60 * time.Second,
+		MaxHeaderBytes: 4096,
+	}
+
+	return
+}
