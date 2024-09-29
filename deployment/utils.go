cacd0304edfb57f7a6030ed13e8889bef84a5a77
Zachary Huff <zach.huff.386@gmail.com>
2024-09-28 23:44:05 -0400

    Add get all ids to deployment utils

--- deployment/utils.go
+++ deployment/utils.go
@@ -1,8 +1,10 @@
 package deployment
 
 import (
+	"github.com/dropbox/godropbox/container/set"
 	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/mongo-go-driver/mongo/options"
 	"github.com/pritunl/pritunl-cloud/database"
 )
 
@@ -40,6 +42,45 @@ func GetAll(db *database.Database) (deplys []*Deployment, err error) {
 		}
 
 		deplys = append(deplys, deply)
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func GetAllIds(db *database.Database) (deplyIds set.Set, err error) {
+	coll := db.Deployments()
+	deplyIds = set.NewSet()
+
+	cursor, err := coll.Find(
+		db,
+		bson.M{},
+		&options.FindOptions{
+			Projection: bson.M{
+				"_id": 1,
+			},
+		},
+	)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		deply := &Deployment{}
+		err = cursor.Decode(deply)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		deplyIds.Add(deply.Id)
 	}
 
 	err = cursor.Err()
