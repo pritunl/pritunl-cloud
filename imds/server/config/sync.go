91dc607415c9bf7e40e3e77784618e7b6194f815
Zachary Huff <zach.huff.386@gmail.com>
2024-09-30 16:02:51 -0400

    Add sync to imds server config

---
+++ imds/server/config/sync.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/config"
+	"github.com/pritunl/pritunl-cloud/imds/server/constants"
+	"github.com/pritunl/pritunl-cloud/imds/server/errortypes"
+	"github.com/pritunl/pritunl-cloud/imds/server/logger"
+)
+
+var (
+	curMod time.Time
+)
+
+func GetModTime() (mod time.Time, err error) {
+	stat, err := os.Stat(constants.ConfPath)
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "config: Failed to stat conf file"),
+		}
+		return
+	}
+
+	mod = stat.ModTime()
+
+	return
+}
+
+func SyncConfig() (err error) {
+	if constants.Interrupt {
+		return
+	}
+
+	mod, err := config.GetModTime()
+	if err != nil {
+		return
+	}
+
+	if mod != curMod {
+		time.Sleep(100 * time.Millisecond)
+
+		mod, err = config.GetModTime()
+		if err != nil {
+			return
+		}
+
+		err = config.Load()
+		if err != nil {
+			return
+		}
+
+		logger.Info("Reloaded config")
+
+		curMod = mod
+	}
+
+	return
+}
+
+func runSyncConfig() {
+	curMod, _ = GetModTime()
+
+	for {
+		time.Sleep(constants.ConfRefresh)
+
+		err := SyncConfig()
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"error": err,
+			}).Info("sync: Failed to sync config")
+		}
+	}
+}
