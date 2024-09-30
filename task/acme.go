30a25e7279868441c8b645c53d3b2d58d3a34d1e
Zachary Huff <zach.huff.386@gmail.com>
2024-09-30 19:34:59 -0400

    Update renew in acme task

--- task/acme.go
+++ task/acme.go
@@ -25,21 +25,13 @@ func acmeRenewHandler(db *database.Database) (err error) {
 			continue
 		}
 
-		err = acme.Update(db, cert)
-		if err != nil {
-			logrus.WithFields(logrus.Fields{
-				"certificate_id":   cert.Id.Hex(),
-				"certificate_name": cert.Name,
-			}).Warning("task: Failed to update certificate")
-			continue
-		}
-
 		err = acme.Renew(db, cert)
 		if err != nil {
 			logrus.WithFields(logrus.Fields{
 				"certificate_id":   cert.Id.Hex(),
 				"certificate_name": cert.Name,
-			}).Warning("task: Failed to renew certificate")
+				"error":            err,
+			}).Error("task: Failed to renew certificate")
 			continue
 		}
 	}
