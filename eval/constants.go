4df7442ab2b27902c01e3a7b546e6c96ce2f97b1
Zachary Huff <zach.huff.386@gmail.com>
2024-09-30 18:43:20 -0400

    Add eval constants

---
+++ eval/constants.go
@@ -0,0 +1,12 @@
+package eval
+
+type Equal struct{}
+type NotEqual struct{}
+type Less struct{}
+type LessEqual struct{}
+type Greater struct{}
+type GreaterEqual struct{}
+type If struct{}
+type And struct{}
+type Or struct{}
+type Then struct{}
