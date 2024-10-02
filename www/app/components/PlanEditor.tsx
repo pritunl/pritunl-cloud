2c3166445de240ab7b8dac5d33d7ca6b00a0aa52
Zachary Huff <zach.huff.386@gmail.com>
2024-10-02 14:04:03 -0400

    Add plan editor component

---
+++ www/app/components/PlanEditor.tsx
@@ -0,0 +1,82 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as PlanTypes from '../types/PlanTypes';
+import * as Theme from "../Theme";
+import * as MonacoEditor from "@monaco-editor/react"
+
+interface Props {
+	disabled?: boolean;
+	statements: PlanTypes.Statement[];
+	onChange: (statements: PlanTypes.Statement[]) => void;
+}
+
+const css = {
+	group: {
+		margin: '5px 0 20px 0',
+	} as React.CSSProperties,
+};
+
+export default class PlanStatement extends React.Component<Props, {}> {
+	onChange = (val: string): void => {
+		let curStatements = this.props.statements
+		let newStatements: PlanTypes.Statement[] = []
+		let lines = val.split("\n")
+
+		for (let i = 0; i < lines.length; i++) {
+			let line = lines[i]
+			if (!line) {
+				continue
+			}
+
+			let newStatement: PlanTypes.Statement = {
+				statement: line,
+			}
+
+			let curStatement = curStatements[i]
+			if (curStatement) {
+				newStatement.id = curStatement.id
+			}
+
+			newStatements.push(newStatement)
+		}
+
+		this.props.onChange(newStatements);
+	}
+
+	render(): JSX.Element {
+		let statements = (this.props.statements || [])
+		let statementsStr: string[] = []
+
+		for (let statement of statements) {
+			statementsStr.push(statement.statement)
+		}
+		let val = statementsStr.join("\n")
+
+		return <div className="layout horizontal" style={css.group}>
+			<MonacoEditor.Editor
+				height="192px"
+				width="100%"
+				defaultLanguage="markdown"
+				theme={Theme.getEditorTheme()}
+				defaultValue={val}
+				options={{
+					folding: false,
+					fontSize: 12,
+					fontFamily: Theme.monospaceFont,
+					fontWeight: "500",
+					tabSize: 4,
+					detectIndentation: false,
+					scrollBeyondLastLine: false,
+					minimap: {
+						enabled: false,
+					},
+					wordWrap: "on",
+					automaticLayout: true,
+				}}
+				onChange={(val): void => {
+					this.onChange(val)
+				}}
+			/>
+		</div>;
+	}
+}
