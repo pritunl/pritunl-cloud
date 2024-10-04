e57dfcf202481d35aa9aee669279771ff660e69e
Zachary Huff <zach.huff.386@gmail.com>
2024-10-03 22:53:48 -0400

    Add defaults to deployments constants

--- deployment/constants.go
+++ deployment/constants.go
@@ -1,8 +1,15 @@
 package deployment
 
+import (
+	"time"
+)
+
 const (
 	Reserved = "reserved"
 	Deployed = "deployed"
 
 	Instance = "instance"
+
+	ThresholdMin = 10
+	ActionLimit  = 1 * time.Minute
 )
