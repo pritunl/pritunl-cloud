64e57d2004b421f0a5978ea60c222af3ddb9f71d
Zachary Huff <zach.huff.386@gmail.com>
2024-10-04 17:08:22 -0400

    Add imds to netconf

---
+++ netconf/imds.go
@@ -0,0 +1,91 @@
+package netconf
+
+import (
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/settings"
+	"github.com/pritunl/pritunl-cloud/utils"
+)
+
+func (n *NetConf) imdsNet(db *database.Database) (err error) {
+	_, err = utils.ExecCombinedOutputLogged(
+		nil,
+		"ip", "netns", "exec", n.Namespace,
+		"ip", "link",
+		"add", n.SpaceImdsIface,
+		"type", "dummy",
+		//"addr", n.ImdsMacAddr,
+	)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (n *NetConf) imdsMtu(db *database.Database) (err error) {
+	_, err = utils.ExecCombinedOutputLogged(
+		nil,
+		"ip", "netns", "exec", n.Namespace,
+		"ip", "link",
+		"set", "dev", n.SpaceImdsIface,
+		"mtu", n.ImdsIfaceMtu,
+	)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (n *NetConf) imdsAddr(db *database.Database) (err error) {
+	_, err = utils.ExecCombinedOutputLogged(
+		nil,
+		"ip", "netns", "exec", n.Namespace,
+		"ip", "addr",
+		"add", settings.Hypervisor.ImdsAddress,
+		"dev", n.SpaceImdsIface,
+	)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (n *NetConf) imdsUp(db *database.Database) (err error) {
+	_, err = utils.ExecCombinedOutputLogged(
+		nil,
+		"ip", "netns", "exec", n.Namespace,
+		"ip", "link",
+		"set", "dev", n.SpaceImdsIface, "up",
+	)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (n *NetConf) Imds(db *database.Database) (err error) {
+	err = n.imdsNet(db)
+	if err != nil {
+		return
+	}
+
+	err = n.imdsMtu(db)
+	if err != nil {
+		return
+	}
+
+	err = n.imdsAddr(db)
+	if err != nil {
+		return
+	}
+
+	err = n.imdsUp(db)
+	if err != nil {
+		return
+	}
+
+	return
+}
