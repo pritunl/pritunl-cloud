3720192f128611fea9a6090d7bd3f75b912dee9d
Zachary Huff <zach.huff.386@gmail.com>
2024-10-03 22:49:02 -0400

    Add config to imds

---
+++ imds/config.go
@@ -0,0 +1,46 @@
+package imds
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/certificate"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/instance"
+	"github.com/pritunl/pritunl-cloud/paths"
+	"github.com/pritunl/pritunl-cloud/permission"
+	"github.com/pritunl/pritunl-cloud/vm"
+)
+
+type Config struct {
+	Instance     *instance.Instance         `json:"instance"`
+	Certificates []*certificate.Certificate `json:"certificates"`
+}
+
+func (c *Config) Write(virt *vm.VirtualMachine) (err error) {
+	pth := paths.GetImdsConfPath(virt.Id)
+
+	data, err := json.MarshalIndent(c, "", "\t")
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrap(err, "imds: File marshal error"),
+		}
+		return
+	}
+
+	err = ioutil.WriteFile(pth, data, 0600)
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrap(err, "imds: File write error"),
+		}
+		return
+	}
+
+	err = permission.InitImds(virt)
+	if err != nil {
+		return
+	}
+
+	return
+}
