11020fa06062a32cfd658e5496aac04287735b8e
Zachary Huff <zach.huff.386@gmail.com>
2024-10-08 14:06:28 -0400

    Add certificate types to imds

---
+++ imds/types/certificate.go
@@ -0,0 +1,36 @@
+package types
+
+import (
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/certificate"
+)
+
+type Certificate struct {
+	Id          primitive.ObjectID `json:"id"`
+	Name        string             `json:"name"`
+	Type        string             `json:"type"`
+	Key         string             `json:"key"`
+	Certificate string             `json:"certificate"`
+}
+
+func NewCertificates(certs []*certificate.Certificate) []*Certificate {
+	datas := []*Certificate{}
+
+	for _, cert := range certs {
+		if cert == nil {
+			continue
+		}
+
+		data := &Certificate{
+			Id:          cert.Id,
+			Name:        cert.Name,
+			Type:        cert.Type,
+			Key:         cert.Key,
+			Certificate: cert.Certificate,
+		}
+
+		datas = append(datas, data)
+	}
+
+	return datas
+}
