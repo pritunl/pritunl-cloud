8b5ae1a883dfb6391b96e6aadec250b933fb95a8
Zachary Huff <zach.huff.386@gmail.com>
2024-10-17 05:46:13 -0400

    Add secret to imds types

---
+++ imds/types/secret.go
@@ -0,0 +1,42 @@
+package types
+
+import (
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/secret"
+)
+
+type Secret struct {
+	Id         primitive.ObjectID `json:"id"`
+	Name       string             `json:"name"`
+	Type       string             `json:"type"`
+	Key        string             `json:"key"`
+	Value      string             `json:"value"`
+	Region     string             `json:"region"`
+	PublicKey  string             `json:"public_key"`
+	PrivateKey string             `json:"private_key"`
+}
+
+func NewSecrets(secrs []*secret.Secret) []*Secret {
+	datas := []*Secret{}
+
+	for _, secr := range secrs {
+		if secr == nil {
+			continue
+		}
+
+		data := &Secret{
+			Id:         secr.Id,
+			Name:       secr.Name,
+			Type:       secr.Type,
+			Key:        secr.Key,
+			Value:      secr.Value,
+			Region:     secr.Region,
+			PublicKey:  secr.PublicKey,
+			PrivateKey: secr.PrivateKey,
+		}
+
+		datas = append(datas, data)
+	}
+
+	return datas
+}
