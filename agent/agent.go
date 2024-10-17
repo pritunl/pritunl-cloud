f15223ba7ad27afde58008352e5deeaea510f2c0
Zachary Huff <zach.huff.386@gmail.com>
2024-10-17 05:48:37 -0400

    Add get to agent main

--- agent/agent.go
+++ agent/agent.go
@@ -6,21 +6,45 @@ import (
 
 	"github.com/pritunl/pritunl-cloud/agent/constants"
 	"github.com/pritunl/pritunl-cloud/agent/engine"
+	"github.com/pritunl/pritunl-cloud/agent/imds"
 )
 
 const help = `
 Usage: pci COMMAND
 
 Commands:
+  get          Get value from IMDS
   version      Show version
 `
 
 func main() {
+	flag.Usage = func() {
+		fmt.Printf(help)
+	}
+
+	flag.Parse()
+
 	switch flag.Arg(0) {
+	case "get":
+		ids := &imds.Imds{}
+
+		err := ids.Init()
+		if err != nil {
+			panic(err)
+		}
+
+		val, err := ids.Get(flag.Arg(1))
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Print(val)
+
+		break
 	case "engine":
 		eng := &engine.Engine{}
 
-		err := eng.Init()
+		err := eng.Init(flag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
@@ -28,5 +52,7 @@ func main() {
 	case "version":
 		fmt.Printf("pci v%s\n", constants.Version)
 		break
+	default:
+		fmt.Printf(help)
 	}
 }
