c4ec8af8baba7602d8cebaa8c943291f4ecea57f
Zachary Huff <zach.huff.386@gmail.com>
2024-10-20 04:56:35 -0400

    Add spec to service unit

--- service/unit.go
+++ service/unit.go
@@ -1,31 +1,23 @@
 package service
 
 import (
-	"regexp"
-	"strings"
-
-	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
 	"github.com/pritunl/mongo-go-driver/mongo/options"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/errortypes"
-	"github.com/pritunl/pritunl-cloud/shape"
-	"gopkg.in/yaml.v3"
+	"github.com/pritunl/pritunl-cloud/spec"
 )
-
-var yamlSpec = regexp.MustCompile("(?s)```yaml(.*?)```")
 
 type Unit struct {
 	Service     *Service           `bson:"-" json:"-"`
 	Id          primitive.ObjectID `bson:"id" json:"id"`
-	Parent      primitive.ObjectID `bson:"parent" json:"parent"`
 	Name        string             `bson:"name" json:"name"`
 	Kind        string             `bson:"kind" json:"kind"`
 	Count       int                `bson:"count" json:"count"`
 	Deployments []*Deployment      `bson:"deployments" json:"deployments"`
 	Spec        string             `bson:"spec" json:"spec"`
-	Instance    *Instance          `bson:"instance,omitempty" json:"instance,omitempty"`
+	Hash        string             `bson:"hash" json:"hash"`
 }
 
 type UnitInput struct {
@@ -38,50 +30,11 @@ type Deployment struct {
 	Id primitive.ObjectID `bson:"id" json:"id"`
 }
 
-// TODO Add firewalls
-// TODO Add secrets
-// TODO Add certificates
-
-// firewall:
-//   - open
-//      port: 22
-//      protocol: tcp
-
-type Instance struct {
-	Plan         primitive.ObjectID   `bson:"plan,omitempty" json:"shape"`
-	Zone         primitive.ObjectID   `bson:"zone" json:"zone"`
-	Node         primitive.ObjectID   `bson:"node,omitempty" json:"node"`
-	Shape        primitive.ObjectID   `bson:"shape,omitempty" json:"shape"`
-	Vpc          primitive.ObjectID   `bson:"vpc" json:"vpc"`
-	Subnet       primitive.ObjectID   `bson:"subnet" json:"subnet"`
-	Roles        []string             `bson:"roles" json:"roles"`
-	Processors   int                  `bson:"processors" json:"processors"`
-	Memory       int                  `bson:"memory" json:"memory"`
-	Image        primitive.ObjectID   `bson:"image" json:"image"`
-	DiskSize     int                  `bson:"disk_size" json:"disk_size"`
-	Certificates []primitive.ObjectID `bson:"certificates" json:"certificates"`
-}
-
-func (i *Instance) MemoryUnits() float64 {
-	return float64(i.Memory) / float64(1024)
-}
-
-type InstanceYaml struct {
-	Name         string   `yaml:"name"`
-	Kind         string   `yaml:"kind"`
-	Count        int      `yaml:"count"`
-	Plan         string   `yaml:"plan"`
-	Zone         string   `yaml:"zone"`
-	Node         string   `yaml:"node,omitempty"`
-	Shape        string   `yaml:"shape,omitempty"`
-	Vpc          string   `yaml:"vpc"`
-	Subnet       string   `yaml:"subnet"`
-	Roles        []string `yaml:"roles"`
-	Processors   int      `yaml:"processors"`
-	Memory       int      `yaml:"memory"`
-	Image        string   `yaml:"image"`
-	Certificates []string `yaml:"certificates"`
-	DiskSize     int      `yaml:"disk_size"`
+func (u *Unit) GetSpecHash() spec.Hash {
+	return spec.Hash{
+		Unit: u.Id,
+		Hash: u.Hash,
+	}
 }
 
 func (u *Unit) Reserve(db *database.Database, deployId primitive.ObjectID) (
@@ -182,219 +135,31 @@ func (u *Unit) RemoveDeployement(db *database.Database,
 	return
 }
 
-func (u *Unit) ExtractSpec() (spec string, err error) {
-	matches := yamlSpec.FindStringSubmatch(u.Spec)
-	if len(matches) > 1 {
-		spec = matches[1]
-		return
-	}
-
-	return
-}
-
-func (u *Unit) Parse(db *database.Database, srvc *Service) (
+func (u *Unit) Parse(db *database.Database) (
 	errData *errortypes.ErrorData, err error) {
 
-	spec, err := u.ExtractSpec()
+	spc := spec.New(u.Service.Id, u.Id, u.Spec)
+
+	errData, err = spc.Parse(db, u.Service.Organization)
 	if err != nil {
 		return
 	}
-
-	if spec == "" {
-		errData = &errortypes.ErrorData{
-			Error:   "unit_yaml_block_missing",
-			Message: "Unit missing yaml spec block",
-		}
+	if errData != nil {
 		return
 	}
 
-	spec = strings.TrimSpace(spec)
+	if u.Hash != spc.Id.Hash {
+		u.Name = spc.Name
+		u.Kind = spc.Kind
+		u.Count = spc.Count
+		u.Spec = spc.Data
+		u.Hash = spc.Id.Hash
 
-	data := &Instance{}
-	dataYaml := &InstanceYaml{}
-	var shpe *shape.Shape
-
-	err = yaml.Unmarshal([]byte(spec), dataYaml)
-	if err != nil {
-		err = &errortypes.ParseError{
-			errors.Wrap(err, "service: Failed to parse yaml spec"),
-		}
-		return
-	}
-
-	if dataYaml.Name == "" {
-		errData = &errortypes.ErrorData{
-			Error:   "unit_name_missing",
-			Message: "Unit name is missing",
-		}
-		return
-	}
-
-	if dataYaml.Kind != "instance" {
-		errData = &errortypes.ErrorData{
-			Error:   "unit_kind_invalid",
-			Message: "Unit kind is invalid",
-		}
-		return
-	}
-
-	if dataYaml.Count == 0 {
-		errData = &errortypes.ErrorData{
-			Error:   "unit_count_missing",
-			Message: "Unit count is missing",
-		}
-		return
-	}
-
-	resources := &Resources{
-		Organization: srvc.Organization,
-	}
-
-	if dataYaml.Zone != "" {
-		kind, e := resources.Find(db, dataYaml.Zone)
-		if e != nil {
-			err = e
+		err = spc.Insert(db)
+		if err != nil {
 			return
 		}
-		if kind == "zone" && resources.Zone != nil {
-			data.Zone = resources.Zone.Id
-		}
 	}
-
-	if data.Zone.IsZero() {
-		errData = &errortypes.ErrorData{
-			Error:   "unit_zone_missing",
-			Message: "Unit zone is missing",
-		}
-		return
-	}
-
-	if dataYaml.Node != "" {
-		kind, e := resources.Find(db, dataYaml.Node)
-		if e != nil {
-			err = e
-			return
-		}
-		if kind == "node" && resources.Node != nil {
-			data.Node = resources.Node.Id
-		}
-	}
-	if dataYaml.Shape != "" {
-		kind, e := resources.Find(db, dataYaml.Shape)
-		if e != nil {
-			err = e
-			return
-		}
-		if kind == "shape" && resources.Shape != nil {
-			shpe = resources.Shape
-			data.Shape = resources.Shape.Id
-		}
-	}
-
-	if data.Node.IsZero() && data.Shape.IsZero() {
-		errData = &errortypes.ErrorData{
-			Error:   "unit_node_missing",
-			Message: "Unit node or shape is missing",
-		}
-		return
-	}
-
-	if dataYaml.Vpc != "" {
-		kind, e := resources.Find(db, dataYaml.Vpc)
-		if e != nil {
-			err = e
-			return
-		}
-		if kind == "vpc" && resources.Vpc != nil {
-			data.Vpc = resources.Vpc.Id
-		}
-	}
-
-	if data.Node.IsZero() && data.Shape.IsZero() {
-		errData = &errortypes.ErrorData{
-			Error:   "unit_vpc_missing",
-			Message: "Unit VPC is missing",
-		}
-		return
-	}
-
-	if dataYaml.Subnet != "" {
-		kind, e := resources.Find(db, dataYaml.Subnet)
-		if e != nil {
-			err = e
-			return
-		}
-		if kind == "subnet" && resources.Subnet != nil {
-			data.Subnet = resources.Subnet.Id
-		}
-	}
-
-	if data.Node.IsZero() && data.Shape.IsZero() {
-		errData = &errortypes.ErrorData{
-			Error:   "unit_vpc_missing",
-			Message: "Unit subnet is missing",
-		}
-		return
-	}
-
-	if dataYaml.Image != "" {
-		kind, e := resources.Find(db, dataYaml.Image)
-		if e != nil {
-			err = e
-			return
-		}
-		if kind == "image" && resources.Image != nil {
-			data.Image = resources.Image.Id
-		}
-	}
-
-	if dataYaml.Certificates != nil {
-		for _, cert := range dataYaml.Certificates {
-			kind, e := resources.Find(db, cert)
-			if e != nil {
-				err = e
-				return
-			}
-			if kind == "certificate" && resources.Certificate != nil {
-				data.Certificates = append(
-					data.Certificates,
-					resources.Certificate.Id,
-				)
-			}
-		}
-	}
-
-	if data.Node.IsZero() && data.Shape.IsZero() {
-		errData = &errortypes.ErrorData{
-			Error:   "unit_image_missing",
-			Message: "Unit image is missing",
-		}
-		return
-	}
-
-	if shpe != nil {
-		data.Processors = shpe.Processors
-		data.Memory = shpe.Memory
-		if shpe.Flexible {
-			if dataYaml.Processors != 0 {
-				data.Processors = dataYaml.Processors
-			}
-			if dataYaml.Memory != 0 {
-				data.Memory = dataYaml.Memory
-			}
-		}
-	} else {
-		data.Processors = dataYaml.Processors
-		data.Memory = dataYaml.Memory
-	}
-
-	data.Roles = dataYaml.Roles
-	data.DiskSize = dataYaml.DiskSize
-
-	u.Name = dataYaml.Name
-	u.Kind = dataYaml.Kind
-	u.Count = dataYaml.Count
-	u.Instance = data
 
 	return
 }
