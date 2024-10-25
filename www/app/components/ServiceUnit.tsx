ca1bd2dc6a8f9d2cb7a2ba9e084c000b3f8a550c
Zachary Huff <zach.huff.386@gmail.com>
2024-10-25 18:51:46 -0400

    Add select to service unit component

--- www/app/components/ServiceUnit.tsx
+++ www/app/components/ServiceUnit.tsx
@@ -1,18 +1,28 @@
 /// <reference path="../References.d.ts"/>
 import * as React from "react"
 import * as Blueprint from "@blueprintjs/core"
+import * as Theme from '../Theme';
 import Help from "./Help"
 import * as ServiceTypes from "../types/ServiceTypes"
 import * as ServiceActions from "../actions/ServiceActions"
+import * as MiscUtils from '../utils/MiscUtils';
+import NonState from './NonState';
 import PageInfo from "./PageInfo"
 
 interface Props {
 	hidden: boolean
 	disabled?: boolean
+	selected: Selected
+	lastSelected: string
+	onSelect: (selected: Selected, lastSelected: string) => void
 	unit: ServiceTypes.ServiceUnit
 }
 
 interface State {
+}
+
+interface Selected {
+	[key: string]: boolean
 }
 
 const css = {
@@ -20,7 +30,13 @@ const css = {
 		flex: 1,
 		minWidth: "280px",
 		margin: "10px 10px 20px 10px",
-		fontSize: "12px",
+		fontSize: "11px",
+		fontFamily: Theme.monospaceFont,
+		fontWeight: Theme.monospaceWeight,
+	} as React.CSSProperties,
+	boxEmpty: {
+		flex: 1,
+		margin: "20px 10px 20px 10px",
 	} as React.CSSProperties,
 	card: {
 		padding: "5px 5px 3px 5px",
@@ -35,21 +51,15 @@ const css = {
 	info: {
 		marginBottom: "0px",
 	} as React.CSSProperties,
-	item1: {
-		flex: 1,
-		minWidth: "150px",
-	} as React.CSSProperties,
-	item2: {
-		flex: 1,
+	itemFirst: {
+		flex: "1 1 auto",
 		minWidth: "100px",
+		margin: "0 5px 0 -4px",
 	} as React.CSSProperties,
-	item3: {
-		flex: 1,
-		minWidth: "125px",
-	} as React.CSSProperties,
-	item4: {
-		flex: 1,
-		minWidth: "270px",
+	item: {
+		flex: "1 1 auto",
+		minWidth: "30px",
+		margin: " 0 5px",
 	} as React.CSSProperties,
 }
 
@@ -66,87 +76,186 @@ export default class ServiceUnit extends React.Component<Props, State> {
 		}
 
 		let cards: JSX.Element[] = []
-		for (let i = 0; i < 250; i++) {
-			for (let deployment of this.props.unit.deployments) {
-				cards.push(<Blueprint.Card compact={true} style={css.card}>
-					<div className="layout horizontal wrap">
-						<div className="layout center" style={css.checkBox}>
-							<Blueprint.Checkbox
-								style={css.check}
-								checked={false}
-								onChange={(): void => {
-
-								}}
-							/>
-						</div>
-						<div style={css.item1}>
-							<PageInfo
-								compact={true}
-								style={css.info}
-								fields={[
-									{
-										label: "Instance",
-										value: deployment.instance_name,
-									},
-									{
-										label: "Uptime",
-										value: deployment.instance_uptime,
-									},
-								]}
-							/>
-						</div>
-						<div style={css.item2}>
-							<PageInfo
-								compact={true}
-								style={css.info}
-								fields={[
-									{
-										label: "Status",
-										value: deployment.instance_status,
-									},
-									{
-										label: "Instance",
-										value: deployment.instance_name,
-									},
-								]}
-							/>
-						</div>
-						<div style={css.item3}>
-							<PageInfo
-								compact={true}
-								style={css.info}
-								fields={[
-									{
-										label: 'Public IPv4',
-										value: deployment.public_ips,
-									},
-									{
-										label: 'Private IPv4',
-										value: deployment.private_ips,
-									},
-								]}
-							/>
-						</div>
-						<div style={css.item4}>
-							<PageInfo
-								compact={true}
-								style={css.info}
-								fields={[
-									{
-										label: 'Public IPv6',
-										value: deployment.public_ips6,
-									},
-									{
-										label: 'Private IPv6',
-										value: deployment.private_ips6,
-									},
-								]}
-							/>
-						</div>
-					</div>
-				</Blueprint.Card>)
-			}
+		let deployments: ServiceTypes.Deployment[]
+		if (this.props.unit && this.props.unit.deployments) {
+			deployments = this.props.unit.deployments
+		} else {
+			deployments = []
 		}
+
+		if (!deployments.length) {
+			return <div className="layout horizontal wrap">
+				<div style={css.boxEmpty}>
+					<NonState
+						hidden={false}
+						iconClass="bp5-icon-dashboard"
+						title="No deployments"
+						description="Update unit spec to create deployments."
+						noDelay={true}
+					/>
+				</div>
+			</div>
+		}
+
+		deployments.forEach((deployment: ServiceTypes.Deployment): void => {
+			let publicIps = deployment.public_ips
+			if (!publicIps || !publicIps.length) {
+				publicIps = ["-"]
+			}
+
+			let publicIps6 = deployment.public_ips6
+			if (!publicIps6 || !publicIps6.length) {
+				publicIps6 = ["-"]
+			}
+
+			let privateIps = deployment.private_ips
+			if (!privateIps || !privateIps.length) {
+				privateIps = ["-"]
+			}
+
+			let privateIps6 = deployment.private_ips6
+			if (!privateIps6 || !privateIps6.length) {
+				privateIps6 = ["-"]
+			}
+
+			cards.push(<Blueprint.Card
+				key={deployment.id}
+				compact={true}
+				style={css.card}
+			>
+				<div className="layout horizontal">
+					<div className="layout center" style={css.checkBox}>
+						<Blueprint.Checkbox
+							style={css.check}
+							checked={!!this.props.selected[deployment.id]}
+							onClick={(evt): void => {
+								let selected = {
+									...this.props.selected,
+								};
+
+								if (evt.shiftKey) {
+									let deployments = this.props.unit.deployments;
+									let start: number;
+									let end: number;
+
+									for (let i = 0; i < deployments.length; i++) {
+										let deply = deployments[i];
+
+										if (deply.id === deployment.id) {
+											start = i;
+										} else if (deply.id === this.props.lastSelected) {
+											end = i;
+										}
+									}
+
+									if (start !== undefined && end !== undefined) {
+										if (start > end) {
+											end = [start, start = end][0];
+										}
+
+										for (let i = start; i <= end; i++) {
+											selected[deployments[i].id] = true;
+										}
+
+										this.props.onSelect(selected, deployment.id)
+
+										return;
+									}
+								}
+
+								if (selected[deployment.id]) {
+									delete selected[deployment.id];
+								} else {
+									selected[deployment.id] = true;
+								}
+
+								this.props.onSelect(selected, deployment.id)
+							}}
+						/>
+					</div>
+					<div style={css.itemFirst}>
+						<PageInfo
+							compact={true}
+							style={css.info}
+							fields={[
+								{
+									label: "Deployment ID",
+									value: deployment.id,
+								},
+								{
+									label: "Uptime",
+									value: deployment.instance_uptime || "-",
+								},
+							]}
+						/>
+					</div>
+					<div style={css.item}>
+						<PageInfo
+							compact={true}
+							style={css.info}
+							fields={[
+								{
+									label: "Node",
+									value: deployment.node_name || "-",
+								},
+								{
+									label: "Instance",
+									value: deployment.instance_name || "-",
+								},
+							]}
+						/>
+					</div>
+					<div style={css.item}>
+						<PageInfo
+							compact={true}
+							style={css.info}
+							fields={[
+								{
+									label: "Status",
+									value: deployment.instance_status || "-",
+								},
+								{
+									label: "State",
+									value: MiscUtils.capitalize(deployment.state) || "-",
+								},
+							]}
+						/>
+					</div>
+					<div style={css.item}>
+						<PageInfo
+							compact={true}
+							style={css.info}
+							fields={[
+								{
+									label: 'Public IPv4',
+									value: publicIps,
+								},
+								{
+									label: 'Private IPv4',
+									value: privateIps,
+								},
+							]}
+						/>
+					</div>
+					<div style={css.item}>
+						<PageInfo
+							compact={true}
+							style={css.info}
+							fields={[
+								{
+									label: 'Public IPv6',
+									value: publicIps6,
+								},
+								{
+									label: 'Private IPv6',
+									value: privateIps6,
+								},
+							]}
+						/>
+					</div>
+				</div>
+			</Blueprint.Card>)
+		})
 
 		return <div className="layout horizontal wrap">
 			<div style={css.box}>
@@ -157,4 +266,3 @@ export default class ServiceUnit extends React.Component<Props, State> {
 		</div>
 	}
 }
-``
