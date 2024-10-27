5f8eeb9c51cd3f601f24eed6473516a2cc64aa4f
Zachary Huff <zach.huff.386@gmail.com>
2024-10-26 22:45:19 -0400

    Update useragent in audit

--- audit/audit.go
+++ audit/audit.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
-	"github.com/pritunl/pritunl-cloud/agent"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/useragent"
 )
 
 type Fields map[string]interface{}
@@ -18,7 +18,7 @@ type Audit struct {
 	Timestamp time.Time          `bson:"t" json:"timestamp"`
 	Type      string             `bson:"y" json:"type"`
 	Fields    Fields             `bson:"f" json:"fields"`
-	Agent     *agent.Agent       `bson:"a" json:"agent"`
+	Agent     *useragent.Agent   `bson:"a" json:"agent"`
 }
 
 func (a *Audit) Insert(db *database.Database) (err error) {
