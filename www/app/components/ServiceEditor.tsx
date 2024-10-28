d9eda981e6ada4332dbabb6f0801f91f9e4d6005
Zachary Huff <zach.huff.386@gmail.com>
2024-10-27 20:47:35 -0400

    Add diff editor to service editor component

--- www/app/components/ServiceEditor.tsx
+++ www/app/components/ServiceEditor.tsx
@@ -11,12 +11,14 @@ import * as MonacoEditor from "@monaco-editor/react"
 import * as Monaco from "monaco-editor";
 
 interface Props {
+	hidden: boolean;
 	readOnly: boolean;
 	expandLeft: boolean;
 	expandRight: boolean;
 	disabled?: boolean;
 	uuid: string;
 	value: string;
+	diffValue: string;
 	onEdit?: () => void;
 	onChange?: (value: string) => void;
 }
@@ -96,6 +98,10 @@ export default class ServiceEditor extends React.Component<Props, State> {
 	curUuid: string
 	editor: Monaco.editor.IStandaloneCodeEditor
 	monaco: MonacoEditor.Monaco
+	diffEditor: Monaco.editor.IStandaloneDiffEditor
+	diffMonaco: MonacoEditor.Monaco
+	diffValue: string
+	diffUuid: string
 	states: Record<string, EditorState>
 
 	constructor(props: any, context: any) {
@@ -134,20 +140,27 @@ export default class ServiceEditor extends React.Component<Props, State> {
 
 	componentWillUnmount(): void {
 		Theme.removeChangeListener(this.onThemeChange);
+		this.curUuid = undefined
 		this.editor = undefined
 		this.monaco = undefined
+		this.diffUuid = undefined
+		this.diffEditor = undefined
+		this.diffMonaco = undefined
+		this.diffValue = undefined
 		this.states = {}
 	}
 
 	onThemeChange = (): void => {
-		if (!this.monaco) {
-			return
+		if (this.monaco) {
+			this.monaco.editor.setTheme(Theme.getEditorTheme())
 		}
-		this.monaco.editor.setTheme(Theme.getEditorTheme())
+		if (this.diffMonaco) {
+			this.diffMonaco.editor.setTheme(Theme.getEditorTheme())
+		}
 	}
 
 	updateState(): void {
-		if (!this.editor || !this.editor.getModel()) {
+		if (!this.editor || !this.editor.getModel() || this.props.diffValue) {
 			return
 		}
 
@@ -155,6 +168,7 @@ export default class ServiceEditor extends React.Component<Props, State> {
 			this.curUuid = this.props.uuid
 		}
 
+		let model: Monaco.editor.ITextModel
 		if (this.curUuid != this.props.uuid) {
 			this.states[this.curUuid] = {
 				model: this.editor.getModel(),
@@ -163,21 +177,34 @@ export default class ServiceEditor extends React.Component<Props, State> {
 
 			let newState = this.states[this.props.uuid]
 			if (newState) {
+				model = newState.model
 				this.editor.setModel(newState.model)
 				this.editor.restoreViewState(newState.view)
 			} else {
-				let model = this.monaco.editor.createModel(
+				model = this.monaco.editor.createModel(
 					this.props.value, "markdown",
 				)
 				this.editor.setModel(model)
 			}
 
 			this.curUuid = this.props.uuid
+		} else {
+			model = this.editor.getModel()
 		}
+
+		if (this.curUuid === this.diffUuid) {
+			model.setValue(this.diffValue)
+		}
+		this.diffUuid = undefined
+		this.diffValue = undefined
 	}
 
 	render(): JSX.Element {
 		this.updateState()
+
+		if (this.props.hidden) {
+			return <div></div>
+		}
 
 		let expandLeft = this.props.expandLeft
 		let expandRight = this.props.expandRight
@@ -220,7 +247,7 @@ export default class ServiceEditor extends React.Component<Props, State> {
 		}
 
 		let editor: JSX.Element;
-		if (!this.props.readOnly) {
+		if (!this.props.readOnly && !this.props.diffValue) {
 			editor = <MonacoEditor.Editor
 				height="900px"
 				width="100%"
@@ -230,12 +257,13 @@ export default class ServiceEditor extends React.Component<Props, State> {
 				beforeMount={CompletionEngine.handleBeforeMount}
 				onMount={(editor: Monaco.editor.IStandaloneCodeEditor,
 						monaco: MonacoEditor.Monaco): void => {
-					this.monaco = monaco
 					this.editor = editor
+					this.monaco = monaco
 					this.editor.onDidDispose((): void => {
 						this.editor = undefined
 						this.monaco = undefined
 						this.states = {}
+						this.curUuid = undefined
 					})
 					this.updateState()
 
@@ -257,6 +285,46 @@ export default class ServiceEditor extends React.Component<Props, State> {
 				}}
 				onChange={(val): void => {
 					this.props.onChange(val)
+				}}
+			/>
+		} else if (!this.props.readOnly && this.props.diffValue) {
+			editor = <MonacoEditor.DiffEditor
+				height="900px"
+				width="100%"
+				theme={Theme.getEditorTheme()}
+				original={this.props.diffValue}
+				modified={this.props.value}
+				originalLanguage="markdown"
+				modifiedLanguage="markdown"
+				beforeMount={CompletionEngine.handleBeforeMount}
+				onMount={(editor: Monaco.editor.IStandaloneDiffEditor,
+						monaco: MonacoEditor.Monaco): void => {
+					this.diffEditor = editor
+					this.diffMonaco = monaco
+					this.diffEditor.onDidDispose((): void => {
+						this.diffEditor = undefined
+						this.diffMonaco = undefined
+					})
+
+					let modifiedEditor = editor.getModifiedEditor()
+					modifiedEditor.onDidChangeModelContent((): void => {
+						this.diffUuid = this.props.uuid
+						this.diffValue = modifiedEditor.getValue()
+					})
+
+					this.updateState()
+				}}
+				options={{
+					folding: false,
+					fontSize: 12,
+					fontFamily: Theme.monospaceFont,
+					fontWeight: Theme.monospaceWeight,
+					rulers: [80],
+					scrollBeyondLastLine: false,
+					minimap: {
+						enabled: false,
+					},
+					wordWrap: "on",
 				}}
 			/>
 		}
