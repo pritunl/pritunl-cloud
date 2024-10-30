625e4438e87e4eba569bcc3c234c2fc240a89780
Zachary Huff <zach.huff.386@gmail.com>
2024-10-29 21:48:20 -0400

    Add format date local to misc utils

--- www/app/utils/MiscUtils.ts
+++ www/app/utils/MiscUtils.ts
@@ -135,6 +135,76 @@ export function formatDate(dateStr: string): string {
 	return str;
 }
 
+export function formatDateLocal(dateStr: string): string {
+	if (!dateStr || dateStr === '0001-01-01T00:00:00Z') {
+		return '';
+	}
+
+	let date = new Date(dateStr);
+	date = new Date(date.getTime());
+	let str = '';
+
+	let hours = date.getHours();
+	let period = 'AM';
+
+	if (hours > 12) {
+		period = 'PM';
+		hours -= 12;
+	} else if (hours === 0) {
+		hours = 12;
+	}
+
+	let month;
+	switch (date.getMonth()) {
+		case 0:
+			month = 'Jan';
+			break;
+		case 1:
+			month = 'Feb';
+			break;
+		case 2:
+			month = 'Mar';
+			break;
+		case 3:
+			month = 'Apr';
+			break;
+		case 4:
+			month = 'May';
+			break;
+		case 5:
+			month = 'Jun';
+			break;
+		case 6:
+			month = 'Jul';
+			break;
+		case 7:
+			month = 'Aug';
+			break;
+		case 8:
+			month = 'Sep';
+			break;
+		case 9:
+			month = 'Oct';
+			break;
+		case 10:
+			month = 'Nov';
+			break;
+		case 11:
+			month = 'Dec';
+			break;
+	}
+
+	str += month + ' ';
+	str += zeroPad(date.getDate(), 2) + ', ';
+	str += date.getFullYear() + ' ';
+	str += zeroPad(hours, 2) + ':';
+	str += zeroPad(date.getMinutes(), 2) + ':';
+	str += zeroPad(date.getSeconds(), 2) + ' ';
+	str += period;
+
+	return str;
+}
+
 export function formatDateShort(dateStr: string): string {
 	if (!dateStr || dateStr === '0001-01-01T00:00:00Z') {
 		return '';
