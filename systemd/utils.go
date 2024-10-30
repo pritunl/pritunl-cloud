a91b9368d35403d8fef844cd6937f3466a39b10c
Zachary Huff <zach.huff.386@gmail.com>
2024-10-29 21:49:55 -0400

    Add format uptime short to systemd utils

--- systemd/utils.go
+++ systemd/utils.go
@@ -28,3 +28,27 @@ func FormatUptime(timestamp time.Time) (uptime string) {
 
 	return
 }
+
+func FormatUptimeShort(timestamp time.Time) (uptime string) {
+	since := time.Since(timestamp)
+	minutes := int64(since.Minutes())
+	days := minutes / 1440
+	hours := (minutes % 1440) / 60
+	minutes = (minutes % 1440) % 60
+
+	if days > 0 {
+		uptime = fmt.Sprintf("%d dy", days)
+	}
+	if hours > 0 || uptime != "" {
+		if uptime != "" {
+			uptime += " "
+		}
+		uptime += fmt.Sprintf("%d hr", hours)
+	}
+	if uptime != "" {
+		uptime += " "
+	}
+	uptime += fmt.Sprintf("%d mn", minutes)
+
+	return
+}
