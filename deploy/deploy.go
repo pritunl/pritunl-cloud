5b223aad054a6079366600f8e88f33d0d556f9e4
Zachary Huff <zach.huff.386@gmail.com>
2024-11-05 16:15:25 -0500

    Store times for deploy

--- deploy/deploy.go
+++ deploy/deploy.go
@@ -1,26 +1,33 @@
 package deploy
 
 import (
+	"time"
+
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/state"
 )
 
-func Deploy(stat *state.State) (err error) {
+func Deploy(stat *state.State, runtimes *state.Runtimes) (err error) {
 	db := database.GetDatabase()
 	defer db.Close()
 
+	start := time.Now()
 	network := NewNetwork(stat)
 	err = network.Deploy()
 	if err != nil {
 		return
 	}
+	runtimes.Network = time.Since(start)
 
+	start = time.Now()
 	ipset := NewIpset(stat)
 	err = ipset.Deploy()
 	if err != nil {
 		return
 	}
+	runtimes.Ipset = time.Since(start)
 
+	start = time.Now()
 	iptables := NewIptables(stat)
 	err = iptables.Deploy()
 	if err != nil {
@@ -31,42 +38,55 @@ func Deploy(stat *state.State) (err error) {
 	if err != nil {
 		return
 	}
+	runtimes.Iptables = time.Since(start)
 
+	start = time.Now()
 	disks := NewDisks(stat)
 	err = disks.Deploy(db)
 	if err != nil {
 		return
 	}
+	runtimes.Disks = time.Since(start)
 
+	start = time.Now()
 	instances := NewInstances(stat)
 	err = instances.Deploy(db)
 	if err != nil {
 		return
 	}
+	runtimes.Instances = time.Since(start)
 
+	start = time.Now()
 	namespaces := NewNamespace(stat)
 	err = namespaces.Deploy()
 	if err != nil {
 		return
 	}
+	runtimes.Namespaces = time.Since(start)
 
+	start = time.Now()
 	services := NewServices(stat)
 	err = services.Deploy(db)
 	if err != nil {
 		return
 	}
+	runtimes.Services = time.Since(start)
 
+	start = time.Now()
 	deployments := NewDeployments(stat)
 	err = deployments.Deploy(db)
 	if err != nil {
 		return
 	}
+	runtimes.Deployments = time.Since(start)
 
+	start = time.Now()
 	imds := NewImds(stat)
 	err = imds.Deploy(db)
 	if err != nil {
 		return
 	}
+	runtimes.Imds = time.Since(start)
 
 	return
 }
