23ee408f79bf627e04fbb0a94db84fa109ee7d03
Zachary Huff <zach.huff.386@gmail.com>
2024-11-04 19:30:36 -0500

    Init imds sync in agent

--- agent/agent.go
+++ agent/agent.go
@@ -3,10 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/pritunl/pritunl-cloud/agent/constants"
 	"github.com/pritunl/pritunl-cloud/agent/engine"
 	"github.com/pritunl/pritunl-cloud/agent/imds"
+	"github.com/pritunl/pritunl-cloud/agent/logging"
+	"github.com/pritunl/tools/logger"
 )
 
 const help = `
@@ -43,11 +46,56 @@ func main() {
 		break
 	case "engine":
 		eng := &engine.Engine{}
+		ids := &imds.Imds{}
+		log := &logging.Redirect{}
 
-		err := eng.Init(flag.Arg(1))
+		err := log.Open()
 		if err != nil {
 			panic(err)
 		}
+		defer log.Close()
+
+		err = ids.Init()
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"error": err,
+			}).Error("agent: Failed to init imds")
+			panic(err)
+		}
+
+		for i := 0; i < 150; i++ {
+			time.Sleep(200 * time.Millisecond)
+
+			err = ids.Sync()
+			if err != nil {
+				continue
+			}
+
+			break
+		}
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"error": err,
+			}).Error("agent: Failed to sync imds initial")
+			panic(err)
+		}
+
+		err = eng.Init(flag.Arg(1))
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"error": err,
+			}).Error("agent: Failed to init engine")
+			panic(err)
+		}
+
+		err = ids.Run()
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"error": err,
+			}).Error("agent: Failed to run")
+			panic(err)
+		}
+
 		break
 	case "version":
 		fmt.Printf("pci v%s\n", constants.Version)
