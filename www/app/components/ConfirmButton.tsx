eb092f27b9338bae5bd848f6d384e3156842bd69
Zachary Huff <zach.huff.386@gmail.com>
2024-11-07 16:16:20 -0500

    Add menu item to confirm button component

--- www/app/components/ConfirmButton.tsx
+++ www/app/components/ConfirmButton.tsx
@@ -1,5 +1,6 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
+import ReactDOM from "react-dom";
 import * as Blueprint from '@blueprintjs/core';
 import * as Constants from '../Constants';
 import * as MiscUtils from '../utils/MiscUtils';
@@ -16,8 +17,10 @@ interface Props {
 	confirmMsg?: string;
 	confirmInput?: boolean;
 	items?: string[];
+	menuItem?: boolean;
 	disabled?: boolean;
 	safe?: boolean;
+	children?: React.ReactNode
 	onConfirm?: () => void;
 }
 
@@ -166,7 +169,7 @@ export default class ConfirmButton extends React.Component<Props, State> {
 	}
 
 	render(): JSX.Element {
-		let dialog = Constants.mobile || this.props.safe;
+		let dialog = Constants.mobile || this.props.safe || this.props.menuItem;
 
 		let style = {
 			...this.props.style,
@@ -221,9 +224,57 @@ export default class ConfirmButton extends React.Component<Props, State> {
 			if (this.props.items) {
 				let items: JSX.Element[] = [];
 				for (let item of this.props.items) {
-					items.push(<li>{item}</li>);
+					items.push(<li key={item}>{item}</li>);
 				}
 				itemsList = <ul>{items}</ul>;
+			}
+
+			let dialogElem = <Blueprint.Dialog
+				title="Confirm"
+				style={css.dialog}
+				isOpen={this.state.dialog}
+				usePortal={true}
+				portalContainer={document.body}
+				onClose={this.closeDialog}
+			>
+				<div className="bp5-dialog-body">
+					{confirmMsg}
+					{itemsList}
+					{confirmInput}
+				</div>
+				<div className="bp5-dialog-footer">
+					<div className="bp5-dialog-footer-actions">
+						<button
+							className="bp5-button"
+							type="button"
+							onClick={this.closeDialog}
+						>Cancel</button>
+						<button
+							className={'bp5-button ' + dialogClassName}
+							type="button"
+							disabled={this.props.confirmInput &&
+								this.state.input !== 'delete'}
+							onClick={this.closeDialogConfirm}
+						>{this.props.dialogLabel || this.props.label}</button>
+					</div>
+				</div>
+			</Blueprint.Dialog>
+
+			if (this.props.menuItem) {
+				return <div>
+					<Blueprint.MenuItem
+						key="menu-new-unit"
+						className={className}
+						disabled={this.props.disabled}
+						onClick={(evt): void => {
+							evt.preventDefault()
+							evt.stopPropagation()
+							this.openDialog()
+						}}
+						text={this.props.label}
+					>{this.props.children}</Blueprint.MenuItem>
+					{dialogElem}
+				</div>
 			}
 
 			return <div style={css.box}>
@@ -238,38 +289,9 @@ export default class ConfirmButton extends React.Component<Props, State> {
 					onMouseLeave={dialog ? undefined : this.clearConfirm}
 					onClick={dialog ? this.openDialog : undefined}
 				>
-					{this.props.label}
+					{this.props.children || this.props.label}
 				</button>
-				<Blueprint.Dialog
-					title="Confirm"
-					style={css.dialog}
-					isOpen={this.state.dialog}
-					usePortal={true}
-					portalContainer={document.body}
-					onClose={this.closeDialog}
-				>
-					<div className="bp5-dialog-body">
-						{confirmMsg}
-						{itemsList}
-						{confirmInput}
-					</div>
-					<div className="bp5-dialog-footer">
-						<div className="bp5-dialog-footer-actions">
-							<button
-								className="bp5-button"
-								type="button"
-								onClick={this.closeDialog}
-							>Cancel</button>
-							<button
-								className={'bp5-button ' + dialogClassName}
-								type="button"
-								disabled={this.props.confirmInput &&
-									this.state.input !== 'delete'}
-								onClick={this.closeDialogConfirm}
-							>{this.props.dialogLabel || this.props.label}</button>
-						</div>
-					</div>
-				</Blueprint.Dialog>
+				{dialogElem}
 			</div>
 		} else {
 			let confirmElem: JSX.Element;
@@ -309,7 +331,7 @@ export default class ConfirmButton extends React.Component<Props, State> {
 				onMouseLeave={dialog ? undefined : this.clearConfirm}
 				onClick={dialog ? this.openDialog : undefined}
 			>
-				{this.props.label}
+				{this.props.children || this.props.label}
 				{confirmElem}
 			</button>;
 		}
