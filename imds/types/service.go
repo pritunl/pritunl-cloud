17ed4e00d352baaa51bcba05233f4aa54caa8e87
Zachary Huff <zach.huff.386@gmail.com>
2024-11-10 12:14:55 -0500

    Add status ips to imds service types

--- imds/types/service.go
+++ imds/types/service.go
@@ -13,34 +13,61 @@ type Service struct {
 }
 
 type Unit struct {
-	Id               primitive.ObjectID `json:"id"`
-	Name             string             `json:"name"`
-	Kind             string             `json:"kind"`
-	Count            int                `json:"count"`
-	PublicIps        []string           `json:"public_ips"`
-	PublicIps6       []string           `json:"public_ips6"`
-	PrivateIps       []string           `json:"private_ips"`
-	PrivateIps6      []string           `json:"private_ips6"`
-	OraclePrivateIps []string           `json:"oracle_private_ips"`
-	OraclePublicIps  []string           `json:"oracle_public_ips"`
-	Deployments      []*Deployment      `json:"deployments"`
+	Id                        primitive.ObjectID `json:"id"`
+	Name                      string             `json:"name"`
+	Kind                      string             `json:"kind"`
+	Count                     int                `json:"count"`
+	PublicIps                 []string           `json:"public_ips"`
+	PublicIps6                []string           `json:"public_ips6"`
+	HealthyPublicIps          []string           `json:"healthy_public_ips"`
+	HealthyPublicIps6         []string           `json:"healthy_public_ips6"`
+	UnhealthyPublicIps        []string           `json:"unhealthy_public_ips"`
+	UnhealthyPublicIps6       []string           `json:"unhealthy_public_ips6"`
+	PrivateIps                []string           `json:"private_ips"`
+	PrivateIps6               []string           `json:"private_ips6"`
+	HealthyPrivateIps         []string           `json:"healthy_private_ips"`
+	HealthyPrivateIps6        []string           `json:"healthy_private_ips6"`
+	UnhealthyPrivateIps       []string           `json:"unhealthy_private_ips"`
+	UnhealthyPrivateIps6      []string           `json:"unhealthy_private_ips6"`
+	OraclePrivateIps          []string           `json:"oracle_private_ips"`
+	OraclePublicIps           []string           `json:"oracle_public_ips"`
+	HealthyOraclePublicIps    []string           `json:"healthy_oracle_public_ips"`
+	HealthyOraclePrivateIps   []string           `json:"healthy_oracle_private_ips"`
+	UnhealthyOraclePublicIps  []string           `json:"unhealthy_oracle_public_ips"`
+	UnhealthyOraclePrivateIps []string           `json:"unhealthy_oracle_private_ips"`
+	Deployments               []*Deployment      `json:"deployments"`
 }
 
+// TODO Add web server on port 9738 to recieve hearthbeat updates from imds
+// TODO server when host pritunl-cloud process is offline
+
 type Deployment struct {
-	Id               primitive.ObjectID `json:"id"`
-	Service          primitive.ObjectID `json:"service"`
-	Unit             primitive.ObjectID `json:"unit"`
-	Spec             primitive.ObjectID `json:"spec"`
-	Kind             string             `json:"kind"`
-	State            string             `json:"state"`
-	Node             primitive.ObjectID `json:"node"`
-	Instance         primitive.ObjectID `json:"instance"`
-	PublicIps        []string           `json:"public_ips"`
-	PublicIps6       []string           `json:"public_ips6"`
-	PrivateIps       []string           `json:"private_ips"`
-	PrivateIps6      []string           `json:"private_ips6"`
-	OraclePrivateIps []string           `json:"oracle_private_ips"`
-	OraclePublicIps  []string           `json:"oracle_public_ips"`
+	Id                        primitive.ObjectID `json:"id"`
+	Service                   primitive.ObjectID `json:"service"`
+	Unit                      primitive.ObjectID `json:"unit"`
+	Spec                      primitive.ObjectID `json:"spec"`
+	Kind                      string             `json:"kind"`
+	State                     string             `json:"state"`
+	Node                      primitive.ObjectID `json:"node"`
+	Instance                  primitive.ObjectID `json:"instance"`
+	PublicIps                 []string           `json:"public_ips"`
+	PublicIps6                []string           `json:"public_ips6"`
+	HealthyPublicIps          []string           `json:"healthy_public_ips"`
+	HealthyPublicIps6         []string           `json:"healthy_public_ips6"`
+	UnhealthyPublicIps        []string           `json:"unhealthy_public_ips"`
+	UnhealthyPublicIps6       []string           `json:"unhealthy_public_ips6"`
+	PrivateIps                []string           `json:"private_ips"`
+	PrivateIps6               []string           `json:"private_ips6"`
+	HealthyPrivateIps         []string           `json:"healthy_private_ips"`
+	HealthyPrivateIps6        []string           `json:"healthy_private_ips6"`
+	UnhealthyPrivateIps       []string           `json:"unhealthy_private_ips"`
+	UnhealthyPrivateIps6      []string           `json:"unhealthy_private_ips6"`
+	OraclePublicIps           []string           `json:"oracle_public_ips"`
+	OraclePrivateIps          []string           `json:"oracle_private_ips"`
+	HealthyOraclePublicIps    []string           `json:"healthy_oracle_public_ips"`
+	HealthyOraclePrivateIps   []string           `json:"healthy_oracle_private_ips"`
+	UnhealthyOraclePublicIps  []string           `json:"unhealthy_oracle_public_ips"`
+	UnhealthyOraclePrivateIps []string           `json:"unhealthy_oracle_private_ips"`
 }
 
 func NewServices(services []*service.Service,
@@ -56,62 +83,163 @@ func NewServices(services []*service.Service,
 		units := []*Unit{}
 		for _, srvcUnit := range servc.Units {
 			unit := &Unit{
-				Id:               srvcUnit.Id,
-				Name:             srvcUnit.Name,
-				Kind:             srvcUnit.Kind,
-				Count:            srvcUnit.Count,
-				PublicIps:        []string{},
-				PublicIps6:       []string{},
-				PrivateIps:       []string{},
-				PrivateIps6:      []string{},
-				OraclePrivateIps: []string{},
-				OraclePublicIps:  []string{},
-				Deployments:      []*Deployment{},
+				Id:                        srvcUnit.Id,
+				Name:                      srvcUnit.Name,
+				Kind:                      srvcUnit.Kind,
+				Count:                     srvcUnit.Count,
+				PublicIps:                 []string{},
+				PublicIps6:                []string{},
+				HealthyPublicIps:          []string{},
+				HealthyPublicIps6:         []string{},
+				UnhealthyPublicIps:        []string{},
+				UnhealthyPublicIps6:       []string{},
+				PrivateIps:                []string{},
+				PrivateIps6:               []string{},
+				HealthyPrivateIps:         []string{},
+				HealthyPrivateIps6:        []string{},
+				UnhealthyPrivateIps:       []string{},
+				UnhealthyPrivateIps6:      []string{},
+				OraclePublicIps:           []string{},
+				OraclePrivateIps:          []string{},
+				HealthyOraclePublicIps:    []string{},
+				HealthyOraclePrivateIps:   []string{},
+				UnhealthyOraclePublicIps:  []string{},
+				UnhealthyOraclePrivateIps: []string{},
+				Deployments:               []*Deployment{},
 			}
 
 			for _, unitDeply := range srvcUnit.Deployments {
 				deply := deployments[unitDeply.Id]
 				if deply != nil {
-					if deply.PublicIps != nil {
-						unit.PublicIps = append(
-							unit.PublicIps, deply.PublicIps...)
+					publicIps := deply.PublicIps
+					if publicIps == nil {
+						publicIps = []string{}
 					}
-					if deply.PublicIps6 != nil {
-						unit.PublicIps6 = append(
-							unit.PublicIps6, deply.PublicIps6...)
+					publicIps6 := deply.PublicIps6
+					if publicIps6 == nil {
+						publicIps6 = []string{}
 					}
-					if deply.PrivateIps != nil {
-						unit.PrivateIps = append(
-							unit.PrivateIps, deply.PrivateIps...)
+					healthyPublicIps := []string{}
+					unhealthyPublicIps := []string{}
+					healthyPublicIps6 := []string{}
+					unhealthyPublicIps6 := []string{}
+
+					privateIps := deply.PrivateIps
+					if privateIps == nil {
+						privateIps = []string{}
 					}
-					if deply.PrivateIps6 != nil {
-						unit.PrivateIps6 = append(
-							unit.PrivateIps6, deply.PrivateIps6...)
+					privateIps6 := deply.PrivateIps6
+					if privateIps6 == nil {
+						privateIps6 = []string{}
 					}
-					if deply.OraclePrivateIps != nil {
-						unit.OraclePrivateIps = append(
-							unit.OraclePrivateIps, deply.OraclePrivateIps...)
+					healthyPrivateIps := []string{}
+					unhealthyPrivateIps := []string{}
+					healthyPrivateIps6 := []string{}
+					unhealthyPrivateIps6 := []string{}
+
+					oraclePublicIps := deply.OraclePublicIps
+					if oraclePublicIps == nil {
+						oraclePublicIps = []string{}
 					}
-					if deply.OraclePublicIps != nil {
-						unit.OraclePublicIps = append(
-							unit.OraclePublicIps, deply.OraclePublicIps...)
+					oraclePrivateIps := deply.OraclePrivateIps
+					if oraclePrivateIps == nil {
+						oraclePrivateIps = []string{}
+					}
+					healthyOraclePublicIps := []string{}
+					unhealthyOraclePublicIps := []string{}
+					healthyOraclePrivateIps := []string{}
+					unhealthyOraclePrivateIps := []string{}
+
+					if deply.IsHealthy() {
+						healthyPublicIps = publicIps
+						healthyPublicIps6 = publicIps6
+						healthyPrivateIps = privateIps
+						healthyPrivateIps6 = privateIps6
+						healthyOraclePublicIps = oraclePublicIps
+						healthyOraclePrivateIps = oraclePrivateIps
+					} else {
+						unhealthyPublicIps = publicIps
+						unhealthyPublicIps6 = publicIps6
+						unhealthyPrivateIps = privateIps
+						unhealthyPrivateIps6 = privateIps6
+						unhealthyOraclePublicIps = oraclePublicIps
+						unhealthyOraclePrivateIps = oraclePrivateIps
 					}
 
+					unit.PublicIps = append(
+						unit.PublicIps, publicIps...)
+					unit.PublicIps6 = append(
+						unit.PublicIps6, publicIps6...)
+					unit.HealthyPublicIps = append(
+						unit.HealthyPublicIps, healthyPublicIps...)
+					unit.HealthyPublicIps6 = append(
+						unit.HealthyPublicIps6, healthyPublicIps6...)
+					unit.UnhealthyPublicIps = append(
+						unit.UnhealthyPublicIps, unhealthyPublicIps...)
+					unit.UnhealthyPublicIps6 = append(
+						unit.UnhealthyPublicIps6, unhealthyPublicIps6...)
+
+					unit.PrivateIps = append(
+						unit.PrivateIps, privateIps...)
+					unit.PrivateIps6 = append(
+						unit.PrivateIps6, privateIps6...)
+					unit.HealthyPrivateIps = append(
+						unit.HealthyPrivateIps, healthyPrivateIps...)
+					unit.HealthyPrivateIps6 = append(
+						unit.HealthyPrivateIps6, healthyPrivateIps6...)
+					unit.UnhealthyPrivateIps = append(
+						unit.UnhealthyPrivateIps, unhealthyPrivateIps...)
+					unit.UnhealthyPrivateIps6 = append(
+						unit.UnhealthyPrivateIps6, unhealthyPrivateIps6...)
+
+					unit.OraclePublicIps = append(
+						unit.OraclePublicIps, oraclePublicIps...)
+					unit.HealthyOraclePublicIps = append(
+						unit.HealthyOraclePublicIps,
+						healthyOraclePublicIps...,
+					)
+					unit.UnhealthyOraclePublicIps = append(
+						unit.UnhealthyOraclePublicIps,
+						unhealthyOraclePublicIps...,
+					)
+					unit.OraclePrivateIps = append(
+						unit.OraclePrivateIps, oraclePrivateIps...)
+					unit.HealthyOraclePrivateIps = append(
+						unit.HealthyOraclePrivateIps,
+						healthyOraclePrivateIps...,
+					)
+					unit.UnhealthyOraclePrivateIps = append(
+						unit.UnhealthyOraclePrivateIps,
+						unhealthyOraclePrivateIps...,
+					)
+
 					unit.Deployments = append(unit.Deployments, &Deployment{
-						Id:               deply.Id,
-						Service:          deply.Service,
-						Unit:             deply.Unit,
-						Spec:             deply.Spec,
-						Kind:             deply.Kind,
-						State:            deply.State,
-						Node:             deply.Node,
-						Instance:         deply.Instance,
-						PublicIps:        deply.PublicIps,
-						PublicIps6:       deply.PublicIps6,
-						PrivateIps:       deply.PrivateIps,
-						PrivateIps6:      deply.PrivateIps6,
-						OraclePrivateIps: deply.OraclePrivateIps,
-						OraclePublicIps:  deply.OraclePublicIps,
+						Id:                        deply.Id,
+						Service:                   deply.Service,
+						Unit:                      deply.Unit,
+						Spec:                      deply.Spec,
+						Kind:                      deply.Kind,
+						State:                     deply.State,
+						Node:                      deply.Node,
+						Instance:                  deply.Instance,
+						PublicIps:                 publicIps,
+						PublicIps6:                publicIps6,
+						HealthyPublicIps:          healthyPublicIps,
+						HealthyPublicIps6:         healthyPublicIps6,
+						UnhealthyPublicIps:        unhealthyPublicIps,
+						UnhealthyPublicIps6:       unhealthyPublicIps6,
+						PrivateIps:                privateIps,
+						PrivateIps6:               privateIps6,
+						HealthyPrivateIps:         healthyPrivateIps,
+						HealthyPrivateIps6:        healthyPrivateIps6,
+						UnhealthyPrivateIps:       unhealthyPrivateIps,
+						UnhealthyPrivateIps6:      unhealthyPrivateIps6,
+						OraclePublicIps:           oraclePublicIps,
+						HealthyOraclePublicIps:    healthyOraclePublicIps,
+						UnhealthyOraclePublicIps:  unhealthyOraclePublicIps,
+						OraclePrivateIps:          oraclePrivateIps,
+						HealthyOraclePrivateIps:   healthyOraclePrivateIps,
+						UnhealthyOraclePrivateIps: unhealthyOraclePrivateIps,
 					})
 				}
 			}
