233a52a7cceced543e879d2d6d95eb21c5c2f955
Zachary Huff <zach.huff.386@gmail.com>
2024-11-12 12:17:05 -0500

    Update deployment task rate

--- task/deployments.go
+++ task/deployments.go
@@ -16,7 +16,7 @@ var deployments = &Task{
 		26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
 		39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
 		52, 53, 54, 55, 56, 57, 58, 59},
-	Seconds:    5 * time.Second,
+	Seconds:    3 * time.Second,
 	Handler:    deploymentsHandler,
 	DebugNodes: []string{"web"},
 }
