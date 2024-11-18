d79c7305cc3b3959e5a0e894feac140d3189b84a
Zachary Huff <zach.huff.386@gmail.com>
2024-11-18 16:32:49 -0500

    Fix org in image

--- image/image.go
+++ image/image.go
@@ -20,7 +20,7 @@ type Image struct {
 	Disk         primitive.ObjectID `bson:"disk,omitempty" json:"disk"`
 	Name         string             `bson:"name" json:"name"`
 	Comment      string             `bson:"comment" json:"comment"`
-	Organization primitive.ObjectID `bson:"organization" json:"organization"`
+	Organization primitive.ObjectID `bson:"organization,omitempty" json:"organization"`
 	Signed       bool               `bson:"signed" json:"signed"`
 	Type         string             `bson:"type" json:"type"`
 	Firmware     string             `bson:"firmware" json:"firmware"`
