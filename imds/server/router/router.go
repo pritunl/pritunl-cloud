49134af549bebfbd2f6382f24e2250d8296f526d
Zachary Huff <zach.huff.386@gmail.com>
2024-12-13 06:23:33 -0500

    Add host router to imds server

--- imds/server/router/router.go
+++ imds/server/router/router.go
@@ -3,12 +3,13 @@ package router
 import (
 	"context"
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/dropbox/godropbox/errors"
 	"github.com/gin-gonic/gin"
-	"github.com/pritunl/pritunl-cloud/imds/server/config"
 	"github.com/pritunl/pritunl-cloud/imds/server/constants"
 	"github.com/pritunl/pritunl-cloud/imds/server/errortypes"
 	"github.com/pritunl/pritunl-cloud/imds/server/handlers"
@@ -16,22 +17,67 @@ import (
 )
 
 type Router struct {
-	server *http.Server
+	virtServer *http.Server
+	hostServer *http.Server
 }
 
 func (r *Router) Run() (err error) {
 	logger.WithFields(logger.Fields{
-		"config":  config.Path,
 		"host":    constants.Host,
 		"port":    constants.Port,
+		"sock":    constants.Sock,
 		"version": constants.Version,
 	}).Info("main: Starting imds server")
 
-	err = r.server.ListenAndServe()
-	if err != nil {
-		err = &errortypes.WriteError{
-			errors.Wrap(err, "main: Server listen error"),
+	waiters := &sync.WaitGroup{}
+	waiters.Add(2)
+
+	go func() {
+		defer waiters.Done()
+
+		e := r.virtServer.ListenAndServe()
+		if e != nil {
+			e = &errortypes.WriteError{
+				errors.Wrap(e, "main: Server listen error"),
+			}
+			if err == nil {
+				err = e
+			}
+			r.Shutdown()
+			return
 		}
+	}()
+
+	go func() {
+		defer waiters.Done()
+
+		listener, e := net.Listen("unix", constants.Sock)
+		if e != nil {
+			e = &errortypes.WriteError{
+				errors.Wrap(e, "main: Failed to create unix socket"),
+			}
+			if err == nil {
+				err = e
+			}
+			r.Shutdown()
+			return
+		}
+
+		e = r.hostServer.Serve(listener)
+		if e != nil {
+			e = &errortypes.WriteError{
+				errors.Wrap(e, "main: Server listen error"),
+			}
+			if err == nil {
+				err = e
+			}
+			r.Shutdown()
+			return
+		}
+	}()
+
+	waiters.Wait()
+	if err != nil {
 		return
 	}
 
@@ -49,23 +95,38 @@ func (r *Router) Shutdown() {
 	)
 	defer webCancel()
 
-	_ = r.server.Shutdown(webCtx)
-	_ = r.server.Close()
+	_ = r.virtServer.Shutdown(webCtx)
+	_ = r.virtServer.Close()
+
+	_ = r.hostServer.Shutdown(webCtx)
+	_ = r.hostServer.Close()
 }
 
 func (r *Router) Init() {
 	gin.SetMode(gin.ReleaseMode)
 
-	router := gin.New()
-	handlers.Register(router)
+	virtRouter := gin.New()
+	handlers.RegisterVirt(virtRouter)
 
-	r.server = &http.Server{
+	r.virtServer = &http.Server{
 		Addr: fmt.Sprintf(
 			"%s:%d",
 			constants.Host,
 			constants.Port,
 		),
-		Handler:        router,
+		Handler:        virtRouter,
+		ReadTimeout:    30 * time.Second,
+		WriteTimeout:   30 * time.Second,
+		IdleTimeout:    60 * time.Second,
+		MaxHeaderBytes: 4096,
+	}
+
+	hostRouter := gin.New()
+	handlers.RegisterHost(hostRouter)
+
+	r.hostServer = &http.Server{
+		Addr:           "127.0.0.1:99999",
+		Handler:        hostRouter,
 		ReadTimeout:    30 * time.Second,
 		WriteTimeout:   30 * time.Second,
 		IdleTimeout:    60 * time.Second,
