61219a801893acdd60a870b287636a4e8a0ee7cd
Zachary Huff <zach.huff.386@gmail.com>
2024-12-12 19:10:19 -0500

    Add config to imds types

---
+++ imds/types/config.go
@@ -0,0 +1,28 @@
+package types
+
+import (
+	"github.com/pritunl/pritunl-cloud/utils"
+)
+
+type Config struct {
+	ClientIps    []string       `json:"client_ips"`
+	Instance     *Instance      `json:"instance"`
+	Vpc          *Vpc           `json:"vpc"`
+	Subnet       *Subnet        `json:"subnet"`
+	Certificates []*Certificate `json:"certificates"`
+	Secrets      []*Secret      `json:"secrets"`
+	Services     []*Service     `json:"services"`
+	Hash         uint32         `json:"hash"`
+}
+
+func (c *Config) ComputeHash() (err error) {
+	c.Hash = 0
+
+	confHash, err := utils.CrcHash(c)
+	if err != nil {
+		return
+	}
+
+	c.Hash = confHash
+	return
+}
