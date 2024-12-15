6ea016fecf49b4a72fa8311753fffdc9cf4aa1c7
Zachary Huff <zach.huff.386@gmail.com>
2024-12-14 20:20:09 -0500

    Remove save from imds server state

--- imds/server/state/state.go
+++ imds/server/state/state.go
@@ -1,21 +1,13 @@
 package state
 
 import (
-	"encoding/json"
-	"io/ioutil"
-
-	"github.com/dropbox/godropbox/errors"
-	"github.com/pritunl/pritunl-cloud/imds/server/errortypes"
 	"github.com/pritunl/pritunl-cloud/imds/types"
 )
 
-var (
-	Path   = ""
-	Global = &Store{
-		State:  &types.State{},
-		output: make(chan *types.Entry, 10000),
-	}
-)
+var Global = &Store{
+	State:  &types.State{},
+	output: make(chan *types.Entry, 10000),
+}
 
 type Store struct {
 	State  *types.State
@@ -38,26 +30,6 @@ func (s *Store) GetOutput() (entries []*types.Entry) {
 			return
 		}
 	}
-}
-
-func (s *Store) Save() (err error) {
-	data, err := json.MarshalIndent(s.State, "", "\t")
-	if err != nil {
-		err = &errortypes.WriteError{
-			errors.Wrap(err, "state: File marshal error"),
-		}
-		return
-	}
-
-	err = ioutil.WriteFile(Path, data, 0600)
-	if err != nil {
-		err = &errortypes.WriteError{
-			errors.Wrap(err, "state: File write error"),
-		}
-		return
-	}
-
-	return
 }
 
 func Init() (err error) {
