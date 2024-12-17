0c6d40a7bf7b47ef1ed76721ba162ca2863e4aaf
Zachary Huff <zach.huff.386@gmail.com>
2024-12-17 09:34:16 -0500

    Fix message in instance agent

--- instance/agent.go
+++ instance/agent.go
@@ -10,5 +10,5 @@ type AgentLog struct {
 	Id        primitive.ObjectID `bson:"_id" json:"id"`
 	Instance  primitive.ObjectID `bson:"i" json:"i"`
 	Timestamp time.Time          `bson:"t" json:"t"`
-	Message   []string           `bson:"m" json:"m"`
+	Message   string             `bson:"m" json:"m"`
 }
