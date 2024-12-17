780ec5240e945d472b59842e4fd2ada08ef6cde1
Zachary Huff <zach.huff.386@gmail.com>
2024-12-17 10:03:54 -0500

    Add string to instance agent

--- instance/agent.go
+++ instance/agent.go
@@ -1,6 +1,7 @@
 package instance
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
@@ -12,6 +13,14 @@ type AgentLog struct {
 	Instance  primitive.ObjectID `bson:"i" json:"i"`
 	Timestamp time.Time          `bson:"t" json:"t"`
 	Message   string             `bson:"m" json:"m"`
+}
+
+func (l *AgentLog) String() string {
+	return fmt.Sprintf(
+		"[%s]%s\n",
+		l.Timestamp.Format("Mon Jan _2 15:04:05 2006"),
+		l.Message,
+	)
 }
 
 func (l *AgentLog) Insert(db *database.Database) (err error) {
