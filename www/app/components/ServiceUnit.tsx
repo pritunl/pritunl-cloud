03d71f40f648aaaeb3db75b355ad4b03ee2c82a5
Zachary Huff <zach.huff.386@gmail.com>
2024-12-26 19:29:38 -0500

    Add service deployment to service unit component

--- www/app/components/ServiceUnit.tsx
+++ www/app/components/ServiceUnit.tsx
@@ -8,7 +8,7 @@ import * as ServiceActions from "../actions/ServiceActions"
 import * as MiscUtils from '../utils/MiscUtils';
 import NonState from './NonState';
 import PageInfo from "./PageInfo"
-import Editor from "./Editor"
+import ServiceDeployment from "./ServiceDeployment"
 import * as PageInfos from './PageInfo';
 
 interface Props {
@@ -137,469 +137,57 @@ export default class ServiceUnit extends React.Component<Props, State> {
 		}
 
 		deployments.forEach((deployment: ServiceTypes.Deployment): void => {
-			let cardStyle = css.card
-			if (deployment.state === "archived") {
-				cardStyle = css.cardInactive
-			}
+			cards.push(<ServiceDeployment
+				key={deployment.id}
+				hidden={this.props.hidden}
+				disabled={this.props.disabled}
+				selected={!!this.props.selected[deployment.id]}
+				commitMap={commitMap}
+				deployment={deployment}
+				onSelect={(shift: boolean): void => {
+					let selected = {
+						...this.props.selected,
+					};
 
-			let stateValue = MiscUtils.capitalize(deployment.state) || "-"
-			let stateClass = ""
-			switch (deployment.state) {
-				case "deployed":
-					stateClass = "bp5-text-intent-success"
-					break
-				case "reserved":
-					stateClass = "bp5-text-intent-primary"
-					break
-				case "restore":
-					stateValue = "Restoring"
-					stateClass = "bp5-text-intent-primary"
-					break
-				case "destroy":
-					stateValue = "Destroying"
-					stateClass = "bp5-text-intent-danger"
-					break
-				case "archive":
-					stateValue = "Archiving"
-					stateClass = "bp5-text-intent-warning"
-					break
-				case "archived":
-					stateClass = "bp5-text-intent-warning"
-					break
-			}
+					if (shift) {
+						let deployments = this.props.unit.deployments;
+						let start: number;
+						let end: number;
 
-			let statusClass = ""
-			switch (deployment.instance_status) {
-				case "Running":
-					statusClass = "bp5-text-intent-success"
-					break
-				case "Starting":
-				case "Restarting":
-				case "Updating":
-				case "Provisioning":
-					statusClass = "bp5-text-intent-primary"
-					break
-				case "Failed":
-				case "Stopping":
-				case "Destroying":
-				case "Stopped":
-					statusClass = "bp5-text-intent-danger"
-					break
-			}
+						for (let i = 0; i < deployments.length; i++) {
+							let deply = deployments[i];
 
-			let instData = deployment.instance_data || {}
+							if (deply.id === deployment.id) {
+								start = i;
+							} else if (deply.id === this.props.lastSelected) {
+								end = i;
+							}
+						}
 
-			let publicIps = instData.public_ips
-			if (!publicIps || !publicIps.length) {
-				publicIps = ["-"]
-			}
+						if (start !== undefined && end !== undefined) {
+							if (start > end) {
+								end = [start, start = end][0];
+							}
 
-			let publicIps6 = instData.public_ips6
-			if (!publicIps6 || !publicIps6.length) {
-				publicIps6 = ["-"]
-			}
+							for (let i = start; i <= end; i++) {
+								selected[deployments[i].id] = true;
+							}
 
-			let privateIps = instData.private_ips
-			if (!privateIps || !privateIps.length) {
-				privateIps = ["-"]
-			}
+							this.props.onSelect(selected, deployment.id)
 
-			let privateIps6 = instData.private_ips6
-			if (!privateIps6 || !privateIps6.length) {
-				privateIps6 = ["-"]
-			}
+							return;
+						}
+					}
 
-			let commitIndex = commitMap[deployment.spec]
-			let commitClass = commitIndex === 0 ? "bp5-text-intent-success" :
-				"bp5-text-intent-danger"
+					if (selected[deployment.id]) {
+						delete selected[deployment.id];
+					} else {
+						selected[deployment.id] = true;
+					}
 
-				let specHover = <div
-				className="bp5-content-popover"
-				style={css.specHover}
-			>
-				<PageInfo
-					compact={true}
-					style={css.info}
-					fields={[
-						{
-							label: "Commit",
-							value: deployment.spec.substring(0, 24) || "-",
-						},
-						{
-							label: "Tag",
-							value: "1.2.5823.344",
-						},
-						{
-							label: "Behind",
-							value: commitIndex,
-							valueClass: commitClass,
-						},
-					]}
-				/>
-			</div>
-
-			let deplyStatus = MiscUtils.capitalize(deployment.status) || "-"
-			let heartbeatClass = ""
-			if (deployment.status === "healthy") {
-				heartbeatClass = "bp5-text-intent-success"
-			} else {
-				heartbeatClass = "bp5-text-intent-danger"
-			}
-
-			let heartbeatHover = <div
-				className="bp5-content-popover"
-				style={css.specHover}
-			>
-				<PageInfo
-					compact={true}
-					style={css.info}
-					fields={[
-						{
-							label: "Status",
-							value: deplyStatus,
-							valueClass: heartbeatClass,
-						},
-						{
-							label: "Heartbeat Timestamp",
-							value: MiscUtils.formatDateLocal(
-								deployment.instance_heartbeat) || "-",
-							valueClass: heartbeatClass,
-						},
-					]}
-				/>
-			</div>
-
-			let resourceBars: PageInfos.Bar[] = []
-			resourceBars.push({
-				label: "Instance Resources",
-				progressClass: 'bp5-no-stripes bp5-intent-success',
-				value: deployment.instance_load1 || 0,
-			})
-			resourceBars.push({
-				progressClass: 'bp5-no-stripes bp5-intent-warning',
-				value: deployment.instance_load5 || 0,
-			})
-			resourceBars.push({
-				progressClass: 'bp5-no-stripes bp5-intent-danger',
-				value: deployment.instance_load15 || 0,
-			})
-			resourceBars.push({
-				progressClass: 'bp5-no-stripes bp5-intent-primary',
-				value: deployment.instance_memory_usage || 0,
-			})
-
-			if (deployment.kind === "image" && deployment.image_id) {
-				cards.push(<Blueprint.Card
-					key={deployment.id}
-					compact={true}
-					style={cardStyle}
-				>
-					<div className="layout horizontal flex">
-						<div className="layout center" style={css.checkBox}>
-							<Blueprint.Checkbox
-								style={css.check}
-								checked={!!this.props.selected[deployment.id]}
-								onClick={(evt): void => {
-									let selected = {
-										...this.props.selected,
-									};
-
-									if (evt.shiftKey) {
-										let deployments = this.props.unit.deployments;
-										let start: number;
-										let end: number;
-
-										for (let i = 0; i < deployments.length; i++) {
-											let deply = deployments[i];
-
-											if (deply.id === deployment.id) {
-												start = i;
-											} else if (deply.id === this.props.lastSelected) {
-												end = i;
-											}
-										}
-
-										if (start !== undefined && end !== undefined) {
-											if (start > end) {
-												end = [start, start = end][0];
-											}
-
-											for (let i = start; i <= end; i++) {
-												selected[deployments[i].id] = true;
-											}
-
-											this.props.onSelect(selected, deployment.id)
-
-											return;
-										}
-									}
-
-									if (selected[deployment.id]) {
-										delete selected[deployment.id];
-									} else {
-										selected[deployment.id] = true;
-									}
-
-									this.props.onSelect(selected, deployment.id)
-								}}
-							/>
-						</div>
-						<div style={css.itemFirst}>
-							<PageInfo
-								compact={true}
-								style={css.info}
-								fields={[
-									{
-										label: "Image ID",
-										value: deployment.id,
-									},
-								]}
-							/>
-						</div>
-						<div style={css.item}>
-							<PageInfo
-								compact={true}
-								style={css.info}
-								fields={[
-									{
-										label: "Commit ID",
-										value: deployment.spec.substring(0, 12),
-										hover: specHover,
-										valueClass: commitClass,
-									},
-								]}
-							/>
-						</div>
-						<div style={css.item}>
-							<PageInfo
-								compact={true}
-								style={css.info}
-								fields={[
-									{
-										label: "Image ID",
-										value: deployment.image_id || "-",
-									},
-								]}
-							/>
-						</div>
-						<div style={css.item}>
-							<PageInfo
-								compact={true}
-								style={css.info}
-								fields={[
-									{
-										label: "Image Name",
-										value: deployment.image_name || "-",
-									},
-								]}
-							/>
-						</div>
-						<div style={css.item}>
-							<PageInfo
-								compact={true}
-								style={css.info}
-								fields={[
-									{
-										label: "Timestamp",
-										value: MiscUtils.formatDateLocal(
-											deployment.instance_heartbeat) || "-",
-									},
-								]}
-							/>
-						</div>
-					</div>
-				</Blueprint.Card>)
-			} else {
-				cards.push(<Blueprint.Card
-					key={deployment.id}
-					compact={true}
-					style={cardStyle}
-				>
-					<div className="layout vertical flex">
-						<div className="layout horizontal flex">
-							<div className="layout center" style={css.checkBox}>
-								<Blueprint.Checkbox
-									style={css.check}
-									checked={!!this.props.selected[deployment.id]}
-									onClick={(evt): void => {
-										let selected = {
-											...this.props.selected,
-										};
-
-										if (evt.shiftKey) {
-											let deployments = this.props.unit.deployments;
-											let start: number;
-											let end: number;
-
-											for (let i = 0; i < deployments.length; i++) {
-												let deply = deployments[i];
-
-												if (deply.id === deployment.id) {
-													start = i;
-												} else if (deply.id === this.props.lastSelected) {
-													end = i;
-												}
-											}
-
-											if (start !== undefined && end !== undefined) {
-												if (start > end) {
-													end = [start, start = end][0];
-												}
-
-												for (let i = start; i <= end; i++) {
-													selected[deployments[i].id] = true;
-												}
-
-												this.props.onSelect(selected, deployment.id)
-
-												return;
-											}
-										}
-
-										if (selected[deployment.id]) {
-											delete selected[deployment.id];
-										} else {
-											selected[deployment.id] = true;
-										}
-
-										this.props.onSelect(selected, deployment.id)
-									}}
-								/>
-							</div>
-							<div style={css.itemFirst}>
-								<PageInfo
-									compact={true}
-									style={css.info}
-									fields={[
-										{
-											label: "Deployment ID",
-											value: deployment.id,
-										},
-										{
-											label: "Commit ID",
-											value: deployment.spec.substring(0, 12),
-											hover: specHover,
-											valueClass: commitClass,
-										},
-									]}
-								/>
-								<button
-									className="bp5-button bp5-small"
-									style={css.cardButton}
-								>Logs</button>
-							</div>
-							<div style={css.item}>
-								<PageInfo
-									compact={true}
-									style={css.info}
-									fields={[
-										{
-											label: "Zone",
-											value: deployment.zone_name || "-",
-										},
-										{
-											label: "Node",
-											value: deployment.node_name || "-",
-										},
-										{
-											label: "Instance",
-											value: deployment.instance_name || "-",
-										},
-									]}
-								/>
-							</div>
-							<div style={css.item}>
-								<PageInfo
-									compact={true}
-									style={css.info}
-									fields={[
-										{
-											label: "State",
-											value: stateValue,
-											valueClass: stateClass,
-										},
-										{
-											label: "Status",
-											value: deployment.instance_status || "-",
-											valueClass: statusClass,
-										},
-									]}
-								/>
-							</div>
-							<div style={css.item}>
-								<PageInfo
-									compact={true}
-									style={css.info}
-									fields={[
-										{
-											label: "Agent Status",
-											value: MiscUtils.capitalize(
-												deployment.instance_guest_status) || "-",
-											valueClass: heartbeatClass,
-										},
-										{
-											label: "Last Heartbeat",
-											value: MiscUtils.formatSinceLocal(
-												deployment.instance_heartbeat) || "-",
-											hover: heartbeatHover,
-											valueClass: heartbeatClass,
-										},
-										{
-											label: "Uptime",
-											value: deployment.instance_uptime || "-",
-										},
-									]}
-								/>
-							</div>
-							<div style={css.item}>
-								<PageInfo
-									compact={true}
-									style={css.info}
-									fields={[
-										{
-											label: 'Public IPv4',
-											value: publicIps,
-										},
-										{
-											label: 'Private IPv4',
-											value: privateIps,
-										},
-									]}
-								/>
-							</div>
-							<div style={css.item}>
-								<PageInfo
-									compact={true}
-									style={css.info}
-									fields={[
-										{
-											label: 'Public IPv6',
-											value: publicIps6,
-										},
-										{
-											label: 'Private IPv6',
-											value: privateIps6,
-										},
-									]}
-								/>
-							</div>
-							<div style={css.itemLast}>
-								<PageInfo
-									compact={true}
-									style={css.info}
-									bars={resourceBars}
-								/>
-							</div>
-						</div>
-						<div className="layout horizontal flex">
-							<Editor
-								value="test"
-								height="500px"
-							/>
-						</div>
-					</div>
-				</Blueprint.Card>)
-			}
+					this.props.onSelect(selected, deployment.id)
+				}}
+			/>)
 		})
 
 		return <div className="layout horizontal wrap" style={css.container}>
