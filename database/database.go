2f678f79936f3fd397e91b76fa096e0563aaf788
Zachary Huff <zach.huff.386@gmail.com>
2024-12-31 18:52:07 -0500

    Remove instance agent from database

--- database/database.go
+++ database/database.go
@@ -228,16 +228,6 @@ func (d *Database) Instances() (coll *Collection) {
 	return
 }
 
-func (d *Database) InstancesAgent() (coll *Collection) {
-	coll = d.getCollection("instances_agent")
-	return
-}
-
-func (d *Database) InstancesKmsg() (coll *Collection) {
-	coll = d.getCollection("instances_kmsg")
-	return
-}
-
 func (d *Database) Pools() (coll *Collection) {
 	coll = d.getCollection("pools")
 	return
