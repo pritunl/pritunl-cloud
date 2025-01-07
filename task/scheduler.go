ec43db7cee49f6a4345b12b191a7d51b67f4213c
Zachary Huff <zach.huff.386@gmail.com>
2025-01-06 19:43:30 -0500

    Fix task scheduler test

--- task/scheduler.go
+++ task/scheduler.go
@@ -20,9 +20,8 @@ var schedule = &Task{
 		26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
 		39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
 		52, 53, 54, 55, 56, 57, 58, 59},
-	Seconds:    5 * time.Second,
-	Handler:    scheduleHandler,
-	DebugNodes: []string{"web"},
+	Seconds: 5 * time.Second,
+	Handler: scheduleHandler,
 }
 
 func scheduleUnits(db *database.Database) (err error) {
