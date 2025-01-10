357d8a3ec5e64aa0d6eac54f7d840dccc777111f
Zachary Huff <zach.huff.386@gmail.com>
2025-01-10 17:07:31 -0500

    Add completion types

---
+++ www/app/types/CompletionTypes.ts
@@ -0,0 +1,49 @@
+/// <reference path="../References.d.ts"/>
+
+import * as OrganizationTypes from "./OrganizationTypes"
+import * as DomainTypes from "./DomainTypes"
+import * as VpcTypes from "./VpcTypes"
+import * as DatacenterTypes from "./DatacenterTypes"
+import * as NodeTypes from "./NodeTypes"
+import * as PoolTypes from "./PoolTypes"
+import * as ZoneTypes from "./ZoneTypes"
+import * as ShapeTypes from "./ShapeTypes"
+import * as ImageTypes from "./ImageTypes"
+import * as InstanceTypes from "./InstanceTypes"
+import * as PlanTypes from "./PlanTypes"
+import * as CertificateTypes from "./CertificateTypes"
+import * as SecretTypes from "./SecretTypes"
+import * as PodTypes from "./PodTypes"
+
+export const SYNC = "completion.sync"
+export const FILTER = "completion.filter"
+export const CHANGE = "completion.change"
+
+export interface Completion {
+	organizations?: OrganizationTypes.Organization[]
+	domains?: DomainTypes.Domain[]
+	vpcs?: VpcTypes.Vpc[]
+	datacenters?: DatacenterTypes.Datacenter[]
+	nodes?: NodeTypes.Node[]
+	pools?: PoolTypes.Pool[]
+	zones?: ZoneTypes.Zone[]
+	shapes?: ShapeTypes.Shape[]
+	images?: ImageTypes.Image[]
+	instances?: InstanceTypes.Instance[]
+	plans?: PlanTypes.Plan[]
+	certificates?: CertificateTypes.Certificate[]
+	secrets?: SecretTypes.Secret[]
+	pods?: PodTypes.Pod[]
+	units?: PodTypes.Unit[]
+}
+
+export interface Filter {
+}
+
+export interface CompletionDispatch {
+	type: string
+	data?: {
+		completion?: Completion
+		filter?: Filter
+	}
+}
