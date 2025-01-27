715f39960a8a94d906f65de2fec3c718c0e22bc3
Zachary Huff <zach.huff.386@gmail.com>
2025-01-27 08:59:35 -0500

    Add validate to spec domain

--- spec/domain.go
+++ spec/domain.go
@@ -1,12 +1,48 @@
 package spec
 
+import (
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/utils"
+)
+
 type Domain struct {
 	Records []*Record `bson:"record" json:"record"`
 }
 
+func (d *Domain) Validate() (errData *errortypes.ErrorData, err error) {
+	for _, rec := range d.Records {
+		rec.Name = utils.FilterDomain(rec.Name)
+
+		switch rec.Type {
+		case Private:
+			break
+		case Private6:
+			break
+		case Public:
+			break
+		case Public6:
+			break
+		case OraclePublic:
+			break
+		case OraclePrivate:
+			break
+		default:
+			errData = &errortypes.ErrorData{
+				Error:   "unknown_domain_record_type",
+				Message: "Unknown domain record type",
+			}
+			return
+		}
+	}
+
+	return
+}
+
 type Record struct {
-	Name   string      `bson:"name" json:"name"`
-	Values []*Refrence `bson:"values" json:"values"`
+	Name   string             `bson:"name" json:"name"`
+	Domain primitive.ObjectID `bson:"domain" json:"domain"`
+	Type   string             `bson:"type" json:"type"`
 }
 
 type DomainYaml struct {
@@ -16,6 +52,7 @@ type DomainYaml struct {
 }
 
 type DomainYamlRecord struct {
-	Name  string `yaml:"name"`
-	Value string `yaml:"value"`
+	Name   string `yaml:"name"`
+	Domain string `yaml:"domain"`
+	Type   string `yaml:"type"`
 }
