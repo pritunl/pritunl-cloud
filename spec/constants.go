b01a30263b9cca306db3ddf679c98ac5d8122e9a
Zachary Huff <zach.huff.386@gmail.com>
2025-01-27 04:06:25 -0500

    Add protocols to spec constants

--- spec/constants.go
+++ spec/constants.go
@@ -6,6 +6,15 @@ import (
 
 var resourcesRe = regexp.MustCompile("(?s)```yaml(.*?)```")
 
+const (
+	All       = "all"
+	Icmp      = "icmp"
+	Tcp       = "tcp"
+	Udp       = "udp"
+	Multicast = "multicast"
+	Broadcast = "broadcast"
+)
+
 type Base struct {
 	Kind string `yaml:"kind"`
 }
