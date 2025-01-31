a2a892262d1076b3a60996027779309e2482f439
Zachary Huff <zach.huff.386@gmail.com>
2025-01-31 14:46:29 -0500

    Add finder constants

---
+++ finder/constants.go
@@ -0,0 +1,21 @@
+package finder
+
+const (
+	DomainKind      = "domain"
+	VpcKind         = "vpc"
+	SubnetKind      = "subnet"
+	DatacenterKind  = "datacenter"
+	NodeKind        = "node"
+	PoolKind        = "pool"
+	ZoneKind        = "zone"
+	ShapeKind       = "shape"
+	DiskKind        = "disk"
+	ImageKind       = "image"
+	InstanceKind    = "instance"
+	FirewallKind    = "firewall"
+	PlanKind        = "plan"
+	CertificateKind = "certificate"
+	SecretKind      = "secret"
+	PodKind         = "pod"
+	UnitKind        = "unit"
+)
