b1a22a1e5dd113978d5baefffb5da1c24af641a2
Zachary Huff <zach.huff.386@gmail.com>
2025-02-04 05:13:08 -0500

    Add storage detailed component

---
+++ www/app/components/StorageDetailed.tsx
@@ -0,0 +1,377 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as StorageTypes from '../types/StorageTypes';
+import * as StorageActions from '../actions/StorageActions';
+import PageInput from './PageInput';
+import PageInfo from './PageInfo';
+import PageSave from './PageSave';
+import PageSelect from './PageSelect';
+import PageSwitch from './PageSwitch';
+import ConfirmButton from './ConfirmButton';
+import * as Alert from "../Alert";
+import PageTextArea from "./PageTextArea";
+
+interface Props {
+	storage: StorageTypes.StorageRo;
+	selected: boolean;
+	onSelect: (shift: boolean) => void;
+	onClose: () => void;
+}
+
+interface State {
+	disabled: boolean;
+	changed: boolean;
+	message: string;
+	storage: StorageTypes.Storage;
+}
+
+const css = {
+	card: {
+		position: 'relative',
+		padding: '48px 10px 0 10px',
+		width: '100%',
+	} as React.CSSProperties,
+	remove: {
+		position: 'absolute',
+		top: '5px',
+		right: '5px',
+	} as React.CSSProperties,
+	item: {
+		margin: '9px 5px 0 5px',
+		height: '20px',
+	} as React.CSSProperties,
+	itemsLabel: {
+		display: 'block',
+	} as React.CSSProperties,
+	itemsAdd: {
+		margin: '8px 0 15px 0',
+	} as React.CSSProperties,
+	group: {
+		flex: 1,
+		minWidth: '280px',
+		margin: '0 10px',
+	} as React.CSSProperties,
+	save: {
+		paddingBottom: '10px',
+	} as React.CSSProperties,
+	label: {
+		width: '100%',
+		maxWidth: '280px',
+	} as React.CSSProperties,
+	inputGroup: {
+		width: '100%',
+	} as React.CSSProperties,
+	protocol: {
+		flex: '0 1 auto',
+	} as React.CSSProperties,
+	port: {
+		flex: '1',
+	} as React.CSSProperties,
+	controlButton: {
+		marginRight: '10px',
+	} as React.CSSProperties,
+	button: {
+		height: '30px',
+	} as React.CSSProperties,
+	buttons: {
+		cursor: 'pointer',
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		padding: '4px',
+		height: '39px',
+		backgroundColor: 'rgba(0, 0, 0, 0.13)',
+	} as React.CSSProperties,
+	select: {
+		margin: '7px 0px 0px 6px',
+		paddingTop: '3px',
+	} as React.CSSProperties,
+};
+
+export default class StorageDetailed extends React.Component<Props, State> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			disabled: false,
+			changed: false,
+			message: '',
+			storage: null,
+		};
+	}
+
+	set(name: string, val: any): void {
+		let storage: any;
+
+		if (this.state.changed) {
+			storage = {
+				...this.state.storage,
+			};
+		} else {
+			storage = {
+				...this.props.storage,
+			};
+		}
+
+		storage[name] = val;
+
+		this.setState({
+			...this.state,
+			changed: true,
+			storage: storage,
+		});
+	}
+
+	onSave = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		StorageActions.commit(this.state.storage).then((): void => {
+			this.setState({
+				...this.state,
+				message: 'Your changes have been saved',
+				changed: false,
+				disabled: false,
+			});
+
+			setTimeout((): void => {
+				if (!this.state.changed) {
+					this.setState({
+						...this.state,
+						message: '',
+						changed: false,
+						storage: null,
+					});
+				}
+			}, 3000);
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				message: '',
+				disabled: false,
+			});
+		});
+	}
+
+	onSync = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		StorageActions.commit(this.props.storage).then((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+
+			Alert.success('Storage sync started');
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+		});
+	}
+
+	onDelete = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		StorageActions.remove(this.props.storage.id).then((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
+		});
+	}
+
+	render(): JSX.Element {
+		let storage: StorageTypes.Storage = this.state.storage ||
+			this.props.storage;
+
+		return <td
+			className="bp5-cell"
+			colSpan={2}
+			style={css.card}
+		>
+			<div className="layout horizontal wrap">
+				<div style={css.group}>
+					<div
+						className="layout horizontal tab-close"
+						style={css.buttons}
+						onClick={(evt): void => {
+							let target = evt.target as HTMLElement;
+
+							if (target.className.indexOf('tab-close') !== -1) {
+								this.props.onClose();
+							}
+						}}
+					>
+						<div>
+							<label
+								className="bp5-control bp5-checkbox"
+								style={css.select}
+							>
+								<input
+									type="checkbox"
+									checked={this.props.selected}
+									onChange={(evt): void => {
+									}}
+									onClick={(evt): void => {
+										this.props.onSelect(evt.shiftKey);
+									}}
+								/>
+								<span className="bp5-control-indicator"/>
+							</label>
+						</div>
+						<div className="flex tab-close"/>
+						<ConfirmButton
+							className="bp5-minimal bp5-intent-danger bp5-icon-trash"
+							style={css.button}
+							safe={true}
+							progressClassName="bp5-intent-danger"
+							dialogClassName="bp5-intent-danger bp5-icon-delete"
+							dialogLabel="Delete Storage"
+							confirmMsg="Permanently delete this storage"
+							confirmInput={true}
+							items={[storage.name]}
+							disabled={this.state.disabled}
+							onConfirm={this.onDelete}
+						/>
+					</div>
+					<PageInput
+						disabled={this.state.disabled}
+						label="Name"
+						help="Name of storage"
+						type="text"
+						placeholder="Enter name"
+						value={storage.name}
+						onChange={(val): void => {
+							this.set('name', val);
+						}}
+					/>
+					<PageTextArea
+						label="Comment"
+						help="Storage comment."
+						placeholder="Storage comment"
+						rows={3}
+						value={storage.comment}
+						onChange={(val: string): void => {
+							this.set('comment', val);
+						}}
+					/>
+					<PageInput
+						disabled={this.state.disabled}
+						label="Endpoint"
+						help="Storage endpoint domain and port"
+						type="text"
+						placeholder="Enter endpoint"
+						value={storage.endpoint}
+						onChange={(val): void => {
+							this.set('endpoint', val);
+						}}
+					/>
+					<PageInput
+						disabled={this.state.disabled}
+						label="Bucket"
+						help="Storage bucket name"
+						type="text"
+						placeholder="Enter bucket"
+						value={storage.bucket}
+						onChange={(val): void => {
+							this.set('bucket', val);
+						}}
+					/>
+				</div>
+				<div style={css.group}>
+					<PageInfo
+						fields={[
+							{
+								label: 'ID',
+								value: this.props.storage.id || 'None',
+							},
+						]}
+					/>
+					<PageSelect
+						disabled={this.state.disabled}
+						label="Type"
+						help="Select public for read only storages with virtual machine images. Select private for read-write storages for snapshots."
+						value={storage.type}
+						onChange={(val): void => {
+							this.set('type', val);
+						}}
+					>
+						<option value="public">Public</option>
+						<option value="private">Private</option>
+						<option value="web">Web</option>
+					</PageSelect>
+					<PageInput
+						disabled={this.state.disabled}
+						label="Access Key"
+						help="Storage access key"
+						type="text"
+						placeholder="Enter access key"
+						value={storage.access_key}
+						onChange={(val): void => {
+							this.set('access_key', val);
+						}}
+					/>
+					<PageInput
+						disabled={this.state.disabled}
+						label="Secret Key"
+						help="Storage secret key"
+						type="text"
+						placeholder="Enter secret key"
+						value={storage.secret_key}
+						onChange={(val): void => {
+							this.set('secret_key', val);
+						}}
+					/>
+					<PageSwitch
+						label="SSL Connection"
+						help="Use secure SSL connection."
+						disabled={this.state.disabled}
+						checked={!storage.insecure}
+						onToggle={(): void => {
+							this.set('insecure', !storage.insecure);
+						}}
+					/>
+				</div>
+			</div>
+			<PageSave
+				style={css.save}
+				hidden={!this.state.storage}
+				message={this.state.message}
+				changed={this.state.changed}
+				disabled={this.state.disabled}
+				light={true}
+				onCancel={(): void => {
+					this.setState({
+						...this.state,
+						changed: false,
+						storage: null,
+					});
+				}}
+				onSave={this.onSave}
+			>
+				<ConfirmButton
+					label="Sync"
+					className="bp5-intent-success bp5-icon-refresh"
+					progressClassName="bp5-intent-success"
+					style={css.controlButton}
+					disabled={this.state.disabled}
+					onConfirm={(): void => {
+						this.onSync();
+					}}
+				/>
+			</PageSave>
+		</td>;
+	}
+}
