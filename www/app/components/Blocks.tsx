d814e69d20e6e08e6c9516bb4629b02d676c4ae9
Zachary Huff <zach.huff.386@gmail.com>
2025-02-06 14:25:25 -0500

    Add select to blocks component

--- www/app/components/Blocks.tsx
+++ www/app/components/Blocks.tsx
@@ -3,17 +3,48 @@ import * as React from 'react';
 import * as BlockTypes from '../types/BlockTypes';
 import BlocksStore from '../stores/BlocksStore';
 import * as BlockActions from '../actions/BlockActions';
-import NonState from './NonState';
 import Block from './Block';
+import BlockNew from './BlockNew';
+import BlocksFilter from './BlocksFilter';
+import BlocksPage from './BlocksPage';
 import Page from './Page';
 import PageHeader from './PageHeader';
+import NonState from './NonState';
+import ConfirmButton from './ConfirmButton';
+
+interface Selected {
+	[key: string]: boolean;
+}
+
+interface Opened {
+	[key: string]: boolean;
+}
 
 interface State {
 	blocks: BlockTypes.BlocksRo;
+	filter: BlockTypes.Filter;
+	selected: Selected;
+	opened: Opened;
+	newOpened: boolean;
+	lastSelected: string;
 	disabled: boolean;
 }
 
 const css = {
+	items: {
+		width: '100%',
+		marginTop: '-5px',
+		display: 'table',
+		borderSpacing: '0 5px',
+	} as React.CSSProperties,
+	itemsBox: {
+		width: '100%',
+		overflowY: 'auto',
+	} as React.CSSProperties,
+	placeholder: {
+		opacity: 0,
+		width: '100%',
+	} as React.CSSProperties,
 	header: {
 		marginTop: '-19px',
 	} as React.CSSProperties,
@@ -26,9 +57,6 @@ const css = {
 	buttons: {
 		marginTop: '8px',
 	} as React.CSSProperties,
-	noCerts: {
-		height: 'auto',
-	} as React.CSSProperties,
 };
 
 export default class Blocks extends React.Component<{}, State> {
@@ -36,8 +64,21 @@ export default class Blocks extends React.Component<{}, State> {
 		super(props, context);
 		this.state = {
 			blocks: BlocksStore.blocks,
+			filter: BlocksStore.filter,
+			selected: {},
+			opened: {},
+			newOpened: false,
+			lastSelected: null,
 			disabled: false,
 		};
+	}
+
+	get selected(): boolean {
+		return !!Object.keys(this.state.selected).length;
+	}
+
+	get opened(): boolean {
+		return !!Object.keys(this.state.opened).length;
 	}
 
 	componentDidMount(): void {
@@ -50,9 +91,47 @@ export default class Blocks extends React.Component<{}, State> {
 	}
 
 	onChange = (): void => {
+		let blocks = BlocksStore.blocks;
+		let selected: Selected = {};
+		let curSelected = this.state.selected;
+		let opened: Opened = {};
+		let curOpened = this.state.opened;
+
+		blocks.forEach((block: BlockTypes.Block): void => {
+			if (curSelected[block.id]) {
+				selected[block.id] = true;
+			}
+			if (curOpened[block.id]) {
+				opened[block.id] = true;
+			}
+		});
+
 		this.setState({
 			...this.state,
-			blocks: BlocksStore.blocks,
+			blocks: blocks,
+			filter: BlocksStore.filter,
+			selected: selected,
+			opened: opened,
+		});
+	}
+
+	onDelete = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		});
+		BlockActions.removeMulti(
+				Object.keys(this.state.selected)).then((): void => {
+			this.setState({
+				...this.state,
+				selected: {},
+				disabled: false,
+			});
+		}).catch((): void => {
+			this.setState({
+				...this.state,
+				disabled: false,
+			});
 		});
 	}
 
@@ -64,8 +143,104 @@ export default class Blocks extends React.Component<{}, State> {
 			blocksDom.push(<Block
 				key={block.id}
 				block={block}
+				selected={!!this.state.selected[block.id]}
+				open={!!this.state.opened[block.id]}
+				onSelect={(shift: boolean): void => {
+					let selected = {
+						...this.state.selected,
+					};
+
+					if (shift) {
+						let blocks = this.state.blocks;
+						let start: number;
+						let end: number;
+
+						for (let i = 0; i < blocks.length; i++) {
+							let usr = blocks[i];
+
+							if (usr.id === block.id) {
+								start = i;
+							} else if (usr.id === this.state.lastSelected) {
+								end = i;
+							}
+						}
+
+						if (start !== undefined && end !== undefined) {
+							if (start > end) {
+								end = [start, start = end][0];
+							}
+
+							for (let i = start; i <= end; i++) {
+								selected[blocks[i].id] = true;
+							}
+
+							this.setState({
+								...this.state,
+								lastSelected: block.id,
+								selected: selected,
+							});
+
+							return;
+						}
+					}
+
+					if (selected[block.id]) {
+						delete selected[block.id];
+					} else {
+						selected[block.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						lastSelected: block.id,
+						selected: selected,
+					});
+				}}
+				onOpen={(): void => {
+					let opened = {
+						...this.state.opened,
+					};
+
+					if (opened[block.id]) {
+						delete opened[block.id];
+					} else {
+						opened[block.id] = true;
+					}
+
+					this.setState({
+						...this.state,
+						opened: opened,
+					});
+				}}
 			/>);
 		});
+
+		let filterClass = 'bp5-button bp5-intent-primary bp5-icon-filter ';
+		if (this.state.filter) {
+			filterClass += 'bp5-active';
+		}
+
+		let selectedNames: string[] = [];
+		for (let instId of Object.keys(this.state.selected)) {
+			let inst = BlocksStore.block(instId);
+			if (inst) {
+				selectedNames.push(inst.name || instId);
+			} else {
+				selectedNames.push(instId);
+			}
+		}
+
+		let newBlockDom: JSX.Element;
+		if (this.state.newOpened) {
+			newBlockDom = <BlockNew
+				onClose={(): void => {
+					this.setState({
+						...this.state,
+						newOpened: false,
+					});
+				}}
+			/>;
+		}
 
 		return <Page>
 			<PageHeader>
@@ -74,39 +249,89 @@ export default class Blocks extends React.Component<{}, State> {
 					<div className="flex"/>
 					<div style={css.buttons}>
 						<button
-							className="bp5-button bp5-intent-success bp5-icon-add"
+							className={filterClass}
 							style={css.button}
-							disabled={this.state.disabled}
+							type="button"
+							onClick={(): void => {
+								if (this.state.filter === null) {
+									BlockActions.filter({});
+								} else {
+									BlockActions.filter(null);
+								}
+							}}
+						>
+							Filters
+						</button>
+						<button
+							className="bp5-button bp5-intent-warning bp5-icon-chevron-up"
+							style={css.button}
+							disabled={!this.opened}
 							type="button"
 							onClick={(): void => {
 								this.setState({
 									...this.state,
-									disabled: true,
+									opened: {},
 								});
-								BlockActions.create(null).then((): void => {
-									this.setState({
-										...this.state,
-										disabled: false,
-									});
-								}).catch((): void => {
-									this.setState({
-										...this.state,
-										disabled: false,
-									});
+							}}
+						>
+							Collapse All
+						</button>
+						<ConfirmButton
+							label="Delete Selected"
+							className="bp5-intent-danger bp5-icon-delete"
+							progressClassName="bp5-intent-danger"
+							safe={true}
+							style={css.button}
+							confirmMsg="Permanently delete the selected blocks"
+							confirmInput={true}
+							items={selectedNames}
+							disabled={!this.selected || this.state.disabled}
+							onConfirm={this.onDelete}
+						/>
+						<button
+							className="bp5-button bp5-intent-success bp5-icon-add"
+							style={css.button}
+							disabled={this.state.disabled || this.state.newOpened}
+							type="button"
+							onClick={(): void => {
+								this.setState({
+									...this.state,
+									newOpened: true,
 								});
 							}}
 						>New</button>
 					</div>
 				</div>
 			</PageHeader>
-			<div>
-				{blocksDom}
+			<BlocksFilter
+				filter={this.state.filter}
+				onFilter={(filter): void => {
+					BlockActions.filter(filter);
+				}}
+			/>
+			<div style={css.itemsBox}>
+				<div style={css.items}>
+					{newBlockDom}
+					{blocksDom}
+					<tr className="bp5-card bp5-row" style={css.placeholder}>
+						<td colSpan={2} style={css.placeholder}/>
+					</tr>
+				</div>
 			</div>
 			<NonState
 				hidden={!!blocksDom.length}
 				iconClass="bp5-icon-ip-address"
-				title="No IP blocks"
-				description="Add a new IP block to get started."
+				title="No blocks"
+				description="Add a new block to get started."
+			/>
+			<BlocksPage
+				onPage={(): void => {
+					this.setState({
+						...this.state,
+						selected: {},
+						lastSelected: null,
+					});
+				}}
 			/>
 		</Page>;
 	}
