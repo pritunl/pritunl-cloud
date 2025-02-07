7f3985a18992bec9da129078965f194ec84d5e45
Zachary Huff <zach.huff.386@gmail.com>
2025-02-07 17:56:34 -0500

    Add zone detailed to zone component

--- www/app/components/Zone.tsx
+++ www/app/components/Zone.tsx
@@ -1,249 +1,151 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
+import * as MiscUtils from '../utils/MiscUtils';
 import * as ZoneTypes from '../types/ZoneTypes';
-import * as ZoneActions from '../actions/ZoneActions';
+import * as DatacenterTypes from '../types/DatacenterTypes';
+import ZoneDetailed from './ZoneDetailed';
 import DatacentersStore from '../stores/DatacentersStore';
-import PageInput from './PageInput';
-import PageInfo from './PageInfo';
-import PageSave from './PageSave';
-import PageSelect from './PageSelect';
-import ConfirmButton from './ConfirmButton';
-import PageTextArea from "./PageTextArea";
 
 interface Props {
 	zone: ZoneTypes.ZoneRo;
-}
-
-interface State {
-	disabled: boolean;
-	changed: boolean;
-	message: string;
-	zone: ZoneTypes.Zone;
-	addCert: string;
-	forwardedChecked: boolean;
+	datacenters: DatacenterTypes.DatacentersRo;
+	selected: boolean;
+	onSelect: (shift: boolean) => void;
+	open: boolean;
+	onOpen: () => void;
 }
 
 const css = {
 	card: {
-		position: 'relative',
-		padding: '10px 10px 0 10px',
-		marginBottom: '5px',
+		display: 'table-row',
+		width: '100%',
+		padding: 0,
+		boxShadow: 'none',
+		cursor: 'pointer',
 	} as React.CSSProperties,
-	remove: {
-		position: 'absolute',
-		top: '5px',
-		right: '5px',
+	cardOpen: {
+		display: 'table-row',
+		width: '100%',
+		padding: 0,
+		boxShadow: 'none',
+		position: 'relative',
+	} as React.CSSProperties,
+	select: {
+		margin: '2px 0 0 0',
+		paddingTop: '3px',
+		minHeight: '18px',
+	} as React.CSSProperties,
+	name: {
+		verticalAlign: 'top',
+		display: 'table-cell',
+		padding: '8px',
+	} as React.CSSProperties,
+	nameSpan: {
+		margin: '1px 5px 0 0',
 	} as React.CSSProperties,
 	item: {
-		margin: '9px 5px 0 5px',
+		verticalAlign: 'top',
+		display: 'table-cell',
+		padding: '9px',
+		whiteSpace: 'nowrap',
+	} as React.CSSProperties,
+	icon: {
+		marginRight: '3px',
+	} as React.CSSProperties,
+	bars: {
+		verticalAlign: 'top',
+		display: 'table-cell',
+		padding: '8px',
+		width: '30px',
+	} as React.CSSProperties,
+	bar: {
+		height: '6px',
+		marginBottom: '1px',
+	} as React.CSSProperties,
+	barLast: {
+		height: '6px',
+	} as React.CSSProperties,
+	roles: {
+		verticalAlign: 'top',
+		display: 'table-cell',
+		padding: '0 8px 8px 8px',
+	} as React.CSSProperties,
+	tag: {
+		margin: '8px 5px 0 5px',
 		height: '20px',
-	} as React.CSSProperties,
-	itemsLabel: {
-		display: 'block',
-	} as React.CSSProperties,
-	itemsAdd: {
-		margin: '8px 0 15px 0',
-	} as React.CSSProperties,
-	group: {
-		flex: 1,
-		minWidth: '280px',
-		margin: '0 10px',
-	} as React.CSSProperties,
-	save: {
-		paddingBottom: '10px',
-	} as React.CSSProperties,
-	label: {
-		width: '100%',
-		maxWidth: '280px',
-	} as React.CSSProperties,
-	inputGroup: {
-		width: '100%',
-	} as React.CSSProperties,
-	protocol: {
-		flex: '0 1 auto',
-	} as React.CSSProperties,
-	port: {
-		flex: '1',
 	} as React.CSSProperties,
 };
 
-export default class Zone extends React.Component<Props, State> {
-	constructor(props: any, context: any) {
-		super(props, context);
-		this.state = {
-			disabled: false,
-			changed: false,
-			message: '',
-			zone: null,
-			addCert: null,
-			forwardedChecked: false,
-		};
-	}
+export default class Zone extends React.Component<Props, {}> {
+	render(): JSX.Element {
+		let zone = this.props.zone;
 
-	set(name: string, val: any): void {
-		let zone: any;
-
-		if (this.state.changed) {
-			zone = {
-				...this.state.zone,
-			};
-		} else {
-			zone = {
-				...this.props.zone,
-			};
+		if (this.props.open) {
+			return <div
+				className="bp5-card bp5-row"
+				style={css.cardOpen}
+			>
+				<ZoneDetailed
+					zone={this.props.zone}
+					selected={this.props.selected}
+					onSelect={this.props.onSelect}
+					onClose={(): void => {
+						this.props.onOpen();
+					}}
+				/>
+			</div>;
 		}
 
-		zone[name] = val;
-
-		this.setState({
-			...this.state,
-			changed: true,
-			zone: zone,
-		});
-	}
-
-	onSave = (): void => {
-		this.setState({
-			...this.state,
-			disabled: true,
-		});
-		ZoneActions.commit(this.state.zone).then((): void => {
-			this.setState({
-				...this.state,
-				message: 'Your changes have been saved',
-				changed: false,
-				disabled: false,
-			});
-
-			setTimeout((): void => {
-				if (!this.state.changed) {
-					this.setState({
-						...this.state,
-						message: '',
-						changed: false,
-						zone: null,
-					});
-				}
-			}, 3000);
-		}).catch((): void => {
-			this.setState({
-				...this.state,
-				message: '',
-				disabled: false,
-			});
-		});
-	}
-
-	onDelete = (): void => {
-		this.setState({
-			...this.state,
-			disabled: true,
-		});
-		ZoneActions.remove(this.props.zone.id).then((): void => {
-			this.setState({
-				...this.state,
-				disabled: false,
-			});
-		}).catch((): void => {
-			this.setState({
-				...this.state,
-				disabled: false,
-			});
-		});
-	}
-
-	render(): JSX.Element {
-		let zone: ZoneTypes.Zone = this.state.zone ||
-			this.props.zone;
+		let cardStyle = {
+			...css.card,
+		};
 
 		let datacenter = DatacentersStore.datacenter(
 			this.props.zone.datacenter);
 
 		return <div
-			className="bp5-card"
-			style={css.card}
+			className="bp5-card bp5-row"
+			style={cardStyle}
+			onClick={(evt): void => {
+				let target = evt.target as HTMLElement;
+
+				if (target.className.indexOf('open-ignore') !== -1) {
+					return;
+				}
+
+				this.props.onOpen();
+			}}
 		>
-			<div className="layout horizontal wrap">
-				<div style={css.group}>
-					<div style={css.remove}>
-						<ConfirmButton
-							safe={true}
-							className="bp5-minimal bp5-intent-danger bp5-icon-trash"
-							progressClassName="bp5-intent-danger"
-							dialogClassName="bp5-intent-danger bp5-icon-delete"
-							dialogLabel="Delete Zone"
-							confirmMsg="Permanently delete this zone"
-							confirmInput={true}
-							disabled={this.state.disabled}
-							onConfirm={this.onDelete}
-						/>
-					</div>
-					<PageInput
-						label="Name"
-						help="Name of zone"
-						type="text"
-						placeholder="Enter name"
-						value={zone.name}
-						onChange={(val): void => {
-							this.set('name', val);
-						}}
-					/>
-					<PageTextArea
-						label="Comment"
-						help="Zone comment."
-						placeholder="Zone comment"
-						rows={3}
-						value={zone.comment}
-						onChange={(val: string): void => {
-							this.set('comment', val);
-						}}
-					/>
-					<PageSelect
-						disabled={this.state.disabled}
-						label="Network Mode"
-						help="Network mode for internal VPC networking. If layer 2 networking with VLAN support isn't available VXLan must be used. A network bridge is required for the node internal interfaces when using default. Other zones in datacenter must use same network mode to support connectivity between zones."
-						value={zone.network_mode}
-						onChange={(val): void => {
-							this.set('network_mode', val);
-						}}
+			<div className="bp5-cell" style={css.name}>
+				<div className="layout horizontal">
+					<label
+						className="bp5-control bp5-checkbox open-ignore"
+						style={css.select}
 					>
-						<option value="default">Default</option>
-						<option value="vxlan_vlan">VXLAN</option>
-					</PageSelect>
-				</div>
-				<div style={css.group}>
-					<PageInfo
-						fields={[
-							{
-								label: 'ID',
-								value: this.props.zone.id || 'None',
-							},
-							{
-								label: 'Datacenter',
-								value: datacenter ? datacenter.name :
-									this.props.zone.datacenter || 'None',
-							},
-						]}
-					/>
+						<input
+							type="checkbox"
+							className="open-ignore"
+							checked={this.props.selected}
+							onChange={(evt): void => {
+							}}
+							onClick={(evt): void => {
+								this.props.onSelect(evt.shiftKey);
+							}}
+						/>
+						<span className="bp5-control-indicator open-ignore"/>
+					</label>
+					<div style={css.nameSpan}>
+						{zone.name}
+					</div>
 				</div>
 			</div>
-			<PageSave
-				style={css.save}
-				hidden={!this.state.zone}
-				message={this.state.message}
-				changed={this.state.changed}
-				disabled={this.state.disabled}
-				light={true}
-				onCancel={(): void => {
-					this.setState({
-						...this.state,
-						changed: false,
-						forwardedChecked: false,
-						zone: null,
-					});
-				}}
-				onSave={this.onSave}
-			/>
+			<div className="bp5-cell" style={css.item}>
+				<span
+					style={css.icon}
+					className="bp5-icon-standard bp5-text-muted bp5-icon-cloud"
+				/>
+				{datacenter?.name || zone.datacenter}
+			</div>
 		</div>;
 	}
 }
