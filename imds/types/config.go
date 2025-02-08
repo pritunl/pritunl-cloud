a25bf2b1d1cd8211dc799097da0b3d2ebc0a1763
Zachary Huff <zach.huff.386@gmail.com>
2025-02-08 15:45:34 -0500

    Add spec data to imds types

--- imds/types/config.go
+++ imds/types/config.go
@@ -1,19 +1,22 @@
 package types
 
 import (
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
 	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 type Config struct {
-	ImdsHostSecret string         `json:"-"`
-	ClientIps      []string       `json:"client_ips"`
-	Instance       *Instance      `json:"instance"`
-	Vpc            *Vpc           `json:"vpc"`
-	Subnet         *Subnet        `json:"subnet"`
-	Certificates   []*Certificate `json:"certificates"`
-	Secrets        []*Secret      `json:"secrets"`
-	Pods           []*Pod         `json:"pods"`
-	Hash           uint32         `json:"hash"`
+	Spec           primitive.ObjectID `json:"spec"`
+	SpecData       string             `json:"spec_data" gob:"-"`
+	ImdsHostSecret string             `json:"-"`
+	ClientIps      []string           `json:"client_ips"`
+	Instance       *Instance          `json:"instance"`
+	Vpc            *Vpc               `json:"vpc"`
+	Subnet         *Subnet            `json:"subnet"`
+	Certificates   []*Certificate     `json:"certificates"`
+	Secrets        []*Secret          `json:"secrets"`
+	Pods           []*Pod             `json:"pods"`
+	Hash           uint32             `json:"hash"`
 }
 
 func (c *Config) ComputeHash() (err error) {
