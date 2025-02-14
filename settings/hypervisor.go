22679f5f1cd1d546baf1fad0a9833c257a8c323f
Zachary Huff <zach.huff.386@gmail.com>
2025-02-14 12:51:03 -0500

    Rename agent path in hypervisor settings

--- settings/hypervisor.go
+++ settings/hypervisor.go
@@ -7,8 +7,8 @@ type hypervisor struct {
 	SystemdPath         string `bson:"systemd_path" default:"/etc/systemd/system"`
 	LibPath             string `bson:"lib_path" default:"/var/lib/pritunl-cloud"`
 	RunPath             string `bson:"run_path" default:"/var/run/pritunl-cloud"`
-	AgentHostPath       string `bson:"cli_host_path" default:"/usr/bin/pritunl-cloud-agent"`
-	CliGuestPath        string `bson:"cli_guest_path" default:"/usr/bin/pci"`
+	AgentHostPath       string `bson:"agent_host_path" default:"/usr/bin/pritunl-cloud-agent"`
+	AgentGuestPath      string `bson:"agent_guest_path" default:"/usr/bin/pci"`
 	InitGuestPath       string `bson:"init_guest_path" default:"/etc/pritunl-cloud-init"`
 	HugepagesPath       string `bson:"hugepages_path" default:"/dev/hugepages/pritunl"`
 	DesktopEnv          string `bson:"desktop_env" default:"gnome"`
