006f1b05c3364b481ae61607396eafbc49880415
Zachary Huff <zach.huff.386@gmail.com>
2025-02-15 14:16:25 -0500

    Add nodeport

---
+++ nodeport/nodeport.go
@@ -0,0 +1,25 @@
+package nodeport
+
+import (
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/database"
+)
+
+type NodePort struct {
+	Id         primitive.ObjectID `bson:"id" json:"id"`
+	Datacenter primitive.ObjectID `bson:"datacenter" json:"datacenter"`
+	Port       int                `bson:"port" json:"port"`
+	Resource   primitive.ObjectID `bson:"resource" json:"resource"`
+}
+
+func (n *NodePort) Insert(db *database.Database) (err error) {
+	coll := db.NodePorts()
+
+	_, err = coll.InsertOne(db, n)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
