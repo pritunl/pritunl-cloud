b574b76ab5dce15dccbb583a54e6d94a1699ea05
Zachary Huff <zach.huff.386@gmail.com>
2025-02-17 18:32:19 -0500

    Remove oracle from deploy network

--- deploy/network.go
+++ deploy/network.go
@@ -5,7 +5,6 @@ import (
 	"github.com/pritunl/pritunl-cloud/interfaces"
 	"github.com/pritunl/pritunl-cloud/state"
 	"github.com/pritunl/pritunl-cloud/vxlan"
-	"github.com/sirupsen/logrus"
 )
 
 type Network struct {
@@ -21,14 +20,6 @@ func (d *Network) Deploy() (err error) {
 	err = vxlan.ApplyState(d.stat)
 	if err != nil {
 		return
-	}
-
-	err = ApplyOracleState(d.stat)
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"error": err,
-		}).Error("deploy: Failed to apply Oracle state")
-		err = nil
 	}
 
 	interfaces.SyncIfaces(d.stat.VxLan())
