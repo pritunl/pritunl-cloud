09d3a8780db5e41bf89f7ea1ea049f93745e58cd
Zachary Huff <zach.huff.386@gmail.com>
2025-02-19 07:34:42 -0500

    Add node port to iptables state

--- iptables/state.go
+++ iptables/state.go
@@ -2,6 +2,7 @@ package iptables
 
 import (
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/block"
 	"github.com/pritunl/pritunl-cloud/firewall"
 	"github.com/pritunl/pritunl-cloud/instance"
 	"github.com/pritunl/pritunl-cloud/node"
@@ -16,7 +17,8 @@ type State struct {
 
 func LoadState(nodeSelf *node.Node, vpcs []*vpc.Vpc,
 	instances []*instance.Instance, nodeFirewall []*firewall.Rule,
-	firewalls map[string][]*firewall.Rule) (state *State) {
+	firewalls map[string][]*firewall.Rule,
+	firewallMaps map[string][]*firewall.Mapping) (state *State) {
 
 	vpcsMap := map[primitive.ObjectID]*vpc.Vpc{}
 	for _, vc := range vpcs {
@@ -31,6 +33,7 @@ func LoadState(nodeSelf *node.Node, vpcs []*vpc.Vpc,
 	if nodeNetworkMode6 == "" {
 		nodeNetworkMode6 = node.Dhcp
 	}
+	nodePortNetwork := !node.Self.NoNodePortNetwork
 
 	state = &State{
 		Interfaces: map[string]*Rules{},
@@ -38,6 +41,11 @@ func LoadState(nodeSelf *node.Node, vpcs []*vpc.Vpc,
 
 	if nodeFirewall != nil {
 		state.Interfaces["0-host"] = generate("0", "host", nodeFirewall)
+	}
+
+	nodePortGateway, err := block.GetNodePortGateway()
+	if err != nil {
+		return
 	}
 
 	for _, inst := range instances {
@@ -48,29 +56,29 @@ func LoadState(nodeSelf *node.Node, vpcs []*vpc.Vpc,
 		namespace := vm.GetNamespace(inst.Id, 0)
 		iface := vm.GetIface(inst.Id, 0)
 		ifaceHost := vm.GetIfaceHost(inst.Id, 1)
-
+		ifaceNodePort := vm.GetIfaceNodePort(inst.Id, 1)
 		ifaceExternal := vm.GetIfaceExternal(inst.Id, 0)
 
 		addr := ""
 		addr6 := ""
 		pubAddr := ""
 		pubAddr6 := ""
-		if inst.PrivateIps != nil && len(inst.PrivateIps) != 0 {
+		if len(inst.PrivateIps) != 0 {
 			addr = inst.PrivateIps[0]
 		}
-		if inst.PrivateIps6 != nil && len(inst.PrivateIps6) != 0 {
+		if len(inst.PrivateIps6) != 0 {
 			addr6 = inst.PrivateIps6[0]
 		}
-		if inst.PublicIps != nil && len(inst.PublicIps) != 0 {
+		if len(inst.PublicIps) != 0 {
 			pubAddr = inst.PublicIps[0]
 		}
-		if inst.PublicIps6 != nil && len(inst.PublicIps6) != 0 {
+		if len(inst.PublicIps6) != 0 {
 			pubAddr6 = inst.PublicIps6[0]
 		}
 
 		oracleAddr := ""
 		oracleIface := vm.GetIfaceOracle(inst.Id, 0)
-		if inst.OraclePrivateIps != nil && len(inst.OraclePrivateIps) != 0 {
+		if len(inst.OraclePrivateIps) != 0 {
 			oracleAddr = inst.OraclePrivateIps[0]
 		}
 
@@ -138,6 +146,12 @@ func LoadState(nodeSelf *node.Node, vpcs []*vpc.Vpc,
 		rules := generateInternal(namespace, ifaceHost,
 			false, false, false, false, "", "", "", "", "", ingress)
 		state.Interfaces[namespace+"-"+ifaceHost] = rules
+
+		if nodePortNetwork {
+			rules := generateNodePort(namespace, ifaceNodePort,
+				addr, nodePortGateway, firewallMaps[namespace])
+			state.Interfaces[namespace+"-"+ifaceNodePort] = rules
+		}
 
 		rules = generateVirt(vpcsMap[inst.Vpc], namespace, iface, addr,
 			addr6, !inst.SkipSourceDestCheck, ingress)
