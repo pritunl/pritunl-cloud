031f27c08ec18f21edfb1bc29fbebcd06b68d943
Zachary Huff <zach.huff.386@gmail.com>
2025-02-23 08:46:07 -0500

    Add virt install scripts

---
+++ tools/virt-install/install/oraclelinux8.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+set -e
+export NAME="oraclelinux8"
+export ISO_URL="https://yum.oracle.com/ISOS/OracleLinux/OL8/u10/x86_64/OracleLinux-R8-U10-x86_64-dvd.iso"
+export ISO_HASH="7676a80eeaafa16903eebb2abba147a3afe230b130cc066d56fdd6854d8da900"
+
+sudo mkdir -p /var/lib/virt/iso
+sudo mkdir -p /var/lib/virt/ks
+sudo mkdir -p /var/lib/virt/images
+
+sudo virsh destroy ${NAME} | true
+sudo virsh undefine ${NAME} --nvram | true
+sudo rm -f /var/lib/virt/${NAME}.qcow2
+
+if [ ! -f "/var/lib/virt/iso/$(basename ${ISO_URL})" ]; then
+  sudo wget -P /var/lib/virt/iso ${ISO_URL}
+fi
+
+echo "${ISO_HASH} /var/lib/virt/iso/$(basename ${ISO_URL})" | sha256sum --check
+if [ $? -ne 0 ]; then
+  echo "Checksum for ISO failed"
+  exit 1
+fi
+
+sudo tee /var/lib/virt/ks/${NAME}.ks << EOF
+text
+cdrom
+
+%addon com_redhat_kdump --disable
+%end
+
+keyboard --xlayouts='us'
+lang en_US.UTF-8
+
+network --bootproto=dhcp --hostname=cloud --activate
+
+%packages
+@^minimal-environment
+@standard
+%end
+
+firstboot --enable
+
+ignoredisk --only-use=vda
+clearpart --all --initlabel
+part /boot/efi --fstype="efi" --ondisk=vda --size=100 --fsoptions="umask=0077,shortname=winnt"
+part / --fstype="xfs" --ondisk=vda --grow
+
+timezone Etc/UTC --utc
+
+rootpw --plaintext cloud
+EOF
+
+sudo virt-install \
+  --name ${NAME} \
+  --vcpus 8 \
+  --memory 8192 \
+  --boot uefi \
+  --disk path=/var/lib/virt/${NAME}.qcow2,size=8,format=qcow2,bus=virtio \
+  --os-variant ol8.10 \
+  --network network=default \
+  --graphics=none \
+  --console pty,target_type=serial \
+  --location=/var/lib/virt/iso/$(basename ${ISO_URL}) \
+  --initrd-inject=/var/lib/virt/ks/${NAME}.ks \
+  --extra-args="console=ttyS0 inst.ks=file:/${NAME}.ks inst.text"
+
+while ! sudo virsh domstate ${NAME} 2>/dev/null | grep -q "shut off"; do
+  sleep 1
+done
+
+echo "Compressing image..."
+
+sudo rm -f /var/lib/virt/images/${NAME}_$(date +%y%m).qcow2
+sudo qemu-img convert -f qcow2 -O qcow2 -c /var/lib/virt/${NAME}.qcow2 /var/lib/virt/images/${NAME}_$(date +%y%m).qcow2
+sha256sum /var/lib/virt/images/${NAME}_$(date +%y%m).qcow2
