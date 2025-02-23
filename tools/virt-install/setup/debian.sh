8cef5bc1d4482d321fd51fd51ad4233247a23eb2
Zachary Huff <zach.huff.386@gmail.com>
2025-02-23 08:37:25 -0500

    Add virt install setup scripts

---
+++ tools/virt-install/setup/debian.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+set -e
+
+if [ $(whoami) != "root" ]; then
+  echo "Must be run as root"
+  exit
+fi
+
+echo '#############################################################'
+echo 'starting debian setup'
+echo '#############################################################'
+
+tee /etc/modprobe.d/floppy-blacklist.conf << EOF
+blacklist floppy
+EOF
+
+apt update
+apt -y upgrade
+apt -y autoremove
+
+apt -y install bash-completion qemu-guest-agent cloud-init cloud-initramfs-growroot chrony openssh-server
+
+systemctl daemon-reload
+systemctl enable qemu-guest-agent.service
+systemctl enable cloud-init-local.service
+systemctl enable cloud-init.service
+systemctl enable cloud-config.service
+systemctl enable cloud-final.service
+
+sed -i '/^PermitRootLogin/d' /etc/ssh/sshd_config
+sed -i '/^PasswordAuthentication/d' /etc/ssh/sshd_config
+sed -i '/^ChallengeResponseAuthentication/d' /etc/ssh/sshd_config
+sed -i '/^KbdInteractiveAuthentication/d' /etc/ssh/sshd_config
+sed -i '/^TrustedUserCAKeys/d' /etc/ssh/sshd_config
+sed -i '/^AuthorizedPrincipalsFile/d' /etc/ssh/sshd_config
+tee -a /etc/ssh/sshd_config << EOF
+
+PermitRootLogin no
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+KbdInteractiveAuthentication no
+TrustedUserCAKeys /etc/ssh/trusted
+AuthorizedPrincipalsFile /etc/ssh/principals
+EOF
+touch /etc/ssh/trusted
+touch /etc/ssh/principals
+
+passwd -d root
+passwd -l root
+passwd -d cloud
+passwd -l cloud
+mkdir -p /home/cloud/.ssh
+chown cloud:cloud /home/cloud/.ssh
+touch /home/cloud/.ssh/authorized_keys
+chown cloud:cloud /home/cloud/.ssh/authorized_keys
+chmod 700 /home/cloud/.ssh
+chmod 600 /home/cloud/.ssh/authorized_keys
+sed -i '/^%sudo/d' /etc/sudoers
+tee -a /etc/sudoers << EOF
+%sudo ALL=(ALL) NOPASSWD:ALL
+EOF
+
+systemctl enable ssh.service
+systemctl restart ssh.service
+ufw disable
+
+apt clean
+apt update
+apt -y upgrade
+
+rm -f /etc/cloud/cloud.cfg.d/90-installer-network.cfg
+rm -f /etc/cloud/cloud.cfg.d/99-installer.cfg
+cloud-init clean --machine-id
+rm -rf /etc/NetworkManager/system-connections/*
+
+sync
+sleep 1
+
+find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
+rm -rf /var/tmp/dnf-*
+rm -rf /home/cloud/.cache
+shred -u /etc/ssh/*_key /etc/ssh/*_key.pub | true
+shred -u /root/.ssh/authorized_keys | true
+shred -u /root/.bash_history | true
+shred -u /home/cloud/.bash_history | true
+shred -u /var/log/lastlog | true
+shred -u /var/log/secure | true
+shred -u /var/log/utmp | true
+shred -u /var/log/wtmp | true
+shred -u /var/log/btmp | true
+shred -u /var/log/dmesg | true
+shred -u /var/log/dmesg.old | true
+shred -u /var/lib/systemd/random-seed | true
+rm -rf /var/log/*.gz
+rm -rf /var/log/*.[0-9]
+rm -rf /var/log/*-????????
+rm -rf /var/lib/cloud/instances/*
+rm -f /var/lib/systemd/random-seed
+rm -f /etc/machine-id
+touch /etc/machine-id
+sync
+fstrim -av
+sync
+
+echo '#############################################################'
+echo 'finished debian setup, clear history and shutdown:'
+echo 'unset HISTFILE && history -c && sudo poweroff'
+echo '#############################################################'
---
+++ tools/virt-install/setup/rhel8.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+set -e
+
+if [ $(whoami) != "root" ]; then
+  echo "Must be run as root"
+  exit
+fi
+
+echo '#############################################################'
+echo 'starting rhel8 setup'
+echo '#############################################################'
+
+tee /etc/modprobe.d/floppy-blacklist.conf << EOF
+blacklist floppy
+EOF
+
+truncate -s 0 /etc/yum/vars/ociregion || true
+
+dnf clean all
+dnf -y update
+dnf -y install yum-utils
+dnf -y install bash-completion qemu-guest-agent yum-utils cloud-init cloud-utils-growpart chrony openssh-server
+dnf -y remove cockpit-ws
+
+sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/g' /etc/default/grub
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
+systemctl daemon-reload
+systemctl enable qemu-guest-agent.service
+
+rm -f /etc/sysconfig/network-scripts/ifcfg-eth*
+
+sed -i 's/^installonly_limit=.*/installonly_limit=2/g' /etc/yum.conf
+sed -i 's/^SELINUX=.*/SELINUX=enforcing/g' /etc/selinux/config || true
+sed -i 's/^SELINUX=.*/SELINUX=enforcing/g' /etc/sysconfig/selinux || true
+
+sed -i '/^PermitRootLogin/d' /etc/ssh/sshd_config
+sed -i '/^PasswordAuthentication/d' /etc/ssh/sshd_config
+sed -i '/^ChallengeResponseAuthentication/d' /etc/ssh/sshd_config
+sed -i '/^TrustedUserCAKeys/d' /etc/ssh/sshd_config
+sed -i '/^AuthorizedPrincipalsFile/d' /etc/ssh/sshd_config
+tee -a /etc/ssh/sshd_config << EOF
+
+PermitRootLogin no
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+TrustedUserCAKeys /etc/ssh/trusted
+AuthorizedPrincipalsFile /etc/ssh/principals
+EOF
+touch /etc/ssh/trusted
+touch /etc/ssh/principals
+restorecon -v /etc/ssh/trusted
+restorecon -v /etc/ssh/principals
+
+useradd -G adm,video,wheel,systemd-journal cloud || true
+passwd -d root
+passwd -l root
+passwd -d cloud
+passwd -l cloud
+mkdir -p /home/cloud/.ssh
+chown cloud:cloud /home/cloud/.ssh
+restorecon -v /home/cloud/.ssh
+touch /home/cloud/.ssh/authorized_keys
+chown cloud:cloud /home/cloud/.ssh/authorized_keys
+restorecon -v /home/cloud/.ssh/authorized_keys
+chmod 700 /home/cloud/.ssh
+chmod 600 /home/cloud/.ssh/authorized_keys
+sed -i '/^%wheel/d' /etc/sudoers
+tee -a /etc/sudoers << EOF
+%wheel ALL=(ALL) NOPASSWD:ALL
+EOF
+
+systemctl enable sshd
+systemctl restart sshd
+systemctl disable firewalld
+systemctl stop firewalld
+systemctl start chronyd
+systemctl enable chronyd
+
+cloud-init clean --machine-id
+rm -rf /etc/NetworkManager/system-connections/*
+tee /etc/resolv.conf << EOF
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOF
+
+dnf clean all
+rm -rf /var/cache/yum
+dnf -y update
+
+sync
+sleep 1
+
+find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
+rm -rf /var/tmp/dnf-*
+rm -rf /home/cloud/.cache
+shred -u /etc/ssh/*_key /etc/ssh/*_key.pub | true
+shred -u /root/.ssh/authorized_keys | true
+shred -u /root/.bash_history | true
+shred -u /home/cloud/.bash_history | true
+shred -u /var/log/lastlog | true
+shred -u /var/log/secure | true
+shred -u /var/log/utmp | true
+shred -u /var/log/wtmp | true
+shred -u /var/log/btmp | true
+shred -u /var/log/dmesg | true
+shred -u /var/log/dmesg.old | true
+shred -u /var/lib/systemd/random-seed | true
+rm -rf /var/log/*.gz
+rm -rf /var/log/*.[0-9]
+rm -rf /var/log/*-????????
+rm -rf /var/lib/cloud/instances/*
+rm -f /var/lib/systemd/random-seed
+rm -f /etc/machine-id
+touch /etc/machine-id
+sync
+fstrim -av
+sync
+
+echo '#############################################################'
+echo 'finished rhel8 setup, clear history and shutdown:'
+echo 'unset HISTFILE && history -c && poweroff'
+echo '#############################################################'
