4e800c209749e9b48e8514e0bf4560ae95d07491
Zachary Huff <zach.huff.386@gmail.com>
2025-02-27 14:21:31 -0500

    Check disk available in spec mount

--- spec/spec.go
+++ spec/spec.go
@@ -92,7 +92,7 @@ func (s *Spec) GetAllNodes(db *database.Database,
 			if mount.Disks != nil {
 				for _, dskId := range mount.Disks {
 					dsk := disksMap[dskId]
-					if dsk == nil {
+					if dsk == nil || !dsk.IsAavailabe() {
 						continue
 					}
 					mountSet.Add(dsk.Node)
