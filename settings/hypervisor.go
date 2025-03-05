9c1d297a16a93015b95e023dcd417048e6808a0c
Zachary Huff <zach.huff.386@gmail.com>
2025-03-05 16:37:32 -0500

    Add bridge iface name to hypervisor settings

--- settings/hypervisor.go
+++ settings/hypervisor.go
@@ -21,6 +21,7 @@ type hypervisor struct {
 	NbdPath             string `bson:"nbd_path" default:"/dev/nbd6"`
 	DiskAio             string `bson:"disk_aio"`
 	NoSandbox           bool   `bson:"no_sandbox"`
+	BridgeIfaceName     string `bson:"bridge_iface_name" default:"br0"`
 	NormalMtu           int    `bson:"normal_mtu" default:"1500"`
 	JumboMtu            int    `bson:"jumbo_mtu" default:"9000"`
 	DiskQueuesMin       int    `bson:"disk_queues_min" default:"1"`
