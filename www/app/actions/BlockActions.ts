90dafef3a87431e536d0565b798d8d7ca0f83f62
Zachary Huff <zach.huff.386@gmail.com>
2025-03-07 16:02:28 -0500

    Add paging to block actions

--- www/app/actions/BlockActions.ts
+++ www/app/actions/BlockActions.ts
@@ -6,6 +6,7 @@ import * as Alert from '../Alert';
 import * as Csrf from '../Csrf';
 import Loader from '../Loader';
 import * as BlockTypes from '../types/BlockTypes';
+import BlocksStore from '../stores/BlocksStore';
 import * as MiscUtils from '../utils/MiscUtils';
 import * as Constants from "../Constants";
 
@@ -20,6 +21,11 @@ export function sync(): Promise<void> {
 	return new Promise<void>((resolve, reject): void => {
 		SuperAgent
 			.get('/block')
+			.query({
+				...BlocksStore.filter,
+				page: BlocksStore.page,
+				page_count: BlocksStore.pageCount,
+			})
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
 			.end((err: any, res: SuperAgent.Response): void => {
@@ -46,12 +52,35 @@ export function sync(): Promise<void> {
 					type: BlockTypes.SYNC,
 					data: {
 						blocks: res.body,
+						count: res.body.count,
 					},
 				});
 
 				resolve();
 			});
 	});
+}
+
+export function traverse(page: number): Promise<void> {
+	Dispatcher.dispatch({
+		type: BlockTypes.TRAVERSE,
+		data: {
+			page: page,
+		},
+	});
+
+	return sync();
+}
+
+export function filter(filt: BlockTypes.Filter): Promise<void> {
+	Dispatcher.dispatch({
+		type: BlockTypes.FILTER,
+		data: {
+			filter: filt,
+		},
+	});
+
+	return sync();
 }
 
 export function commit(block: BlockTypes.Block): Promise<void> {
@@ -118,6 +147,35 @@ export function remove(blockId: string): Promise<void> {
 	return new Promise<void>((resolve, reject): void => {
 		SuperAgent
 			.delete('/block/' + blockId)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to delete blocks');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function removeMulti(blockIds: string[]): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.delete('/block')
+			.send(blockIds)
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
 			.end((err: any, res: SuperAgent.Response): void => {
