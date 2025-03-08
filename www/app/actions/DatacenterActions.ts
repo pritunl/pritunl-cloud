1a0d6e773564f8fb62ac8e7953457b2439397fe0
Zachary Huff <zach.huff.386@gmail.com>
2025-03-08 16:51:19 -0500

    Add paging to datacenter actions

--- www/app/actions/DatacenterActions.ts
+++ www/app/actions/DatacenterActions.ts
@@ -6,8 +6,10 @@ import * as Alert from '../Alert';
 import * as Csrf from '../Csrf';
 import Loader from '../Loader';
 import * as DatacenterTypes from '../types/DatacenterTypes';
+import DatacentersStore from '../stores/DatacentersStore';
 import OrganizationsStore from '../stores/OrganizationsStore';
 import * as MiscUtils from '../utils/MiscUtils';
+import * as Constants from "../Constants";
 
 let syncId: string;
 
@@ -20,6 +22,11 @@ export function sync(): Promise<void> {
 	return new Promise<void>((resolve, reject): void => {
 		SuperAgent
 			.get('/datacenter')
+			.query({
+				...DatacentersStore.filter,
+				page: DatacentersStore.page,
+				page_count: DatacentersStore.pageCount,
+			})
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
 			.set('Organization', OrganizationsStore.current)
@@ -47,12 +54,33 @@ export function sync(): Promise<void> {
 					type: DatacenterTypes.SYNC,
 					data: {
 						datacenters: res.body,
+						count: res.body.count,
 					},
 				});
 
 				resolve();
 			});
 	});
+}
+
+export function traverse(page: number): Promise<void> {
+	Dispatcher.dispatch({
+		type: DatacenterTypes.TRAVERSE,
+		data: {
+			page: page,
+		},
+	});
+	return sync();
+}
+
+export function filter(filt: DatacenterTypes.Filter): Promise<void> {
+	Dispatcher.dispatch({
+		type: DatacenterTypes.FILTER,
+		data: {
+			filter: filt,
+		},
+	});
+	return sync();
 }
 
 export function commit(datacenter: DatacenterTypes.Datacenter): Promise<void> {
@@ -144,10 +172,41 @@ export function remove(datacenterId: string): Promise<void> {
 	});
 }
 
+export function removeMulti(datacenterIds: string[]): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.delete('/datacenter')
+			.send(datacenterIds)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to delete datacenters');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
 EventDispatcher.register((action: DatacenterTypes.DatacenterDispatch) => {
 	switch (action.type) {
 		case DatacenterTypes.CHANGE:
-			sync();
+			if (!Constants.user) {
+				sync();
+			}
 			break;
 	}
 });
