38ab827de19134d30186a48bf8d0c87b40b5ad37
Zachary Huff <zach.huff.386@gmail.com>
2025-03-11 17:59:01 -0400

    Add oracle tenancy to node oracle provider

--- node/oracle.go
+++ node/oracle.go
@@ -8,6 +8,10 @@ func (n *NodeOracleAuthProvider) OracleUser() string {
 	return n.nde.OracleUser
 }
 
+func (n *NodeOracleAuthProvider) OracleTenancy() string {
+	return n.nde.OracleTenancy
+}
+
 func (n *NodeOracleAuthProvider) OraclePrivateKey() string {
 	return n.nde.OraclePrivateKey
 }
