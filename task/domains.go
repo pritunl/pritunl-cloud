f47f83e10de05169550bfc55aaad3dbd96f96ad0
Zachary Huff <zach.huff.386@gmail.com>
2025-03-15 18:43:47 -0400

    Add domains task

---
+++ task/domains.go
@@ -0,0 +1,41 @@
+package task
+
+import (
+	"time"
+
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/domain"
+)
+
+var domains = &Task{
+	Name:    "domains",
+	Version: 1,
+	Hours: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+		13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
+	Minutes: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+		13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+		26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+		39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+		52, 53, 54, 55, 56, 57, 58, 59},
+	Handler:    domainsHandler,
+	DebugNodes: []string{"web"},
+}
+
+func domainsHandler(db *database.Database) (err error) {
+	domns, err := domain.GetAll(db, &bson.M{
+		"last_update": &bson.M{
+			"$gte": time.Now().Add(-5 * time.Minute),
+		},
+	})
+
+	for _, domn := range domns {
+		domain.Refresh(db, domn.Id)
+	}
+
+	return
+}
+
+func init() {
+	register(domains)
+}
