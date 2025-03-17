c10ed60a61ac46e476b2d5a0fd1a29767369c1a6
Zachary Huff <zach.huff.386@gmail.com>
2025-03-17 09:08:10 -0400

    Add paging to storage actions

--- www/app/actions/StorageActions.ts
+++ www/app/actions/StorageActions.ts
@@ -6,6 +6,7 @@ import * as Alert from '../Alert';
 import * as Csrf from '../Csrf';
 import Loader from '../Loader';
 import * as StorageTypes from '../types/StorageTypes';
+import StoragesStore from '../stores/StoragesStore';
 import * as MiscUtils from '../utils/MiscUtils';
 import * as Constants from "../Constants";
 
@@ -20,6 +21,11 @@ export function sync(): Promise<void> {
 	return new Promise<void>((resolve, reject): void => {
 		SuperAgent
 			.get('/storage')
+			.query({
+				...StoragesStore.filter,
+				page: StoragesStore.page,
+				page_count: StoragesStore.pageCount,
+			})
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
 			.end((err: any, res: SuperAgent.Response): void => {
@@ -46,12 +52,33 @@ export function sync(): Promise<void> {
 					type: StorageTypes.SYNC,
 					data: {
 						storages: res.body,
+						count: res.body.count,
 					},
 				});
 
 				resolve();
 			});
 	});
+}
+
+export function traverse(page: number): Promise<void> {
+	Dispatcher.dispatch({
+		type: StorageTypes.TRAVERSE,
+		data: {
+			page: page,
+		},
+	});
+	return sync();
+}
+
+export function filter(filt: StorageTypes.Filter): Promise<void> {
+	Dispatcher.dispatch({
+		type: StorageTypes.FILTER,
+		data: {
+			filter: filt,
+		},
+	});
+	return sync();
 }
 
 export function commit(storage: StorageTypes.Storage): Promise<void> {
@@ -118,6 +145,35 @@ export function remove(storageId: string): Promise<void> {
 	return new Promise<void>((resolve, reject): void => {
 		SuperAgent
 			.delete('/storage/' + storageId)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to delete storages');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function removeMulti(storageIds: string[]): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.delete('/storage')
+			.send(storageIds)
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
 			.end((err: any, res: SuperAgent.Response): void => {
