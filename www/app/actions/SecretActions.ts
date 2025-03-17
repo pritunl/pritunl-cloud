43b50baf1dd44b22a63471b4068ab27313cee071
Zachary Huff <zach.huff.386@gmail.com>
2025-03-17 09:04:05 -0400

    Add paging to secret actions

--- www/app/actions/SecretActions.ts
+++ www/app/actions/SecretActions.ts
@@ -6,6 +6,7 @@ import * as Alert from '../Alert';
 import * as Csrf from '../Csrf';
 import Loader from '../Loader';
 import * as SecretTypes from '../types/SecretTypes';
+import SecretsStore from '../stores/SecretsStore';
 import * as MiscUtils from '../utils/MiscUtils';
 import * as Constants from "../Constants";
 import OrganizationsStore from "../stores/OrganizationsStore";
@@ -21,6 +22,11 @@ export function sync(): Promise<void> {
 	return new Promise<void>((resolve, reject): void => {
 		SuperAgent
 			.get('/secret')
+			.query({
+				...SecretsStore.filter,
+				page: SecretsStore.page,
+				page_count: SecretsStore.pageCount,
+			})
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
 			.set('Organization', OrganizationsStore.current)
@@ -48,12 +54,33 @@ export function sync(): Promise<void> {
 					type: SecretTypes.SYNC,
 					data: {
 						secrets: res.body,
+						count: res.body.count,
 					},
 				});
 
 				resolve();
 			});
 	});
+}
+
+export function traverse(page: number): Promise<void> {
+	Dispatcher.dispatch({
+		type: SecretTypes.TRAVERSE,
+		data: {
+			page: page,
+		},
+	});
+	return sync();
+}
+
+export function filter(filt: SecretTypes.Filter): Promise<void> {
+	Dispatcher.dispatch({
+		type: SecretTypes.FILTER,
+		data: {
+			filter: filt,
+		},
+	});
+	return sync();
 }
 
 export function commit(secr: SecretTypes.Secret): Promise<void> {
@@ -125,6 +152,35 @@ export function remove(secrId: string): Promise<void> {
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
 			.set('Organization', OrganizationsStore.current)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to delete secrets');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
+export function removeMulti(secretIds: string[]): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.delete('/secret')
+			.send(secretIds)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
 			.end((err: any, res: SuperAgent.Response): void => {
 				loader.done();
 
