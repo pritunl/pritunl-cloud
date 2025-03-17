e20c452cb0be706becf634e610e563edeff498bc
Zachary Huff <zach.huff.386@gmail.com>
2025-03-17 09:08:20 -0400

    Add paging to zone actions

--- www/app/actions/ZoneActions.ts
+++ www/app/actions/ZoneActions.ts
@@ -6,8 +6,10 @@ import * as Alert from '../Alert';
 import * as Csrf from '../Csrf';
 import Loader from '../Loader';
 import * as ZoneTypes from '../types/ZoneTypes';
+import ZonesStore from '../stores/ZonesStore';
 import OrganizationsStore from '../stores/OrganizationsStore';
 import * as MiscUtils from '../utils/MiscUtils';
+import * as Constants from "../Constants";
 
 let syncId: string;
 
@@ -20,6 +22,11 @@ export function sync(): Promise<void> {
 	return new Promise<void>((resolve, reject): void => {
 		SuperAgent
 			.get('/zone')
+			.query({
+				...ZonesStore.filter,
+				page: ZonesStore.page,
+				page_count: ZonesStore.pageCount,
+			})
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
 			.set('Organization', OrganizationsStore.current)
@@ -47,12 +54,33 @@ export function sync(): Promise<void> {
 					type: ZoneTypes.SYNC,
 					data: {
 						zones: res.body,
+						count: res.body.count,
 					},
 				});
 
 				resolve();
 			});
 	});
+}
+
+export function traverse(page: number): Promise<void> {
+	Dispatcher.dispatch({
+		type: ZoneTypes.TRAVERSE,
+		data: {
+			page: page,
+		},
+	});
+	return sync();
+}
+
+export function filter(filt: ZoneTypes.Filter): Promise<void> {
+	Dispatcher.dispatch({
+		type: ZoneTypes.FILTER,
+		data: {
+			filter: filt,
+		},
+	});
+	return sync();
 }
 
 export function commit(zone: ZoneTypes.Zone): Promise<void> {
@@ -144,10 +172,41 @@ export function remove(zoneId: string): Promise<void> {
 	});
 }
 
+export function removeMulti(zoneIds: string[]): Promise<void> {
+	let loader = new Loader().loading();
+
+	return new Promise<void>((resolve, reject): void => {
+		SuperAgent
+			.delete('/zone')
+			.send(zoneIds)
+			.set('Accept', 'application/json')
+			.set('Csrf-Token', Csrf.token)
+			.end((err: any, res: SuperAgent.Response): void => {
+				loader.done();
+
+				if (res && res.status === 401) {
+					window.location.href = '/login';
+					resolve();
+					return;
+				}
+
+				if (err) {
+					Alert.errorRes(res, 'Failed to delete zones');
+					reject(err);
+					return;
+				}
+
+				resolve();
+			});
+	});
+}
+
 EventDispatcher.register((action: ZoneTypes.ZoneDispatch) => {
 	switch (action.type) {
 		case ZoneTypes.CHANGE:
-			sync();
+			if (!Constants.user) {
+				sync();
+			}
 			break;
 	}
 });
