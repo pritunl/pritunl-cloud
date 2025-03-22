a5b7476cc22bb971e66cb11bf7a6eea918ec2773
Zachary Huff <zach.huff.386@gmail.com>
2025-03-22 13:09:49 -0400

    Add paging to secret uhandlers

--- uhandlers/secret.go
+++ uhandlers/secret.go
@@ -1,11 +1,19 @@
 package uhandlers
 
 import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
 	"github.com/dropbox/godropbox/container/set"
+	"github.com/dropbox/godropbox/errors"
 	"github.com/gin-gonic/gin"
+	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/demo"
+	"github.com/pritunl/pritunl-cloud/errortypes"
 	"github.com/pritunl/pritunl-cloud/event"
 	"github.com/pritunl/pritunl-cloud/secret"
 	"github.com/pritunl/pritunl-cloud/utils"
@@ -19,6 +27,11 @@ type secretData struct {
 	Key     string             `json:"key"`
 	Value   string             `json:"value"`
 	Region  string             `json:"region"`
+}
+
+type secretsData struct {
+	Secrets []*secret.Secret `json:"secrets"`
+	Count   int64            `json:"count"`
 }
 
 func secretPut(c *gin.Context) {
@@ -162,6 +175,35 @@ func secretDelete(c *gin.Context) {
 	c.JSON(200, nil)
 }
 
+func secretsDelete(c *gin.Context) {
+	if demo.Blocked(c) {
+		return
+	}
+
+	db := c.MustGet("db").(*database.Database)
+	userOrg := c.MustGet("organization").(primitive.ObjectID)
+	data := []primitive.ObjectID{}
+
+	err := c.Bind(&data)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "handler: Bind error"),
+		}
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	err = secret.RemoveMultiOrg(db, userOrg, data)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	event.PublishDispatch(db, "secret.change")
+
+	c.JSON(200, nil)
+}
+
 func secretGet(c *gin.Context) {
 	db := c.MustGet("db").(*database.Database)
 	userOrg := c.MustGet("organization").(primitive.ObjectID)
@@ -190,18 +232,44 @@ func secretsGet(c *gin.Context) {
 	db := c.MustGet("db").(*database.Database)
 	userOrg := c.MustGet("organization").(primitive.ObjectID)
 
-	secrs, err := secret.GetAllOrg(db, userOrg)
+	page, _ := strconv.ParseInt(c.Query("page"), 10, 0)
+	pageCount, _ := strconv.ParseInt(c.Query("page_count"), 10, 0)
+
+	query := bson.M{
+		"organization": userOrg,
+	}
+
+	secretId, ok := utils.ParseObjectId(c.Query("id"))
+	if ok {
+		query["_id"] = secretId
+	}
+
+	name := strings.TrimSpace(c.Query("name"))
+	if name != "" {
+		query["name"] = &bson.M{
+			"$regex":   fmt.Sprintf(".*%s.*", regexp.QuoteMeta(name)),
+			"$options": "i",
+		}
+	}
+
+	comment := strings.TrimSpace(c.Query("comment"))
+	if comment != "" {
+		query["comment"] = &bson.M{
+			"$regex":   fmt.Sprintf(".*%s.*", comment),
+			"$options": "i",
+		}
+	}
+
+	secrs, count, err := secret.GetAllPaged(db, &query, page, pageCount)
 	if err != nil {
 		utils.AbortWithError(c, 500, err)
 		return
 	}
 
-	if demo.IsDemo() {
-		for _, secr := range secrs {
-			secr.Key = "demo"
-			secr.Value = "demo"
-		}
+	data := &secretsData{
+		Secrets: secrs,
+		Count:   count,
 	}
 
-	c.JSON(200, secrs)
+	c.JSON(200, data)
 }
