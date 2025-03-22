d75c1dbe586bb2eb09415c51317f7df5b8a905c0
Zachary Huff <zach.huff.386@gmail.com>
2025-03-22 13:03:36 -0400

    Add get all paged to policy utils

--- policy/utils.go
+++ policy/utils.go
@@ -3,7 +3,9 @@ package policy
 import (
 	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/mongo-go-driver/mongo/options"
 	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 func Get(db *database.Database, policyId primitive.ObjectID) (
@@ -136,11 +138,92 @@ func GetAll(db *database.Database) (policies []*Policy, err error) {
 	return
 }
 
+func GetAllPaged(db *database.Database, query *bson.M,
+	page, pageCount int64) (polcies []*Policy, count int64, err error) {
+
+	coll := db.Policies()
+	polcies = []*Policy{}
+
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	}
+
+	maxPage := count / pageCount
+	if count == pageCount {
+		maxPage = 0
+	}
+	page = utils.Min64(page, maxPage)
+	skip := utils.Min64(page*pageCount, count)
+
+	cursor, err := coll.Find(
+		db,
+		query,
+		&options.FindOptions{
+			Sort: &bson.D{
+				{"name", 1},
+			},
+			Skip:  &skip,
+			Limit: &pageCount,
+		},
+	)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		polcy := &Policy{}
+		err = cursor.Decode(polcy)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		polcies = append(polcies, polcy)
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
 func Remove(db *database.Database, policyId primitive.ObjectID) (err error) {
 	coll := db.Policies()
 
 	_, err = coll.DeleteMany(db, &bson.M{
 		"_id": policyId,
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func RemoveMulti(db *database.Database, polcyIds []primitive.ObjectID) (
+	err error) {
+	coll := db.Policies()
+
+	_, err = coll.DeleteMany(db, &bson.M{
+		"_id": &bson.M{
+			"$in": polcyIds,
+		},
 	})
 	if err != nil {
 		err = database.ParseError(err)
