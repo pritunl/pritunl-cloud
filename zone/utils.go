b5f94f68014996355cae06259bf1a793650b35f0
Zachary Huff <zach.huff.386@gmail.com>
2025-03-22 13:09:57 -0400

    Add get all paged to zone utils

--- zone/utils.go
+++ zone/utils.go
@@ -5,6 +5,7 @@ import (
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
 	"github.com/pritunl/mongo-go-driver/mongo/options"
 	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 func Get(db *database.Database, zoneId primitive.ObjectID) (
@@ -62,6 +63,70 @@ func GetAll(db *database.Database) (zones []*Zone, err error) {
 		}
 
 		zones = append(zones, zne)
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func GetAllPaged(db *database.Database, query *bson.M,
+	page, pageCount int64) (znes []*Zone, count int64, err error) {
+
+	coll := db.Zones()
+	znes = []*Zone{}
+
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	}
+
+	maxPage := count / pageCount
+	if count == pageCount {
+		maxPage = 0
+	}
+	page = utils.Min64(page, maxPage)
+	skip := utils.Min64(page*pageCount, count)
+
+	cursor, err := coll.Find(
+		db,
+		query,
+		&options.FindOptions{
+			Sort: &bson.D{
+				{"name", 1},
+			},
+			Skip:  &skip,
+			Limit: &pageCount,
+		},
+	)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		zne := &Zone{}
+		err = cursor.Decode(zne)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		znes = append(znes, zne)
 	}
 
 	err = cursor.Err()
@@ -175,6 +240,23 @@ func Remove(db *database.Database, zoneId primitive.ObjectID) (err error) {
 		default:
 			return
 		}
+	}
+
+	return
+}
+
+func RemoveMulti(db *database.Database, zneIds []primitive.ObjectID) (
+	err error) {
+	coll := db.Zones()
+
+	_, err = coll.DeleteMany(db, &bson.M{
+		"_id": &bson.M{
+			"$in": zneIds,
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
 	}
 
 	return
