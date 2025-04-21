3e906b3c107d16dd4fb626c4bc67b96b5515c1c1
Zachary Huff <zach.huff.386@gmail.com>
2025-04-21 15:12:07 -0400

    Add relations

---
+++ relations/relations.go
@@ -0,0 +1,199 @@
+package relations
+
+import (
+	"fmt"
+
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/database"
+)
+
+type Query struct {
+	Id         any
+	Label      string
+	Collection string
+	Project    []Project
+	Relations  []Relation
+}
+
+type Relation struct {
+	Key          string
+	Label        string
+	From         string
+	LocalField   string
+	ForeignField string
+	Sort         map[string]int
+	Project      []Project
+	Relations    []Relation
+}
+
+type Project struct {
+	Key   string
+	Label string
+}
+
+func (r *Query) addRelation(pipeline []bson.M, relation Relation) []bson.M {
+	lookup := bson.M{
+		"from":         relation.From,
+		"localField":   relation.LocalField,
+		"foreignField": relation.ForeignField,
+		"as":           relation.From,
+	}
+
+	if len(relation.Project) > 0 || len(relation.Sort) > 0 ||
+		len(relation.Relations) > 0 {
+
+		nestedPipeline := []bson.M{}
+		if len(relation.Project) > 0 {
+			projection := bson.M{
+				"_id": 1,
+			}
+			for _, proj := range relation.Project {
+				projection[proj.Key] = 1
+			}
+			nestedPipeline = append(nestedPipeline, bson.M{
+				"$project": projection,
+			})
+		}
+
+		if len(relation.Sort) > 0 {
+			nestedPipeline = append(nestedPipeline, bson.M{
+				"$sort": relation.Sort,
+			})
+		}
+
+		for _, nestedRelation := range relation.Relations {
+			nestedPipeline = r.addRelation(nestedPipeline, nestedRelation)
+		}
+
+		lookup["pipeline"] = nestedPipeline
+	}
+
+	return append(pipeline, bson.M{
+		"$lookup": lookup,
+	})
+}
+
+func (r *Query) convertToResponse(doc bson.M) *Response {
+	response := &Response{
+		Id:        doc["_id"],
+		Label:     r.Label,
+		Fields:    []Field{},
+		Relations: []Related{},
+	}
+
+	for _, proj := range r.Project {
+		value, ok := doc[proj.Key]
+		if ok {
+			response.Fields = append(response.Fields, Field{
+				Key:   proj.Key,
+				Label: proj.Label,
+				Value: value,
+			})
+		}
+	}
+
+	for _, relation := range r.Relations {
+		docs, ok := doc[relation.From].(primitive.A)
+		if ok {
+			response.Relations = append(
+				response.Relations,
+				r.convertToRelated(relation, docs),
+			)
+		}
+	}
+
+	return response
+}
+
+func (r *Query) convertToRelated(relation Relation,
+	docs primitive.A) Related {
+
+	related := Related{
+		Label:     relation.Label,
+		Resources: []Resource{},
+	}
+
+	for _, docInf := range docs {
+		doc, ok := docInf.(bson.M)
+		if !ok {
+			continue
+		}
+
+		resource := Resource{
+			Id:        doc["_id"],
+			Type:      relation.Label,
+			Fields:    []Field{},
+			Relations: []Related{},
+		}
+
+		for _, proj := range relation.Project {
+			value, ok := doc[proj.Key]
+			if ok {
+				resource.Fields = append(resource.Fields, Field{
+					Key:   proj.Key,
+					Label: proj.Label,
+					Value: value,
+				})
+			}
+		}
+
+		for _, relation := range relation.Relations {
+			docs, ok := doc[relation.From].(primitive.A)
+			if ok {
+				resource.Relations = append(
+					resource.Relations,
+					r.convertToRelated(relation, docs),
+				)
+			}
+		}
+
+		related.Resources = append(related.Resources, resource)
+	}
+
+	return related
+}
+
+func (r *Query) Aggregate(db *database.Database) (*Response, error) {
+	coll := db.GetCollection(r.Collection)
+
+	pipeline := []bson.M{
+		{
+			"$match": bson.M{
+				"_id": r.Id,
+			},
+		},
+	}
+
+	if len(r.Project) > 0 {
+		projection := bson.M{"_id": 1}
+		for _, proj := range r.Project {
+			projection[proj.Key] = 1
+		}
+		pipeline = append(pipeline, bson.M{"$project": projection})
+	}
+
+	for _, relation := range r.Relations {
+		pipeline = r.addRelation(pipeline, relation)
+	}
+
+	cursor, err := coll.Aggregate(db, pipeline)
+	if err != nil {
+		return nil, fmt.Errorf("aggregation error: %w", err)
+	}
+	defer cursor.Close(db)
+
+	var results []bson.M
+	err = cursor.All(db, &results)
+	if err != nil {
+		return nil, fmt.Errorf("cursor decode error: %w", err)
+	}
+
+	if len(results) == 0 {
+		return nil, fmt.Errorf("resource with id %v not found", r.Id)
+	}
+
+	response := r.convertToResponse(results[0])
+
+	return response, nil
+}
