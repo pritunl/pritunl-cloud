8781aafacc7df28bedc5709cda13e808f1c31a2a
Zachary Huff <zach.huff.386@gmail.com>
2025-04-21 15:12:33 -0400

    Add relations utils

---
+++ relations/utils.go
@@ -0,0 +1,42 @@
+package relations
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+)
+
+func PrintPipeline(pipeline []bson.M) {
+	println("**************************************************")
+	for _, stage := range pipeline {
+		jsonData, err := json.MarshalIndent(stage, "", "  ")
+		if err != nil {
+			err = &errortypes.ParseError{
+				errors.Wrap(err, "relations: Failed to marshal json"),
+			}
+			fmt.Println(err.Error())
+			continue
+		}
+
+		fmt.Printf("%s\n", string(jsonData))
+	}
+	println("**************************************************")
+}
+
+func PrintResults(results []bson.M) {
+	jsonData, err := json.MarshalIndent(results, "", "  ")
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "relations: Failed to marshal json"),
+		}
+		fmt.Println(err.Error())
+		return
+	}
+
+	println("**************************************************")
+	fmt.Printf("%s\n", string(jsonData))
+	println("**************************************************")
+}
