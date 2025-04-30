41c7985eaae2b81adfaf181cd28f4860cd6e8ccb
Zachary Huff <zach.huff.386@gmail.com>
2025-04-30 18:52:19 -0400

    Clear challenges in redirect acme

--- redirect/acme.go
+++ redirect/acme.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sync"
+	"time"
 )
 
 type Challenge struct {
@@ -12,9 +13,28 @@ type Challenge struct {
 var (
 	challenges     = map[string]*Challenge{}
 	challengesLock = sync.Mutex{}
+	clearTimer     *time.Timer
+	timerLock      = sync.Mutex{}
 )
 
 func AddChallenge(chal *Challenge) {
+	timerLock.Lock()
+	if clearTimer != nil {
+		clearTimer.Stop()
+		clearTimer = nil
+	}
+
+	clearTimer = time.AfterFunc(60*time.Second, func() {
+		challengesLock.Lock()
+		challenges = map[string]*Challenge{}
+		challengesLock.Unlock()
+
+		timerLock.Lock()
+		clearTimer = nil
+		timerLock.Unlock()
+	})
+	timerLock.Unlock()
+
 	challengesLock.Lock()
 	challenges[chal.Token] = chal
 	challengesLock.Unlock()
