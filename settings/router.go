1f6a99ee9858e133f8982780b241bd5c0b5212dc
Zachary Huff <zach.huff.386@gmail.com>
2025-05-03 18:49:01 -0400

    Add force redirect systemd to router settings

--- settings/router.go
+++ settings/router.go
@@ -3,20 +3,21 @@ package settings
 var Router *router
 
 type router struct {
-	Id                  string `bson:"_id"`
-	ReadTimeout         int    `bson:"read_timeout" default:"300"`
-	ReadHeaderTimeout   int    `bson:"read_header_timeout" default:"60"`
-	WriteTimeout        int    `bson:"write_timeout" default:"300"`
-	IdleTimeout         int    `bson:"idle_timeout" default:"60"`
-	DialTimeout         int    `bson:"dial_timeout" default:"60"`
-	DialKeepAlive       int    `bson:"dial_keep_alive" default:"60"`
-	MaxIdleConns        int    `bson:"max_idle_conns" default:"1000"`
-	MaxIdleConnsPerHost int    `bson:"max_idle_conns_per_host" default:"100"`
-	IdleConnTimeout     int    `bson:"idle_conn_timeout" default:"90"`
-	HandshakeTimeout    int    `bson:"handshake_timeout" default:"10"`
-	ContinueTimeout     int    `bson:"continue_timeout" default:"10"`
-	MaxHeaderBytes      int    `bson:"max_header_bytes" default:"4194304"`
-	SkipVerify          bool   `bson:"skip_verify"`
+	Id                   string `bson:"_id"`
+	ReadTimeout          int    `bson:"read_timeout" default:"300"`
+	ReadHeaderTimeout    int    `bson:"read_header_timeout" default:"60"`
+	WriteTimeout         int    `bson:"write_timeout" default:"300"`
+	IdleTimeout          int    `bson:"idle_timeout" default:"60"`
+	DialTimeout          int    `bson:"dial_timeout" default:"60"`
+	DialKeepAlive        int    `bson:"dial_keep_alive" default:"60"`
+	MaxIdleConns         int    `bson:"max_idle_conns" default:"1000"`
+	MaxIdleConnsPerHost  int    `bson:"max_idle_conns_per_host" default:"100"`
+	IdleConnTimeout      int    `bson:"idle_conn_timeout" default:"90"`
+	HandshakeTimeout     int    `bson:"handshake_timeout" default:"10"`
+	ContinueTimeout      int    `bson:"continue_timeout" default:"10"`
+	MaxHeaderBytes       int    `bson:"max_header_bytes" default:"4194304"`
+	ForceRedirectSystemd bool   `bson:"force_redirect_systemd"`
+	SkipVerify           bool   `bson:"skip_verify"`
 }
 
 func newRouter() interface{} {
