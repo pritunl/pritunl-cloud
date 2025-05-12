dca86f3c5f42057bc09625b71d6d0caa5cc48214
Zachary Huff <zach.huff.386@gmail.com>
2025-05-12 10:21:14 -0400

    Update vga modes in node constants

--- node/constants.go
+++ node/constants.go
@@ -9,11 +9,15 @@ const (
 	Qemu = "qemu"
 	Kvm  = "kvm"
 
-	Std             = "std"
-	Vmware          = "vmware"
-	Virtio          = "virtio"
-	VirtioEgl       = "virtio_egl"
-	VirtioEglVulkan = "virtio_egl_vulkan"
+	Std               = "std"
+	Vmware            = "vmware"
+	Virtio            = "virtio"               // virtio-vga
+	VirtioPci         = "virtio_pci"           // virtio-gpu-pci
+	VirtioVgaGl       = "virtio_vga_gl"        // virtio-vga-gl
+	VirtioGl          = "virtio_gl"            // virtio-gpu-gl
+	VirtioGlVulkan    = "virtio_gl_vulkan"     // virtio-gpu-gl,venus=true
+	VirtioPciGl       = "virtio_pci_gl"        // virtio-gpu-gl-pci
+	VirtioPciGlVulkan = "virtio_pci_gl_vulkan" // virtio-gpu-gl-pci,venus=true
 
 	Sdl = "sdl"
 	Gtk = "gtk"
