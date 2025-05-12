fed4ca8c8d1418ef276442b0669e731a661dfe67
Zachary Huff <zach.huff.386@gmail.com>
2025-05-12 17:46:35 -0400

    Add concurrent to acme settings

--- settings/acme.go
+++ settings/acme.go
@@ -5,6 +5,7 @@ var Acme *acme
 type acme struct {
 	Id                string `bson:"_id"`
 	Url               string `bson:"url" default:"https://acme-v01.api.letsencrypt.org"`
+	DnsMaxConcurrent  int    `bson:"dns_max_concurrent" default:"10"`
 	DnsRetryRate      int    `bson:"dns_retry_rate" default:"3"`
 	DnsTimeout        int    `bson:"dns_timeout" default:"45"`
 	DnsDelay          int    `bson:"dns_delay" default:"15"`
