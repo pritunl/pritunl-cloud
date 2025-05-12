9dc4595e36fbac40a04746f1b1703b63a5793d61
Zachary Huff <zach.huff.386@gmail.com>
2025-05-12 17:46:46 -0400

    Add compare slices to misc utils

--- utils/misc.go
+++ utils/misc.go
@@ -277,3 +277,29 @@ func IsSystemd() bool {
 	isSystemd = &isSysd
 	return false
 }
+
+func CompareStringSlices(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func CompareStringSlicesUnsorted(a, b []string) bool {
+	aSet := set.NewSet()
+	for _, val := range a {
+		aSet.Add(val)
+	}
+
+	bSet := set.NewSet()
+	for _, val := range b {
+		bSet.Add(val)
+	}
+
+	return aSet.IsEqual(bSet)
+}
