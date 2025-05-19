778b17014aeeb3bd6190bc6a22a181ec2206bf38
Zachary Huff <zach.huff.386@gmail.com>
2025-05-19 06:40:34 -0400

    Add virtiofs

---
+++ virtiofs/virtiofs.go
@@ -0,0 +1,91 @@
+package virtiofs
+
+import (
+	"time"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/errortypes"
+	"github.com/pritunl/pritunl-cloud/node"
+	"github.com/pritunl/pritunl-cloud/organization"
+	"github.com/pritunl/pritunl-cloud/paths"
+	"github.com/pritunl/pritunl-cloud/systemd"
+	"github.com/pritunl/pritunl-cloud/utils"
+	"github.com/pritunl/pritunl-cloud/vm"
+)
+
+func StartAll(db *database.Database,
+	virt *vm.VirtualMachine) (err error) {
+
+	matchPath := false
+	matchRoles := false
+	mounts := []*vm.Mount{}
+
+	if len(virt.Mounts) == 0 {
+		return
+	}
+
+	org, err := organization.Get(db, virt.Organization)
+	if err != nil {
+		return
+	}
+
+	if org == nil {
+		err = &errortypes.ParseError{
+			errors.New("virtiofs: Failed to get org"),
+		}
+		return
+	}
+
+	for _, mount := range virt.Mounts {
+		for _, share := range node.Self.Shares {
+			if share.MatchPath(mount.Path) {
+				matchPath = true
+
+				if utils.HasMatchingItem(share.Roles, org.Roles) {
+					matchRoles = true
+					break
+				}
+			}
+		}
+
+		if !matchPath && !matchRoles {
+			err = &errortypes.ParseError{
+				errors.Newf("virtiofs: Failed to find matching "+
+					"share path for mount '%s'", mount.Path),
+			}
+			return
+		}
+
+		if !matchPath || !matchRoles {
+			err = &errortypes.ParseError{
+				errors.Newf("virtiofs: Failed to find matching "+
+					"role for mount '%s'", mount.Path),
+			}
+			return
+		}
+
+		mounts = append(mounts, mount)
+	}
+
+	for _, mount := range mounts {
+		shareId := paths.GetShareId(virt.Id, mount.Name)
+
+		err = Start(db, virt, shareId, mount.Path)
+		if err != nil {
+			return
+		}
+	}
+
+	time.Sleep(1 * time.Second)
+
+	return
+}
+
+func StopAll(virt *vm.VirtualMachine) (err error) {
+	unit := paths.GetUnitNameShares(virt.Id)
+
+	_ = systemd.Stop(unit)
+
+	return
+}
