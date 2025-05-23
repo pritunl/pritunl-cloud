b46d22ffbce02b859a65394749d010cd9b84192d
Zachary Huff <zach.huff.386@gmail.com>
2025-05-23 18:48:34 -0400

    Add wg to datacenter constants

--- datacenter/constants.go
+++ datacenter/constants.go
@@ -1,6 +1,10 @@
 package datacenter
 
 const (
-	Default   = "default"
-	VxlanVlan = "vxlan_vlan"
+	Default     = "default"
+	VxlanVlan   = "vxlan_vlan"
+	WgVxlanVlan = "wg_vxlan_vlan"
+
+	Wg4 = "wg4"
+	Wg6 = "wg6"
 )
