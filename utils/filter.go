14fead3111153648ca63ec1c6f37b79b53924df2
Zachary Huff <zach.huff.386@gmail.com>
2025-05-25 17:12:25 -0400

    Add filter name cmd to filter utils

--- utils/filter.go
+++ utils/filter.go
@@ -75,6 +75,73 @@ var nameSafeChar = set.NewSet(
 	'.',
 )
 
+var nameCmdSafeChar = set.NewSet(
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'-',
+	'.',
+)
+
 func FilterName(s string) string {
 	if len(s) == 0 {
 		return ""
@@ -90,12 +157,35 @@ func FilterName(s string) string {
 
 	ns := ""
 	for _, c := range s {
-		if safeChars.Contains(c) {
+		if nameSafeChar.Contains(c) {
 			ns += string(c)
 		}
 	}
 
 	return ns
+}
+
+func FilterNameCmd(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	if s == "self" {
+		s = "invalid-name"
+	}
+
+	if len(s) > nameSafeLimit {
+		s = s[:nameSafeLimit]
+	}
+
+	ns := ""
+	for _, c := range s {
+		if nameCmdSafeChar.Contains(c) {
+			ns += string(c)
+		}
+	}
+
+	return strings.ToLower(ns)
 }
 
 func FilterDomain(s string) string {
