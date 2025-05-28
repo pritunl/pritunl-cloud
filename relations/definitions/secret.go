0c1a5a52b2a26b37098e0585341701beca76b0f0
Zachary Huff <zach.huff.386@gmail.com>
2025-05-28 18:51:47 -0400

    Add secret to relations

---
+++ relations/definitions/secret.go
@@ -0,0 +1,54 @@
+package definitions
+
+import (
+	"github.com/pritunl/pritunl-cloud/relations"
+)
+
+var Secret = relations.Query{
+	Label:      "Secret",
+	Collection: "secrets",
+	Project: []relations.Project{{
+		Key:   "name",
+		Label: "Name",
+	}},
+	Relations: []relations.Relation{{
+		Key:          "domains",
+		Label:        "Domain",
+		From:         "domains",
+		LocalField:   "_id",
+		ForeignField: "secret",
+		Sort: map[string]int{
+			"name": 1,
+		},
+		Project: []relations.Project{{
+			Key:   "name",
+			Label: "Name",
+		}, {
+			Key:   "root_domain",
+			Label: "Root Domain",
+		}},
+	}, {
+		Key:          "certificates",
+		Label:        "Certificate",
+		From:         "certificates",
+		LocalField:   "_id",
+		ForeignField: "acme_secret",
+		Sort: map[string]int{
+			"name": 1,
+		},
+		Project: []relations.Project{{
+			Key:   "name",
+			Label: "Name",
+		}, {
+			Key:   "type",
+			Label: "Type",
+		}, {
+			Key:   "acme_domains",
+			Label: "Lets Encrypt Domains",
+		}},
+	}},
+}
+
+func init() {
+	relations.Register("secret", Secret)
+}
