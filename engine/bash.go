fb2f36c8ee103366dd1cf67178c56bf32cb5ba08
Zachary Huff <zach.huff.386@gmail.com>
2025-06-02 13:39:23 -0400

    Update bash engine run

--- engine/bash.go
+++ engine/bash.go
@@ -98,7 +98,7 @@ func (b *BashEngine) streamErr(reader io.Reader) {
 }
 
 func (b *BashEngine) Run(block string) (err error) {
-	cmd := exec.Command(b.shell, "-x", "-c", block+shellEnvExport)
+	cmd := exec.Command(b.shell, "-v", "-c", block+shellEnvExport)
 	cmd.Env = b.starter.GetEnviron()
 	cmd.Dir = b.starter.GetCwd()
 
