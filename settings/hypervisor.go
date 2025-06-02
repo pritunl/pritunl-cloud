8756d7bd50b682b54e0021fc2dd70e275e5ee200
Zachary Huff <zach.huff.386@gmail.com>
2025-06-02 13:39:45 -0400

    Add usb ports to hypervisor settings

--- settings/hypervisor.go
+++ settings/hypervisor.go
@@ -52,6 +52,8 @@ type hypervisor struct {
 	ImdsPort              int    `bson:"imds_port" default:"80"`
 	ImdsSyncLogTimeout    int    `bson:"imds_sync_log_timeout" default:"20"`
 	InfoTtl               int    `bson:"info_ttl" default:"5"`
+	UsbHsPorts            int    `bson:"usb_hs_ports" default:"4"`
+	UsbSsPorts            int    `bson:"usb_ss_ports" default:"4"`
 	JournalDisplayLimit   int64  `bson:"journal_display_limit" default:"5000"`
 	DnsServerPrimary      string `bson:"dns_server_primary" default:"8.8.8.8"`
 	DnsServerSecondary    string `bson:"dns_server_secondary" default:"8.8.4.4"`
