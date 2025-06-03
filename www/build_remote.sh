91227315088429822bd05d7ca791bf0247cc4bf7
Zachary Huff <zach.huff.386@gmail.com>
2025-06-03 19:05:55 -0400

    Update www build remote

--- www/build_remote.sh
+++ www/build_remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/go/src/github.com/pritunl/pritunl-cloud/www/ $NPM_SERVER:/home/cloud/pritunl-cloud-www/
+rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/git/pritunl-cloud/www/ $NPM_SERVER:/home/cloud/pritunl-cloud-www/
 
 ssh cloud@$NPM_SERVER "
 cd /home/cloud/pritunl-cloud-www/
@@ -12,15 +12,15 @@ ln -sf ./esm/* ./
 cd ../../../../
 "
 
-scp $NPM_SERVER:/home/cloud/pritunl-cloud-www/package.json /home/cloud/go/src/github.com/pritunl/pritunl-cloud/www/package.json
-scp $NPM_SERVER:/home/cloud/pritunl-cloud-www/package-lock.json /home/cloud/go/src/github.com/pritunl/pritunl-cloud/www/package-lock.json
-rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-cloud-www/node_modules/ /home/cloud/go/src/github.com/pritunl/pritunl-cloud/www/node_modules/
-rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/go/src/github.com/pritunl/pritunl-cloud/www/ $NPM_SERVER:/home/cloud/pritunl-cloud-www/
+scp $NPM_SERVER:/home/cloud/pritunl-cloud-www/package.json /home/cloud/git/pritunl-cloud/www/package.json
+scp $NPM_SERVER:/home/cloud/pritunl-cloud-www/package-lock.json /home/cloud/git/pritunl-cloud/www/package-lock.json
+rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-cloud-www/node_modules/ /home/cloud/git/pritunl-cloud/www/node_modules/
+rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/git/pritunl-cloud/www/ $NPM_SERVER:/home/cloud/pritunl-cloud-www/
 
 ssh cloud@$NPM_SERVER "
 cd /home/cloud/pritunl-cloud-www/
 sh build.sh
 "
 
-rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-cloud-www/dist/ /home/cloud/go/src/github.com/pritunl/pritunl-cloud/www/dist/
-rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-cloud-www/dist-dev/ /home/cloud/go/src/github.com/pritunl/pritunl-cloud/www/dist-dev/
+rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-cloud-www/dist/ /home/cloud/git/pritunl-cloud/www/dist/
+rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-cloud-www/dist-dev/ /home/cloud/git/pritunl-cloud/www/dist-dev/
