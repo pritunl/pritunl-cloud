a88e9ad510f9bc4104044cfda9399400730a6f57
Zachary Huff <zach.huff.386@gmail.com>
2025-06-06 07:34:37 -0400

    Add instance state to upgrade

---
+++ upgrade/state.go
@@ -0,0 +1,31 @@
+package upgrade
+
+import (
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/pritunl-cloud/database"
+)
+
+func instStateUpgrade(db *database.Database) (err error) {
+	coll := db.Instances()
+
+	_, err = coll.UpdateMany(db, bson.M{
+		"virt_state": bson.M{
+			"$exists": true,
+		},
+	}, []bson.M{
+		bson.M{
+			"$set": bson.M{
+				"state": "$virt_state",
+			},
+		},
+		bson.M{
+			"$unset": "virt_state",
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
