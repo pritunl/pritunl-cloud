6b18f3dee7f1e26ba0480538d06c6bc496a0dca9
Zachary Huff <zach.huff.386@gmail.com>
2025-06-09 17:55:12 -0400

    Add firewalls to state

---
+++ state/firewalls.go
@@ -0,0 +1,87 @@
+package state
+
+import (
+	"github.com/dropbox/godropbox/container/set"
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/arp"
+	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/firewall"
+	"github.com/pritunl/pritunl-cloud/node"
+)
+
+var (
+	Firewalls    = &FirewallsState{}
+	FirewallsPkg = NewPackage(Firewalls)
+)
+
+type FirewallsState struct {
+	nodeFirewall       []*firewall.Rule
+	firewalls          map[string][]*firewall.Rule
+	firewallMaps       map[string][]*firewall.Mapping
+	instanceNamespaces map[primitive.ObjectID][]string
+	arpRecords         map[string]set.Set
+}
+
+func (p *FirewallsState) NodeFirewall() []*firewall.Rule {
+	return p.nodeFirewall
+}
+
+func (p *FirewallsState) Firewalls() map[string][]*firewall.Rule {
+	return p.firewalls
+}
+
+func (p *FirewallsState) FirewallMaps() map[string][]*firewall.Mapping {
+	return p.firewallMaps
+}
+
+func (p *FirewallsState) GetInstanceNamespaces(
+	instId primitive.ObjectID) []string {
+
+	return p.instanceNamespaces[instId]
+}
+
+func (p *FirewallsState) ArpRecords(namespace string) set.Set {
+	return p.arpRecords[namespace]
+}
+
+func (p *FirewallsState) Refresh(pkg *Package,
+	db *database.Database) (err error) {
+
+	specRules, err := firewall.GetSpecRules(Instances.Instances(),
+		Deployments.DeploymentsNode(), Deployments.SpecsMap(),
+		Deployments.SpecsUnitsMap(), Deployments.DeploymentsDeployed())
+	if err != nil {
+		return
+	}
+
+	nodeFirewall, firewalls, firewallMaps, instNamespaces, err :=
+		firewall.GetAllIngress(db, node.Self, Instances.Instances(),
+			specRules, Instances.NodePortsMap())
+	if err != nil {
+		return
+	}
+	p.nodeFirewall = nodeFirewall
+	p.firewalls = firewalls
+	p.firewallMaps = firewallMaps
+	p.instanceNamespaces = instNamespaces
+
+	p.arpRecords = arp.BuildState(Instances.Instances(),
+		Vpcs.VpcsMap(), Vpcs.VpcIpsMap())
+
+	return
+}
+
+func (p *FirewallsState) Apply(st *State) {
+	st.NodeFirewall = p.NodeFirewall
+	st.Firewalls = p.Firewalls
+	st.FirewallMaps = p.FirewallMaps
+	st.GetInstanceNamespaces = p.GetInstanceNamespaces
+	st.ArpRecords = p.ArpRecords
+}
+
+func init() {
+	FirewallsPkg.
+		After(Instances).
+		After(Vpcs).
+		After(Deployments)
+}
