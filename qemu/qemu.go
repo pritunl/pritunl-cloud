db2d2dccd54eb2a870b5cbc1e7854d6ba815b2f8
Zachary Huff <zach.huff.386@gmail.com>
2025-06-09 17:53:27 -0400

    Update memory options in qemu

--- qemu/qemu.go
+++ qemu/qemu.go
@@ -399,7 +399,7 @@ func (q *Qemu) Marshal() (output string, err error) {
 			cmd = append(cmd, "-object")
 			cmd = append(cmd, fmt.Sprintf(
 				"memory-backend-file,id=pc.ram,"+
-					"size=%dM,mem-path=%s,prealloc=off,share=%s,merge=on",
+					"size=%dM,mem-path=%s,prealloc=on,share=%s,merge=on",
 				q.Memory,
 				paths.GetHugepagePath(q.Id),
 				memShare,
