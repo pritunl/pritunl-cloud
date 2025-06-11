e272252b48251a334c44e5448b39bc21b00d20b5
Zachary Huff <zach.huff.386@gmail.com>
2025-06-11 15:34:19 -0400

    Add global org to authority constants

--- authority/constants.go
+++ authority/constants.go
@@ -1,6 +1,14 @@
 package authority
 
+import (
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+)
+
 const (
 	SshKey         = "ssh_key"
 	SshCertificate = "ssh_certificate"
+)
+
+var (
+	Global = primitive.NilObjectID
 )
