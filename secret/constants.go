9428f19180d4b02c16c657b5776f4f160acfeffe
Zachary Huff <zach.huff.386@gmail.com>
2025-06-14 20:14:40 -0400

    Add global to secret constants

--- secret/constants.go
+++ secret/constants.go
@@ -1,7 +1,15 @@
 package secret
 
+import (
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+)
+
 const (
 	AWS         = "aws"
 	Cloudflare  = "cloudflare"
 	OracleCloud = "oracle_cloud"
+)
+
+var (
+	Global = primitive.NilObjectID
 )
