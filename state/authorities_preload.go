d560bd9ef768a1838f95c91c5c46f15d6b2a5b59
Zachary Huff <zach.huff.386@gmail.com>
2025-06-17 13:17:24 -0400

    Add authorities preload to state

---
+++ state/authorities_preload.go
@@ -0,0 +1,53 @@
+package state
+
+import (
+	"github.com/dropbox/godropbox/container/set"
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/pritunl-cloud/authority"
+	"github.com/pritunl/pritunl-cloud/database"
+)
+
+var (
+	AuthoritiesPreload    = &AuthoritiesPreloadState{}
+	AuthoritiesPreloadPkg = NewPackage(AuthoritiesPreload)
+)
+
+type AuthoritiesPreloadState struct {
+	authoritiesMap      map[string][]*authority.Authority
+	authoritiesRolesSet set.Set
+}
+
+func (p *AuthoritiesPreloadState) Authorities() map[string][]*authority.Authority {
+	return p.authoritiesMap
+}
+
+func (p *AuthoritiesPreloadState) RolesSet() set.Set {
+	return p.authoritiesRolesSet
+}
+
+func (p *AuthoritiesPreloadState) Refresh(pkg *Package,
+	db *database.Database) (err error) {
+
+	roles, rolesSet := Instances.GetRoles()
+	if len(roles) == 0 {
+		p.authoritiesMap = map[string][]*authority.Authority{}
+		p.authoritiesRolesSet = set.NewSet()
+		return
+	}
+
+	authrsMap, err := authority.GetMapRoles(db, &bson.M{
+		"network_roles": &bson.M{
+			"$in": roles,
+		},
+	})
+	if err != nil {
+		return
+	}
+	p.authoritiesMap = authrsMap
+	p.authoritiesRolesSet = rolesSet
+
+	return
+}
+
+func (p *AuthoritiesPreloadState) Apply(st *State) {
+}
