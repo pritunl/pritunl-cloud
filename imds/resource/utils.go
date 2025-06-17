e26f73e5184542b935f0eacf9d09155295eb9436
Zachary Huff <zach.huff.386@gmail.com>
2025-06-16 20:33:37 -0400

    Add json type to imds resource selector

--- imds/resource/utils.go
+++ imds/resource/utils.go
@@ -1,8 +1,10 @@
 package resource
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 
 	"github.com/dropbox/godropbox/errors"
@@ -10,7 +12,7 @@ import (
 	"github.com/pritunl/pritunl-cloud/errortypes"
 )
 
-func selector(v interface{}, key string) (val string, err error) {
+func selector(v interface{}, key string, isJson bool) (val string, err error) {
 	valRef := reflect.ValueOf(v)
 
 	if valRef.Kind() != reflect.Ptr || valRef.IsNil() {
@@ -26,6 +28,17 @@ func selector(v interface{}, key string) (val string, err error) {
 			errors.New("Selector kind invalid"),
 		}
 		return
+	}
+
+	jsonKey := ""
+	if isJson {
+		keys := strings.SplitN(key, ".", 2)
+		if len(keys) == 2 {
+			key = keys[0]
+			jsonKey = keys[1]
+		} else {
+			isJson = false
+		}
 	}
 
 	typ := elm.Type()
@@ -48,6 +61,23 @@ func selector(v interface{}, key string) (val string, err error) {
 				val = strings.Join(elements, ",")
 			} else {
 				val = selectString(elm.Field(i).Interface())
+
+				if isJson && jsonKey != "" {
+					var jsonData map[string]any
+					err = json.Unmarshal([]byte(val), &jsonData)
+					if err != nil {
+						val = ""
+						return
+					}
+
+					jsonValue, exists := jsonData[jsonKey]
+					if !exists {
+						val = ""
+						return
+					}
+
+					val = jsonValString(jsonValue)
+				}
 			}
 
 			return
@@ -55,6 +85,26 @@ func selector(v interface{}, key string) (val string, err error) {
 	}
 
 	return
+}
+
+func jsonValString(value any) string {
+	switch val := value.(type) {
+	case string:
+		return val
+	case bool:
+		return strconv.FormatBool(val)
+	case float64:
+		return strconv.FormatFloat(val, 'f', -1, 64)
+	case int:
+		return strconv.Itoa(val)
+	case int64:
+		return strconv.FormatInt(val, 10)
+	case nil:
+		return ""
+	default:
+		jsonBytes, _ := json.Marshal(val)
+		return string(jsonBytes)
+	}
 }
 
 func selectString(obj interface{}) string {
