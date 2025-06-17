28bea4e39c2f2165e6ec0c940e8ee7b978e947ea
Zachary Huff <zach.huff.386@gmail.com>
2025-06-17 13:17:33 -0400

    Add authorities to state

---
+++ state/authorities.go
@@ -0,0 +1,82 @@
+package state
+
+import (
+	"github.com/dropbox/godropbox/container/set"
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/authority"
+	"github.com/pritunl/pritunl-cloud/database"
+)
+
+var (
+	Authorities    = &AuthoritiesState{}
+	AuthoritiesPkg = NewPackage(Authorities)
+)
+
+type AuthoritiesState struct {
+	authoritiesMap map[string][]*authority.Authority
+}
+
+func (p *AuthoritiesState) GetInstaceAuthorities(
+	orgId primitive.ObjectID, roles []string) []*authority.Authority {
+
+	authrSet := set.NewSet()
+	authrs := []*authority.Authority{}
+
+	for _, role := range roles {
+		for _, authr := range p.authoritiesMap[role] {
+			if authrSet.Contains(authr.Id) || authr.Organization != orgId {
+				continue
+			}
+			authrSet.Add(authr.Id)
+			authrs = append(authrs, authr)
+		}
+	}
+
+	return authrs
+}
+
+func (p *AuthoritiesState) Refresh(pkg *Package,
+	db *database.Database) (err error) {
+
+	_, rolesSet := Instances.GetRoles()
+	authorities := AuthoritiesPreload.Authorities()
+	preloadRolesSet := AuthoritiesPreload.RolesSet()
+	roles := rolesSet.Copy()
+	roles.Subtract(preloadRolesSet)
+
+	missRoles := []string{}
+	for roleInf := range roles.Iter() {
+		missRoles = append(missRoles, roleInf.(string))
+	}
+
+	if len(missRoles) > 0 {
+		missAuthorities, e := authority.GetMapRoles(db, &bson.M{
+			"network_roles": &bson.M{
+				"$in": missRoles,
+			},
+		})
+		if e != nil {
+			err = e
+			return
+		}
+
+		for role, authrs := range missAuthorities {
+			authorities[role] = authrs
+		}
+	}
+
+	p.authoritiesMap = authorities
+
+	return
+}
+
+func (p *AuthoritiesState) Apply(st *State) {
+	st.GetInstaceAuthorities = p.GetInstaceAuthorities
+}
+
+func init() {
+	AuthoritiesPkg.
+		After(AuthoritiesPreload).
+		After(Instances)
+}
