39daab0dab11481d8aa652215dc8b981f79de8cc
Zachary Huff <zach.huff.386@gmail.com>
2025-06-18 16:13:56 -0400

    Remove instances load from instances state

--- state/instances.go
+++ state/instances.go
@@ -1,14 +1,10 @@
 package state
 
 import (
-	"sync"
-
 	"github.com/dropbox/godropbox/container/set"
-	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/instance"
-	"github.com/pritunl/pritunl-cloud/node"
 	"github.com/pritunl/pritunl-cloud/nodeport"
 )
 
@@ -18,28 +14,9 @@ var (
 )
 
 type InstancesState struct {
-	roles        []string
-	rolesSet     set.Set
-	rolesLock    sync.Mutex
 	instances    []*instance.Instance
 	instancesMap map[primitive.ObjectID]*instance.Instance
 	nodePortsMap map[string][]*nodeport.Mapping
-}
-
-func (p *InstancesState) GetRoles() (roles []string, rolesSet set.Set) {
-	p.rolesLock.Lock()
-	roles = p.roles
-	rolesSet = p.rolesSet
-	p.rolesLock.Unlock()
-	return
-}
-
-func (p *InstancesState) setRoles(roles []string, rolesSet set.Set) {
-	p.rolesLock.Lock()
-	p.roles = roles
-	p.rolesSet = rolesSet
-	p.rolesLock.Unlock()
-	return
 }
 
 func (p *InstancesState) GetInstace(
@@ -62,20 +39,14 @@ func (p *InstancesState) NodePortsMap() map[string][]*nodeport.Mapping {
 func (p *InstancesState) Refresh(pkg *Package,
 	db *database.Database) (err error) {
 
-	ndeId := node.Self.Id
-
-	instances, err := instance.GetAllVirtMapped(db, &bson.M{
-		"node": ndeId,
-	}, Pools.NodePools(), Disks.InstaceDisksMap())
-	if err != nil {
-		return
-	}
+	instances := InstancesPreload.GetInstances()
+	instances = instance.LoadAllVirt(instances,
+		Pools.NodePools(), Disks.InstaceDisksMap())
 
 	p.instances = instances
 
 	instId := set.NewSet()
 	instancesMap := map[primitive.ObjectID]*instance.Instance{}
-	rolesSet := set.NewSet()
 	nodePortsMap := map[string][]*nodeport.Mapping{}
 	for _, inst := range instances {
 		instId.Add(inst.Id)
@@ -83,28 +54,9 @@ func (p *InstancesState) Refresh(pkg *Package,
 
 		nodePortsMap[inst.NetworkNamespace] = append(
 			nodePortsMap[inst.NetworkNamespace], inst.NodePorts...)
-
-		for _, role := range inst.NetworkRoles {
-			rolesSet.Add(role)
-		}
 	}
 	p.instancesMap = instancesMap
 	p.nodePortsMap = nodePortsMap
-
-	nde := node.Self
-	if nde.Firewall {
-		roles := nde.NetworkRoles
-		for _, role := range roles {
-			rolesSet.Add(role)
-		}
-	}
-
-	roles := []string{}
-	for instRoleInf := range rolesSet.Iter() {
-		roles = append(roles, instRoleInf.(string))
-	}
-
-	p.setRoles(roles, rolesSet)
 
 	return
 }
@@ -117,6 +69,7 @@ func (p *InstancesState) Apply(st *State) {
 
 func init() {
 	InstancesPkg.
+		After(InstancesPreload).
 		After(Disks).
 		After(Pools)
 }
