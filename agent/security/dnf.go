f5482c701adcb9bff928228183e465045a1cf889
Zachary Huff <zach.huff.386@gmail.com>
2025-06-19 15:53:47 -0400

    Sort updates in agent security

--- agent/security/dnf.go
+++ agent/security/dnf.go
@@ -35,6 +35,10 @@ func dnfGetReport() (report *Report) {
 
 	lines := strings.Split(string(resp.Output), "\n")
 
+	moderateUpdates := []*Update{}
+	importantUpdates := []*Update{}
+	criticalUpdates := []*Update{}
+
 	for _, line := range lines {
 		line = strings.TrimSpace(line)
 		if line == "" || strings.HasPrefix(line, "Last metadata") {
@@ -48,22 +52,32 @@ func dnfGetReport() (report *Report) {
 			pkg := parts[2]
 
 			if strings.Contains(strings.ToLower(severity), "moderate") {
-				severity = "moderate"
+				moderateUpdates = append(moderateUpdates, &Update{
+					Advisory: advisory,
+					Severity: "moderate",
+					Package:  pkg,
+				})
 			} else if strings.Contains(strings.ToLower(severity), "important") {
-				severity = "important"
+				importantUpdates = append(importantUpdates, &Update{
+					Advisory: advisory,
+					Severity: "important",
+					Package:  pkg,
+				})
 			} else if strings.Contains(strings.ToLower(severity), "critical") {
-				severity = "critical"
+				criticalUpdates = append(criticalUpdates, &Update{
+					Advisory: advisory,
+					Severity: "critical",
+					Package:  pkg,
+				})
 			} else {
 				continue
 			}
-
-			report.Updates = append(report.Updates, &Update{
-				Advisory: advisory,
-				Severity: severity,
-				Package:  pkg,
-			})
 		}
 	}
+
+	report.Updates = append(report.Updates, criticalUpdates...)
+	report.Updates = append(report.Updates, importantUpdates...)
+	report.Updates = append(report.Updates, moderateUpdates...)
 
 	return
 }
