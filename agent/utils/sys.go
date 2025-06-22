1ee9899471415f37f17eba8bb408c46ac7b158fc
Zachary Huff <zach.huff.386@gmail.com>
2025-06-22 19:44:09 -0400

    Add read to agent utils

--- agent/utils/sys.go
+++ agent/utils/sys.go
@@ -1,16 +1,28 @@
 package utils
 
 import (
+	"io/ioutil"
 	"os"
 	"time"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-cloud/errortypes"
 )
+
+func Read(path string) (data string, err error) {
+	dataByt, err := ioutil.ReadFile(path)
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrapf(err, "utils: Failed to read '%s'", path),
+		}
+		return
+	}
+
+	data = string(dataByt)
+	return
+}
 
 func DelayExit(code int, delay time.Duration) {
 	time.Sleep(delay)
 	os.Exit(code)
-}
-
-func IsDnf() bool {
-	_, err := os.Stat("/usr/bin/dnf")
-	return err == nil
 }
