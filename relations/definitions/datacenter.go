4d0a8ebbf772f59aab6ae9d2bab22da2cd9817b8
Zachary Huff <zach.huff.386@gmail.com>
2025-06-26 17:12:24 -0400

    Add instance to datacenter relation defintition

--- relations/definitions/datacenter.go
+++ relations/definitions/datacenter.go
@@ -1,7 +1,13 @@
 package definitions
 
 import (
+	"time"
+
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/instance"
 	"github.com/pritunl/pritunl-cloud/relations"
+	"github.com/pritunl/pritunl-cloud/systemd"
+	"github.com/pritunl/pritunl-cloud/vm"
 )
 
 var Datacenter = relations.Query{
@@ -102,7 +108,7 @@ var Datacenter = relations.Query{
 		Label:        "Deployment",
 		From:         "deployments",
 		LocalField:   "_id",
-		ForeignField: "unit",
+		ForeignField: "datacenter",
 		BlockDelete:  true,
 		Sort: map[string]int{
 			"name": 1,
@@ -128,7 +134,7 @@ var Datacenter = relations.Query{
 		Label:        "Instance",
 		From:         "instances",
 		LocalField:   "_id",
-		ForeignField: "deployment",
+		ForeignField: "datacenter",
 		Sort: map[string]int{
 			"name": 1,
 		},
@@ -136,14 +142,111 @@ var Datacenter = relations.Query{
 			Key:   "name",
 			Label: "Name",
 		}, {
-			Key:   "state",
-			Label: "State",
-		}, {
-			Key:   "status",
+			Keys: []string{
+				"action",
+				"state",
+			},
 			Label: "Status",
+			Format: func(vals ...any) any {
+				action, _ := vals[0].(string)
+				state, _ := vals[1].(string)
+
+				switch action {
+				case instance.Start:
+					switch state {
+					case vm.Starting:
+						return "Starting"
+					case vm.Running:
+						return "Running"
+					case vm.Stopped:
+						return "Starting"
+					case vm.Failed:
+						return "Starting"
+					case vm.Updating:
+						return "Updating"
+					case vm.Provisioning:
+						return "Provisioning"
+					case "":
+						return "Provisioning"
+					}
+				case instance.Cleanup:
+					switch state {
+					case vm.Starting:
+						return "Stopping"
+					case vm.Running:
+						return "Stopping"
+					case vm.Stopped:
+						return "Stopping"
+					case vm.Failed:
+						return "Stopping"
+					case vm.Updating:
+						return "Updating"
+					case vm.Provisioning:
+						return "Stopping"
+					case "":
+						return "Stopping"
+					}
+				case instance.Stop:
+					switch state {
+					case vm.Starting:
+						return "Stopping"
+					case vm.Running:
+						return "Stopping"
+					case vm.Stopped:
+						return "Stopped"
+					case vm.Failed:
+						return "Failed"
+					case vm.Updating:
+						return "Updating"
+					case vm.Provisioning:
+						return "Stopped"
+					case "":
+						return "Stopped"
+					}
+				case instance.Restart:
+					return "Restarting"
+				case instance.Destroy:
+					return "Destroying"
+				}
+
+				return state
+			},
 		}, {
-			Key:   "virt_timestamp",
-			Label: "Virtual Timestamp",
+			Keys: []string{
+				"virt_timestamp",
+				"action",
+				"state",
+			},
+			Label: "Uptime",
+			Format: func(vals ...any) any {
+				val := vals[0]
+				action, _ := vals[1].(string)
+				state, _ := vals[2].(string)
+				isActive := action == instance.Start ||
+					state == vm.Running || state == vm.Starting ||
+					state == vm.Provisioning
+
+				if !isActive {
+					return "-"
+				}
+
+				if mongoTime, ok := val.(primitive.DateTime); ok {
+					valTime := mongoTime.Time()
+					return systemd.FormatUptimeShort(valTime)
+				}
+
+				if goTime, ok := val.(time.Time); ok {
+					return systemd.FormatUptimeShort(goTime)
+				}
+
+				return "-"
+			},
+		}, {
+			Key:   "processors",
+			Label: "Processors",
+		}, {
+			Key:   "memory",
+			Label: "Memory",
 		}, {
 			Key:   "private_ips",
 			Label: "Private IPv4",
@@ -156,7 +259,8 @@ var Datacenter = relations.Query{
 		Label:        "Disk",
 		From:         "disks",
 		LocalField:   "_id",
-		ForeignField: "deployment",
+		ForeignField: "datacenter",
+		BlockDelete:  true,
 		Sort: map[string]int{
 			"index": 1,
 		},
