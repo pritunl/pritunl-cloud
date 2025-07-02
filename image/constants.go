c75931b7f9251c11a8e2b7faf7a97675ad020861
Zachary Huff <zach.huff.386@gmail.com>
2025-07-02 10:24:43 -0400

    Add releases to image constants

--- image/constants.go
+++ image/constants.go
@@ -2,6 +2,7 @@ package image
 
 import (
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/tools/set"
 )
 
 const (
@@ -12,8 +13,39 @@ const (
 	Linux         = "linux"
 	LinuxUnsigned = "linux_unsigned"
 	Bsd           = "bsd"
+
+	AlmaLinux8    = "almalinux8"
+	AlmaLinux9    = "almalinux9"
+	AlmaLinux10   = "almalinux10"
+	AlpineLinux   = "alpinelinux"
+	Fedora42      = "fedora42"
+	FreeBSD       = "freebsd"
+	OracleLinux7  = "oraclelinux7"
+	OracleLinux8  = "oraclelinux8"
+	OracleLinux9  = "oraclelinux9"
+	OracleLinux10 = "oraclelinux10"
+	RockyLinux8   = "rockylinux8"
+	RockyLinux9   = "rockylinux9"
+	RockyLinux10  = "rockylinux10"
+	Ubuntu2404    = "ubuntu2404"
 )
 
 var (
-	Global = primitive.NilObjectID
+	Global   = primitive.NilObjectID
+	Releases = set.NewSet(
+		AlmaLinux8,
+		AlmaLinux9,
+		AlmaLinux10,
+		AlpineLinux,
+		Fedora42,
+		FreeBSD,
+		OracleLinux7,
+		OracleLinux8,
+		OracleLinux9,
+		OracleLinux10,
+		RockyLinux8,
+		RockyLinux9,
+		RockyLinux10,
+		Ubuntu2404,
+	)
 )
