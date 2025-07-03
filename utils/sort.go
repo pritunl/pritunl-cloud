ea8f6d0fba7c1da60b0d8cce9bac9ced7e3e04a9
Zachary Huff <zach.huff.386@gmail.com>
2025-07-03 11:42:20 -0400

    Add natural compare to sort utils

--- utils/sort.go
+++ utils/sort.go
@@ -1,9 +1,16 @@
 package utils
 
 import (
+	"regexp"
 	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
+)
+
+var (
+	numRe = regexp.MustCompile(`(\d+|\D+)`)
 )
 
 type ObjectIdSlice []primitive.ObjectID
@@ -22,4 +29,39 @@ func (o ObjectIdSlice) Swap(i, j int) {
 
 func SortObjectIds(x []primitive.ObjectID) {
 	sort.Sort(ObjectIdSlice(x))
+}
+
+func NaturalCompare(a, b string) int {
+	aParts := numRe.FindAllString(a, -1)
+	bParts := numRe.FindAllString(b, -1)
+
+	minLen := len(aParts)
+	if len(bParts) < minLen {
+		minLen = len(bParts)
+	}
+
+	for i := 0; i < minLen; i++ {
+		aPart := aParts[i]
+		bPart := bParts[i]
+
+		aNum, aErr := strconv.Atoi(aPart)
+		bNum, bErr := strconv.Atoi(bPart)
+
+		if aErr == nil && bErr == nil {
+			if aNum != bNum {
+				return aNum - bNum
+			}
+		} else if (aErr == nil) != (bErr == nil) {
+			if aErr == nil {
+				return -1
+			}
+			return 1
+		} else {
+			if aPart != bPart {
+				return strings.Compare(aPart, bPart)
+			}
+		}
+	}
+
+	return len(aParts) - len(bParts)
 }
