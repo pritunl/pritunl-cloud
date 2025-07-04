3bcced30a4d1851dcb7bf7e315ca9a72f2388a02
Zachary Huff <zach.huff.386@gmail.com>
2025-07-04 15:56:02 -0400

    Add refresh to proxy resolver

--- proxy/resolver.go
+++ proxy/resolver.go
@@ -9,7 +9,12 @@ import (
 
 	"github.com/dropbox/godropbox/container/set"
 	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/mongo-go-driver/mongo/options"
+	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/imds/server/errortypes"
+	"github.com/pritunl/pritunl-cloud/instance"
+	"github.com/pritunl/pritunl-cloud/settings"
 )
 
 var (
@@ -23,6 +28,108 @@ var (
 type Remote struct {
 	Timestamp time.Time
 	Remote    net.IP
+}
+
+func ResolverRefresh(db *database.Database) (err error) {
+	coll := db.Instances()
+	ipDatabase := set.NewSet()
+	ttl := time.Duration(settings.Router.ProxyResolverTtl) * time.Second
+
+	cursor, err := coll.Find(
+		db,
+		&bson.M{},
+		&options.FindOptions{
+			Projection: &bson.M{
+				"public_ips":         1,
+				"public_ips6":        1,
+				"oracle_private_ips": 1,
+				"oracle_public_ips":  1,
+				"oracle_public_ips6": 1,
+			},
+		},
+	)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		inst := &instance.Instance{}
+
+		err = cursor.Decode(inst)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		for _, ipStr := range inst.PublicIps {
+			ip := net.ParseIP(ipStr)
+			if ip != nil {
+				ipDatabase.Add(ip.String())
+			}
+		}
+
+		for _, ipStr := range inst.PublicIps6 {
+			ip := net.ParseIP(ipStr)
+			if ip != nil {
+				ipDatabase.Add(ip.String())
+			}
+		}
+
+		for _, ipStr := range inst.OraclePublicIps {
+			ip := net.ParseIP(ipStr)
+			if ip != nil {
+				ipDatabase.Add(ip.String())
+			}
+		}
+
+		for _, ipStr := range inst.OraclePublicIps6 {
+			ip := net.ParseIP(ipStr)
+			if ip != nil {
+				ipDatabase.Add(ip.String())
+			}
+		}
+
+		for _, ipStr := range inst.OraclePrivateIps {
+			ip := net.ParseIP(ipStr)
+			if ip != nil {
+				ipDatabase.Add(ip.String())
+			}
+		}
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	_, hostNetwork, err := net.ParseCIDR(
+		settings.Hypervisor.HostNetwork)
+	if err != nil {
+		return
+	}
+
+	_, nodePortNetwork, err := net.ParseCIDR(
+		settings.Hypervisor.NodePortNetwork)
+	if err != nil {
+		return
+	}
+
+	now := time.Now()
+	ResolverLock.Lock()
+	for hostname, cached := range ResolverCache {
+		if now.Sub(cached.Timestamp) > ttl {
+			delete(ResolverCache, hostname)
+		}
+	}
+	IpDatabase = ipDatabase
+	HostNetwork = hostNetwork
+	NodePortNetwork = nodePortNetwork
+	ResolverLock.Unlock()
+
+	return
 }
 
 func ResolverValidate(ip net.IP) bool {
