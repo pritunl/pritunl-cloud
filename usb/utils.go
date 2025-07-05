a07b92ad39443bfa3858b40aaab8e9ee88b0718c
Zachary Huff <zach.huff.386@gmail.com>
2025-07-05 18:51:07 -0400

    Add available to usb utils

--- usb/utils.go
+++ usb/utils.go
@@ -3,11 +3,57 @@ package usb
 import (
 	"regexp"
 	"strings"
+
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/pritunl/pritunl-cloud/database"
 )
 
 var (
 	reg = regexp.MustCompile("[^a-z0-9]+")
 )
+
+func Available(db *database.Database, instId primitive.ObjectID,
+	device *Device) (available bool, err error) {
+
+	coll := db.Instances()
+
+	query := bson.M{}
+
+	if !instId.IsZero() {
+		query["_id"] = &bson.M{
+			"$ne": instId,
+		}
+	}
+
+	if device.Vendor != "" && device.Product != "" {
+		query["usb_devices"] = bson.M{
+			"$elemMatch": bson.M{
+				"vendor":  device.Vendor,
+				"product": device.Product,
+			},
+		}
+	} else if device.Bus != "" && device.Address != "" {
+		query["usb_devices"] = bson.M{
+			"$elemMatch": bson.M{
+				"bus":     device.Bus,
+				"address": device.Address,
+			},
+		}
+	}
+
+	count, err := coll.CountDocuments(db, query)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	if count == 0 {
+		available = true
+	}
+
+	return
+}
 
 func FilterId(deviceId string) string {
 	deviceId = strings.ToLower(deviceId)
