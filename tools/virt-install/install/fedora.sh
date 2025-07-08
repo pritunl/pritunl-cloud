5810c52281107527761b57bb4900fc70fd8a59f8
Zachary Huff <zach.huff.386@gmail.com>
2025-07-08 09:55:00 -0400

    Update fedora virt install

--- tools/virt-install/install/fedora.sh
+++ tools/virt-install/install/fedora.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ev
 NAME="fedora42"
 ISO_URL="https://sjc.mirror.rackspace.com/fedora/releases/42/Server/x86_64/iso/Fedora-Server-dvd-x86_64-42-1.1.iso"
 ISO_HASH="7fee9ac23b932c6a8be36fc1e830e8bba5f83447b0f4c81fe2425620666a7043"
