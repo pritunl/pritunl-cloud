a65778acf09226c695e141e5d4edf38767c6d00c
Zachary Huff <zach.huff.386@gmail.com>
2025-07-09 13:09:25 -0400

    Update rocky linux 10 virt install

--- tools/virt-install/install/rockylinux10.sh
+++ tools/virt-install/install/rockylinux10.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ev
 NAME="rockylinux10"
 ISO_URL="https://sjc.mirror.rackspace.com/rocky/10/isos/x86_64/Rocky-10.0-x86_64-dvd1.iso"
 ISO_HASH="678ea3e1eea6f5d6c220c46fab34f5e0add260e7b64f0139ee3b9f7b7ca7d2f3"
