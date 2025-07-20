a4108bd4818bc176bd5bcc3cf2d4a24319b375f5
Zachary Huff <zach.huff.386@gmail.com>
2025-07-20 15:30:11 -0400

    Add dhcp handler to imds server

---
+++ imds/server/handlers/dhcp.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"github.com/dropbox/godropbox/errors"
+	"github.com/gin-gonic/gin"
+	"github.com/pritunl/pritunl-cloud/dhcpc"
+	"github.com/pritunl/pritunl-cloud/imds/server/errortypes"
+	"github.com/pritunl/pritunl-cloud/imds/server/state"
+	"github.com/pritunl/pritunl-cloud/utils"
+)
+
+func dhcpPut(c *gin.Context) {
+	data := &dhcpc.Lease{}
+
+	err := c.Bind(data)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "handler: Bind error"),
+		}
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	state.Global.State.DhcpIp = data.Address
+	state.Global.State.DhcpGateway = data.Gateway
+
+	c.JSON(200, map[string]string{})
+}
