ce8cc08f3e24a96faa1bb7b1eb53841807fbf16f
Zachary Huff <zach.huff.386@gmail.com>
2025-07-20 16:00:20 -0400

    Add sync to dhcpc

--- dhcpc/dhcpc.go
+++ dhcpc/dhcpc.go
@@ -19,21 +19,44 @@ type Dhcpc struct {
 	DhcpIface6  string
 	DhcpIp      *net.IPNet
 	DhcpIp6     *net.IPNet
+	lease       *Lease
+}
+
+func (d *Dhcpc) startSync() {
+	im := &Imds{
+		Address: d.ImdsAddress,
+		Port:    d.ImdsPort,
+		Secret:  d.ImdsSecret,
+	}
+
+	for {
+		e := im.Sync(d.lease)
+		if e != nil {
+			logger.WithFields(logger.Fields{
+				"error": e,
+			}).Error("dhcpc: Failed to sync lease with imds")
+			time.Sleep(1 * time.Second)
+		}
+
+		time.Sleep(60 * time.Second)
+	}
 }
 
 func (d *Dhcpc) run() (err error) {
-	lease := &Lease{
+	d.lease = &Lease{
 		Iface:   d.DhcpIface,
 		Address: d.DhcpIp,
 	}
 
+	go d.startSync()
+
 	for {
 		for {
-			ok, e := lease.Exchange()
+			ok, e := d.lease.Exchange()
 			if e != nil {
 				logger.WithFields(logger.Fields{
 					"interface": d.DhcpIface,
-					"address":   lease.Address.String(),
+					"address":   d.lease.Address.String(),
 					"error":     e,
 				}).Error("dhcpc: Failed to exchange lease")
 				time.Sleep(500 * time.Millisecond)
@@ -52,15 +75,15 @@ func (d *Dhcpc) run() (err error) {
 
 		logger.WithFields(logger.Fields{
 			"interface": d.DhcpIface,
-			"address":   lease.Address.String(),
-			"gateway":   lease.Gateway.String(),
-			"server":    lease.ServerAddress.String(),
-			"time":      lease.LeaseTime.String(),
+			"address":   d.lease.Address.String(),
+			"gateway":   d.lease.Gateway.String(),
+			"server":    d.lease.ServerAddress.String(),
+			"time":      d.lease.LeaseTime.String(),
 		}).Info("dhcpc: Exchanged")
 
 		ready := false
 		for i := 0; i < 20; i++ {
-			ready = lease.IfaceReady()
+			ready = d.lease.IfaceReady()
 			if ready {
 				break
 			}
@@ -71,27 +94,27 @@ func (d *Dhcpc) run() (err error) {
 		if !ready {
 			logger.WithFields(logger.Fields{
 				"interface": d.DhcpIface,
-				"address":   lease.Address.String(),
+				"address":   d.lease.Address.String(),
 			}).Error("dhcpc: Interface ready timeout")
 		}
 
 		for {
 			time.Sleep(60 * time.Second)
 
-			ready := lease.IfaceReady()
+			ready := d.lease.IfaceReady()
 			if !ready {
 				logger.WithFields(logger.Fields{
 					"interface": d.DhcpIface,
-					"address":   lease.Address.String(),
+					"address":   d.lease.Address.String(),
 				}).Error("dhcpc: Interface not ready")
 				break
 			}
 
-			ok, e := lease.Renew()
+			ok, e := d.lease.Renew()
 			if e != nil {
 				logger.WithFields(logger.Fields{
 					"interface": d.DhcpIface,
-					"address":   lease.Address.String(),
+					"address":   d.lease.Address.String(),
 					"error":     e,
 				}).Error("dhcpc: Failed to renew lease")
 				break
@@ -100,7 +123,7 @@ func (d *Dhcpc) run() (err error) {
 			if !ok {
 				logger.WithFields(logger.Fields{
 					"interface": d.DhcpIface,
-					"address":   lease.Address.String(),
+					"address":   d.lease.Address.String(),
 					"error":     err,
 				}).Error("dhcpc: Failed to receive lease renewal")
 				break
@@ -108,10 +131,10 @@ func (d *Dhcpc) run() (err error) {
 
 			logger.WithFields(logger.Fields{
 				"interface": d.DhcpIface,
-				"address":   lease.Address.String(),
-				"gateway":   lease.Gateway.String(),
-				"server":    lease.ServerAddress.String(),
-				"time":      lease.LeaseTime.String(),
+				"address":   d.lease.Address.String(),
+				"gateway":   d.lease.Gateway.String(),
+				"server":    d.lease.ServerAddress.String(),
+				"time":      d.lease.LeaseTime.String(),
 			}).Info("dhcpc: Renewed")
 		}
 	}
