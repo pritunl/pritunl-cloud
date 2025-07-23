ec6a9ff048fc3c0da16a90bfccbe2a976aae545d
Zachary Huff <zach.huff.386@gmail.com>
2025-07-23 17:46:30 -0400

    Add ip6 to dhcpc lease ready

--- dhcpc/lease.go
+++ dhcpc/lease.go
@@ -3,19 +3,27 @@ package dhcpc
 import (
 	"net"
 	"time"
+
+	"github.com/insomniacslk/dhcp/dhcpv6"
 )
 
 type Lease struct {
-	Iface         string        `json:"iface"`
-	Address       *net.IPNet    `json:"address"`
-	Gateway       net.IP        `json:"gateway"`
-	Address6      *net.IPNet    `json:"address6"`
-	ServerAddress net.IP        `json:"server"`
-	LeaseTime     time.Duration `json:"ttl"`
-	TransactionId string        `json:"-"`
+	Iface          string        `json:"iface"`
+	Iface6         string        `json:"iface6"`
+	Address        *net.IPNet    `json:"address"`
+	Gateway        net.IP        `json:"gateway"`
+	Address6       *net.IPNet    `json:"address6"`
+	ServerAddress  net.IP        `json:"server"`
+	ServerAddress6 net.IP        `json:"server"`
+	LeaseTime      time.Duration `json:"ttl"`
+	LeaseTime6     time.Duration `json:"ttl"`
+	TransactionId  string        `json:"-"`
+	TransactionId6 string        `json:"-"`
+	IaId6          [4]byte       `json:"-"`
+	ServerId6      dhcpv6.DUID   `json:"-"`
 }
 
-func (l *Lease) IfaceReady() (ready bool) {
+func (l *Lease) IfaceReady() (ready4, ready6 bool) {
 	iface, err := net.InterfaceByName(l.Iface)
 	if err != nil {
 		return
@@ -25,9 +33,12 @@ func (l *Lease) IfaceReady() (ready bool) {
 	for _, addr := range addrs {
 		ipnet, ok := addr.(*net.IPNet)
 		if ok {
-			if ipnet.IP.Equal(l.Address.IP) {
-				ready = true
-				break
+			if l.Address != nil && ipnet.IP.Equal(l.Address.IP) {
+				ready4 = true
+			}
+
+			if l.Address6 != nil && ipnet.IP.Equal(l.Address6.IP) {
+				ready6 = true
 			}
 		}
 	}
