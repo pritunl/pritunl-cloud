2eb6b356e500b8807ed471456508a79169b9e929
Zachary Huff <zach.huff.386@gmail.com>
2025-07-29 18:29:21 -0400

    Add roles to upgrade

---
+++ upgrade/roles.go
@@ -0,0 +1,70 @@
+package upgrade
+
+import (
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/pritunl-cloud/database"
+)
+
+func rolesUpgrade(db *database.Database) (err error) {
+	coll := db.Instances()
+	_, err = coll.UpdateMany(db, bson.M{
+		"network_roles": bson.M{
+			"$exists": true,
+		},
+	}, []bson.M{
+		bson.M{
+			"$set": bson.M{
+				"roles": "$network_roles",
+			},
+		},
+		bson.M{
+			"$unset": "network_roles",
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	coll = db.Nodes()
+	_, err = coll.UpdateMany(db, bson.M{
+		"network_roles": bson.M{
+			"$exists": true,
+		},
+	}, []bson.M{
+		bson.M{
+			"$set": bson.M{
+				"roles": "$network_roles",
+			},
+		},
+		bson.M{
+			"$unset": "network_roles",
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	coll = db.Firewalls()
+	_, err = coll.UpdateMany(db, bson.M{
+		"network_roles": bson.M{
+			"$exists": true,
+		},
+	}, []bson.M{
+		bson.M{
+			"$set": bson.M{
+				"roles": "$network_roles",
+			},
+		},
+		bson.M{
+			"$unset": "network_roles",
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
