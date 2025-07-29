d7acca391c1501155b7a06360e77e8b7256718d8
Zachary Huff <zach.huff.386@gmail.com>
2025-07-29 18:41:08 -0400

    Add instance to upgrade

---
+++ upgrade/instance.go
@@ -0,0 +1,30 @@
+package upgrade
+
+import (
+	"github.com/pritunl/mongo-go-driver/bson"
+	"github.com/pritunl/pritunl-cloud/database"
+)
+
+func instanceUpgrade(db *database.Database) (err error) {
+	coll := db.Instances()
+	_, err = coll.UpdateMany(db, bson.M{
+		"virt_timestamp": bson.M{
+			"$exists": true,
+		},
+	}, []bson.M{
+		bson.M{
+			"$set": bson.M{
+				"timestamp": "$virt_timestamp",
+			},
+		},
+		bson.M{
+			"$unset": "virt_timestamp",
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
