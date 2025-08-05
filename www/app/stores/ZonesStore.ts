f09dd12e62929fd881c4de7978dcdc33c0e61c9c
Zachary Huff <zach.huff.386@gmail.com>
2025-08-05 08:08:48 -0400

    Add paging to zones store

--- www/app/stores/ZonesStore.ts
+++ www/app/stores/ZonesStore.ts
@@ -6,11 +6,19 @@ import * as GlobalTypes from '../types/GlobalTypes';
 
 class ZonesStore extends EventEmitter {
 	_zones: ZoneTypes.ZonesRo = Object.freeze([]);
+	_page: number;
+	_pageCount: number;
+	_filter: ZoneTypes.Filter = null;
+	_count: number;
 	_map: {[key: string]: number} = {};
 	_token = Dispatcher.register((this._callback).bind(this));
 
 	_reset(): void {
 		this._zones = Object.freeze([]);
+		this._page = undefined;
+		this._pageCount = undefined;
+		this._filter = null;
+		this._count = undefined;
 		this._map = {};
 		this.emitChange();
 	}
@@ -21,13 +29,32 @@ class ZonesStore extends EventEmitter {
 
 	get zonesM(): ZoneTypes.Zones {
 		let zones: ZoneTypes.Zones = [];
-		this._zones.forEach((
-				zone: ZoneTypes.ZoneRo): void => {
+		this._zones.forEach((zone: ZoneTypes.ZoneRo): void => {
 			zones.push({
 				...zone,
 			});
 		});
 		return zones;
+	}
+
+	get page(): number {
+		return this._page || 0;
+	}
+
+	get pageCount(): number {
+		return this._pageCount || 20;
+	}
+
+	get pages(): number {
+		return Math.ceil(this.count / this.pageCount);
+	}
+
+	get filter(): ZoneTypes.Filter {
+		return this._filter;
+	}
+
+	get count(): number {
+		return this._count || 0;
 	}
 
 	zone(id: string): ZoneTypes.ZoneRo {
@@ -50,14 +77,33 @@ class ZonesStore extends EventEmitter {
 		this.removeListener(GlobalTypes.CHANGE, callback);
 	}
 
-	_sync(zones: ZoneTypes.Zone[]): void {
+	_traverse(page: number): void {
+		this._page = Math.min(this.pages, page);
+	}
+
+	_filterCallback(filter: ZoneTypes.Filter): void {
+		if ((this._filter !== null && filter === null) ||
+			(!Object.keys(this._filter || {}).length && filter !== null) || (
+				filter && this._filter && (
+					filter.name !== this._filter.name
+				))) {
+			this._traverse(0);
+		}
+		this._filter = filter;
+		this.emitChange();
+	}
+
+	_sync(zones: ZoneTypes.Zone[], count: number): void {
 		this._map = {};
 		for (let i = 0; i < zones.length; i++) {
 			zones[i] = Object.freeze(zones[i]);
 			this._map[zones[i].id] = i;
 		}
 
+		this._count = count;
 		this._zones = Object.freeze(zones);
+		this._page = Math.min(this.pages, this.page);
+
 		this.emitChange();
 	}
 
@@ -67,8 +113,16 @@ class ZonesStore extends EventEmitter {
 				this._reset();
 				break;
 
+			case ZoneTypes.TRAVERSE:
+				this._traverse(action.data.page);
+				break;
+
+			case ZoneTypes.FILTER:
+				this._filterCallback(action.data.filter);
+				break;
+
 			case ZoneTypes.SYNC:
-				this._sync(action.data.zones);
+				this._sync(action.data.zones, action.data.count);
 				break;
 		}
 	}
