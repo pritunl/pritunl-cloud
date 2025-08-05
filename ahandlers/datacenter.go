4f531d6cf8624e7115ea054acf1df8c54a905c14
Zachary Huff <zach.huff.386@gmail.com>
2025-08-05 10:01:51 -0400

    Add names query to datacenter ahandler

--- ahandlers/datacenter.go
+++ ahandlers/datacenter.go
@@ -263,6 +263,17 @@ func datacenterGet(c *gin.Context) {
 func datacentersGet(c *gin.Context) {
 	db := c.MustGet("db").(*database.Database)
 
+	if c.Query("names") == "true" {
+		dcs, err := datacenter.GetAllNames(db, &bson.M{})
+		if err != nil {
+			utils.AbortWithError(c, 500, err)
+			return
+		}
+
+		c.JSON(200, dcs)
+		return
+	}
+
 	page, _ := strconv.ParseInt(c.Query("page"), 10, 0)
 	pageCount, _ := strconv.ParseInt(c.Query("page_count"), 10, 0)
 
