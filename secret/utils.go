3376ffeeb29c41332172db4b49488af943bbe71b
Zachary Huff <zach.huff.386@gmail.com>
2025-08-07 18:16:23 -0400

    Add get all names to secret utils

--- secret/utils.go
+++ secret/utils.go
@@ -149,6 +149,50 @@ func GetAllOrg(db *database.Database, orgId primitive.ObjectID) (
 	return
 }
 
+func GetAllNames(db *database.Database, query *bson.M) (
+	secrs []*database.Named, err error) {
+
+	coll := db.Certificates()
+	secrs = []*database.Named{}
+
+	cursor, err := coll.Find(
+		db,
+		query,
+		&options.FindOptions{
+			Sort: &bson.D{
+				{"name", 1},
+			},
+			Projection: &bson.D{
+				{"name", 1},
+			},
+		},
+	)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		secr := &database.Named{}
+		err = cursor.Decode(secr)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		secrs = append(secrs, secr)
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
 func GetAllPaged(db *database.Database, query *bson.M,
 	page, pageCount int64) (secrs []*Secret, count int64, err error) {
 
