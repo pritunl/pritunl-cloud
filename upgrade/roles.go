912f14167f8943b0143243b4d6624df45726f579
Zachary Huff <zach.huff.386@gmail.com>
2025-08-13 19:02:22 -0400

    Add authority to roles upgrade

--- upgrade/roles.go
+++ upgrade/roles.go
@@ -66,5 +66,25 @@ func rolesUpgrade(db *database.Database) (err error) {
 		return
 	}
 
+	coll = db.Authorities()
+	_, err = coll.UpdateMany(db, bson.M{
+		"network_roles": bson.M{
+			"$exists": true,
+		},
+	}, []bson.M{
+		bson.M{
+			"$set": bson.M{
+				"roles": "$network_roles",
+			},
+		},
+		bson.M{
+			"$unset": "network_roles",
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
 	return
 }
