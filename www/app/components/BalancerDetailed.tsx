86053cc7b9336951089f3abe19571723fae5131d
Zachary Huff <zach.huff.386@gmail.com>
2025-08-23 19:09:30 -0400

    Update columns in balancer detailed component

--- www/app/components/BalancerDetailed.tsx
+++ www/app/components/BalancerDetailed.tsx
@@ -704,7 +704,7 @@ export default class BalancerDetailed extends React.Component<Props, State> {
 
 		return <td
 			className="bp5-cell"
-			colSpan={5}
+			colSpan={3}
 			style={css.card}
 		>
 			<div className="layout horizontal wrap">
