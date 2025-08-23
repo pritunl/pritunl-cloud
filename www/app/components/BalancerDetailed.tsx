c7fd0b9e532affdb6a474f9adafc636fabda4733
Zachary Huff <zach.huff.386@gmail.com>
2025-08-23 19:09:36 -0400

    Update close in balancer detailed component

--- www/app/components/BalancerDetailed.tsx
+++ www/app/components/BalancerDetailed.tsx
@@ -710,12 +710,11 @@ export default class BalancerDetailed extends React.Component<Props, State> {
 			<div className="layout horizontal wrap">
 				<div style={css.group}>
 					<div
-						className="layout horizontal tab-close"
+						className="layout horizontal tab-close bp5-card-header"
 						style={css.buttons}
 						onClick={(evt): void => {
-							let target = evt.target as HTMLElement;
-
-							if (target.className.indexOf('tab-close') !== -1) {
+							if (evt.target instanceof HTMLElement &&
+									evt.target.className.indexOf('tab-close') !== -1) {
 								this.props.onClose();
 							}
 						}}
