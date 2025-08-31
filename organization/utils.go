308a496995317dc86443851c06c5d828251b962f
Zachary Huff <zach.huff.386@gmail.com>
2025-08-31 17:07:40 -0400

    Add get all paged to organization utils

--- organization/utils.go
+++ organization/utils.go
@@ -5,6 +5,7 @@ import (
 	"github.com/pritunl/mongo-go-driver/bson/primitive"
 	"github.com/pritunl/mongo-go-driver/mongo/options"
 	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 func Get(db *database.Database, dcId primitive.ObjectID) (
@@ -121,6 +122,70 @@ func GetAllNameRoles(db *database.Database, roles []string) (
 			Projection: &bson.D{
 				{"name", 1},
 			},
+		},
+	)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		org := &Organization{}
+		err = cursor.Decode(org)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		orgs = append(orgs, org)
+	}
+
+	err = cursor.Err()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func GetAllPaged(db *database.Database, query *bson.M,
+	page, pageCount int64) (orgs []*Organization, count int64, err error) {
+
+	coll := db.Organizations()
+	orgs = []*Organization{}
+
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	}
+
+	maxPage := count / pageCount
+	if count == pageCount {
+		maxPage = 0
+	}
+	page = utils.Min64(page, maxPage)
+	skip := utils.Min64(page*pageCount, count)
+
+	cursor, err := coll.Find(
+		db,
+		query,
+		&options.FindOptions{
+			Sort: &bson.D{
+				{"name", 1},
+			},
+			Skip:  &skip,
+			Limit: &pageCount,
 		},
 	)
 	if err != nil {
