3cd34a7af152276c3bddf3af7ca93ceb4f0cd8b6
Zachary Huff <zach.huff.386@gmail.com>
2025-09-01 18:43:16 -0400

    Add plan to demo

--- demo/demo.go
+++ demo/demo.go
@@ -19,6 +19,7 @@ import (
 	"github.com/pritunl/pritunl-cloud/node"
 	"github.com/pritunl/pritunl-cloud/organization"
 	"github.com/pritunl/pritunl-cloud/pci"
+	"github.com/pritunl/pritunl-cloud/plan"
 	"github.com/pritunl/pritunl-cloud/policy"
 	"github.com/pritunl/pritunl-cloud/secret"
 	"github.com/pritunl/pritunl-cloud/session"
@@ -758,5 +759,25 @@ var Balancers = []*balancer.Balancer{
 			},
 		},
 		CheckPath: "/check",
+	},
+}
+
+// Plans
+var Plans = []*plan.Plan{
+	{
+		Id:           utils.ObjectIdHex("66e8993f1fbc6db8e20819f8"),
+		Name:         "primary",
+		Comment:      "",
+		Organization: utils.ObjectIdHex("5a3245a50accad1a8a53bc82"),
+		Statements: []*plan.Statement{
+			{
+				Id:        utils.ObjectIdHex("67c9bed42c125c5ddf24d0a1"),
+				Statement: "IF instance.last_timestamp < 60 AND instance.last_heartbeat > 60 FOR 15 THEN 'stop'",
+			},
+			{
+				Id:        utils.ObjectIdHex("683d645e2956cdd93d3e08d2"),
+				Statement: "IF instance.state != 'running' THEN 'start'",
+			},
+		},
 	},
 }
