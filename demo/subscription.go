86527e251ae21e9736fbfd85837501d092f79577
Zachary Huff <zach.huff.386@gmail.com>
2025-09-03 18:10:41 -0400

    Add subscription to demo

---
+++ demo/subscription.go
@@ -0,0 +1,20 @@
+package demo
+
+import (
+	"time"
+
+	"github.com/pritunl/pritunl-cloud/subscription"
+)
+
+var Subscription = &subscription.Subscription{
+	Active:            true,
+	Status:            "active",
+	Plan:              "zero",
+	Quantity:          1,
+	Amount:            5000,
+	PeriodEnd:         time.Unix(1893499200, 0),
+	TrialEnd:          time.Time{},
+	CancelAtPeriodEnd: false,
+	Balance:           0,
+	UrlKey:            "demo",
+}
