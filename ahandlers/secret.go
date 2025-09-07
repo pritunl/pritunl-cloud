a6b3f8a3fa7e361530b73dcbe655189787e4681a
Zachary Huff <zach.huff.386@gmail.com>
2025-09-07 18:52:53 -0400

    Add demo to secret ahandlers

--- ahandlers/secret.go
+++ ahandlers/secret.go
@@ -237,6 +237,12 @@ func secretsDelete(c *gin.Context) {
 }
 
 func secretGet(c *gin.Context) {
+	if demo.IsDemo() {
+		secr := demo.Secrets[0]
+		c.JSON(200, secr)
+		return
+	}
+
 	db := c.MustGet("db").(*database.Database)
 
 	secrId, ok := utils.ParseObjectId(c.Param("secr_id"))
@@ -260,6 +266,16 @@ func secretGet(c *gin.Context) {
 }
 
 func secretsGet(c *gin.Context) {
+	if demo.IsDemo() {
+		data := &secretsData{
+			Secrets: demo.Secrets,
+			Count:   int64(len(demo.Secrets)),
+		}
+
+		c.JSON(200, data)
+		return
+	}
+
 	db := c.MustGet("db").(*database.Database)
 
 	if c.Query("names") == "true" {
