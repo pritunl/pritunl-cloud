b799cab06143791d842d1b3ac1a32712ddc6934a
Zachary Huff <zach.huff.386@gmail.com>
2025-09-08 13:14:23 -0400

    Add demo to shape uhandler

--- uhandlers/shape.go
+++ uhandlers/shape.go
@@ -4,11 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/pritunl/mongo-go-driver/bson"
 	"github.com/pritunl/pritunl-cloud/database"
+	"github.com/pritunl/pritunl-cloud/demo"
 	"github.com/pritunl/pritunl-cloud/shape"
 	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 func shapesGet(c *gin.Context) {
+	if demo.IsDemo() {
+		c.JSON(200, demo.Shapes)
+		return
+	}
+
 	db := c.MustGet("db").(*database.Database)
 
 	shapes, err := shape.GetAllNames(db, &bson.M{})
