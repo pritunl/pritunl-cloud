a5bac04d96182d783dc38a9078ad905c7ca82baa
Zachary Huff <zach.huff.386@gmail.com>
2025-09-10 19:38:37 -0400

    Remove leases from backup

--- backup/backup.go
+++ backup/backup.go
@@ -255,102 +255,6 @@ func (b *Backup) backupBackingDisks(db *database.Database) (err error) {
 	return
 }
 
-func (b *Backup) backupLeases(db *database.Database) (err error) {
-	logrus.WithFields(logrus.Fields{
-		"node_id": b.node.Id.Hex(),
-	}).Info("backup: Exporting leases")
-
-	trashDir := path.Join(b.Destination, "trash")
-	leasesDir := path.Join(b.Destination, "leases")
-	curLeasesDir := path.Join(b.virtPath, "leases")
-	err = utils.ExistsMkdir(leasesDir, 0755)
-	if err != nil {
-		return
-	}
-
-	exists, err := utils.Exists(curLeasesDir)
-	if err != nil {
-		return
-	}
-
-	curLeases := []os.FileInfo{}
-	if exists {
-		curLeases, err = ioutil.ReadDir(curLeasesDir)
-		if err != nil {
-			err = &errortypes.ReadError{
-				errors.Wrap(
-					err,
-					"backup: Failed to read leases directory",
-				),
-			}
-			return
-		}
-	}
-
-	leaseFilename := set.NewSet()
-	for _, item := range curLeases {
-		filename := item.Name()
-		leaseFilename.Add(filename)
-
-		leasePath := path.Join(curLeasesDir, filename)
-		newLeasePath := path.Join(leasesDir, filename)
-
-		_ = os.Remove(newLeasePath)
-
-		err = utils.Exec("", "cp", leasePath, newLeasePath)
-		if err != nil {
-			return
-		}
-
-		logrus.WithFields(logrus.Fields{
-			"node_id": b.node.Id.Hex(),
-			"lease":   filename,
-		}).Info("backup: Lease exported")
-	}
-
-	exportedLeases, err := ioutil.ReadDir(leasesDir)
-	if err != nil {
-		err = &errortypes.ReadError{
-			errors.Wrap(
-				err,
-				"backup: Failed to read leases directory",
-			),
-		}
-		return
-	}
-
-	trashLeasesDir := path.Join(trashDir, "leases")
-	for _, item := range exportedLeases {
-		filename := item.Name()
-		leasePath := path.Join(leasesDir, filename)
-		newLeasePath := path.Join(trashLeasesDir, filename)
-
-		if !leaseFilename.Contains(filename) {
-			err = utils.ExistsMkdir(trashLeasesDir, 0755)
-			if err != nil {
-				return
-			}
-
-			err = os.Rename(leasePath, newLeasePath)
-			if err != nil {
-				b.errorCount += 1
-				logrus.WithFields(logrus.Fields{
-					"node_id": b.node.Id.Hex(),
-					"lease":   filename,
-					"error":   err,
-				}).Error("backup: Failed to move lease to trash")
-			} else {
-				logrus.WithFields(logrus.Fields{
-					"node_id": b.node.Id.Hex(),
-					"lease":   filename,
-				}).Info("backup: Lease moved to trash")
-			}
-		}
-	}
-
-	return
-}
-
 func (b *Backup) Run() (err error) {
 	db := database.GetDatabase()
 	defer db.Close()
@@ -377,11 +281,6 @@ func (b *Backup) Run() (err error) {
 	}
 
 	err = b.backupBackingDisks(db)
-	if err != nil {
-		return
-	}
-
-	err = b.backupLeases(db)
 	if err != nil {
 		return
 	}
