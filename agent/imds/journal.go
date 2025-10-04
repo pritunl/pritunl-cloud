9ed6584fdf1135af797b72fb2b44f7efb722066f
Zachary Huff <zach.huff.386@gmail.com>
2025-10-04 19:46:40 -0400

    Add journal to agent imds

---
+++ agent/imds/journal.go
@@ -0,0 +1,56 @@
+package imds
+
+import (
+	"github.com/pritunl/pritunl-cloud/agent/logging"
+	"github.com/pritunl/tools/logger"
+)
+
+type Journal struct {
+	Index   int32            `json:"-"`
+	Key     string           `json:"-"`
+	Type    string           `json:"-"`
+	Unit    string           `json:"-"`
+	Path    string           `json:"-"`
+	Handler *logging.Systemd `json:"-"`
+}
+
+func (j *Journal) Open() (err error) {
+	logger.WithFields(logger.Fields{
+		"index": j.Index,
+		"key":   j.Key,
+		"type":  j.Type,
+		"unit":  j.Unit,
+		"path":  j.Path,
+	}).Info("agent: Starting journal")
+
+	err = j.Handler.Open()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (j *Journal) Close() {
+	logger.WithFields(logger.Fields{
+		"index": j.Index,
+		"key":   j.Key,
+		"type":  j.Type,
+		"unit":  j.Unit,
+		"path":  j.Path,
+	}).Info("agent: Stopping journal")
+
+	err := j.Handler.Close()
+	if err != nil {
+		logger.WithFields(logger.Fields{
+			"index": j.Index,
+			"key":   j.Key,
+			"type":  j.Type,
+			"unit":  j.Unit,
+			"path":  j.Path,
+			"error": err,
+		}).Error("agent: Error stopping journal")
+	}
+
+	return
+}
