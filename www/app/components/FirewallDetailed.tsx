dcfdf3d20a4812ab591d227d99bed7ca3ae8b2ed
Zachary Huff <zach.huff.386@gmail.com>
2025-10-09 18:56:10 -0400

    Update columns in firewall detailed component

--- www/app/components/FirewallDetailed.tsx
+++ www/app/components/FirewallDetailed.tsx
@@ -426,7 +426,7 @@ export default class FirewallDetailed extends React.Component<Props, State> {
 
 		return <td
 			className="bp5-cell"
-			colSpan={5}
+			colSpan={3}
 			style={css.card}
 		>
 			<div className="layout horizontal wrap">
