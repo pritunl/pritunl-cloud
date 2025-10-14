032b8918dbd04a6a5a89da834f71eadb6a30d936
Zachary Huff <zach.huff.386@gmail.com>
2025-10-14 19:40:03 -0400

    Handle changed record in aws dns

--- dns/aws.go
+++ dns/aws.go
@@ -120,6 +120,28 @@ func (a *Aws) DnsCommit(db *database.Database,
 			operations = append(operations, "add:"+op.Value)
 			updateResourceRecs = append(updateResourceRecs, resourceRec)
 		} else {
+			curVals, e := a.DnsFind(db, domain, recordType)
+			if e != nil {
+				err = e
+				return
+			}
+
+			exists := false
+			for _, val := range curVals {
+				if val == op.Value {
+					exists = true
+					break
+				}
+			}
+
+			if !exists {
+				logrus.WithFields(logrus.Fields{
+					"domain":    domain,
+					"operation": "remove:" + op.Value,
+				}).Info("domain: Skipping delete on changed record")
+				continue
+			}
+
 			operations = append(operations, "remove:"+op.Value)
 			deleteResourceRecs = append(deleteResourceRecs, resourceRec)
 		}
