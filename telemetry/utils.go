d973e2b9518b0735ea8ceeeb26cbc231d154fe28
Zachary Huff <zach.huff.386@gmail.com>
2025-10-15 19:46:22 -0400

    Add has sevs to telemetry utils

--- telemetry/utils.go
+++ telemetry/utils.go
@@ -1,10 +1,49 @@
 package telemetry
 
 import (
+	"bytes"
 	"os"
+	"time"
+
+	"github.com/pritunl/tools/commander"
+)
+
+var (
+	hasSevs = 0
 )
 
 func IsDnf() bool {
 	_, err := os.Stat("/usr/bin/dnf")
 	return err == nil
+}
+
+func HasSevs() bool {
+	if hasSevs == 1 {
+		return false
+	} else if hasSevs == 2 {
+		return true
+	}
+
+	resp, err := commander.Exec(&commander.Opt{
+		Name: "dnf",
+		Args: []string{
+			"updateinfo",
+			"list",
+			"--help",
+		},
+		Timeout: 8 * time.Second,
+		PipeOut: true,
+		PipeErr: true,
+	})
+	if err != nil {
+		hasSevs = 1
+		return false
+	}
+
+	if bytes.Contains(resp.Output, []byte("--advisory-severities")) {
+		hasSevs = 2
+		return true
+	}
+
+	return false
 }
