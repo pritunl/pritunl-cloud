bacacc1a93ee64c2c5e7166c1122a748c3d41854
Zachary Huff <zach.huff.386@gmail.com>
2025-10-24 19:12:18 -0400

    Rename roles in firewalls filter

--- www/app/components/FirewallsFilter.tsx
+++ www/app/components/FirewallsFilter.tsx
@@ -120,16 +120,16 @@ export default class FirewallsFilter extends React.Component<Props, {}> {
 			<SearchInput
 				style={css.role}
 				placeholder="Network Role"
-				value={this.props.filter.network_role}
+				value={this.props.filter.role}
 				onChange={(val: string): void => {
 					let filter = {
 						...this.props.filter,
 					};
 
 					if (val) {
-						filter.network_role = val;
+						filter.role = val;
 					} else {
-						delete filter.network_role;
+						delete filter.role;
 					}
 
 					this.props.onFilter(filter);
