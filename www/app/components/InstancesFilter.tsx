0cec05041f3a98e8e9efe7a8c9591b57e86c40a7
Zachary Huff <zach.huff.386@gmail.com>
2025-10-28 13:27:59 -0400

    Rename roles in instances filter component

--- www/app/components/InstancesFilter.tsx
+++ www/app/components/InstancesFilter.tsx
@@ -182,16 +182,16 @@ export default class InstancesFilter extends React.Component<Props, {}> {
 			<SearchInput
 				style={css.shortInput}
 				placeholder="Network Role"
-				value={this.props.filter.network_role}
+				value={this.props.filter.role}
 				onChange={(val: string): void => {
 					let filter = {
 						...this.props.filter,
 					};
 
 					if (val) {
-						filter.network_role = val;
+						filter.role = val;
 					} else {
-						delete filter.network_role;
+						delete filter.role;
 					}
 
 					this.props.onFilter(filter);
