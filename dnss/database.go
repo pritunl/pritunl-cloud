3546d3ba4f050b53ed8e726fda3ee1d65172e2ea
Zachary Huff <zach.huff.386@gmail.com>
2025-11-15 18:03:22 -0500

    Add load config to dnss database

--- dnss/database.go
+++ dnss/database.go
@@ -3,6 +3,9 @@ package dnss
 import (
 	"net"
 	"sync/atomic"
+
+	"github.com/pritunl/pritunl-cloud/domain"
+	"github.com/pritunl/pritunl-cloud/imds/types"
 )
 
 var (
@@ -25,4 +28,25 @@ func init() {
 
 func UpdateDatabase(db *Database) {
 	database.Store(db)
+}
+
+func LoadConfig(domains []*types.Domain) {
+	db := &Database{
+		A:     map[string][]net.IP{},
+		AAAA:  map[string][]net.IP{},
+		CNAME: map[string]string{},
+	}
+
+	for _, domn := range domains {
+		switch domn.Type {
+		case domain.A:
+			db.A[domn.Domain] = append(db.A[domn.Domain], domn.Ip)
+		case domain.AAAA:
+			db.AAAA[domn.Domain] = append(db.AAAA[domn.Domain], domn.Ip)
+		case domain.CNAME:
+			db.CNAME[domn.Domain] = db.CNAME[domn.Domain]
+		}
+	}
+
+	UpdateDatabase(db)
 }
