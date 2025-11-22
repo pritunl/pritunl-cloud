306775c6c948e55528bfc6bd73f4f0bfef31902a
Zachary Huff <zach.huff.386@gmail.com>
2025-11-22 10:05:15 -0500

    Update ubuntu install in virt tools

--- tools/virt-install/install/ubuntu.sh
+++ tools/virt-install/install/ubuntu.sh
@@ -77,7 +77,8 @@ local-hostname: cloud
 EOF
 
 sudo rm -f /var/lib/virt/init/${NAME}.iso
-sudo genisoimage -output /var/lib/virt/init/${NAME}.iso \
+sudo xorriso -as mkisofs \
+  -output /var/lib/virt/init/${NAME}.iso \
   -volid cidata \
   -joliet \
   -rock \
