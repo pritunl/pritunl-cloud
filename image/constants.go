d86fe7927cbeae83d50876012373a8c7d3d071ae
Zachary Huff <zach.huff.386@gmail.com>
2025-11-29 13:03:04 -0500

    Add fedora releases to image constants

--- image/constants.go
+++ image/constants.go
@@ -21,6 +21,26 @@ const (
 	AlpineLinux   = "alpinelinux"
 	ArchLinux     = "archlinux"
 	Fedora42      = "fedora42"
+	Fedora43      = "fedora43"
+	Fedora44      = "fedora44"
+	Fedora45      = "fedora45"
+	Fedora46      = "fedora46"
+	Fedora47      = "fedora47"
+	Fedora48      = "fedora48"
+	Fedora49      = "fedora49"
+	Fedora50      = "fedora50"
+	Fedora51      = "fedora51"
+	Fedora52      = "fedora52"
+	Fedora53      = "fedora53"
+	Fedora54      = "fedora54"
+	Fedora55      = "fedora55"
+	Fedora56      = "fedora56"
+	Fedora57      = "fedora57"
+	Fedora58      = "fedora58"
+	Fedora59      = "fedora59"
+	Fedora60      = "fedora60"
+	Fedora61      = "fedora61"
+	Fedora62      = "fedora62"
 	FreeBSD       = "freebsd"
 	OracleLinux7  = "oraclelinux7"
 	OracleLinux8  = "oraclelinux8"
@@ -41,6 +61,26 @@ var (
 		AlpineLinux,
 		ArchLinux,
 		Fedora42,
+		Fedora43,
+		Fedora44,
+		Fedora45,
+		Fedora46,
+		Fedora47,
+		Fedora48,
+		Fedora49,
+		Fedora50,
+		Fedora51,
+		Fedora52,
+		Fedora53,
+		Fedora54,
+		Fedora55,
+		Fedora56,
+		Fedora57,
+		Fedora58,
+		Fedora59,
+		Fedora60,
+		Fedora61,
+		Fedora62,
 		FreeBSD,
 		OracleLinux7,
 		OracleLinux8,
