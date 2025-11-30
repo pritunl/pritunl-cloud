8168c88d49f717e2ad05165681cdd20f106fb3f3
Zachary Huff <zach.huff.386@gmail.com>
2025-11-30 06:43:41 -0500

    Add future releases to image constants

--- image/constants.go
+++ image/constants.go
@@ -18,6 +18,12 @@ const (
 	AlmaLinux8    = "almalinux8"
 	AlmaLinux9    = "almalinux9"
 	AlmaLinux10   = "almalinux10"
+	AlmaLinux11   = "almalinux11"
+	AlmaLinux12   = "almalinux12"
+	AlmaLinux13   = "almalinux13"
+	AlmaLinux14   = "almalinux14"
+	AlmaLinux15   = "almalinux15"
+	AlmaLinux16   = "almalinux16"
 	AlpineLinux   = "alpinelinux"
 	ArchLinux     = "archlinux"
 	Fedora42      = "fedora42"
@@ -46,10 +52,32 @@ const (
 	OracleLinux8  = "oraclelinux8"
 	OracleLinux9  = "oraclelinux9"
 	OracleLinux10 = "oraclelinux10"
+	OracleLinux11 = "oraclelinux11"
+	OracleLinux12 = "oraclelinux12"
+	OracleLinux13 = "oraclelinux13"
+	OracleLinux14 = "oraclelinux14"
+	OracleLinux15 = "oraclelinux15"
+	OracleLinux16 = "oraclelinux16"
 	RockyLinux8   = "rockylinux8"
 	RockyLinux9   = "rockylinux9"
 	RockyLinux10  = "rockylinux10"
+	RockyLinux11  = "rockylinux11"
+	RockyLinux12  = "rockylinux12"
+	RockyLinux13  = "rockylinux13"
+	RockyLinux14  = "rockylinux14"
+	RockyLinux15  = "rockylinux15"
+	RockyLinux16  = "rockylinux16"
 	Ubuntu2404    = "ubuntu2404"
+	Ubuntu2604    = "ubuntu2604"
+	Ubuntu2804    = "ubuntu2804"
+	Ubuntu3004    = "ubuntu3004"
+	Ubuntu3204    = "ubuntu3204"
+	Ubuntu3404    = "ubuntu3404"
+	Ubuntu3604    = "ubuntu3604"
+	Ubuntu3804    = "ubuntu3804"
+	Ubuntu4004    = "ubuntu4004"
+	Ubuntu4204    = "ubuntu4204"
+	Ubuntu4404    = "ubuntu4404"
 )
 
 var (
@@ -58,6 +86,12 @@ var (
 		AlmaLinux8,
 		AlmaLinux9,
 		AlmaLinux10,
+		AlmaLinux11,
+		AlmaLinux12,
+		AlmaLinux13,
+		AlmaLinux14,
+		AlmaLinux15,
+		AlmaLinux16,
 		AlpineLinux,
 		ArchLinux,
 		Fedora42,
@@ -86,9 +120,31 @@ var (
 		OracleLinux8,
 		OracleLinux9,
 		OracleLinux10,
+		OracleLinux11,
+		OracleLinux12,
+		OracleLinux13,
+		OracleLinux14,
+		OracleLinux15,
+		OracleLinux16,
 		RockyLinux8,
 		RockyLinux9,
 		RockyLinux10,
+		RockyLinux11,
+		RockyLinux12,
+		RockyLinux13,
+		RockyLinux14,
+		RockyLinux15,
+		RockyLinux16,
 		Ubuntu2404,
+		Ubuntu2604,
+		Ubuntu2804,
+		Ubuntu3004,
+		Ubuntu3204,
+		Ubuntu3404,
+		Ubuntu3604,
+		Ubuntu3804,
+		Ubuntu4004,
+		Ubuntu4204,
+		Ubuntu4404,
 	)
 )
