5028263ea87c9b85ce857ea3ddf86a42857673ba
Zachary Huff <zach.huff.386@gmail.com>
2025-12-20 12:42:42 -0500

    Rename roles in firewall detailed component

--- www/app/components/FirewallDetailed.tsx
+++ www/app/components/FirewallDetailed.tsx
@@ -530,10 +530,10 @@ export default class FirewallDetailed extends React.Component<Props, State> {
 						{organizationsSelect}
 					</PageSelect>
 					<label className="bp5-label">
-						Network Roles
+						Roles
 						<Help
-							title="Network Roles"
-							content="Network roles that will be matched with firewall rules. Network roles are case-sensitive."
+							title="Roles"
+							content="Roles that will be matched with firewall rules. Roles are case-sensitive."
 						/>
 						<div>
 							{roles}
