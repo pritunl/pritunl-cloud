3d50442041b5c653d5e7a2cf1175dfc9e9fa0905
Zachary Huff <zach.huff.386@gmail.com>
2026-01-23 10:57:12 -0500

    Update cloud init in fedora virt install setup

--- tools/virt-install/setup/fedora.sh
+++ tools/virt-install/setup/fedora.sh
@@ -25,8 +25,8 @@ grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # cloud-init fix
 if [[ "$(cloud-init --version 2>&1)" == *"25.2"* ]]; then
-  wget https://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/x86_64/os/Packages/c/cloud-init-25.3-1.fc44.noarch.rpm
-  echo "51c33d2b8780480fa1aa482824e7850e21363e2713a8197e2ed97a8d28c14adb  cloud-init-25.3-1.fc44.noarch.rpm" | dnf -y install cloud-init-25.3-1.fc44.noarch.rpm
+  wget https://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/x86_64/os/Packages/c/cloud-init-25.3-3.fc44.noarch.rpm
+  echo "877c3b272f2202d46a7c1d06185eea262feb5bda637fa49575ae8c9a96e62652  cloud-init-25.3-1.fc44.noarch.rpm" | dnf -y install cloud-init-25.3-1.fc44.noarch.rpm
   rm -f cloud-init-25.3-1.fc44.noarch.rpm
 fi
 
