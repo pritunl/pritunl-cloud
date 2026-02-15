19f952ced05577ffa3246712c0c9545153fbc1e5
Zachary Huff <zach.huff.386@gmail.com>
2026-02-15 17:55:49 -0500

    Add valid types to advisory constants

--- advisory/constants.go
+++ advisory/constants.go
@@ -1,5 +1,11 @@
 package advisory
 
+import (
+	"regexp"
+
+	"github.com/dropbox/godropbox/container/set"
+)
+
 const (
 	None     = "none"
 	Low      = "low"
@@ -23,4 +29,59 @@ const (
 	Deferred         = "deferred"
 
 	nvdApi = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+)
+
+var (
+	cveIdReg = regexp.MustCompile(`^CVE-\d{4}-\d{4,}$`)
+
+	ValidStatuses = set.NewSet(
+		Analyzed,
+		AwaitingAnalysis,
+		Rejected,
+		Undergoing,
+		Modified,
+		Deferred,
+	)
+
+	ValidSeverities = set.NewSet(
+		None,
+		Low,
+		Medium,
+		High,
+		Critical,
+	)
+
+	ValidVectors = set.NewSet(
+		Network,
+		Adjacent,
+		Local,
+		Physical,
+	)
+
+	ValidComplexities = set.NewSet(
+		Low,
+		High,
+	)
+
+	ValidPrivileges = set.NewSet(
+		None,
+		Low,
+		High,
+	)
+
+	ValidInteractions = set.NewSet(
+		None,
+		Required,
+	)
+
+	ValidScopes = set.NewSet(
+		Unchanged,
+		Changed,
+	)
+
+	ValidImpacts = set.NewSet(
+		None,
+		Low,
+		High,
+	)
 )
