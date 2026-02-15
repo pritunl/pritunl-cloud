65b00711a0ccd3c4b9c7fc9c859c62e0336fa6e7
Zachary Huff <zach.huff.386@gmail.com>
2026-02-15 18:01:53 -0500

    Add timestamp to advisory

--- advisory/advisory.go
+++ advisory/advisory.go
@@ -13,19 +13,20 @@ var client = &http.Client{
 }
 
 type Advisory struct {
-	Id              string  `bson:"id" json:"id"`
-	Status          string  `bson:"status" json:"status"`
-	Description     string  `bson:"description" json:"description"`
-	Score           float64 `bson:"score" json:"score"`
-	Severity        string  `bson:"severity" json:"severity"`
-	Vector          string  `bson:"vector" json:"vector"`
-	Complexity      string  `bson:"complexity" json:"complexity"`
-	Privileges      string  `bson:"privileges" json:"privileges"`
-	Interaction     string  `bson:"interaction" json:"interaction"`
-	Scope           string  `bson:"scope" json:"scope"`
-	Confidentiality string  `bson:"confidentiality" json:"confidentiality"`
-	Integrity       string  `bson:"integrity" json:"integrity"`
-	Availability    string  `bson:"availability" json:"availability"`
+	Id              string    `bson:"id" json:"id"`
+	Timestamp       time.Time `bson:"timestamp" json:"timestamp"`
+	Status          string    `bson:"status" json:"status"`
+	Description     string    `bson:"description" json:"description"`
+	Score           float64   `bson:"score" json:"score"`
+	Severity        string    `bson:"severity" json:"severity"`
+	Vector          string    `bson:"vector" json:"vector"`
+	Complexity      string    `bson:"complexity" json:"complexity"`
+	Privileges      string    `bson:"privileges" json:"privileges"`
+	Interaction     string    `bson:"interaction" json:"interaction"`
+	Scope           string    `bson:"scope" json:"scope"`
+	Confidentiality string    `bson:"confidentiality" json:"confidentiality"`
+	Integrity       string    `bson:"integrity" json:"integrity"`
+	Availability    string    `bson:"availability" json:"availability"`
 }
 
 func (a *Advisory) Validate(db *database.Database) (
