63ea8365153a3f6cbbf26f6e58ef45b3f8f5f854
Zachary Huff <zach.huff.386@gmail.com>
2026-02-17 17:25:13 -0500

    Sync details in instance task

--- task/instance.go
+++ task/instance.go
@@ -1,7 +1,9 @@
 package task
 
 import (
+	"github.com/dropbox/godropbox/container/set"
 	"github.com/pritunl/mongo-go-driver/v2/bson"
+	"github.com/pritunl/pritunl-cloud/advisory"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/instance"
 )
@@ -15,13 +17,42 @@ var instanceData = &Task{
 }
 
 func instanceDataHandler(db *database.Database) (err error) {
+	advisories := map[string]*advisory.Advisory{}
+
 	instances, err := instance.GetAll(db, &bson.M{})
 	if err != nil {
 		return
 	}
 
 	for _, inst := range instances {
-		_ = inst
+		if inst.Guest == nil {
+			continue
+		}
+
+		for _, updt := range inst.Guest.Updates {
+			details := []*advisory.Advisory{}
+
+			for _, cve := range updt.Cves {
+				adv := advisories[cve]
+				if adv == nil {
+					adv, err = advisory.GetOne(db, cve)
+					if err != nil {
+						return
+					}
+				}
+
+				if adv != nil {
+					details = append(details, adv)
+				}
+			}
+
+			updt.Details = details
+		}
+
+		err = inst.CommitFields(db, set.NewSet("guest"))
+		if err != nil {
+			return
+		}
 	}
 
 	return
