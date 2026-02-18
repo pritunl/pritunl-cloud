05dd63a37fcdccca8df978f1636bb80b36055d93
Zachary Huff <zach.huff.386@gmail.com>
2026-02-18 16:57:54 -0500

    Add nvd api auth limit to system settings

--- settings/system.go
+++ settings/system.go
@@ -30,6 +30,7 @@ type system struct {
 	NvdTtl               int    `bson:"nvd_ttl" default:"21600"`
 	NvdFinalTtl          int    `bson:"nvd_final_ttl" default:"604800"`
 	NvdApiLimit          int    `bson:"nvd_api_limit" default:"8"`
+	NvdApiAuthLimit      int    `bson:"nvd_api_auth_limit" default:"1"`
 }
 
 func newSystem() interface{} {
