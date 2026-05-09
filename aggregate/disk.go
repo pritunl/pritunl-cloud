1ed527848537120f0a402c953db3daba8b838946
Zachary Huff <zach.huff.386@gmail.com>
2026-05-09 19:06:19 -0400

    Optimize disk aggregate

--- aggregate/disk.go
+++ aggregate/disk.go
@@ -1,16 +1,24 @@
 package aggregate
 
 import (
+	"sync"
+
+	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/v2/bson"
+	"github.com/pritunl/mongo-go-driver/v2/mongo"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/disk"
 	"github.com/pritunl/pritunl-cloud/node"
-	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 type DiskPipe struct {
 	disk.Disk `bson:",inline"`
 	ImageDocs []*node.Node `bson:"image_docs"`
+}
+
+type DisksPipe struct {
+	Metadata []*Metadata `bson:"meta"`
+	Disks    []*DiskPipe `bson:"disks"`
 }
 
 type DiskBackup struct {
@@ -29,68 +37,12 @@ func GetDiskPaged(db *database.Database, query *bson.M, page,
 	coll := db.Disks()
 	disks = []*DiskAggregate{}
 
-	if len(*query) == 0 {
-		count, err = coll.EstimatedDocumentCount(db)
-		if err != nil {
-			err = database.ParseError(err)
-			return
-		}
-	} else {
-		count, err = coll.CountDocuments(db, query)
-		if err != nil {
-			err = database.ParseError(err)
-			return
-		}
-	}
-
 	if pageCount == 0 {
 		pageCount = 20
 	}
-	maxPage := count / pageCount
-	if count == pageCount {
-		maxPage = 0
-	}
-	page = utils.Min64(page, maxPage)
-	skip := utils.Min64(page*pageCount, count)
+	skip := page * pageCount
 
-	cursor, err := coll.Aggregate(db, []*bson.M{
-		&bson.M{
-			"$match": query,
-		},
-		&bson.M{
-			"$sort": &bson.M{
-				"name": 1,
-			},
-		},
-		&bson.M{
-			"$skip": skip,
-		},
-		&bson.M{
-			"$limit": pageCount,
-		},
-		&bson.M{
-			"$lookup": &bson.M{
-				"from":         "images",
-				"localField":   "_id",
-				"foreignField": "disk",
-				"as":           "image_docs",
-			},
-		},
-	})
-	if err != nil {
-		err = database.ParseError(err)
-		return
-	}
-	defer cursor.Close(db)
-
-	for cursor.Next(db) {
-		doc := &DiskPipe{}
-		err = cursor.Decode(doc)
-		if err != nil {
-			err = database.ParseError(err)
-			return
-		}
-
+	addDisk := func(doc *DiskPipe) {
 		backups := []*DiskBackup{}
 
 		for _, img := range doc.ImageDocs {
@@ -108,6 +60,133 @@ func GetDiskPaged(db *database.Database, query *bson.M, page,
 		}
 
 		disks = append(disks, dsk)
+	}
+
+	var cursor *mongo.Cursor
+	if len(*query) == 0 {
+		waiter := &sync.WaitGroup{}
+		var countErr error
+
+		waiter.Add(1)
+		go func() {
+			defer waiter.Done()
+
+			count, countErr = coll.EstimatedDocumentCount(db)
+			if countErr != nil {
+				countErr = database.ParseError(countErr)
+				return
+			}
+		}()
+
+		cursor, err = coll.Aggregate(db, []*bson.M{
+			&bson.M{
+				"$match": query,
+			},
+			&bson.M{
+				"$sort": &bson.M{
+					"name": 1,
+				},
+			},
+			&bson.M{
+				"$skip": skip,
+			},
+			&bson.M{
+				"$limit": pageCount,
+			},
+			&bson.M{
+				"$lookup": &bson.M{
+					"from":         "images",
+					"localField":   "_id",
+					"foreignField": "disk",
+					"as":           "image_docs",
+				},
+			},
+		})
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+		defer cursor.Close(db)
+
+		for cursor.Next(db) {
+			doc := &DiskPipe{}
+			err = cursor.Decode(doc)
+			if err != nil {
+				err = database.ParseError(err)
+				return
+			}
+
+			addDisk(doc)
+		}
+
+		waiter.Wait()
+		if countErr != nil {
+			err = countErr
+			return
+		}
+	} else {
+		cursor, err = coll.Aggregate(db, []*bson.M{
+			&bson.M{
+				"$match": query,
+			},
+			&bson.M{
+				"$sort": &bson.M{
+					"name": 1,
+				},
+			},
+			&bson.M{
+				"$facet": &bson.M{
+					"meta": []*bson.M{
+						&bson.M{
+							"$count": "count",
+						},
+					},
+					"disks": []*bson.M{
+						&bson.M{
+							"$skip": skip,
+						},
+						&bson.M{
+							"$limit": pageCount,
+						},
+						&bson.M{
+							"$lookup": &bson.M{
+								"from":         "images",
+								"localField":   "_id",
+								"foreignField": "disk",
+								"as":           "image_docs",
+							},
+						},
+					},
+				},
+			},
+		})
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+		defer cursor.Close(db)
+
+		if !cursor.Next(db) {
+			err = &database.NotFoundError{
+				errors.New("aggregate: Not found"),
+			}
+			return
+		}
+
+		doc := &DisksPipe{}
+		err = cursor.Decode(doc)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		if len(doc.Metadata) > 0 {
+			count = doc.Metadata[0].Count
+		}
+
+		for _, diskDoc := range doc.Disks {
+			addDisk(diskDoc)
+		}
 	}
 
 	err = cursor.Err()
