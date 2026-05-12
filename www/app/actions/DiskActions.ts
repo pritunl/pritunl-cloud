5981a18188839adc15bf2872299c6b9602199d89
Zachary Huff <zach.huff.386@gmail.com>
2026-05-12 17:13:55 -0400

    Update commit in disk actions

--- www/app/actions/DiskActions.ts
+++ www/app/actions/DiskActions.ts
@@ -242,7 +242,7 @@ export function forceRemoveMulti(diskIds: string[]): Promise<void> {
 }
 
 export function updateMulti(diskIds: string[],
-		state: string): Promise<void> {
+		action: string): Promise<void> {
 	let loader = new Loader().loading();
 
 	return new Promise<void>((resolve, reject): void => {
@@ -250,7 +250,7 @@ export function updateMulti(diskIds: string[],
 			.put('/disk')
 			.send({
 				"ids": diskIds,
-				"state": state,
+				"action": action,
 			})
 			.set('Accept', 'application/json')
 			.set('Csrf-Token', Csrf.token)
