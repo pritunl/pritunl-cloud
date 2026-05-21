407500d76d37a609acef5a371cd621a6a8dfef28
Zachary Huff <zach.huff.386@gmail.com>
2026-05-21 19:33:34 -0400

    Add redhat api to advisory utils

--- advisory/utils.go
+++ advisory/utils.go
@@ -2,7 +2,9 @@ package advisory
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -59,6 +61,109 @@ type nvdVulnerability struct {
 type nvdResponse struct {
 	TotalResults    int                `json:"totalResults"`
 	Vulnerabilities []nvdVulnerability `json:"vulnerabilities"`
+}
+
+type redhatCvss3 struct {
+	BaseScore     string `json:"cvss3_base_score"`
+	ScoringVector string `json:"cvss3_scoring_vector"`
+	Status        string `json:"status"`
+}
+
+type redhatResponse struct {
+	Name           string      `json:"name"`
+	ThreatSeverity string      `json:"threat_severity"`
+	PublicDate     string      `json:"public_date"`
+	Details        []string    `json:"details"`
+	Cvss3          redhatCvss3 `json:"cvss3"`
+}
+
+func normalizeRedhatSeverity(severity string) string {
+	switch strings.ToLower(severity) {
+	case "critical":
+		return Critical
+	case "important":
+		return High
+	case "moderate":
+		return Medium
+	case "low":
+		return Low
+	case "none":
+		return None
+	default:
+		return strings.ToLower(severity)
+	}
+}
+
+func parseCvss3Vector(vector string) (av, ac, pr, ui, scope, c, i, a string) {
+	parts := strings.Split(vector, "/")
+	for _, part := range parts {
+		kv := strings.SplitN(part, ":", 2)
+		if len(kv) != 2 {
+			continue
+		}
+		switch kv[0] {
+		case "AV":
+			switch kv[1] {
+			case "N":
+				av = Network
+			case "A":
+				av = Adjacent
+			case "L":
+				av = Local
+			case "P":
+				av = Physical
+			}
+		case "AC":
+			switch kv[1] {
+			case "L":
+				ac = Low
+			case "H":
+				ac = High
+			}
+		case "PR":
+			switch kv[1] {
+			case "N":
+				pr = None
+			case "L":
+				pr = Low
+			case "H":
+				pr = High
+			}
+		case "UI":
+			switch kv[1] {
+			case "N":
+				ui = None
+			case "R":
+				ui = Required
+			}
+		case "S":
+			switch kv[1] {
+			case "U":
+				scope = Unchanged
+			case "C":
+				scope = Changed
+			}
+		case "C":
+			c = impactFromCode(kv[1])
+		case "I":
+			i = impactFromCode(kv[1])
+		case "A":
+			a = impactFromCode(kv[1])
+		}
+	}
+	return
+}
+
+func impactFromCode(code string) string {
+	switch code {
+	case "N":
+		return None
+	case "L":
+		return Low
+	case "H":
+		return High
+	}
+	return ""
 }
 
 func normalizeStatus(status string) string {
@@ -235,6 +340,110 @@ func getOneNvd(db *database.Database, cveId string) (
 	return
 }
 
+func getOneRedhat(db *database.Database, cveId string) (
+	adv *Advisory, err error) {
+
+	url := fmt.Sprintf(redhatApi, strings.ToUpper(cveId)) // TODO dangerous
+
+	println(url)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "advisory: Failed to create request"),
+		}
+		return
+	}
+
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "advisory: Request failed"),
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusNotFound {
+		err = &errortypes.NotFoundError{
+			errors.New("advisory: Not found"),
+		}
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		err = &errortypes.RequestError{
+			errors.Newf("advisory: Bad status status %d", resp.StatusCode),
+		}
+		return
+	}
+
+	rhResp := &redhatResponse{}
+	err = json.NewDecoder(resp.Body).Decode(rhResp)
+	if err != nil {
+		err = &errortypes.ParseError{
+			errors.Wrap(err, "advisory: Failed to decode response"),
+		}
+		return
+	}
+
+	if rhResp.Name == "" {
+		err = &errortypes.NotFoundError{
+			errors.New("advisory: Not found"),
+		}
+		return
+	}
+
+	adv = &Advisory{
+		Id:        strings.ToUpper(rhResp.Name),
+		Timestamp: time.Now(),
+		Status:    Analyzed,
+		Severity:  normalizeRedhatSeverity(rhResp.ThreatSeverity),
+	}
+
+	if len(rhResp.Details) > 0 {
+		adv.Description = rhResp.Details[0]
+	}
+
+	if rhResp.Cvss3.BaseScore != "" {
+		score, e := strconv.ParseFloat(rhResp.Cvss3.BaseScore, 64)
+		if e == nil {
+			adv.Score = score
+		}
+	}
+
+	if rhResp.Cvss3.ScoringVector != "" {
+		av, ac, pr, ui, scope, c, i, a := parseCvss3Vector(
+			rhResp.Cvss3.ScoringVector)
+		adv.Vector = av
+		adv.Complexity = ac
+		adv.Privileges = pr
+		adv.Interaction = ui
+		adv.Scope = scope
+		adv.Confidentiality = c
+		adv.Integrity = i
+		adv.Availability = a
+	}
+
+	errData, err := adv.Validate(db)
+	if err != nil {
+		return
+	}
+	if errData != nil {
+		err = errData.GetError()
+		return
+	}
+
+	err = adv.Commit(db)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func GetOneLimit(db *database.Database, cveId string) (
 	adv *Advisory, err error) {
 
@@ -265,7 +474,11 @@ func GetOneLimit(db *database.Database, cveId string) (
 	}
 	lastCall = time.Now()
 
-	adv, err = getOneNvd(db, cveId)
+	if settings.Telemetry.CveSource == RedHat {
+		adv, err = getOneRedhat(db, cveId)
+	} else {
+		adv, err = getOneNvd(db, cveId)
+	}
 	if err != nil {
 		return
 	}
@@ -289,7 +502,11 @@ func GetOne(db *database.Database, cveId string) (adv *Advisory, err error) {
 		return
 	}
 
-	adv, err = getOneNvd(db, cveId)
+	if settings.Telemetry.CveSource == RedHat {
+		adv, err = getOneRedhat(db, cveId)
+	} else {
+		adv, err = getOneNvd(db, cveId)
+	}
 	if err != nil {
 		return
 	}
