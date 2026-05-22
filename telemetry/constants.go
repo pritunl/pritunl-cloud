dc0029b1f4550103679d16cdcf73b3c2cb8d1718
Zachary Huff <zach.huff.386@gmail.com>
2026-05-22 18:36:34 -0400

    Add id reg to telemetry constants

--- telemetry/constants.go
+++ telemetry/constants.go
@@ -1,7 +1,16 @@
 package telemetry
 
+import (
+	"regexp"
+)
+
 const (
 	Moderate  = "moderate"
 	Important = "important"
 	Critical  = "critical"
+)
+
+var (
+	idReg = regexp.MustCompile(
+		`^[a-zA-Z]{1,10}-[a-zA-Z0-9]{1,12}-[a-zA-Z0-9]{1,12}$`)
 )
