6a33bf9ddb3802e7a26d1796cfda1c71feddae93
Zachary Huff <zach.huff.386@gmail.com>
2026-05-22 18:52:32 -0400

    Add description limit to telemetry settings

--- settings/telemetry.go
+++ settings/telemetry.go
@@ -3,16 +3,17 @@ package settings
 var Telemetry *telemetry
 
 type telemetry struct {
-	Id              string `bson:"_id"`
-	CveSource       string `bson:"cve_source" default:"nist"`
-	NvdTtl          int    `bson:"nvd_ttl" default:"21600"`
-	NvdFinalTtl     int    `bson:"nvd_final_ttl" default:"604800"`
-	NvdApiLimit     int    `bson:"nvd_api_limit" default:"8"`
-	NvdApiAuthLimit int    `bson:"nvd_api_auth_limit" default:"1"`
-	NvdApiKey       string `bson:"nvd_api_key"`
-	RedhatTtl       int    `bson:"redhat_ttl" default:"21600"`
-	RedhatFinalTtl  int    `bson:"redhat_final_ttl" default:"604800"`
-	RedhatApiLimit  int    `bson:"redhat_api_limit" default:"1"`
+	Id               string `bson:"_id"`
+	CveSource        string `bson:"cve_source" default:"nist"`
+	NvdTtl           int    `bson:"nvd_ttl" default:"21600"`
+	NvdFinalTtl      int    `bson:"nvd_final_ttl" default:"604800"`
+	NvdApiLimit      int    `bson:"nvd_api_limit" default:"8"`
+	NvdApiAuthLimit  int    `bson:"nvd_api_auth_limit" default:"1"`
+	NvdApiKey        string `bson:"nvd_api_key"`
+	RedhatTtl        int    `bson:"redhat_ttl" default:"21600"`
+	RedhatFinalTtl   int    `bson:"redhat_final_ttl" default:"604800"`
+	RedhatApiLimit   int    `bson:"redhat_api_limit" default:"1"`
+	DescriptionLimit int    `bson:"description_limit" default:"10000"`
 }
 
 func newTelemetry() interface{} {
