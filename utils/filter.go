e1f3e8a4196da50ee9cf9dd869a3906bd5a8ea85
Zachary Huff <zach.huff.386@gmail.com>
2026-05-23 19:32:20 -0400

    Add filter id to utils

--- utils/filter.go
+++ utils/filter.go
@@ -35,6 +35,14 @@ var unitSafeChar = set.NewSet(
 	':', '-', '_', '.', '\\', '@',
 )
 
+var idSafeChar = set.NewSet(
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '-', '_',
+)
+
 func FilterName(s string) string {
 	if len(s) == 0 {
 		return ""
@@ -93,6 +101,25 @@ func FilterUnit(s string) string {
 	var ns strings.Builder
 	for _, c := range s {
 		if unitSafeChar.Contains(c) {
+			ns.WriteString(string(c))
+		}
+	}
+
+	return ns.String()
+}
+
+func FilterId(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	if len(s) > nameSafeLimit {
+		s = s[:nameSafeLimit]
+	}
+
+	var ns strings.Builder
+	for _, c := range s {
+		if idSafeChar.Contains(c) {
 			ns.WriteString(string(c))
 		}
 	}
