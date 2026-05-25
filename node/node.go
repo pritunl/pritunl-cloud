8466be501bde189071032a3390f7a76ad43247cf
Zachary Huff <zach.huff.386@gmail.com>
2026-05-25 19:26:19 -0400

    Persist score in node telemetry update

--- node/node.go
+++ node/node.go
@@ -1278,14 +1278,17 @@ func (n *Node) getUpdateDetails(db *database.Database) (
 	}
 
 	detailsMap := map[string][]*advisory.Advisory{}
+	scoreMap := map[string]int{}
 	for _, upd := range n.Updates {
 		if upd.Advisory != "" && len(upd.Details) > 0 {
 			detailsMap[upd.Advisory] = upd.Details
+			scoreMap[upd.Advisory] = upd.Score
 		}
 	}
 
 	for _, upd := range curUpdates {
 		upd.Details = detailsMap[upd.Advisory]
+		upd.Score = scoreMap[upd.Advisory]
 
 		errData, e := upd.Validate(db)
 		if e != nil {
