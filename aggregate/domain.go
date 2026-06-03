f673c04b7ab0603d8742cb9e3e8ff9a09d3491c5
Zachary Huff <zach.huff.386@gmail.com>
2026-06-03 19:01:21 -0400

    Remove facet in aggregate domain

--- aggregate/domain.go
+++ aggregate/domain.go
@@ -1,23 +1,15 @@
 package aggregate
 
 import (
-	"sync"
-
-	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/v2/bson"
-	"github.com/pritunl/mongo-go-driver/v2/mongo"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/domain"
+	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 type Domain struct {
 	domain.Domain `bson:",inline"`
 	Records       []*domain.Record `bson:"records" json:"records"`
-}
-
-type DomainsPipe struct {
-	Metadata []*Metadata `bson:"meta"`
-	Domains  []*Domain   `bson:"domains"`
 }
 
 func GetDomainPaged(db *database.Database, query *bson.M, page,
@@ -26,142 +18,73 @@ func GetDomainPaged(db *database.Database, query *bson.M, page,
 	coll := db.Domains()
 	domains = []*domain.Domain{}
 
-	if pageCount == 0 {
-		pageCount = 20
-	}
-	skip := page * pageCount
-
-	addDomain := func(domn *Domain) {
-		domn.Domain.Records = domn.Records
-		domn.Json()
-		domains = append(domains, &domn.Domain)
-	}
-
-	var cursor *mongo.Cursor
 	if len(*query) == 0 {
-		waiter := &sync.WaitGroup{}
-		var countErr error
-
-		waiter.Add(1)
-		go func() {
-			defer waiter.Done()
-
-			count, countErr = coll.EstimatedDocumentCount(db)
-			if countErr != nil {
-				countErr = database.ParseError(countErr)
-				return
-			}
-		}()
-
-		cursor, err = coll.Aggregate(db, []*bson.M{
-			&bson.M{
-				"$match": query,
-			},
-			&bson.M{
-				"$sort": &bson.M{
-					"name": 1,
-				},
-			},
-			&bson.M{
-				"$skip": skip,
-			},
-			&bson.M{
-				"$limit": pageCount,
-			},
-			&bson.M{
-				"$lookup": &bson.M{
-					"from":         "domains_records",
-					"localField":   "_id",
-					"foreignField": "domain",
-					"as":           "records",
-				},
-			},
-		})
+		count, err = coll.EstimatedDocumentCount(db)
 		if err != nil {
 			err = database.ParseError(err)
-			return
-		}
-		defer cursor.Close(db)
-
-		for cursor.Next(db) {
-			domn := &Domain{}
-			err = cursor.Decode(domn)
-			if err != nil {
-				err = database.ParseError(err)
-				return
-			}
-
-			addDomain(domn)
-		}
-
-		waiter.Wait()
-		if countErr != nil {
-			err = countErr
 			return
 		}
 	} else {
-		cursor, err = coll.Aggregate(db, []*bson.M{
-			&bson.M{
-				"$match": query,
-			},
-			&bson.M{
-				"$sort": &bson.M{
-					"name": 1,
-				},
-			},
-			&bson.M{
-				"$facet": &bson.M{
-					"meta": []*bson.M{
-						&bson.M{
-							"$count": "count",
-						},
-					},
-					"domains": []*bson.M{
-						&bson.M{
-							"$skip": skip,
-						},
-						&bson.M{
-							"$limit": pageCount,
-						},
-						&bson.M{
-							"$lookup": &bson.M{
-								"from":         "domains_records",
-								"localField":   "_id",
-								"foreignField": "domain",
-								"as":           "records",
-							},
-						},
-					},
-				},
-			},
-		})
+		count, err = coll.CountDocuments(db, query)
 		if err != nil {
 			err = database.ParseError(err)
 			return
 		}
-		defer cursor.Close(db)
+	}
 
-		if !cursor.Next(db) {
-			err = &database.NotFoundError{
-				errors.New("aggregate: Not found"),
-			}
-			return
-		}
+	if pageCount == 0 {
+		pageCount = 20
+	}
+	maxPage := count / pageCount
+	if count == pageCount {
+		maxPage = 0
+	}
+	page = utils.Min64(page, maxPage)
+	skip := utils.Min64(page*pageCount, count)
 
-		doc := &DomainsPipe{}
-		err = cursor.Decode(doc)
+	cursor, err := coll.Aggregate(db, []*bson.M{
+		&bson.M{
+			"$match": query,
+		},
+		&bson.M{
+			"$sort": &bson.M{
+				"name": 1,
+			},
+		},
+		&bson.M{
+			"$skip": skip,
+		},
+		&bson.M{
+			"$limit": pageCount,
+		},
+		&bson.M{
+			"$lookup": &bson.M{
+				"from":         "domains_records",
+				"localField":   "_id",
+				"foreignField": "domain",
+				"as":           "records",
+			},
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		domn := &Domain{}
+		err = cursor.Decode(domn)
 		if err != nil {
 			err = database.ParseError(err)
 			return
 		}
 
-		if len(doc.Metadata) > 0 {
-			count = doc.Metadata[0].Count
-		}
+		domn.Domain.Records = domn.Records
 
-		for _, domn := range doc.Domains {
-			addDomain(domn)
-		}
+		domn.Json()
+
+		domains = append(domains, &domn.Domain)
 	}
 
 	err = cursor.Err()
