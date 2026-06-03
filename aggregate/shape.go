41cc566ecf73de6067bc1c87b5af951e9611e48c
Zachary Huff <zach.huff.386@gmail.com>
2026-06-03 19:06:39 -0400

    Remove facet from shape aggregate

--- aggregate/shape.go
+++ aggregate/shape.go
@@ -1,24 +1,16 @@
 package aggregate
 
 import (
-	"sync"
-
-	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/v2/bson"
-	"github.com/pritunl/mongo-go-driver/v2/mongo"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/node"
 	"github.com/pritunl/pritunl-cloud/shape"
+	"github.com/pritunl/pritunl-cloud/utils"
 )
 
 type ShapePipe struct {
 	shape.Shape `bson:",inline"`
 	NodeDocs    []*node.Node `bson:"node_docs"`
-}
-
-type ShapesPipe struct {
-	Metadata []*Metadata  `bson:"meta"`
-	Shapes   []*ShapePipe `bson:"shapes"`
 }
 
 func GetShapePaged(db *database.Database, query *bson.M, page,
@@ -27,10 +19,29 @@ func GetShapePaged(db *database.Database, query *bson.M, page,
 	coll := db.Shapes()
 	shapes = []*shape.Shape{}
 
+	if len(*query) == 0 {
+		count, err = coll.EstimatedDocumentCount(db)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	} else {
+		count, err = coll.CountDocuments(db, query)
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+	}
+
 	if pageCount == 0 {
 		pageCount = 20
 	}
-	skip := page * pageCount
+	maxPage := count / pageCount
+	if count == pageCount {
+		maxPage = 0
+	}
+	page = utils.Min64(page, maxPage)
+	skip := utils.Min64(page*pageCount, count)
 
 	sizeQuery := &bson.M{
 		"$ifNull": bson.A{
@@ -44,147 +55,63 @@ func GetShapePaged(db *database.Database, query *bson.M, page,
 		},
 	}
 
-	nodeLookup := &bson.M{
-		"$lookup": &bson.M{
-			"from": "nodes",
-			"let": &bson.M{
-				"shape_roles": "$roles",
+	cursor, err := coll.Aggregate(db, []*bson.M{
+		&bson.M{
+			"$match": query,
+		},
+		&bson.M{
+			"$sort": &bson.M{
+				"name": 1,
 			},
-			"pipeline": []*bson.M{
-				{
-					"$match": &bson.M{
-						"$expr": &bson.M{
-							"$gt": bson.A{
-								&bson.M{
-									"$size": sizeQuery,
+		},
+		&bson.M{
+			"$skip": skip,
+		},
+		&bson.M{
+			"$limit": pageCount,
+		},
+		&bson.M{
+			"$lookup": &bson.M{
+				"from": "nodes",
+				"let": &bson.M{
+					"shape_roles": "$roles",
+				},
+				"pipeline": []*bson.M{
+					{
+						"$match": &bson.M{
+							"$expr": &bson.M{
+								"$gt": bson.A{
+									&bson.M{
+										"$size": sizeQuery,
+									},
+									0,
 								},
-								0,
 							},
 						},
 					},
 				},
+				"as": "node_docs",
 			},
-			"as": "node_docs",
 		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
 	}
+	defer cursor.Close(db)
 
-	addShape := func(doc *ShapePipe) {
-		shpe := &doc.Shape
-		shpe.NodeCount = len(doc.NodeDocs)
-		shapes = append(shapes, shpe)
-	}
-
-	var cursor *mongo.Cursor
-	if len(*query) == 0 {
-		waiter := &sync.WaitGroup{}
-		var countErr error
-
-		waiter.Add(1)
-		go func() {
-			defer waiter.Done()
-
-			count, countErr = coll.EstimatedDocumentCount(db)
-			if countErr != nil {
-				countErr = database.ParseError(countErr)
-				return
-			}
-		}()
-
-		cursor, err = coll.Aggregate(db, []*bson.M{
-			&bson.M{
-				"$match": query,
-			},
-			&bson.M{
-				"$sort": &bson.M{
-					"name": 1,
-				},
-			},
-			&bson.M{
-				"$skip": skip,
-			},
-			&bson.M{
-				"$limit": pageCount,
-			},
-			nodeLookup,
-		})
-		if err != nil {
-			err = database.ParseError(err)
-			return
-		}
-		defer cursor.Close(db)
-
-		for cursor.Next(db) {
-			doc := &ShapePipe{}
-			err = cursor.Decode(doc)
-			if err != nil {
-				err = database.ParseError(err)
-				return
-			}
-
-			addShape(doc)
-		}
-
-		waiter.Wait()
-		if countErr != nil {
-			err = countErr
-			return
-		}
-	} else {
-		cursor, err = coll.Aggregate(db, []*bson.M{
-			&bson.M{
-				"$match": query,
-			},
-			&bson.M{
-				"$sort": &bson.M{
-					"name": 1,
-				},
-			},
-			&bson.M{
-				"$facet": &bson.M{
-					"meta": []*bson.M{
-						&bson.M{
-							"$count": "count",
-						},
-					},
-					"shapes": []*bson.M{
-						&bson.M{
-							"$skip": skip,
-						},
-						&bson.M{
-							"$limit": pageCount,
-						},
-						nodeLookup,
-					},
-				},
-			},
-		})
-		if err != nil {
-			err = database.ParseError(err)
-			return
-		}
-		defer cursor.Close(db)
-
-		if !cursor.Next(db) {
-			err = &database.NotFoundError{
-				errors.New("aggregate: Not found"),
-			}
-			return
-		}
-
-		doc := &ShapesPipe{}
+	for cursor.Next(db) {
+		doc := &ShapePipe{}
 		err = cursor.Decode(doc)
 		if err != nil {
 			err = database.ParseError(err)
 			return
 		}
 
-		if len(doc.Metadata) > 0 {
-			count = doc.Metadata[0].Count
-		}
+		shpe := &doc.Shape
+		shpe.NodeCount = len(doc.NodeDocs)
 
-		for _, shapeDoc := range doc.Shapes {
-			addShape(shapeDoc)
-		}
+		shapes = append(shapes, shpe)
 	}
 
 	err = cursor.Err()
