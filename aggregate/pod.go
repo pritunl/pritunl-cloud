a438d7cd5b6e8a0efc85119733e2aac298529c9d
Zachary Huff <zach.huff.386@gmail.com>
2026-06-03 19:03:49 -0400

    Remove facet from pod aggregate

--- aggregate/pod.go
+++ aggregate/pod.go
@@ -1,35 +1,17 @@
 package aggregate
 
 import (
-	"sort"
-	"sync"
-
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/mongo-go-driver/v2/bson"
-	"github.com/pritunl/mongo-go-driver/v2/mongo"
 	"github.com/pritunl/pritunl-cloud/database"
 	"github.com/pritunl/pritunl-cloud/pod"
 	"github.com/pritunl/pritunl-cloud/unit"
+	"github.com/pritunl/pritunl-cloud/utils"
 )
-
-func sortUnits(units []*unit.Unit) {
-	sort.SliceStable(units, func(i, j int) bool {
-		return units[i].Name < units[j].Name
-	})
-}
 
 type PodPipe struct {
 	pod.Pod  `bson:",inline"`
 	UnitDocs []*unit.Unit `bson:"unit_docs"`
-}
-
-type Metadata struct {
-	Count int64 `bson:"count"`
-}
-
-type PodsPipe struct {
-	Metadata []*Metadata `bson:"meta"`
-	Pods     []*PodPipe  `bson:"pods"`
 }
 
 type PodAggregate struct {
@@ -75,8 +57,6 @@ func GetPod(db *database.Database, usrId bson.ObjectID, query *bson.M) (
 		return
 	}
 
-	sortUnits(doc.UnitDocs)
-
 	pd = &PodAggregate{
 		Pod:   doc.Pod,
 		Units: doc.UnitDocs,
@@ -100,154 +80,76 @@ func GetPodsPaged(db *database.Database, usrId bson.ObjectID,
 	coll := db.Pods()
 	pods = []*PodAggregate{}
 
-	if pageCount == 0 {
-		pageCount = 20
-	}
-	skip := page * pageCount
-
-	var cursor *mongo.Cursor
 	if len(*query) == 0 {
-		waiter := &sync.WaitGroup{}
-		var countErr error
-
-		waiter.Add(1)
-		go func() {
-			defer waiter.Done()
-
-			count, countErr = coll.EstimatedDocumentCount(db)
-			if countErr != nil {
-				countErr = database.ParseError(countErr)
-				return
-			}
-		}()
-
-		cursor, err = coll.Aggregate(db, []*bson.M{
-			&bson.M{
-				"$match": query,
-			},
-			&bson.M{
-				"$sort": &bson.M{
-					"name": 1,
-				},
-			},
-			&bson.M{
-				"$skip": skip,
-			},
-			&bson.M{
-				"$limit": pageCount,
-			},
-			&bson.M{
-				"$lookup": &bson.M{
-					"from":         "units",
-					"localField":   "_id",
-					"foreignField": "pod",
-					"as":           "unit_docs",
-				},
-			},
-		})
+		count, err = coll.EstimatedDocumentCount(db)
 		if err != nil {
 			err = database.ParseError(err)
-			return
-		}
-		defer cursor.Close(db)
-
-		for cursor.Next(db) {
-			doc := &PodPipe{}
-			err = cursor.Decode(doc)
-			if err != nil {
-				err = database.ParseError(err)
-				return
-			}
-
-			sortUnits(doc.UnitDocs)
-
-			pd := &PodAggregate{
-				Pod:   doc.Pod,
-				Units: doc.UnitDocs,
-			}
-
-			pd.Json(usrId)
-
-			pods = append(pods, pd)
-		}
-
-		waiter.Wait()
-		if countErr != nil {
-			err = countErr
 			return
 		}
 	} else {
-		cursor, err = coll.Aggregate(db, []*bson.M{
-			&bson.M{
-				"$match": query,
-			},
-			&bson.M{
-				"$sort": &bson.M{
-					"name": 1,
-				},
-			},
-			&bson.M{
-				"$facet": &bson.M{
-					"meta": []*bson.M{
-						&bson.M{
-							"$count": "count",
-						},
-					},
-					"pods": []*bson.M{
-						&bson.M{
-							"$skip": skip,
-						},
-						&bson.M{
-							"$limit": pageCount,
-						},
-						&bson.M{
-							"$lookup": &bson.M{
-								"from":         "units",
-								"localField":   "_id",
-								"foreignField": "pod",
-								"as":           "unit_docs",
-							},
-						},
-					},
-				},
-			},
-		})
+		count, err = coll.CountDocuments(db, query)
 		if err != nil {
 			err = database.ParseError(err)
 			return
 		}
-		defer cursor.Close(db)
+	}
 
-		if !cursor.Next(db) {
-			err = &database.NotFoundError{
-				errors.New("aggregate: Not found"),
-			}
-			return
-		}
+	if pageCount == 0 {
+		pageCount = 20
+	}
+	maxPage := count / pageCount
+	if count == pageCount {
+		maxPage = 0
+	}
+	page = utils.Min64(page, maxPage)
+	skip := utils.Min64(page*pageCount, count)
 
-		doc := &PodsPipe{}
+	cursor, err := coll.Aggregate(db, []*bson.M{
+		&bson.M{
+			"$match": query,
+		},
+		&bson.M{
+			"$sort": &bson.M{
+				"name": 1,
+			},
+		},
+		&bson.M{
+			"$skip": skip,
+		},
+		&bson.M{
+			"$limit": pageCount,
+		},
+		&bson.M{
+			"$lookup": &bson.M{
+				"from":         "units",
+				"localField":   "_id",
+				"foreignField": "pod",
+				"as":           "unit_docs",
+			},
+		},
+	})
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+	defer cursor.Close(db)
+
+	for cursor.Next(db) {
+		doc := &PodPipe{}
 		err = cursor.Decode(doc)
 		if err != nil {
 			err = database.ParseError(err)
 			return
 		}
 
-		if len(doc.Metadata) > 0 {
-			count = doc.Metadata[0].Count
+		pd := &PodAggregate{
+			Pod:   doc.Pod,
+			Units: doc.UnitDocs,
 		}
 
-		for _, podDoc := range doc.Pods {
-			sortUnits(podDoc.UnitDocs)
+		pd.Json(usrId)
 
-			pd := &PodAggregate{
-				Pod:   podDoc.Pod,
-				Units: podDoc.UnitDocs,
-			}
-
-			pd.Json(usrId)
-
-			pods = append(pods, pd)
-		}
+		pods = append(pods, pd)
 	}
 
 	err = cursor.Err()
