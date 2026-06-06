9e1b9c8697704deb17207f0391635ad59fd5b047
Zachary Huff <zach.huff.386@gmail.com>
2026-06-06 18:50:48 -0400

    Add manifest resource index to database

--- database/database.go
+++ database/database.go
@@ -966,6 +966,18 @@ func addIndexes() (err error) {
 	}
 
 	index = &Index{
+		Collection: db.Manifests(),
+		Keys: &bson.D{
+			{"type", 1},
+			{"resource", 1},
+		},
+	}
+	err = index.Create()
+	if err != nil {
+		return
+	}
+
+	index = &Index{
 		Collection: db.Journal(),
 		Keys: &bson.D{
 			{"r", 1},
