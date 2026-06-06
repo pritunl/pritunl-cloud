43921dabe27c07f81b0e5c7fee222ac4d082355e
Zachary Huff <zach.huff.386@gmail.com>
2026-06-06 07:17:34 -0400

    Use manifest for advisory task

--- task/advisory.go
+++ task/advisory.go
@@ -3,13 +3,11 @@ package task
 import (
 	"time"
 
-	"github.com/dropbox/godropbox/container/set"
 	"github.com/pritunl/mongo-go-driver/v2/bson"
 	"github.com/pritunl/mongo-go-driver/v2/mongo/options"
 	"github.com/pritunl/pritunl-cloud/advisory"
 	"github.com/pritunl/pritunl-cloud/database"
-	"github.com/pritunl/pritunl-cloud/instance"
-	"github.com/pritunl/pritunl-cloud/node"
+	"github.com/pritunl/pritunl-cloud/manifest"
 	"github.com/pritunl/pritunl-cloud/vulnerability"
 	"github.com/sirupsen/logrus"
 )
@@ -24,32 +22,23 @@ var advisoryData = &Task{
 
 func advisoryDataHandler(db *database.Database) (err error) {
 	vulnerabilities := map[string]*vulnerability.Vulnerability{}
-
-	coll := db.Instances()
 	advisories := map[bson.ObjectID]map[string]*advisory.Advisory{}
 	now := time.Now()
 
-	cursor, err := coll.Find(db, &bson.M{})
+	cursor, err := manifest.FindUpdates(db)
 	if err != nil {
-		err = database.ParseError(err)
 		return
 	}
-	defer cursor.Close(db)
+	defer cursor.Close()
 
-	for cursor.Next(db) {
-		inst := &instance.Instance{}
-		err = cursor.Decode(inst)
-		if err != nil {
-			err = database.ParseError(err)
+	for cursor.Next() {
+		updts, e := cursor.Decode()
+		if e != nil {
+			err = e
 			return
 		}
 
-		if inst.Guest == nil {
-			continue
-		}
-
-		vulns := []*vulnerability.Vulnerability{}
-		for _, updt := range inst.Guest.Updates {
+		for _, updt := range updts.Updates {
 			updtVulns := []*vulnerability.Vulnerability{}
 
 			for _, vulnId := range updt.Vulnerabilities {
@@ -68,117 +57,43 @@ func advisoryDataHandler(db *database.Database) (err error) {
 				}
 
 				if vuln != nil {
-					vulns = append(vulns, vuln)
 					updtVulns = append(updtVulns, vuln)
 				}
 			}
 
-			orgAdvs := advisories[inst.Organization]
+			orgAdvs := advisories[updts.Organization]
 			if orgAdvs == nil {
 				orgAdvs = map[string]*advisory.Advisory{}
-				advisories[inst.Organization] = orgAdvs
+				advisories[updts.Organization] = orgAdvs
 			}
 
 			adv := orgAdvs[updt.Id]
 			if adv == nil {
 				adv = advisory.FromUpdate(
-					updt, inst.Organization, now, updtVulns)
+					updt, updts.Organization, now, updtVulns)
 				orgAdvs[updt.Id] = adv
 			}
-			adv.Instances = append(adv.Instances, inst.Id)
-		}
-		inst.Guest.Vulnerabilities = vulns
 
-		err = inst.CommitFields(db, set.NewSet(
-			"guest.updates_data",
-			"guest.vulnerabilities",
-		))
-		if err != nil {
-			return
+			switch updts.Variant {
+			case manifest.InstanceVariant:
+				adv.Instances = append(adv.Instances, updts.Resource)
+			case manifest.NodeVariant:
+				adv.Nodes = append(adv.Nodes, updts.Resource)
+			}
 		}
 	}
 
 	err = cursor.Err()
 	if err != nil {
-		err = database.ParseError(err)
 		return
 	}
 
-	coll = db.Nodes()
-
-	cursor, err = coll.Find(db, &bson.M{})
-	if err != nil {
-		err = database.ParseError(err)
-		return
-	}
-	defer cursor.Close(db)
-
-	for cursor.Next(db) {
-		nde := &node.Node{}
-		err = cursor.Decode(nde)
-		if err != nil {
-			err = database.ParseError(err)
-			return
-		}
-
-		vulns := []*vulnerability.Vulnerability{}
-		for _, updt := range nde.Updates {
-			updtVulns := []*vulnerability.Vulnerability{}
-
-			for _, vulnId := range updt.Vulnerabilities {
-				vuln, ok := vulnerabilities[vulnId]
-				if !ok {
-					vuln, err = vulnerability.GetOneLimit(db, vulnId)
-					if err != nil {
-						logrus.WithFields(logrus.Fields{
-							"vulnerability": vulnId,
-							"error":         err,
-						}).Error("task: Failed to query vulnerability")
-						err = nil
-						vuln = nil
-					}
-					vulnerabilities[vulnId] = vuln
-				}
-
-				if vuln != nil {
-					vulns = append(vulns, vuln)
-					updtVulns = append(updtVulns, vuln)
-				}
-			}
-
-			orgAdvs := advisories[advisory.Global]
-			if orgAdvs == nil {
-				orgAdvs = map[string]*advisory.Advisory{}
-				advisories[advisory.Global] = orgAdvs
-			}
-
-			adv := orgAdvs[updt.Id]
-			if adv == nil {
-				adv = advisory.FromUpdate(
-					updt, advisory.Global, now, updtVulns)
-				orgAdvs[updt.Id] = adv
-			}
-			adv.Nodes = append(adv.Nodes, nde.Id)
-		}
-		nde.Vulnerabilities = vulns
-
-		err = nde.CommitFields(
-			db, set.NewSet("updates_data", "vulnerabilities"))
-		if err != nil {
-			return
-		}
-	}
-
-	err = cursor.Err()
-	if err != nil {
-		err = database.ParseError(err)
-		return
-	}
-
-	coll = db.Advisories()
+	coll := db.Advisories()
 
 	for orgId, orgAdvs := range advisories {
 		for advId, adv := range orgAdvs {
+			adv.UpdateScore()
+
 			_, err = coll.UpdateOne(db, &bson.M{
 				"organization": orgId,
 				"reference":    advId,
