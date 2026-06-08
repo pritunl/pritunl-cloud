6a39a7e9921819855382e6c2fb5ea1ce611eb50f
Zachary Huff <zach.huff.386@gmail.com>
2026-06-08 11:11:48 -0400

    Rename dismissed resources in advisory

--- advisory/advisory.go
+++ advisory/advisory.go
@@ -9,20 +9,20 @@ import (
 )
 
 type Advisory struct {
-	Id              bson.ObjectID                  `bson:"_id" json:"id"`
-	Organization    bson.ObjectID                  `bson:"organization" json:"organization"`
-	Reference       string                         `bson:"reference" json:"reference"`
-	Dismissed       bool                           `bson:"dismissed" json:"dismissed"`
-	Type            string                         `bson:"type" json:"type"`
-	Updated         time.Time                      `bson:"updated" json:"updated"`
-	Severity        string                         `bson:"severity" json:"severity"`
-	Description     string                         `bson:"description" json:"description"`
-	Score           int                            `bson:"score" json:"score"`
-	Packages        []string                       `bson:"packages" json:"packages"`
-	Vulnerabilities []*vulnerability.Vulnerability `bson:"vulnerabilities" json:"vulnerabilities"`
-	Instances       []bson.ObjectID                `bson:"instances" json:"instances"`
-	Nodes           []bson.ObjectID                `bson:"nodes" json:"nodes"`
-	Dismissals      []bson.ObjectID                `bson:"dismissals" json:"dismissals"`
+	Id                 bson.ObjectID                  `bson:"_id" json:"id"`
+	Organization       bson.ObjectID                  `bson:"organization" json:"organization"`
+	Reference          string                         `bson:"reference" json:"reference"`
+	Dismissed          bool                           `bson:"dismissed" json:"dismissed"`
+	Type               string                         `bson:"type" json:"type"`
+	Updated            time.Time                      `bson:"updated" json:"updated"`
+	Severity           string                         `bson:"severity" json:"severity"`
+	Description        string                         `bson:"description" json:"description"`
+	Score              int                            `bson:"score" json:"score"`
+	Packages           []string                       `bson:"packages" json:"packages"`
+	Vulnerabilities    []*vulnerability.Vulnerability `bson:"vulnerabilities" json:"vulnerabilities"`
+	Instances          []bson.ObjectID                `bson:"instances" json:"instances"`
+	Nodes              []bson.ObjectID                `bson:"nodes" json:"nodes"`
+	DismissedResources []bson.ObjectID                `bson:"dismissed_resources" json:"dismissed_resources"`
 }
 
 func (a *Advisory) scoreAdvisory(vuln *vulnerability.Vulnerability) int {
