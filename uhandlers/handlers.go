7ee2f8846f38ba63c84fdf92715a354a1b2be1b4
Zachary Huff <zach.huff.386@gmail.com>
2026-06-08 10:33:52 -0400

    Add advisory dismiss to uhandlers init

--- uhandlers/handlers.go
+++ uhandlers/handlers.go
@@ -106,6 +106,8 @@ func Register(engine *gin.Engine) {
 
 	orgGroup.GET("/advisory", advisoriesGet)
 	orgGroup.GET("/advisory/:advisory_id", advisoryGet)
+	orgGroup.PUT("/advisory/:advisory_id/dismiss", advisoryDismissPut)
+	orgGroup.PUT("/advisory/:advisory_id/dismissals", advisoryDismissalsPut)
 	orgGroup.DELETE("/advisory", advisoriesDelete)
 	orgGroup.DELETE("/advisory/:advisory_id", advisoryDelete)
 
