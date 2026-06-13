190e66d7da038f49730da747a98bba2944fa0fc3
Zachary Huff <zach.huff.386@gmail.com>
2026-06-13 12:38:38 -0400

    Add metrics to imds state types

--- imds/types/state.go
+++ imds/types/state.go
@@ -4,24 +4,20 @@ import (
 	"net"
 	"time"
 
+	"github.com/pritunl/pritunl-cloud/metric"
 	"github.com/pritunl/pritunl-cloud/telemetry"
 )
 
 type State struct {
 	Hash        uint32              `json:"hash"`
 	Status      string              `json:"status"`
-	Memory      float64             `json:"memory"`
-	HugePages   float64             `json:"hugepages"`
-	Load1       float64             `json:"load1"`
-	Load5       float64             `json:"load5"`
-	Load15      float64             `json:"load15"`
 	DhcpIface   string              `json:"dhcp_iface"`
 	DhcpIface6  string              `json:"dhcp_iface6"`
 	DhcpIp      *net.IPNet          `json:"dhcp_ip"`
 	DhcpIp6     *net.IPNet          `json:"dhcp_ip6"`
 	DhcpGateway net.IP              `json:"dhcp_gateway"`
-	Disks       []*telemetry.Disk   `json:"disks"`
 	Updates     []*telemetry.Update `json:"updates"`
+	Metrics     []*metric.Sample    `json:"metrics,omitempty"`
 	Timestamp   time.Time           `json:"timestamp"`
 	Output      []*Entry            `json:"output,omitempty"`
 	Journals    map[string][]*Entry `json:"journals,omitempty"`
@@ -38,18 +34,13 @@ func (s *State) Copy() *State {
 	return &State{
 		Hash:        s.Hash,
 		Status:      s.Status,
-		Memory:      s.Memory,
-		HugePages:   s.HugePages,
-		Load1:       s.Load1,
-		Load5:       s.Load5,
-		Load15:      s.Load15,
 		DhcpIface:   s.DhcpIface,
 		DhcpIface6:  s.DhcpIface6,
 		DhcpIp:      s.DhcpIp,
 		DhcpIp6:     s.DhcpIp6,
 		DhcpGateway: s.DhcpGateway,
-		Disks:       s.Disks,
 		Updates:     s.Updates,
+		Metrics:     s.Metrics,
 		Timestamp:   s.Timestamp,
 	}
 }
