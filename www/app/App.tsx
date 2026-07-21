a7678d8cf0cdab479883cce3e414d4fa640978f4
Zachary Huff <zach.huff.386@gmail.com>
2026-07-21 19:32:53 -0400

    Init chart in app

--- www/app/App.tsx
+++ www/app/App.tsx
@@ -6,15 +6,52 @@ MonacoEditor.loader.config({
 	monaco: Monaco
 })
 
+import 'chartjs-adapter-moment';
+import * as ChartJs from 'chart.js';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Blueprint from '@blueprintjs/core';
 import Main from './components/Main';
 import * as Alert from './Alert';
 import * as Event from './Event';
+import * as Theme from './Theme';
 import * as Csrf from './Csrf';
 import * as MiscUtils from './utils/MiscUtils';
 import * as CompletionActions from './actions/CompletionActions';
+
+ChartJs.Chart.register(ChartJs.LineController);
+ChartJs.Chart.register(ChartJs.CategoryScale);
+ChartJs.Chart.register(ChartJs.LinearScale);
+ChartJs.Chart.register(ChartJs.TimeScale);
+ChartJs.Chart.register(ChartJs.PointElement);
+ChartJs.Chart.register(ChartJs.LineElement);
+ChartJs.Chart.register(ChartJs.Title);
+ChartJs.Chart.register(ChartJs.Tooltip);
+ChartJs.Chart.register(ChartJs.Filler);
+
+class LineTracerController extends ChartJs.LineController {
+	draw(): void {
+		super.draw();
+
+		let chart = this.chart as any;
+		if (chart.tooltip._active && chart.tooltip._active.length) {
+			let ctx = this.chart.ctx;
+			let x = chart.tooltip.caretX;
+			let topY = chart.scales.y.top;
+			let bottomY = chart.scales.y.bottom;
+
+			ctx.save();
+			ctx.beginPath();
+			ctx.moveTo(x, topY);
+			ctx.lineTo(x, bottomY);
+			ctx.lineWidth = 0.7;
+			ctx.strokeStyle = Theme.chartColor3();
+			ctx.stroke();
+			ctx.restore();
+		}
+	}
+}
+(ChartJs.Chart as any).registry.controllers.items.line = LineTracerController;
 
 import hljs from 'highlight.js/lib/core';
 import plaintext from 'highlight.js/lib/languages/plaintext';
