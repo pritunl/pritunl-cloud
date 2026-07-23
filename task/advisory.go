cd9d249d39824b9c07c1ed84d7136ececbc6923d
Zachary Huff <zach.huff.386@gmail.com>
2026-07-23 09:28:36 -0400

    Update schedule in advisory task

--- task/advisory.go
+++ task/advisory.go
@@ -18,7 +18,8 @@ import (
 var advisoryData = &Task{
 	Name:    "advisory",
 	Version: 1,
-	Hours:   []int{0, 3, 6, 9, 12, 15, 18, 21},
+	Hours: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+		13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
 	Minutes: []int{22},
 	Handler: advisoryDataHandler,
 }
