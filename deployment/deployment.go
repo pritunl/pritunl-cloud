e9024cc176665b18f04bafcd8ebccce70886fb6a
Zachary Huff <zach.huff.386@gmail.com>
2026-08-14 18:44:58 -0400

    Add failover to deployment

--- deployment/deployment.go
+++ deployment/deployment.go
@@ -24,6 +24,7 @@ type Deployment struct {
 	State        string                    `bson:"state" json:"state"`
 	Action       string                    `bson:"action" json:"action"`
 	Status       string                    `bson:"status" json:"status"`
+	Failover     string                    `bson:"failover" json:"failover"`
 	SyncDomains  bool                      `bson:"sync_domains" json:"sync_domains"`
 	Datacenter   bson.ObjectID             `bson:"datacenter" json:"datacenter"`
 	Zone         bson.ObjectID             `bson:"zone" json:"zone"`
