6ff5edecef6bae952b3a2da615fb10600732c27d
Zachary Huff <zach.huff.386@gmail.com>
2026-08-15 19:05:48 -0400

    Add ntp server to system settings

--- settings/system.go
+++ settings/system.go
@@ -22,6 +22,7 @@ type system struct {
 	DiskBackupTime       int    `bson:"disk_backup_time" default:"10"`
 	PlannerBatchSize     int    `bson:"planner_batch_size" default:"10"`
 	NoMigrateRefresh     bool   `bson:"no_migrate_refresh"`
+	NtpServer            string `bson:"ntp_server" default:"time.cloudflare.com:123"`
 	OracleApiRetryRate   int    `bson:"oracle_api_retry_rate" default:"1"`
 	OracleApiRetryCount  int    `bson:"oracle_api_retry_count" default:"120"`
 	TwilioAccount        string `bson:"twilio_account"`
