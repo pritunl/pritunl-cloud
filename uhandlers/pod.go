d487c2ac03ef300b51ecfe9861b87e760f71d9de
Zachary Huff <zach.huff.386@gmail.com>
2026-09-11 05:43:43 -0400

    Add realm to pod deployment uhandler

--- uhandlers/pod.go
+++ uhandlers/pod.go
@@ -46,6 +46,7 @@ type podsData struct {
 type podsDeployData struct {
 	Count int           `json:"count"`
 	Spec  bson.ObjectID `json:"spec"`
+	Realm string        `json:"realm"`
 }
 
 type deploymentData struct {
@@ -743,13 +744,16 @@ func podUnitDeploymentPost(c *gin.Context) {
 		return
 	}
 
+	data.Realm = utils.FilterName(data.Realm)
+
 	unt, err := unit.GetOrg(db, userOrg, unitId)
 	if err != nil {
 		utils.AbortWithError(c, 500, err)
 		return
 	}
 
-	errData, err := scheduler.ManualSchedule(db, unt, data.Spec, data.Count)
+	errData, err := scheduler.ManualSchedule(db, unt, data.Spec,
+		data.Count, data.Realm)
 	if err != nil {
 		utils.AbortWithError(c, 500, err)
 		return
