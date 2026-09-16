d024b258db74548b890ef0e254bc416f99d6df87
Zachary Huff <zach.huff.386@gmail.com>
2026-09-16 11:17:09 -0400

    Add components to instance

--- instance/instance.go
+++ instance/instance.go
@@ -32,6 +32,7 @@ import (
 	"github.com/pritunl/pritunl-cloud/settings"
 	"github.com/pritunl/pritunl-cloud/shape"
 	"github.com/pritunl/pritunl-cloud/systemd"
+	"github.com/pritunl/pritunl-cloud/telemetry"
 	"github.com/pritunl/pritunl-cloud/tpm"
 	"github.com/pritunl/pritunl-cloud/usb"
 	"github.com/pritunl/pritunl-cloud/utils"
@@ -173,6 +174,7 @@ type GuestData struct {
 	Load1      float64                   `bson:"load1" json:"load1"`
 	Load5      float64                   `bson:"load5" json:"load5"`
 	Load15     float64                   `bson:"load15" json:"load15"`
+	Components []*telemetry.Component    `bson:"components" json:"components"`
 	Disks      []*metric.DiskStatic      `bson:"disks" json:"disks"`
 	Mounts     []*metric.MountStatic     `bson:"mounts" json:"mounts"`
 	Interfaces []*metric.InterfaceStatic `bson:"interfaces" json:"interfaces"`
