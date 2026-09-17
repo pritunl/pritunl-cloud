b64f4650f68edaf2c537322ab32b4bac14b7d121
Zachary Huff <zach.huff.386@gmail.com>
2026-09-17 06:10:34 -0400

    Add nist fallback to telemetry settings

--- settings/telemetry.go
+++ settings/telemetry.go
@@ -3,18 +3,20 @@ package settings
 var Telemetry *telemetry
 
 type telemetry struct {
-	Id               string `bson:"_id"`
-	CveSource        string `bson:"cve_source" default:"redhat"`
-	NvdTtl           int    `bson:"nvd_ttl" default:"21600"`
-	NvdFinalTtl      int    `bson:"nvd_final_ttl" default:"604800"`
-	NvdApiLimit      int    `bson:"nvd_api_limit" default:"8"`
-	NvdApiAuthLimit  int    `bson:"nvd_api_auth_limit" default:"1"`
-	NvdApiKey        string `bson:"nvd_api_key"`
-	RedhatTtl        int    `bson:"redhat_ttl" default:"21600"`
-	RedhatFinalTtl   int    `bson:"redhat_final_ttl" default:"604800"`
-	RedhatApiLimit   int    `bson:"redhat_api_limit" default:"1"`
-	DescriptionLimit int    `bson:"description_limit" default:"10000"`
-	VuxmlSizeLimit   int    `bson:"vuxml_size_limit" default:"67108864"`
+	Id                string `bson:"_id"`
+	CveSource         string `bson:"cve_source" default:"redhat"`
+	CveSourceFallback string `bson:"cve_source_fallback" default:"nist"`
+	NvdTtl            int    `bson:"nvd_ttl" default:"21600"`
+	NvdFinalTtl       int    `bson:"nvd_final_ttl" default:"604800"`
+	NvdApiLimit       int    `bson:"nvd_api_limit" default:"8"`
+	NvdApiAuthLimit   int    `bson:"nvd_api_auth_limit" default:"1"`
+	NvdApiKey         string `bson:"nvd_api_key"`
+	RedhatTtl         int    `bson:"redhat_ttl" default:"21600"`
+	RedhatFinalTtl    int    `bson:"redhat_final_ttl" default:"604800"`
+	RedhatApiLimit    int    `bson:"redhat_api_limit" default:"1"`
+	AnalysisTtl       int    `bson:"analysis_ttl" default:"600"`
+	DescriptionLimit  int    `bson:"description_limit" default:"10000"`
+	VuxmlSizeLimit    int    `bson:"vuxml_size_limit" default:"67108864"`
 }
 
 func newTelemetry() interface{} {
