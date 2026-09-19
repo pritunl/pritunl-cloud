31660fece9e4a807d6dcacab7ab98558a6acdfda
Zachary Huff <zach.huff.386@gmail.com>
2026-09-19 09:50:04 -0400

    Add dismissed resources to advisory

--- advisory/dismissed.go
+++ advisory/dismissed.go
@@ -1,14 +1,22 @@
 package advisory
 
 import (
+	"github.com/dropbox/godropbox/container/set"
 	"github.com/pritunl/mongo-go-driver/v2/bson"
 	"github.com/pritunl/mongo-go-driver/v2/mongo/options"
 	"github.com/pritunl/pritunl-cloud/database"
 )
 
 type Dismissal struct {
-	Dismissed          bool
-	DismissedResources []bson.ObjectID
+	Dismissed bool
+	Resources set.Set
+}
+
+func (d *Dismissal) HasResource(resourceId bson.ObjectID) bool {
+	if d == nil || d.Resources == nil {
+		return false
+	}
+	return d.Resources.Contains(resourceId)
 }
 
 func GetDismissals(db *database.Database) (
@@ -17,14 +25,33 @@ func GetDismissals(db *database.Database) (
 	coll := db.Advisories()
 	dismissals = map[bson.ObjectID]map[string]*Dismissal{}
 
+	getDismissal := func(orgId bson.ObjectID, ref string) *Dismissal {
+		orgDismissals := dismissals[orgId]
+		if orgDismissals == nil {
+			orgDismissals = map[string]*Dismissal{}
+			dismissals[orgId] = orgDismissals
+		}
+
+		dism := orgDismissals[ref]
+		if dism == nil {
+			dism = &Dismissal{
+				Resources: set.NewSet(),
+			}
+			orgDismissals[ref] = dism
+		}
+
+		return dism
+	}
+
 	cursor, err := coll.Find(
 		db,
-		&bson.M{},
+		&bson.M{
+			"dismissed": true,
+		},
 		options.Find().SetProjection(&bson.M{
-			"organization":        1,
-			"reference":           1,
-			"dismissed":           1,
-			"dismissed_resources": 1,
+			"organization": 1,
+			"reference":    1,
+			"dismissed":    1,
 		}),
 	)
 	if err != nil {
@@ -41,22 +68,29 @@ func GetDismissals(db *database.Database) (
 			return
 		}
 
-		orgDismissals := dismissals[adv.Organization]
-		if orgDismissals == nil {
-			orgDismissals = map[string]*Dismissal{}
-			dismissals[adv.Organization] = orgDismissals
-		}
-
-		orgDismissals[adv.Reference] = &Dismissal{
-			Dismissed:          adv.Dismissed,
-			DismissedResources: adv.DismissedResources,
-		}
+		getDismissal(adv.Organization, adv.Reference).Dismissed = true
 	}
 
 	err = cursor.Err()
 	if err != nil {
 		err = database.ParseError(err)
 		return
+	}
+
+	resources, err := GetResources(db, &bson.M{
+		"dismissed": true,
+	}, options.Find().SetProjection(&bson.M{
+		"organization": 1,
+		"reference":    1,
+		"resource":     1,
+	}))
+	if err != nil {
+		return
+	}
+
+	for _, res := range resources {
+		getDismissal(res.Organization, res.Reference).Resources.Add(
+			res.Resource)
 	}
 
 	return
