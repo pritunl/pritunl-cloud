9a33b8317955fd01567aff3118adc77ef7486669
Zachary Huff <zach.huff.386@gmail.com>
2026-09-24 09:43:34 -0400

    Update timers in advisories task

--- task/advisories.go
+++ task/advisories.go
@@ -23,7 +23,7 @@ var advisoriesUpdate = &Task{
 	Version: 1,
 	Hours: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
 		13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
-	Minutes: []int{22},
+	Minutes: []int{22, 52},
 	Handler: advisoriesUpdateHandler,
 }
 
